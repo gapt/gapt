fb905fb67ef26152ba38f158a9032b982838f466
bruno.wp <bruno.wp@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2009-10-04 22:06:37 +0000

    Tests for typed Lambda Calculus.
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@32 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- syntax/src/main/scala/at/logic/language/abstractTypedLambdaCalculus.scala
+++ syntax/src/main/scala/at/logic/language/abstractTypedLambdaCalculus.scala
@@ -8,44 +8,14 @@
 package at.logic.language
 
 
-/**
- * Base class of all symbols which are used in terms
- */
-abstract class SymbolA
+object Types {
+    abstract class TA {
+        def ->(that:TA) = new ->(this, that)
+    }
+    abstract class TAtomicA extends TA
+    abstract class TComplexA extends TA
+    case class Ti() extends TAtomicA ; def i = Ti()
+    case class To() extends TAtomicA ; def o = To()
+    case class ->(in:TA, out:TA) extends TComplexA
 
-
-/**
- * Base class of all type classes
- */
-abstract class TA
-/**
- * Base class of all basics types
- */
-abstract class TAtomicA extends TA
-/**
- * Base class of all complex types
- */
-abstract class TComplexA extends TA
-/**
- * Implementation of the individual basic type
- */
-case class TInd() extends TAtomicA
-/**
- * Implementation of the boolean basic type
- */
-case class TBool() extends TAtomicA
-
-/**
- * Implementation of the arrow complex type constructor
- */
-case class TArrow[+T1 <: TA, +T2 <: TA]() extends TComplexA
-
-
-abstract class LambdaExpressionA[+T <: TA]
-
-abstract case class LambdaVarA[+T <: TA](symbol: SymbolA ) extends LambdaExpressionA[T]
-
-abstract case class LambdaAbstractionA[+T1 <: TA, +T2 <: TA](variable: LambdaVarA[T1], expression: LambdaExpressionA[T2] ) extends LambdaExpressionA[TArrow[T1,T2]]
-
-abstract case class LambdaApplicationA[+T1 <: TA, +T2 <: TA](function: LambdaExpressionA[TArrow[T1, T2]], argument: LambdaExpressionA[T1] ) extends LambdaExpressionA[T2]
-
+}
