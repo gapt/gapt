3f1d4f5cc2b98ee19a65d5af9c786ec90ba48093
bruno.wp <bruno.wp@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2009-10-08 16:07:12 +0000

    First Draft of general Logic Expressions
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@48 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- syntax/src/main/scala/at/logic/language/TypedLambdaCalculus.scala
+++ syntax/src/main/scala/at/logic/language/TypedLambdaCalculus.scala
@@ -42,39 +42,6 @@ object TypedLambdaCalculus {
         }
 
 
-//    case class AbsN(variables: ::[Var], override val expression: LambdaExpression)
-//        extends Abs(variables.head,
-//                    variables.tail match {
-//                        case Nil => expression
-//                        case h::t => AbsN(::(h,t),expression)
-//                    }
-//                   )
-
-//    case class AbsN(variables: List[Var], override val expression: LambdaExpression)
-//        extends Abs(variables.head,
-//                    if (variables.tail.isEmpty) expression
-//                    else AbsN(variables.tail,expression)
-//                   ) {
-//            require(!variables.isEmpty)
-//        }
-
-//    case class AbsN(variables: List[Var], expression: LambdaExpression)
-//        extends LambdaExpression {
-//            def exptype: TA = variables match {
-//                case Nil => expression.exptype
-//                case head::tail => ->(head.exptype, AbsN(tail, expression).exptype  )
-//            }
-//        }
-//
-//    implicit def convertAbsNToAbs(a: AbsN):Abs = a.variables match {
-//        case head::tail => tail match {
-//                case Nil => Abs(head, a.expression)
-//                case _ => Abs(head,convertAbsNToAbs(AbsN(tail, a.expression)))
-//        }
-//        case Nil => throw new IllegalArgumentException("N-ary Abstraction cannot be converted to a standard abstraction when the list of variables is empty: "+a)
-//    }
-//
-//    implicit def convertAbsToAbsN(a:Abs):AbsN =
 
     def AbsN(variables: List[Var], expression: LambdaExpression):LambdaExpression =
         if (!variables.isEmpty) (variables :\ expression)(Abs)
