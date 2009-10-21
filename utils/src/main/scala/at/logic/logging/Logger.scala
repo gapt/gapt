df89a264a93108d28e7c76d98b501820ebe47de5
shaolintl <shaolintl@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2009-10-21 10:56:52 +0000

    added methods to logger
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@65 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- utils/src/main/scala/at/logic/logging/Logger.scala
+++ utils/src/main/scala/at/logic/logging/Logger.scala
@@ -8,6 +8,9 @@ trait Logger {
 
   def debug(msg: => String) = if (log.isDebugEnabled) log.debug(msg)
   def info(msg: => String) = if (log.isInfoEnabled) log.info(msg)
+  def error(msg: => String) = if (log.isErrorEnabled) log.error(msg)
   def error(msg: => String, e:Throwable) = if (log.isErrorEnabled) log.error(msg,e)
-
+  def warn(msg: => String) = if (log.isWarnEnabled) log.warn(msg)
+  def warn(msg: => String, e:Throwable) = if (log.isWarnEnabled) log.warn(msg,e)
+  def trace(msg: => String) = if (log.isTraceEnabled) log.trace(msg)
 }
