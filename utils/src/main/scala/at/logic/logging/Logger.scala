0d679796df5d7b0af9dbeb70df0db4d65e8eb50c
shaolintl <shaolintl@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2009-10-21 09:57:58 +0000

    added testing file to the logger
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@60 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- utils/src/main/scala/at/logic/logging/Logger.scala
+++ utils/src/main/scala/at/logic/logging/Logger.scala
@@ -1,4 +1,13 @@
-/** 
- * Description: 
-**/
+package at.logic.logging
 
+import org.slf4j.LoggerFactory
+
+trait Logging {
+
+  val log = LoggerFactory.getLogger(getClass)
+
+  def debug(msg: => String) = if (log.isDebugEnabled) log.debug(msg)
+  def info(msg: => String) = if (log.isInfoEnabled) log.info(msg)
+  def error(msg: => String, e:Throwable) = if (log.isErrorEnabled) log.error(msg,e)
+
+}
