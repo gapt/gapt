423cb0fce953aa64d4adfb8347ff366990ce7b8d
Tomer Libal <shaolintl@gmail.com>
2009-10-21 10:27:55 +0000

    updating the pom file
    git-svn-id: http://gapt.googlecode.com/svn/trunk@61 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/utils/src/test/scala/at/logic/logging/LoggerTest.scala
+++ source/utils/src/test/scala/at/logic/logging/LoggerTest.scala
@@ -1,4 +1,9 @@
-/** 
- * Description: 
-**/
+package at.logic.logging
 
+import org.specs._
+import org.specs.runner._
+
+class LoggerTest  extends Specification with JUnit {
+  "Logger" should {
+  }
+}
