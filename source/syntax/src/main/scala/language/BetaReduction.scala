540133681f4b9b0935c5c1b3cbadb8462af2c915
Bruno Woltzenlogel Paleo <bruno.wp@gmail.com>
2009-10-26 21:23:44 +0000

    Now it is possible to use betaReduce and betaNormalize without specifying any strategy. In this, a default strategy is used.  The trick was to use implicit parameters.
    If the user imports the object "ImplicitStandardStrategy", then the standard outermost leftmost strategy will be used by default.
    
    git-svn-id: http://gapt.googlecode.com/svn/trunk@86 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/syntax/src/main/scala/language/BetaReduction.scala
+++ source/syntax/src/main/scala/language/BetaReduction.scala
@@ -36,26 +36,31 @@ object BetaReduction {
         val Leftmost = Value
         val Rightmost = Value
     }
+
+    object ImplicitStandardStrategy {
+        implicit val implicitOuter = StrategyOuterInner.Outermost
+        implicit val implicitLeft = StrategyLeftRight.Leftmost
+    }
     
-    def betaNormalize(expression: LambdaExpression, strategy: StrategyOuterInner.Value):LambdaExpression = expression match {
+    def betaNormalize(expression: LambdaExpression)(implicit strategy: StrategyOuterInner.Value):LambdaExpression = expression match {
         case App(Abs(x,body),arg) => {
                 strategy match {
                     case StrategyOuterInner.Outermost => {
                             val sigma: Substitution = (x,arg)
-                            betaNormalize(sigma(body),strategy)                          // If it is outermost strategy, we first reduce the current redex by applying sigma, and then we call betaNormalize recursively on the result.
+                            betaNormalize(sigma(body))(strategy)                          // If it is outermost strategy, we first reduce the current redex by applying sigma, and then we call betaNormalize recursively on the result.
                     }
                     case StrategyOuterInner.Innermost => {
-                            val sigma: Substitution = (x,betaNormalize(arg,strategy))    // If it is innermost strategy, we first normalize the argument and the body and then we reduce the current redex.
-                            sigma(betaNormalize(body,strategy))
+                            val sigma: Substitution = (x,betaNormalize(arg)(strategy))    // If it is innermost strategy, we first normalize the argument and the body and then we reduce the current redex.
+                            sigma(betaNormalize(body)(strategy))
                     }
                 }
         }
-        case App(m,n) => App(betaNormalize(m,strategy),betaNormalize(n,strategy))
-        case Abs(x,m) => Abs(x,betaNormalize(m,strategy))
+        case App(m,n) => App(betaNormalize(m)(strategy),betaNormalize(n)(strategy))
+        case Abs(x,m) => Abs(x,betaNormalize(m)(strategy))
         case x: Var => x
     }
 
-    def betaReduce(expression: LambdaExpression, strategyOI: StrategyOuterInner.Value, strategyLR: StrategyLeftRight.Value):LambdaExpression = expression match {
+    def betaReduce(expression: LambdaExpression)(implicit strategyOI: StrategyOuterInner.Value, strategyLR: StrategyLeftRight.Value):LambdaExpression = expression match {
         case App(Abs(x,body),arg) => {
                 strategyOI match {
                     case StrategyOuterInner.Outermost => {
@@ -65,13 +70,10 @@ object BetaReduction {
                     case StrategyOuterInner.Innermost => {
                             strategyLR match {
                                 case StrategyLeftRight.Rightmost => {
-                                    val argr = betaReduce(arg,strategyOI,strategyLR)    // Since it is innerrightmost redex strategy, we try first to reduce the argument.
-                                    println(argr)
-                                    println(arg)
-                                    println(argr != arg)
+                                    val argr = betaReduce(arg)(strategyOI,strategyLR)    // Since it is innerrightmost redex strategy, we try first to reduce the argument.
                                     if (argr != arg) App(Abs(x, body), argr)            // If it succeeds, great!
                                     else {                                              // If it doesn't, then we try to find an innermost redex in the left side, i.e. in the body.
-                                        val bodyr = betaReduce(body,strategyOI,strategyLR)
+                                        val bodyr = betaReduce(body)(strategyOI,strategyLR)
                                         if (bodyr != body) App(Abs(x, bodyr), arg)      // If it succeeds, great!
                                         else {                                          // If it doesn't, then the current redex is innermost, and hence we reduce it.
                                             val sigma: Substitution = (x,arg)
@@ -80,16 +82,10 @@ object BetaReduction {
                                     }
                                 }
                                 case StrategyLeftRight.Leftmost => {                    // Analogous to the previous case, but giving priority to the left side (body) instead of the ride side (arg)
-                                    val bodyr = betaReduce(body,strategyOI,strategyLR)
-                                    println(bodyr)
-                                    println(body)
-                                    println(bodyr != body)
+                                    val bodyr = betaReduce(body)(strategyOI,strategyLR)
                                     if (bodyr != body) App(Abs(x, bodyr), arg)
                                     else {
-                                        val argr = betaReduce(arg,strategyOI,strategyLR)
-                                        println(argr)
-                                        println(arg)
-                                        println(argr != arg)
+                                        val argr = betaReduce(arg)(strategyOI,strategyLR)
                                         if (argr != arg) App(Abs(x, body), argr)
                                         else {
                                             val sigma: Substitution = (x,arg)
@@ -97,27 +93,25 @@ object BetaReduction {
                                         }
                                     }
                                 }
-
                             }
-
                     }
                 }
         }
         case App(m,n) => {
                 strategyLR match {
                     case StrategyLeftRight.Leftmost => {
-                            val mr = betaReduce(m,strategyOI,strategyLR)        // Since it is leftmost redex strategy, we try first to reduce the left side (m).
+                            val mr = betaReduce(m)(strategyOI,strategyLR)        // Since it is leftmost redex strategy, we try first to reduce the left side (m).
                             if (mr != m) App(mr,n)                              // If it succeeds, great!
-                            else App(m, betaReduce(n,strategyOI,strategyLR))    // If it doesn't, then we try to find and reduce a redex in the right side (n)
+                            else App(m, betaReduce(n)(strategyOI,strategyLR))    // If it doesn't, then we try to find and reduce a redex in the right side (n)
                     }
                     case StrategyLeftRight.Rightmost => {
-                            val nr = betaReduce(n,strategyOI,strategyLR)        // Since it is rightmost redex strategy, we try first to reduce the right side (n).
+                            val nr = betaReduce(n)(strategyOI,strategyLR)        // Since it is rightmost redex strategy, we try first to reduce the right side (n).
                             if (nr != n) App(m,nr)                              // If it succeeds, great!
-                            else App(betaReduce(m,strategyOI,strategyLR), n )   // If it doesn't, then we try to find and reduce a redex in the left side (m)
+                            else App(betaReduce(m)(strategyOI,strategyLR), n )   // If it doesn't, then we try to find and reduce a redex in the left side (m)
                     }
                 }
         }
-        case Abs(x,m) => Abs(x,betaReduce(m,strategyOI,strategyLR))
+        case Abs(x,m) => Abs(x,betaReduce(m)(strategyOI,strategyLR))
         case x: Var => x
     }
 }
--- source/syntax/src/test/scala/language/BetaReductionTest.scala
+++ source/syntax/src/test/scala/language/BetaReductionTest.scala
@@ -29,39 +29,51 @@ class BetaReductionTest extends Specification with JUnit {
   "BetaReduction" should {
     "betaReduce a simple redex" in {
         val e = App(Abs(x, App(f, x)),v)
-        ( betaReduce(e, Outermost, Leftmost) ) must beEqual ( App(f, v) )
+        ( betaReduce(e)(Outermost, Leftmost) ) must beEqual ( App(f, v) )
     }
     "betaReduce correctly with outermost strategy" in {
         val e = App(Abs(v, App(Abs(x, App(f, x)),v)),y)
-        ( betaReduce(e, Outermost, Leftmost) ) must beEqual ( App(Abs(x, App(f, x)),y) )
+        ( betaReduce(e)(Outermost, Leftmost) ) must beEqual ( App(Abs(x, App(f, x)),y) )
     }
     "betaReduce correctly with innermost strategy" in {
         val e = App(Abs(v, App(Abs(x, App(f, x)),v)),y)
-        ( betaReduce(e, Innermost, Leftmost) ) must beEqual ( App(Abs(v, App(f, v)),y) )
+        ( betaReduce(e)(Innermost, Leftmost) ) must beEqual ( App(Abs(v, App(f, v)),y) )
     }
     "betaReduce correctly with leftmost strategy" in {
         val er = App(Abs(v, App(Abs(x, App(f, x)),v)),y)
         val el = Abs(v, App(Abs(x, App(f, x)),v))
         val e = App(el,er)
-        ( betaReduce(e, Innermost, Leftmost) ) must beEqual ( App(Abs(v, App(f, v)),er) )
+        ( betaReduce(e)(Innermost, Leftmost) ) must beEqual ( App(Abs(v, App(f, v)),er) )
     }
     "betaReduce correctly with rightmost strategy" in {
         val er = App(Abs(v, App(Abs(x, App(f, x)),v)),y)
         val el = Abs(v, App(Abs(x, App(f, x)),v))
         val e = App(el,er)
-        ( betaReduce(e, Innermost, Rightmost) ) must beEqual ( App(el,App(Abs(v, App(f, v)),y)) )
+        ( betaReduce(e)(Innermost, Rightmost) ) must beEqual ( App(el,App(Abs(v, App(f, v)),y)) )
     }
     "betaNormalize correctly with outermost strategy" in {
         val er = App(Abs(v, App(Abs(x, App(f, x)),v)),y)
         val el = Abs(v, App(Abs(x, App(f, x)),v))
         val e = App(el,er)
-        ( betaNormalize(e, Outermost) ) must beEqual ( App(f,App(f,y)) )
+        ( betaNormalize(e)(Outermost) ) must beEqual ( App(f,App(f,y)) )
     }
     "betaNormalize correctly with innermost strategy" in {
         val er = App(Abs(v, App(Abs(x, App(f, x)),v)),y)
         val el = Abs(v, App(Abs(x, App(f, x)),v))
         val e = App(el,er)
-        ( betaNormalize(e, Innermost) ) must beEqual ( App(f,App(f,y)) )
+        ( betaNormalize(e)(Innermost) ) must beEqual ( App(f,App(f,y)) )
+    }
+    "betaNormalize correctly with implicit standard strategy" in {
+        import ImplicitStandardStrategy._
+        val er = App(Abs(v, App(Abs(x, App(f, x)),v)),y)
+        val el = Abs(v, App(Abs(x, App(f, x)),v))
+        val e = App(el,er)
+        ( betaNormalize(e) ) must beEqual ( App(f,App(f,y)) )
+    }
+    "betaReduce correctly with implicit standard strategy" in {
+        import ImplicitStandardStrategy._
+        val e = App(Abs(v, App(Abs(x, App(f, x)),v)),y)
+        ( betaReduce(e) ) must beEqual ( App(Abs(x, App(f, x)),y) )
     }
   }
 }
