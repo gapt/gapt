e7ce893bc927d6bf66aab7ab176f0c202f875e4c
Tomer Libal <shaolintl@gmail.com>
2009-10-26 18:15:57 +0000

    implemented inductive graphs and trees. Right now the graphs support an addiiton of one vertex only and the trees are inductively constructed from a leaf and a linear node. Not very usefull but I have some problems using the union in jgrapht (mainly because of maven). I will add an extractor for tNodeTree which will be based on the linear and none-linear versions once I will have both. Any comments?
    git-svn-id: http://gapt.googlecode.com/svn/trunk@80 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/utils/src/main/scala/utils/Graphs.scala
+++ source/utils/src/main/scala/utils/Graphs.scala
@@ -8,9 +8,18 @@
 package at.logic.utils
 
 object Graphs {
-    
-    case class Graph[V]() extends org.jgrapht.graph.DefaultDirectedGraph[V,org.jgrapht.graph.DefaultEdge](classOf[org.jgrapht.graph.DefaultEdge])
 
-    // this version has an arbitrary edge type but I am not sure we need it. What do you think?
-    //case class Graph[V <: AnyRef, E <: AnyRef](implicit m: scala.reflect.Manifest[E]) extends org.jgrapht.graph.DefaultDirectedGraph[V,E](m.erasure.asInstanceOf[java.lang.Class[E]] )
+    sealed abstract class Graph[V](var graph: org.jgrapht.DirectedGraph[V,org.jgrapht.graph.DefaultEdge])
+    case class EmptyGraph[V]() extends Graph[V](new org.jgrapht.graph.ListenableDirectedGraph[V,org.jgrapht.graph.DefaultEdge](classOf[org.jgrapht.graph.DefaultEdge]))
+    case class VertexGraph[V](v: V, vls: List[V], g: Graph[V]) extends Graph[V](g.graph) {
+        graph.addVertex(v)
+        for (v0 <- vls) graph.addEdge(v0, v)
+        g.graph = {val vset = new java.util.HashSet[V](graph.vertexSet()); vset.remove(v); val eset = graph.edgeSet(); new org.jgrapht.graph.DirectedSubgraph[V,org.jgrapht.graph.DefaultEdge](graph, vset, eset)}
+    }
+
+    sealed trait Tree[V] extends Graph[V] {
+        def vertex: V
+    }
+    case class LeafTree[V](vertex: V) extends VertexGraph[V](vertex, Nil, EmptyGraph[V]) with Tree[V]
+    case class LinearTree[V](vertex: V, t: Tree[V]) extends VertexGraph[V](vertex, t.vertex::Nil, t) with Tree[V]
 }
--- source/utils/src/test/scala/utils/GraphsTest.scala
+++ source/utils/src/test/scala/utils/GraphsTest.scala
@@ -14,11 +14,33 @@ import Graphs._
 
 class GraphsTest extends Specification with JUnit {
   "Graph" should {
-    "not contain a vertex when empty" in {
-        (Graph[String]).containsVertex("x") must beEqual (false)
+      val g1 = EmptyGraph[String]
+      val g2 = VertexGraph[String]("a", Nil, g1)
+      val g3 = VertexGraph[String]("b", "a"::Nil, g2)
+      val g4 = VertexGraph[String]("c", "a"::"b"::Nil, g3)
+    "maintain subgraph property with vertices" in {
+        (g3.graph.vertexSet().size()) must beEqual (2)
+        (g2.graph.vertexSet().size()) must beEqual (1)
+        (g2.graph.vertexSet().iterator.next()) must beEqual("a")
+        (g1.graph.vertexSet().isEmpty()) must beEqual (true)
     }
-    "contain a vertex when having it" in {
-        val g = Graph[String]; g.addVertex("x"); g.containsVertex("x") must beEqual (true)
+    "maintain subgraph property with edges" in {
+        (g3.graph.edgeSet().size()) must beEqual (1)
+        (g2.graph.edgeSet().isEmpty()) must beEqual (true)
+        (g1.graph.edgeSet().isEmpty()) must beEqual (true)
+    }
+    "inductively extend" in {
+        (g4.graph.vertexSet.size()) must beEqual (3)
+        (g4.graph.edgeSet.size()) must beEqual (3)
+    }
+  }
+  "Tree" should {
+      val t1 = LeafTree[String]("a")
+      val t2 = LinearTree[String]("b", t1)
+      val t3 = LinearTree[String]("c", t2)
+      val t4 = LinearTree[String]("d", t3)
+    "contains no cycles" in {
+        (new org.jgrapht.alg.CycleDetector[String, org.jgrapht.graph.DefaultEdge](t4.graph).detectCycles()) must beEqual (false)
     }
   }
 }
