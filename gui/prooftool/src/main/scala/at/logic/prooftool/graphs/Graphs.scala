26e5ea9c01a7b0bde770f260adf4b24e4eb10e7d
Martin.Riener <Martin.Riener@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2009-10-27 00:43:27 +0000

    found the adapter classes linking jgraph to jgrapht at http://jgrapht.sourceforge.net/visualizations.html -- scala doesn't see the constructor mentioned yet
    adding edges to a jgraph works now - you can add single vertices/edges without null pointer exception, but not arrays.
    
    
    
    
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@87 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- gui/prooftool/src/main/scala/at/logic/prooftool/graphs/Graphs.scala
+++ gui/prooftool/src/main/scala/at/logic/prooftool/graphs/Graphs.scala
@@ -1,13 +1,78 @@
 package at.logic.prooftool.graphs
 
+//import at.logic.prooftool.graphs.wrappers._
+
 import org.jgraph._
 import org.jgraph.graph._
-import org.jgrapht._
-import org.jgrapht.graph._
 import javax.swing._
 import java.awt.geom._
 import java.awt._
 
+import org.jgrapht._
+import org.jgrapht.ext._
+import org.jgrapht.graph._
+
+
+/**
+ * Hello world!
+ *
+ */
+object JGraphApp extends Application {
+  var jf = new JFrame
+  var jm = new DefaultGraphModel
+  var cache = new GraphLayoutCache(jm, new DefaultCellViewFactory)
+  var jg = new JGraph (jm, cache)
+  
+  var cells = new Array[DefaultGraphCell](3)
+  cells.update(0, new DefaultGraphCell(new String("cell 1") ))
+  
+  GraphConstants.setBounds(cells.apply(0).getAttributes(), new Rectangle2D.Double(20,20,40,20));
+  GraphConstants.setGradientColor(cells.apply(0).getAttributes(), Color.orange);
+  GraphConstants.setOpaque(cells.apply(0).getAttributes(), true);
+
+  
+  cells.update(1, new DefaultGraphCell(new String("cell 2") ))
+  GraphConstants.setBounds(cells(1).getAttributes(), new Rectangle2D.Double(20,20,40,20));
+  GraphConstants.setGradientColor(cells(1).getAttributes(), Color.orange);
+  GraphConstants.setOpaque(cells(1).getAttributes(), true);
+  
+  var port0 = new DefaultPort
+  var port1 = new DefaultPort
+  
+  cells(0).add(port0)
+  cells(1).add(port1)
+  
+  var edge = new org.jgraph.graph.DefaultEdge("edge")
+  edge.setSource(cells(0).getChildAt(0))
+  edge.setTarget(cells(1).getChildAt(0))
+  
+  cells(2) = edge
+  
+    
+  for (c <- cells) {
+    jg.getGraphLayoutCache().insert(c)
+  }
+
+  // jg.getGraphLayoutCache().insert(cells) // adding the whole array does only work in java!
+
+  
+  jf.getContentPane.add(new JScrollPane(jg))
+  jf.pack
+  jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+  jf.setVisible(true)
+
+  var c = new DefaultGraphCell(new String("cell 3") )
+  GraphConstants.setBounds(c.getAttributes(), new Rectangle2D.Double(20,20,40,20));
+  GraphConstants.setGradientColor(c.getAttributes(), Color.orange);
+  GraphConstants.setOpaque(c.getAttributes(), true);
+
+  jg.getGraphLayoutCache().insert(c)  
+  
+  
+}
+
+
+/* Mini Vertex and Edge Classes*/
 
 class Vertex(desc : String) {
   var description: String = desc
@@ -28,12 +93,10 @@ class GEdgeFactory extends EdgeFactory[Vertex,Edge] {
   def createEdge(v1:Vertex, v2: Vertex) : Edge = { new Edge("edge", v1,v2) }
 } 
 
-/**
- * Hello world!
- *
- */
-object App extends Application {
-  
+
+/* JGrapht Adapter*/
+
+object AdapterApp extends Application {
   var v1 = new Vertex("v1")
   var v2 = new Vertex("v2")
   var v3 = new Vertex("v3")
@@ -56,57 +119,17 @@ object App extends Application {
   g.addEdge(v2,v3)
   g.addEdge(v3,v5)
 
-  Console.print(g.toString())
-  
-  var jf = new JFrame
-  var jm = new DefaultGraphModel
-  var cache = new GraphLayoutCache(jm, new DefaultCellViewFactory)
-  var jg = new JGraph (jm, cache)
-  
-  var cells = new Array[DefaultGraphCell](3)
-  cells.update(0, new DefaultGraphCell(new String("cell 1") ))
-  
-  GraphConstants.setBounds(cells.apply(0).getAttributes(), new Rectangle2D.Double(20,20,40,20));
-  GraphConstants.setGradientColor(cells.apply(0).getAttributes(), Color.orange);
-  GraphConstants.setOpaque(cells.apply(0).getAttributes(), true);
+  //Console.print(g.toString())
 
-  
-  cells.update(1, new DefaultGraphCell(new String("cell 2") ))
-  
-  var port0 = new DefaultPort
-  var port1 = new DefaultPort
-  
-  cells.apply(0).add(port0)
-  cells.apply(1).add(port1)
-  
-  var edge = new org.jgraph.graph.DefaultEdge
-  edge.setSource(cells.apply(0).getChildAt(0))
-  edge.setSource(cells.apply(1).getChildAt(0))
-  
-  Console.println()
-  Console.println("edge="+edge.getTarget() )
-  if (edge==null)  {
-    Console.println("!!!")
-    }
-  else {
-    Console.println("!= null");
-  }
-  
-  cells.update(2, edge)
-  
-  Console.println()
-  for (c <- cells) {
-    Console.println(c)
-    
-  }
-  
-  Console.println(cells)
-  
-  jg.getGraphLayoutCache().insert(cells)
-  
-  jf.getContentPane.add(new JScrollPane(jg))
-  jf.pack
-  jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-  jf.setVisible(true)
-  
+  // create a JGraphT graph
+  /* scala does not see the ListenableDirectedGraph(java.lang.Class) constructor,
+     solve via java wrapper hiding the copy constructor?
+   */
+  // var graph = new ListenableDirectedGraph( classOf[org.jgrapht.graph.DefaultEdge] )
+
+  // create a visualization using JGraph, via the adapter
+//  var jgraph = new JGraph( new JGraphModelAdapter( graph ) )
+
+
+
 }
