f75bfef0c6f474180d17c378dfb1b76923af6b54
Tomer Libal <shaolintl@gmail.com>
2009-10-28 13:56:39 +0000

    Logger - now error method when given an exception will also throw this exception Graphs - I think we misunderstood the extractor/case classes idea. It seems they are synonyms. I have changed all case classes to be with extractors and I will send soon a message to gap group regarding it. I also added support for arbitrary trees but they are not backed up by an actual tree object but by a graph mixed with the Tree trait. Still remain there is to ensure this tree is actually a tree (add require in the constructor)
    git-svn-id: http://gapt.googlecode.com/svn/trunk@91 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/utils/src/test/scala/utils/GraphsTest.scala
+++ source/utils/src/test/scala/utils/GraphsTest.scala
@@ -11,6 +11,7 @@ import org.specs._
 import org.specs.runner._
 
 import Graphs._
+import TreeImplicitConverters._
 
 class GraphsTest extends Specification with JUnit {
   "Graph" should {
@@ -20,6 +21,14 @@ class GraphsTest extends Specification with JUnit {
       val g4 = EdgeGraph("a", "c", EdgeGraph("b", "c", VertexGraph("c", g3)))
       val g5 = EdgeGraph("e", "f", VertexGraph("f", VertexGraph("e", EmptyGraph[String])))
       val g6 = UnionGraph(g4, g5)
+    "check extractors" in {
+        (g2 match {
+            case EmptyGraph() => false
+            case UnionGraph(x1, x2) => false
+            case VertexGraph("a", x2) => true
+            case _ => false
+        }) must beEqual (true)
+    }
     "maintain subgraph property with vertices" in {
         (g3.graph.vertexSet().size()) must beEqual (2)
         (g2.graph.vertexSet().size()) must beEqual (1)
@@ -44,20 +53,29 @@ class GraphsTest extends Specification with JUnit {
         (g4.graph.edgeSet.size()) must beEqual (3)
     }
   }
-  import TreeImplicitConverters._
   "Tree" should {
+    "contains no cycles" in {
         val t1 = UnaryTree("d",UnaryTree("c",UnaryTree("b","a")))
         val t2 = UnaryTree("3", UnaryTree("2", "1"))
-    "contains no cycles" in {
         (new org.jgrapht.alg.CycleDetector[String, org.jgrapht.graph.DefaultEdge](t1.graph).detectCycles()) must beEqual (false)
-        (BinaryTree("X", t1, t2)) must throwA [java.lang.IllegalArgumentException]
+        (BinaryTree("X", t1, t2)) mustNot throwA [java.lang.IllegalArgumentException]
         (UnaryTree("a", "a")) must throwA [java.lang.IllegalArgumentException]
     }
     "be backed up by a correctly-constructed graph" in {
+        val t1 = UnaryTree("d",UnaryTree("c",UnaryTree("b","a")))
+        val t2 = UnaryTree("3", UnaryTree("2", "1"))
         val g10 = EdgeGraph("c", "d", VertexGraph[String]("d", EdgeGraph("b", "c", VertexGraph[String]("c", EdgeGraph("a", "b", VertexGraph[String]("b", VertexGraph[String]("a", EmptyGraph[String])))))))
         (t1.graph.vertexSet()) must beEqual (g10.graph.vertexSet())
         (t1.graph.edgeSet().toString) must beEqual (g10.graph.edgeSet().toString) // equals on DefaultEdge is comparing pointers and not values
     }
-
+    "test creation with ArbitraryTree" in {
+        val t1 = UnaryTree("d",UnaryTree("c",UnaryTree("b","a")))
+        val t2 = UnaryTree("3", UnaryTree("2", "1"))
+        val lt = LeafTree("y")
+        (ArbitraryTree("x", t1, t2, lt)) must beLike {case ArbitraryTree("x", t1::t2::lt::Nil) => true}
+        (ArbitraryTree("x", t1, t2)) must beLike {case BinaryTree("x", t1, t2) => true}
+        (ArbitraryTree("x", t1)) must beLike {case UnaryTree("x", t1) => true}
+        (ArbitraryTree("x")) must beLike {case LeafTree("x") => true}
+    }
   }
 }
