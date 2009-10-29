31f91f1b15c18d3b1b36de427b0da9a9efbe2d51
Tomer Libal <shaolintl@gmail.com>
2009-10-29 11:17:54 +0000

    added a test for recursive and cross-layer pattern matching and changed implicit converters to use tuples
    git-svn-id: http://gapt.googlecode.com/svn/trunk@92 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/utils/src/test/scala/utils/GraphsTest.scala
+++ source/utils/src/test/scala/utils/GraphsTest.scala
@@ -11,16 +11,18 @@ import org.specs._
 import org.specs.runner._
 
 import Graphs._
+import GraphImplicitConverters._
 import TreeImplicitConverters._
 
 class GraphsTest extends Specification with JUnit {
   "Graph" should {
-      val g1 = EmptyGraph[String]
-      val g2 = VertexGraph("a", g1)
-      val g3 = EdgeGraph("a", "b", VertexGraph("b", g2))
-      val g4 = EdgeGraph("a", "c", EdgeGraph("b", "c", VertexGraph("c", g3)))
+      val g1: EmptyGraph[String] = ()
+      val g2: VertexGraph[String] = ("a", g1)
+      val g21: VertexGraph[String] = ("b", g2)
+      val g3: EdgeGraph[String] = ("a", "b", g21)
+      val g4: EdgeGraph[String] = ("a", "c", EdgeGraph("b", "c", VertexGraph("c", g3)))
       val g5 = EdgeGraph("e", "f", VertexGraph("f", VertexGraph("e", EmptyGraph[String])))
-      val g6 = UnionGraph(g4, g5)
+      val g6: UnionGraph[String] = (g4, g5)
     "check extractors" in {
         (g2 match {
             case EmptyGraph() => false
@@ -60,6 +62,17 @@ class GraphsTest extends Specification with JUnit {
         (new org.jgrapht.alg.CycleDetector[String, org.jgrapht.graph.DefaultEdge](t1.graph).detectCycles()) must beEqual (false)
         (BinaryTree("X", t1, t2)) mustNot throwA [java.lang.IllegalArgumentException]
         (UnaryTree("a", "a")) must throwA [java.lang.IllegalArgumentException]
+    }
+    "pattern match as graphs and recursively" in {
+        val lt = ArbitraryTree("y", LeafTree("a"), LeafTree("b"), LeafTree("c"))
+        ((lt) match {
+            case EmptyGraph() => false
+            case UnionGraph(x1, x2) => false
+            case VertexGraph("y", _) => false
+            case UnaryTree(_,_) => false
+            case EdgeGraph(_, "y", UnionGraph(EdgeGraph(_, _, _),_)) => true
+            case _ => false
+        }) must beEqual (true)
     }
     "be backed up by a correctly-constructed graph" in {
         val t1 = UnaryTree("d",UnaryTree("c",UnaryTree("b","a")))
