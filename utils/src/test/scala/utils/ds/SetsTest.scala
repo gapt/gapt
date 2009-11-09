d9a6d9e857d2e0bbe48c6435cb377c82ce780c3b
shaolintl <shaolintl@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2009-11-09 12:53:36 +0000

    added stubs for sets and multisets
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@102 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ utils/src/test/scala/utils/ds/SetsTest.scala
@@ -0,0 +1,18 @@
+/*
+ * SetsTest.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package at.logic.utils.ds
+
+import org.specs._
+import org.specs.runner._
+
+import Sets._
+
+
+class SetsTest extends Specification with JUnit {
+
+}
