81ef743d53a702417e8f693cb933fc151e3f6144
Martin.Riener <Martin.Riener@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2009-11-15 12:41:16 +0000

    Since i got stuck with the graph library, i tried to get something useful to run. This is a proposal for a covariant set, but not rigorously tested right now.
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@114 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- utils/src/main/scala/utils/ds/Sets.scala
+++ utils/src/main/scala/utils/ds/Sets.scala
@@ -7,11 +7,101 @@
 
 package at.logic.utils.ds
 
+import scala.collection.mutable.HashSet
+import scala.util.Sorting
+
 object Sets {
     trait Set[+A] extends Collection[A]{
+        //def apply[U>:A](s:Set[A])
+    }
+
+    /* a hashset based implementation of a covariant set */
+    class CovariantSet[+A] private (
+        private[this] var set : HashSet[A]  )
+    extends Set[A] 
+    {
+        def this() = this(new HashSet[A])
+        
+        def size() : Int = return set.size
+        def elements : Iterator[A]= { return set.elements }
+
+
+        /* gives restricted access to the hash set*/
+        protected def getHashSet[B>:A]() : HashSet[B] = {
+            var bset = new HashSet[B]
+            bset ++= set
+            return bset
+        }
+
+        /* there is no difference in adding in the beginning or the end in a hashset */
+        def +[B>:A](x:B) : CovariantSet[B] = this.::(x)
+
+        /* override of list constructor */
+        def ::[B>:A](x:B) : CovariantSet[B] = {
+            var setb : HashSet[B] = new HashSet[B]
+            setb ++= set
+
+            var s : CovariantSet[B] = new CovariantSet[B](setb)
+            val r = s.find( (v:B) => v == x )
+            if (r == None) {
+                setb += x
+            }
+
+            return s
+        }
+
+        /* two CovariantSets are equal, if their private HashSets are equal.
+         * two HasSets are equal, if their Elements are reference equal.
+         *
+         * see also http://programming-scala.labs.oreilly.com/ch06.html
+         * chapter "Array Equality and the sameElements Method"
+         */
+        override def equals(x:Any) : Boolean = {
+            try {
+                var i = x.asInstanceOf[CovariantSet[A]]
+                var bset = new HashSet[A]()
+                bset ++= set
+
+                if (i.getHashSet().equals(bset) ) return true
+            } catch {
+                case _ =>
+            }
+
+            return false
+        }
+
+    /** two sets A and B are the same, if they have the same size,
+     * and if every element from A is in B and vice versa          
+     *
+     */
+    override def sameElements[B>:A](that : Iterable[B]) : Boolean = {
+        // TODO: check, if the HashMap.sameElements method always iterates the same over if objects in it are equal, but not reference equal - then this method is not necessary
+        var that_size = 0
+        for (b <- that) {
+            that_size += 1
+            if (this.elements.find((x : B) => x == b) == None) {
+                //Console.println("could not find "+b)
+                return false
+            } else {
+                //Console.println("found "+b)
+            }
+        }
+
+        for (a <- this.elements) {
+            if (that.find((x:B) => x == a ) == None) {
+                //Console.println("could not find "+a)
+                return false
+            } else {
+                //Console.println("found "+a)
+            }
+
+        }
+
+        return this.size == that_size
     }
 
     /*class HashSet[+A] extends Set[A] {
 
     }*/
-}
+    }
+ }
--- utils/src/test/scala/utils/ds/SetsTest.scala
+++ utils/src/test/scala/utils/ds/SetsTest.scala
@@ -10,9 +10,82 @@ package at.logic.utils.ds
 import org.specs._
 import org.specs.runner._
 
-import Sets._
+import at.logic.utils.ds.Sets._
 
 
 class SetsTest extends Specification with JUnit {
+
+    "Sets" should {
+        var set = new Sets.CovariantSet[String]
+        set = set + "abc"
+        set = set + "abc"
+        set = set + "abc"
+        set = set + "xyz"
+        set = set + "xyz"
+        set = set + "xyz"
+
+        class Fruit(var name : String) {
+            override def equals(x:Any) : Boolean = {
+                try {
+                    var f : Fruit = x.asInstanceOf[Fruit]
+                    return f.name == this.name
+                } catch {
+                    case _ =>
+                }
+                return false
+            }
+        }
+
+        class Apple(name : String, var colors : Int) extends Fruit(name) {
+            override def equals(x:Any) : Boolean = {
+                try {
+                    if (! super.equals(x)) return false
+                    var a = x.asInstanceOf[Apple]
+                    return (a.colors == colors)
+
+                } catch { case _ => }
+                return false
+            }
+        }
+        class Orange(name : String, var bloodorange : Boolean) extends Fruit(name) {
+
+        }
+
+        var apple = new Apple("apfel",2)
+        var apple2 = new Apple("apfel",2)
+        var orange = new Orange("orange",true)
+
+
+    "be invariant to multiple additions of an element" in {
+        var set2 = new Sets.CovariantSet[String]
+        set2 = set2 + "xyz"
+        set2 = set2 + "abc"
+        //set == set2
+
+        //Console.println(set == set2)
+        set.sameElements(set2) must beEqual (true)
+    }
+
+    "be covariant" in {
+        var set1 = (new Sets.CovariantSet) + apple + orange
+        var set2 = (new Sets.CovariantSet) + orange + apple2
+
+        //var set3 = set1 + orange
+        //var set4 = set2 + apple
+        /*
+        Console.print("Fruit:"+(apple.asInstanceOf[Fruit]).name)
+        Console.print(" Apple:"+(apple.asInstanceOf[Apple]).name)
+        Console.print(" "+ ((apple.asInstanceOf[Fruit]).name eq (apple.asInstanceOf[Fruit]).name))
+        Console.println(" Apple:"+(apple.asInstanceOf[Apple]).colors)
+        Console.println("check for apple equality" + (apple == apple2))
+        Console.println(set1)
+        Console.println(set2)
+        Console.println(set1.sameElements(set2))
+        */
+        //val f = x
+
+        set1.sameElements(set2) must beEqual (true)
+    }
+    }
 
 }
