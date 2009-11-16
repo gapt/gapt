2833e1fa8219b85d69914a82ffb4adca23849211
Tomer Libal <shaolintl@gmail.com>
2009-11-16 15:12:13 +0000

    LK rules are finished without quantifier rules. No testing has been done yet
    git-svn-id: http://gapt.googlecode.com/svn/trunk@115 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/syntax/src/main/scala/calculi/ExpressionOccurrences.scala
+++ source/syntax/src/main/scala/calculi/ExpressionOccurrences.scala
@@ -7,19 +7,40 @@
 package at.logic.calculi.lk
 
 import at.logic.language.lambda.TypedLambdaCalculus._
+import at.logic.language.lambda.Types._
 import at.logic.language.hol.HigherOrderLogic._
 import at.logic.utils.labeling.Labels._
 
 object ExpressionOccurrences {
 
-    abstract class Occur[A]
-    case class BaseOccur[A](vl: A) extends Occur[A]
-    case class CombinedOccur[A](vl1: Occur[A], vl2: Occur[A]) extends Occur[A]
+    // Occurs are equal if they are syntactically equal or one contains the other
+    abstract class Occur[A <% Ordered[A]] {
+        def size: Int 
+    }
+    case class BaseOccur[A <% Ordered[A]](vl: A) extends Occur[A] {
+        def size = 1
+        override def equals(a: Any) = a match {
+            case b: BaseOccur[_] => vl == b.vl
+            case b: CombinedOccur[_] => b.equals(this)
+            case _ => false
+        }
+    }
+    case class CombinedOccur[A <% Ordered[A]](vl1: Occur[A], vl2: Occur[A]) extends Occur[A] {
+        def size = vl1.size + vl2.size
+        override def equals(a: Any) = a match {
+            case b: BaseOccur[_] => vl1.equals(b) || vl2.equals(b)
+            case b: CombinedOccur[_] =>
+                if (size == b.size) (vl1.equals(b.vl1) && vl2.equals(b.vl2))
+                else if (size > b.size) (vl1.equals(b) || vl2.equals(b))
+                else (b.vl1.equals(this) || b.vl2.equals(this))
+            case _ => false
+        }
+    }
     
-    // parameterized by the element used as an idea (which must be orderable via an implicit conversion)
-    trait Occurrence[A] extends Labeled[Occur[A]] {
+    // parameterized by the element used as an id (which must be orderable via an implicit conversion)
+    abstract class Occurrence[A <% Ordered[A]] extends Labeled[Occur[A]] {
         def merge(other: Occurrence[A]): Occur[A] = CombinedOccur(label, other.label)
     }
 
-    case class FormulaOccurrence[+A <: HOL](formula: Formula[A], val label: Occur[Int]) extends Occurrence[Int]
+    case class FormulaOccurrence[+A <: HOL](formula: Formula[A], val label: Occur[Int]) extends Occurrence[Int] 
 }
--- source/syntax/src/test/scala/calculi/lk/LKTest.scala
+++ source/syntax/src/test/scala/calculi/lk/LKTest.scala
@@ -28,7 +28,7 @@ class LKTest extends Specification with JUnit {
         val f1 = App(c1,v1).asInstanceOf[Formula[HOL]]
         val f2 = App(c1,v1).asInstanceOf[Formula[HOL]]
         val a1 = Axiom(Sequent(f1::Nil, f2::Nil))
-        (a1) must beLike {case Axiom(x,perm) => true}
+        (a1) must beLike {case Axiom(x) => true}
     }
     "AndRightRule build and extract correctly" in {
         val c1 = Var[HOL]("a", i->o)
@@ -39,7 +39,7 @@ class LKTest extends Specification with JUnit {
         val f4 = App(c1,v1).asInstanceOf[Formula[HOL]]
         val a1 = Axiom(Sequent(f1::Nil, f2::Nil))
         val a2 = Axiom(Sequent(f3::Nil, f4::Nil))
-        (AndRightRule(a1, a2, 0, 0)) must beLike {case AndRightRule(a1, a2, x, aux1, aux2, p1,perm) => true}
+        (AndRightRule(a1, a2, a1.root.succedent(0), a2.root.succedent(0))) must beLike {case AndRightRule(a1, a2, x, aux1, aux2, prin1) => true}
     }
   }
 }
