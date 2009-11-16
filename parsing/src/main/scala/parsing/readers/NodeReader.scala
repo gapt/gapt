0c50ef9435d29527c89a6fb2c6fcb219881ac2de
fraini <fraini@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2009-11-16 16:20:31 +0000

    Added an abstract base class to allow XML parsing from any Reader object.
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@117 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- parsing/src/main/scala/parsing/readers/NodeReader.scala
+++ parsing/src/main/scala/parsing/readers/NodeReader.scala
@@ -5,9 +5,15 @@
 package at.logic.parsing.readers
 
 import at.logic.parsing.InputParser
-import scala.xml.Node
+import scala.xml.{XML,Node}
 import at.logic.parsing.language.xml.XMLParser.XMLNodeParser
 
-abstract class NodeReader(n: Node) extends XMLNodeParser {
-  def getInput() : Node = n
+object XMLReaders {
+  abstract class NodeReader(n: Node) extends XMLNodeParser {
+    def getInput() : Node = n
+  }
+
+  abstract class XMLReader( r: java.io.Reader ) extends XMLNodeParser {
+    def getInput() : Node = XML.load( r )
+  }
 }
