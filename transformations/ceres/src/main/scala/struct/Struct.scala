f28c3635f8631cdd2c63e6443c2d005880e5f5f6
bruno.wp <bruno.wp@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2009-11-22 17:52:18 +0000

    Extraction of structs is ready...
    Not tested yet...
    
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@178 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- transformations/ceres/src/main/scala/struct/Struct.scala
+++ transformations/ceres/src/main/scala/struct/Struct.scala
@@ -17,11 +17,29 @@ trait Struct
 case class Times(left: Struct, right: Struct) extends Struct
 case class Plus(left: Struct, right: Struct) extends Struct
 case class Dual(sub: Struct) extends Struct
-case class Atom(exp: LambdaExpression)
+case class A(fo: FormulaOccurrence) extends Struct // Atomic Struct
+case class EmptyTimesJunction() extends Struct
 
 object StructCreators {
 
-  def extract(p: LKProof) = false //ToDo
+  def extract(p: LKProof):Struct = p match {
+    case Axiom(so) => {
+      val cutAncInAntecedent = so.antecedent.toList.filter(x => isCutAncestor(x,p)).map(x => Dual(A(x)))   //
+      val cutAncInSuccedent = so.succedent.toList.filter(x => isCutAncestor(x,p)).map(x => A(x))
+      makeTimesJunction(cutAncInAntecedent:::cutAncInSuccedent)
+    }
+    case UnaryLKProof(_,upperProof,_,_,_) => extract(upperProof)
+    case BinaryLKProof(_, upperProofLeft, upperProofRight, _, aux1, aux2, _) => {
+      if (isCutAncestor(aux1,p)) Plus(extract(upperProofLeft), extract(upperProofRight))
+      else Times(extract(upperProofLeft), extract(upperProofRight))
+    }
+  }
+
+  def makeTimesJunction(structs: List[Struct]):Struct = structs match {
+    case Nil => EmptyTimesJunction()
+    case s1::Nil => s1
+    case s1::tail => Times(s1, makeTimesJunction(tail))
+  }
 
   def isCutAncestor(fo: FormulaOccurrence, p: LKProof):Boolean = {
     try {
@@ -31,5 +49,4 @@ object StructCreators {
     }
     return false
   }
-
 }
