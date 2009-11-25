d536f34aa6bf7745121525c93531d49db2203707
Martin Riener <martin.riener@googlemail.com>
2009-11-25 18:57:02 +0000

    factored out the scalacheck beMatchers for scalatest, added test for termgenerator
    git-svn-id: http://gapt.googlecode.com/svn/trunk@198 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/syntax/src/main/scala/generators/termgenerator.scala
+++ source/syntax/src/main/scala/generators/termgenerator.scala
@@ -5,19 +5,26 @@ package at.logic.utils.generators
 
 import scala.util.Random
 import at.logic.language.fol._
+import at.logic.language.lambda.symbols._
+import at.logic.language.lambda.types._
+import at.logic.language.lambda.types.Definitions._
 
 class FOLtermGenerator {
     var random : Random = null
+    var count = 0 // every generated variable, constant symbol
 
     def apply(seed : Int) = new FOLtermGenerator(seed)
 
-    //def this() = {}
     def this(seed : Int) = {
         this()
         random = new Random(seed);
     }
 
 
-    def generateVariable() {}
+    def generateVariable() = {
+        count += 1
+        var x = new VariableStringSymbol("x_"+count) ;
+        FOLFactory.createVar(x, i)
+    }
     
 }
---
+++ source/utils/src/main/scala/utils/testing/scalacheckmatchers.scala
@@ -0,0 +1,43 @@
+/* matcher for scalacheck property tests for the specs tests - you can write something like
+   import at.logic.utils.testing.PropMatcher._
+
+   "xyz" should {
+    "pass test x" in {
+        var prop = //define property
+        prop must bePassed
+    }
+
+    also take look at http://www.scalatest.org/scaladoc/doc-1.0/org/scalatest/matchers/BeMatcher.html
+ */
+package at.logic.utils.testing
+
+import org.specs._
+import org.specs.runner._
+import org.scalacheck._
+import org.scalacheck.Prop._
+
+import org.specs.matcher._
+
+trait ResultMatcher {
+        class PositiveResultMatcher extends Matcher[Test.Result] {
+            def apply(r : => Test.Result) : (Boolean,String,String)=
+                (r.passed,"ScalaCheck passed!","SclaCheck failed!")
+        }
+
+        val bePassed = new PositiveResultMatcher
+}
+
+object ResultMatcher extends ResultMatcher
+
+trait PropMatcher {
+        class PositiveResultMatcher extends Matcher[Prop] {
+            def apply(r : => Prop) =
+                (Test.check(Test.defaultParams,r).passed,
+                 "ScalaCheck passed!",
+                 "SclaCheck failed!")
+        }
+
+        val bePassed = new PositiveResultMatcher
+}
+
+object PropMatcher extends PropMatcher
