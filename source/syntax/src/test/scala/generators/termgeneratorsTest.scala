be6d0aede0aed53aca623e26e78cafef25e5b4e9
Martin Riener <martin.riener@googlemail.com>
2009-12-02 17:14:30 +0000

    generation of terms works now
    git-svn-id: http://gapt.googlecode.com/svn/trunk@202 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/syntax/src/test/scala/generators/termgeneratorsTest.scala
+++ source/syntax/src/test/scala/generators/termgeneratorsTest.scala
@@ -8,6 +8,8 @@ import at.logic.language.fol._
 import at.logic.language.lambda.symbols._
 import at.logic.language.lambda.types._
 import at.logic.language.lambda.types.Definitions._
+import at.logic.language.hol._
+import at.logic.language.hol.logicSymbols._
 
 import org.specs._
 import org.specs.runner._
@@ -20,23 +22,25 @@ import at.logic.utils.testing.PropMatcher.bePassed
 
 class termgeneratorTest extends SpecificationWithJUnit {
     "The generator " should {
-        val seed = 123
         "be able to convert between tuples and langauges" in {
-            import Language.tupleFromLanguage
             import Language.languageFromTuple
-            var generator = new FOLtermGenerator(seed)
+            import Language.tupleFromLanguage
 
-            val prop = forAll( (nv:Int, nc:Int, nf:Int) => {
-                val s  : (List[SymbolA], List[SymbolA], List[SymbolA]) = generator.generateSymbols(nv,nv,nf)
+            val prop = forAll( (seed:Int, nv:Int, nc:Int, nf:Int) => {
+                var generator = new FOLtermGenerator(seed)
+                
+                val s  : (List[(VariableSymbolA,Int)], List[(ConstantSymbolA,Int)],List[(ConstantSymbolA,Int)]) = generator.generateSymbols(nv,nv,nf,7)
                 val l  : Language = s
-                val s2 : (List[SymbolA], List[SymbolA], List[SymbolA]) = l
+                val s2 : (List[(VariableSymbolA,Int)], List[(ConstantSymbolA,Int)],List[(ConstantSymbolA,Int)]) = l
                 val l2 : Language = s2
+
                 s == s2 && l == l2
             })
 
             prop must bePassed
         }
 
+        val seed = 123
         "generate languages of a given size" in {
             var generator = new FOLtermGenerator(seed)
             var prop = forAll( (nv:Int, nc:Int, nf:Int) => {
@@ -44,7 +48,7 @@ class termgeneratorTest extends SpecificationWithJUnit {
                     var c = if (nc<0) 0 else nc
                     var f = if (nf<0) 0 else nf
 
-                    generator.generateSymbols(v,c,f) match {
+                    generator.generateSymbols(v,c,f,7) match {
                         case (vs,cs,fs) => //println((v,c,f)); println((vs,cs,fs));
                                            vs.length == v &&
                                            cs.length == c &&
@@ -54,15 +58,59 @@ class termgeneratorTest extends SpecificationWithJUnit {
             prop must bePassed
         }
 
+        "generate languages of a given size from random strings" in {
+            var generator = new FOLtermGenerator(seed)
+            var prop = forAll( (vs:List[String],cs:List[String],fs:List[String]) => {
+                    val l:Language = Language.tupleFromStrings((vs,cs,fs))
+                        l match {
+                            case Language(v,c,f) => v.length == vs.length &&
+                                                    c.length == cs.length &&
+                                                    f.length == fs.length
+                        }
+                })
+            prop must bePassed
+        }
+
         "generate first order variables" in {
             var generator = new FOLtermGenerator(seed)
-            generator.generateVariable match {
-                case x:FOLVar => 
-                    x.name must beEqual (new VariableStringSymbol("x_1"))
+            val l = generator.generateSymbols(10,5,6,5)
+            generator.generateVariable(l) match {
+                case x:FOLVar =>
+                    x.name.toString must be matching ("x_[0-9]+")
                     x.exptype  must beEqual (i)
                 case _ => true must beEqual (false)
             }
-            
+
+
+        }
+
+        "generate first order functions" in {
+            val prop = forAll( (seed : Int) => {
+                var generator = new FOLtermGenerator(seed)
+                val l = generator.generateSymbols(10,5,6,5)
+                val x = generator.generateFunction(l, 30, 50, 7)
+                //println(x)
+                //    println(Language.flattenTerm(x))
+                //println(Language.printFunction(x))
+                //val f = ((t:FOLTerm) => {f_ (f_,t) })
+                val symbols : List[String] = (l._1 ++ l._2 ++ l._3).map( (x=>x._1.toString) )
+                val getName : (FOLTerm=>String) = ((t:FOLTerm)=>{t match {
+                            case FOLVar(x)     => x.toString
+                            case FOLConst(x)   => x.toString
+                            case Function(x,y) => x.toString
+                            case _=> ""
+                    }})
+                val fun = ((ft:FOLTerm)=> { symbols.contains(getName(ft))} )
+                val conj = ((b1:Boolean,b2:Boolean)=> {b1 && b2})
+                val tests = Language.flattenTerm(x).map(fun)
+                //println(symbols)
+                //println(Language.flattenTerm(x).map(getName))
+                val r : Boolean = tests.foldLeft(true)(conj)
+                r
+                
+                })
+                prop must bePassed
+
         }
     }
 }
