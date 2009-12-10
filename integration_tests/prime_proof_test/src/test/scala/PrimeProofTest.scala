8b3f0b507c5f45e87122a439b9e702f387d2d630
fraini <fraini@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2009-12-10 16:04:15 +0000

    Now really added prime proof integration test file.
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@210 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ integration_tests/prime_proof_test/src/test/scala/PrimeProofTest.scala
@@ -0,0 +1,56 @@
+/** 
+ * Description: 
+**/
+
+package at.logic.parsing.language.xml
+
+import org.specs._
+import org.specs.runner._
+import org.specs.matcher.Matcher
+
+import scala.xml._
+
+import at.logic.transformations.ceres.struct.StructCreators
+import at.logic.transformations.ceres.clauseSets.StandardClauseSet
+
+import at.logic.parsing.language.xml.XMLParser._
+import at.logic.parsing.readers.XMLReaders._
+import at.logic.language.hol.propositions._
+import at.logic.language.hol.quantifiers._
+import at.logic.language.hol.propositions.TypeSynonyms._
+import at.logic.language.hol.propositions.Definitions._
+import at.logic.language.hol.propositions.ImplicitConverters._
+import at.logic.language.hol.logicSymbols.ConstantStringSymbol
+import at.logic.language.lambda.typedLambdaCalculus._
+import at.logic.language.lambda.types.ImplicitConverters._
+import at.logic.language.lambda.types.Definitions._
+import at.logic.language.lambda.symbols.VariableStringSymbol
+import at.logic.language.lambda.symbols.ImplicitConverters._
+import at.logic.calculi.lk.propositionalRules._
+import at.logic.calculi.lk.lkSpecs.beMultisetEqual
+import at.logic.calculi.lk.base._
+
+import java.util.zip.GZIPInputStream
+import java.io.{FileReader, FileInputStream, InputStreamReader}
+import java.io.File.separator
+
+class PrimeProofTest extends SpecificationWithJUnit {
+  "The system" should {
+    "parse correctly the second-order primeproof" in {
+      val proofs = (new XMLReader(new InputStreamReader(new GZIPInputStream(new FileInputStream("target" + separator + "test-classes" + separator + "prime2.xml.gz")))) with XMLProofDatabaseParser).getProofs()
+      val proof = proofs.first
+      val cut_occs = getCutAncestors( proof )
+      val s = StructCreators.extract( proof, cut_occs )
+      val cs = StandardClauseSet.transformStructToClauseSet( s )
+//      def sequentToString( s: Sequent ) = {
+//        var ret = ""
+//        s.antecedent.foreach( formula => ret += formula.toStringSimple + ", ")
+//        ret += " :- "
+//        s.succedent.foreach( formula => ret += formula.toStringSimple + ", ")
+//        ret
+//      }
+//      cs.foreach( s => print( sequentToString( s ) + "\n") )
+      proofs.size must beEqual(1)
+    }
+  }
+}
