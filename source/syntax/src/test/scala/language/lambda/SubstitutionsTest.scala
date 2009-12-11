f5a478aa0c163c9c55c87e8f9be4b8c691ce8b5a
Cvetan Dunchev <cdunchev@gmail.com>
2009-12-11 14:08:05 +0000

    uncomment the commented bt Bruno code
    git-svn-id: http://gapt.googlecode.com/svn/trunk@219 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/syntax/src/test/scala/language/lambda/SubstitutionsTest.scala
+++ source/syntax/src/test/scala/language/lambda/SubstitutionsTest.scala
@@ -51,84 +51,84 @@ class SubstitutionsTest extends SpecificationWithJUnit {
       val expression = App(f, v)
       ( App(f, App(f, x)) ) must beEqual ( sigma(expression) )
     }
-//    "substitute correctly when SingleSubstitution is applied (3)" in {
-//        val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
-//        val y = LambdaVar("y", i)
-//        val e = App(f, x)
-//        val sigma: SingleSubstitution[Lambda] = (v,e)
-//        val expression = Abs(y, App(f, v))
-//        ( Abs(y,App(f, App(f, x))) ) must beEqual ( sigma(expression) )
-//    }
-//    "substitute correctly when SingleSubstitution is applied, renaming bound variables (1)" in {
-//        val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
-//        val e = App(f, x)
-//        val sigma: SingleSubstitution[Lambda] = (v,e)
-//        val exp1 = Abs(x, App(f, v))
-//        val exp2 = sigma(exp1)
-//        debug(exp2.toString)
-//        val exp3 = Abs(x,App(f, App(f, x)))
-//        val isDifferent = !(exp2==exp3)
-//        ( isDifferent ) must beEqual ( true )
-//    }
-//    "substitute correctly when SingleSubstitution is applied, renaming bound variables (2)" in {
-//        val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
-//        val e = App(f, x)
-//        val sigma: SingleSubstitution[Lambda] = (v,e)
-//        val exp1 = Abs(f, App(f, v))
-//        val exp2 = sigma(exp1)
-//        debug(exp2.toString)
-//        val exp3 = Abs(f,App(f, App(f, x)))
-//        val isDifferent = !(exp2==exp3)
-//        ( isDifferent ) must beEqual ( true )
-//    }
-//    "create Substitution correctly" in {
-//        val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
-//        val e = App(f, x)
-//        val sigma: SingleSubstitution[Lambda] = (v,e)
-//        val sigma1 = Substitution[Lambda]( sigma::Nil )
-//        val sigma2 = Substitution[Lambda]( List(sigma) )
-//        val sigma3 = new Substitution[Lambda](v,e)  // Unfortunately, we need the keyword "new" when we use alternative constructors of the case class...
-//        val sigma4 = new Substitution[Lambda](sigma)  // Unfortunately, we need the keyword "new" when we use alternative constructors of the case class...
-//        val sigma5:Substitution[Lambda] = (v,e)
-//        val sigma6:Substitution[Lambda] = sigma
-//        val areEqual = ((sigma1==sigma2) && (sigma2==sigma3) && (sigma3==sigma4) && (sigma4==sigma5) && (sigma5==sigma6))
-//        ( areEqual ) must beEqual ( true )
-//    }
-//    "substitute correctly when Substitution is applied" in {
-//        val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
-//        val e = App(f, x)
-//        val sigma: SingleSubstitution[Lambda] = (v,e)
-//        val sigma1 = Substitution[Lambda]( sigma::Nil )
-//        val exp = App(f, v)
-//        ( sigma(exp) ) must beEqual ( sigma1(exp) )
-//    }
-//    "cons SingleSubstitution with Substitution correctly" in {
-//        val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
-//        val e = App(f, x)
-//        val sigma: SingleSubstitution[Lambda] = (v,e)
-//        val sigma1 = Substitution[Lambda]( sigma::Nil )
-//        val sigma2 = Substitution[Lambda]( sigma::sigma::Nil )
-//        val sigma3 = sigma::sigma1
-//        val sigma4 = sigma1:::sigma1
-//        ( sigma2 ) must beEqual ( sigma3 )
-//    }
-//    "concatenate/compose 2 Substitutions correctly" in {
-//        val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
-//        val e = App(f, x)
-//        val sigma: SingleSubstitution[Lambda] = (v,e)
-//        val sigma1 = Substitution[Lambda]( sigma::Nil )
-//        val sigma2 = Substitution[Lambda]( sigma::sigma::Nil )
-//        val sigma3 = sigma1:::sigma1
-//        ( sigma2 ) must beEqual ( sigma3 )
-//    }
-//    "substitute correctly when Substitution is applied" in {
-//        val v = LambdaVar("v", i); val x = LambdaVar("x", i); val y = LambdaVar("y", i);
-//        val f = LambdaVar("f", i -> i)
-//        val e = App(f, v)
-//        val sigma1: SingleSubstitution[Lambda] = (v,x)
-//        val sigma2: SingleSubstitution[Lambda] = (x,y)
-//        val sigma3 = Substitution[Lambda]( sigma1::sigma2::Nil )
-//        ( sigma3(e) ) must beEqual ( App(f,y) )
-//    }
+    "substitute correctly when SingleSubstitution is applied (3)" in {
+                val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
+                val y = LambdaVar("y", i)
+                val e = App(f, x)
+                val sigma: SingleSubstitution = (v,e)
+                val expression = Abs(y, App(f, v))
+                ( Abs(y,App(f, App(f, x))) ) must beEqual ( sigma(expression) )
+            }
+            "substitute correctly when SingleSubstitution is applied, renaming bound variables (1)" in {
+                val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
+                val e = App(f, x)
+                val sigma: SingleSubstitution = (v,e)
+                val exp1 = Abs(x, App(f, v))
+                val exp2 = sigma(exp1)
+                debug(exp2.toString)
+                val exp3 = Abs(x,App(f, App(f, x)))
+                val isDifferent = !(exp2==exp3)
+                ( isDifferent ) must beEqual ( true )
+            }
+            "substitute correctly when SingleSubstitution is applied, renaming bound variables (2)" in {
+                val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
+                val e = App(f, x)
+                val sigma: SingleSubstitution = (v,e)
+                val exp1 = Abs(f, App(f, v))
+                val exp2 = sigma(exp1)
+                debug(exp2.toString)
+                val exp3 = Abs(f,App(f, App(f, x)))
+                val isDifferent = !(exp2==exp3)
+                ( isDifferent ) must beEqual ( true )
+            }
+            "create Substitution correctly" in {
+                val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
+                val e = App(f, x)
+                val sigma: SingleSubstitution = (v,e)
+                val sigma1 = Substitution( sigma::Nil )
+                val sigma2 = Substitution( List(sigma) )
+                val sigma3 = new Substitution(v,e)  // Unfortunately, we need the keyword "new" when we use alternative constructors of the case class...
+                val sigma4 = new Substitution(sigma)  // Unfortunately, we need the keyword "new" when we use alternative constructors of the case class...
+                val sigma5:Substitution = (v,e)
+                val sigma6:Substitution = sigma
+                val areEqual = ((sigma1==sigma2) && (sigma2==sigma3) && (sigma3==sigma4) && (sigma4==sigma5) && (sigma5==sigma6))
+                ( areEqual ) must beEqual ( true )
+            }
+            "substitute correctly when Substitution is applied" in {
+                val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
+                val e = App(f, x)
+                val sigma: SingleSubstitution = (v,e)
+                val sigma1 = Substitution( sigma::Nil )
+                val exp = App(f, v)
+                ( sigma(exp) ) must beEqual ( sigma1(exp) )
+            }
+            "cons SingleSubstitution with Substitution correctly" in {
+                val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
+                val e = App(f, x)
+                val sigma: SingleSubstitution = (v,e)
+                val sigma1 = Substitution( sigma::Nil )
+                val sigma2 = Substitution( sigma::sigma::Nil )
+                val sigma3 = sigma::sigma1
+                val sigma4 = sigma1:::sigma1
+                ( sigma2 ) must beEqual ( sigma3 )
+            }
+            "concatenate/compose 2 Substitutions correctly" in {
+                val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
+                val e = App(f, x)
+                val sigma: SingleSubstitution = (v,e)
+                val sigma1 = Substitution( sigma::Nil )
+                val sigma2 = Substitution( sigma::sigma::Nil )
+                val sigma3 = sigma1:::sigma1
+                ( sigma2 ) must beEqual ( sigma3 )
+            }
+            "substitute correctly when Substitution is applied" in {
+                val v = LambdaVar("v", i); val x = LambdaVar("x", i); val y = LambdaVar("y", i);
+                val f = LambdaVar("f", i -> i)
+                val e = App(f, v)
+                val sigma1: SingleSubstitution = (v,x)
+                val sigma2: SingleSubstitution = (x,y)
+                val sigma3 = Substitution( sigma1::sigma2::Nil )
+                ( sigma3(e) ) must beEqual ( App(f,y) )
+            }
   }
 }
