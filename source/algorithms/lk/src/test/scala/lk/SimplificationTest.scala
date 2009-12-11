7b39c1023c5228415fdfe04b6a2d9f6b1421c122
Daniel Weller <fraini@gmail.com>
2009-12-11 07:50:09 +0000

    Renamed files, added set-normalization for sequents.
    git-svn-id: http://gapt.googlecode.com/svn/trunk@214 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/algorithms/lk/src/test/scala/lk/SimplificationTest.scala
+++ source/algorithms/lk/src/test/scala/lk/SimplificationTest.scala
@@ -2,7 +2,7 @@
  * Description: 
 **/
 
-package at.logic.algorithms.misc
+package at.logic.algorithms.lk.simplification
 
 import org.specs._
 import org.specs.runner._
@@ -13,18 +13,25 @@ import at.logic.language.lambda.symbols.ImplicitConverters._
 import at.logic.calculi.lk.base.Sequent
 import scala.collection.immutable.EmptySet
 
-class TautologyDeletionTest extends SpecificationWithJUnit {
-  "TautologyDeletion" should {
-    "correctly delete tautologous sequents" in {
+class SimplificationTest extends SpecificationWithJUnit {
+  "Simplifications" should {
       val a = HOLVarFormula( "a" )
       val b = HOLVarFormula( "b" )
       val s1 = Sequent( a::Nil, a::Nil )
       val s2 = Sequent( b::a::b::Nil, b::b::b::a::b::Nil )
       val s3 = Sequent( a::Nil, b::Nil )
       val s4 = Sequent( b::Nil, a::Nil )
+
+    "correctly delete tautologous sequents" in {
       val list = s1::s2::s3::s4::Nil
       val dlist = deleteTautologies( list )
       dlist.size must beEqual( 2 )
+    }
+
+    "correctly set-normalize a list of Sequents" in {
+      val list = s1::s2::s2::s1::s2::s3::s1::s2::s4::s3::s2::s1::s2::s3::Nil
+      val set = setNormalize( list )
+      set.size must beEqual( 4 )
     }
   }
 }
