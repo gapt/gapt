e676d99bb076cf569ab9539ea263102a9700e470
fraini <fraini@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2009-12-11 07:50:09 +0000

    Renamed files, added set-normalization for sequents.
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@214 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ algorithms/lk/src/main/scala/lk/simplification.scala
@@ -0,0 +1,22 @@
+package at.logic.algorithms.lk
+
+import scala.collection.immutable.Set
+
+import at.logic.calculi.lk.base.Sequent
+
+package simplification {
+
+  object deleteTautologies
+  {
+    def apply(sequents: List[Sequent]) : List[Sequent] =
+      sequents.filter( s => s.antecedent.exists( f => s.succedent.contains( f ) ) )
+  }
+
+  object setNormalize
+  {
+    def apply(sequents: List[Sequent]) : Set[Sequent] = sequents match {
+      case x :: rest => setNormalize( rest ) + x
+      case Nil => Set[Sequent]()
+    }
+  }
+}
