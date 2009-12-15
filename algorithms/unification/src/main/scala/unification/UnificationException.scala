56b733043677a790be4039f455aad5be4bb9b6a1
shaolintl <shaolintl@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2009-12-15 16:56:35 +0000

    some implementaton of unification in fol
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@230 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ algorithms/unification/src/main/scala/unification/UnificationException.scala
@@ -0,0 +1,10 @@
+/*
+ * UnificationException.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package at.logic.unification
+
+class UnificationException(msg: String) extends Exception(msg)
--- algorithms/unification/src/test/scala/unification/UnificationTest.scala
+++ algorithms/unification/src/test/scala/unification/UnificationTest.scala
@@ -7,23 +7,50 @@ package at.logic.unification
 
 import org.specs._
 import org.specs.runner._
-import at.logic.language.fol
+import at.logic.language.fol._
+import at.logic.language.lambda.symbols._
+import at.logic.language.lambda.types._
+import at.logic.language.lambda.substitutions._
+import at.logic.language.lambda.typedLambdaCalculus._
+import at.logic.language.lambda.types.Definitions._
+import at.logic.language.hol._
+import at.logic.language.hol.propositions._
+import at.logic.language.hol.propositions.TypeSynonyms._
+import at.logic.language.hol.logicSymbols._
+import at.logic.language.lambda.typedLambdaCalculus._
 
 class UnificationTest extends SpecificationWithJUnit {
-  //val alg = new FOLUnification {}
+  val alg = new FOLUnification {}
   "Unification" should {
     "return None if terms are not unifiable" in
     {
-      "term 1" in
+      "- both are constants" in
       {
-        var a: FOLConst = FOLConst(ConstantStringSymbol("a"))
-        var sym: ConstantSymbolA = ConstantStringSymbol("f")
-        var args: List[FOLTerm]
-        args = args+a
-        var func1 = Function(sym, args)
+        val a = FOLConst(ConstantStringSymbol("a"))
+        val b = FOLConst(ConstantStringSymbol("b"))
+        (alg.unify(a,b)) must beEqual (None)
       }
-
-      "term 2" in
+      "- both are functions" in {
+        val a = Function(ConstantStringSymbol("f"), FOLConst(ConstantStringSymbol("a"))::Nil)
+        val b = Function(ConstantStringSymbol("g"), FOLConst(ConstantStringSymbol("a"))::Nil)
+        (alg.unify(a,b)) must beEqual (None)
+      }
+    }
+    "return Empty substitution if both terms are" in {
+      "constants" in {
+        val a = FOLConst(ConstantStringSymbol("a"))
+        val b = FOLConst(ConstantStringSymbol("a"))
+        (alg.unify(a,b)) must beEqual (Some(Substitution(Nil)))
+      }
+      "functions" in {
+        val a = Function(ConstantStringSymbol("f"), FOLConst(ConstantStringSymbol("a"))::Nil)
+        val b = Function(ConstantStringSymbol("f"), FOLConst(ConstantStringSymbol("a"))::Nil)
+        (alg.unify(a,b)) must beEqual (Some(Substitution(Nil)))
+      }
+    }
+  }
+}
+      /*"term 2" in
       {
         var b: FOLConst = FOLConst(ConstantStringSymbol("b"))
         var sym: ConstantSymbolA = ConstantStringSymbol("f")
@@ -56,5 +83,4 @@ class UnificationTest extends SpecificationWithJUnit {
       () must throw
     }
   }
-}
-*/
+}*/
