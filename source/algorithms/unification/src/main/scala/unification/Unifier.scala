e38be8a2ec35d938c2c548bef445ece8a5dbbda6
Tomer Libal <shaolintl@gmail.com>
2009-12-15 16:56:35 +0000

    some implementaton of unification in fol
    git-svn-id: http://gapt.googlecode.com/svn/trunk@230 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/algorithms/unification/src/main/scala/unification/Unifier.scala
+++ source/algorithms/unification/src/main/scala/unification/Unifier.scala
@@ -1,13 +1,14 @@
 /* Description: A unifier represents a map between variables and terms
 **/
 
-package at.logic.unification
+package at.logic.unification.fol
 
 import scala.collection.mutable._
 
 import at.logic.language.fol._
 import at.logic.language.lambda.symbols._
 import at.logic.language.lambda.types._
+import at.logic.language.lambda.substitutions._
 import at.logic.language.lambda.typedLambdaCalculus._
 import at.logic.language.lambda.types.Definitions._
 import at.logic.language.hol._
@@ -16,12 +17,23 @@ import at.logic.language.hol.propositions.TypeSynonyms._
 import at.logic.language.hol.logicSymbols._
 import at.logic.language.lambda.typedLambdaCalculus._
 
-
-
-
 //trait Unifier extends Map[VariableA[TypeA], TermA[TypeA]]
-trait Unifier extends Substitution//Map[Var, LambdaExpression]
-class FOLUnifier extends HashMap[Var,LambdaExpression] with Unifier
+//trait Unifier extends Substitution//Map[Var, LambdaExpression]
+
+trait FOLUnification {
+  def unify(f: FOLTerm, g: FOLTerm) : Option[Substitution] = (f,g) match {
+    case (FOLConst(x), FOLConst(y)) if x != y => None // symbol clash constants
+    case (FOLConst(x), FOLConst(y)) if x == y => Some(Substitution(Nil))
+    case (Function(x, _), Function(y, _)) if x != y => None // symbol clash functions
+    case (Function(_, args1), Function(_, args2)) if args1.length != args2.length => None // symbol clash functions arity
+    case (Function(_, args1), Function(_, args2)) => args1.zip(args2).foldLeft(Some(Substitution(Nil)): Option[Substitution])(func)
+    case _ => throw new UnificationException("Unknown terms was given to first order unification: " + f.toString + " - " + g.toString)
+  }
+  private def func(sigmaOption: Option[Substitution], x: Pair[FOLTerm, FOLTerm]): Option[Substitution] = sigmaOption match {
+    case None => None
+    case Some(sigma) => unify(sigma(x._1).asInstanceOf[FOLTerm], sigma(x._2).asInstanceOf[FOLTerm])
+  }
+}
 /*
 
 trait FOLUnification {
