6abe6280e5cd6a3103a9c0f86eca9e012a9d3a16
cdunchev <cdunchev@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2010-01-22 19:04:22 +0000

    Unification for FOL.
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@292 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- algorithms/unification/src/main/scala/unification/fol/FOLUnificationAlgorithm.scala
+++ algorithms/unification/src/main/scala/unification/fol/FOLUnificationAlgorithm.scala
@@ -8,10 +8,10 @@
 package at.logic.unification.fol
 
 import at.logic.language.lambda.typedLambdaCalculus._
-import at.logic.language.lambda.substitutions._
+import at.logic.language.fol.substitutions._
 import at.logic.unification.UnificationAlgorithm
 import at.logic.language.fol._
-/*
+
 object FOLUnificationAlgorithm extends UnificationAlgorithm {
   def unifiy(term1: LambdaExpression, term2: LambdaExpression) = {
     require(term1.isInstanceOf[FOLTerm] && term2.isInstanceOf[FOLTerm])
@@ -22,13 +22,15 @@ object FOLUnificationAlgorithm extends UnificationAlgorithm {
 
  def applySubToListOfPairs(l : List[Tuple2[FOLTerm, FOLTerm]], s : Substitution) : List[Tuple2[FOLTerm, FOLTerm]] =
     {
-      l.foldLeft(Nil)((x,v)=> (s.applyFOL(x),s.applyFOL(v)))
+    //  l.foldLeft(Nil)((Tuple2(x,v))=> (Tuple2(s.applyFOL(x),s.applyFOL(v))))
+      l.map((a) => (s.applyFOL(a._1), s.applyFOL(a._2)))
+      return l;
     }
 
-  def isSolvedVarIn(x : FOLVar, s : List[Tuple2[FOLTerm, FOLTerm]]) : Boolean =
+  def isSolvedVarIn(x : FOLVar, l : List[Tuple2[FOLTerm, FOLTerm]]) : Boolean =
   {
-      for ( term <- ((s.foldLeft(Nil)((a,b)=>a::b._1)) ::: (s.foldLeft(Nil)((a,b)=>a::b._2))))
-        if(x.exists(getVars(term)))
+      for ( term <- ((l.map((a)=>a._1)) ::: (l.map((a)=>a._2))))
+        if(getVars(term).contains(x))
             false
       true
 
@@ -43,19 +45,19 @@ object FOLUnificationAlgorithm extends UnificationAlgorithm {
 
   def unifySetOfTuples(s1: List[Tuple2[FOLTerm, FOLTerm]], s2 : List[Tuple2[FOLTerm, FOLTerm]]) : Option[(List[Tuple2[FOLTerm, FOLTerm]], List[Tuple2[FOLTerm, FOLTerm]])] = (s1,s2) match
   {
-    case (((a1,a2)::s), s2) if a1 == a2 => Some(unifySetOfTuples(s, s2))
-    case (((Function(f1,args1), Function(f2, args2))::s), s2)
+    case (((a1,a2)::s), s2) if a1 == a2 => unifySetOfTuples(s, s2)
+    case (((Function(f1,args1), Function(f2, args2)):: (s : List[Tuple2[FOLTerm, FOLTerm]])), s2)
       if args1.length == args2.length && f1==f2  =>
         {
-            return Some((unifySetOfTuples(zip(args1,args2)::s),s2))
+            return unifySetOfTuples(args1.zip(args2) ::: s, s2)
         }
     case (((x : FOLVar,v)::s), s2) if !getVars(v).contains(x) && isSolvedVarIn(x,s) =>
       //  x does not occur in v && x is not in solved form =>
-        Some(unifySetOfTuples(applySubToListOfPairs(s,Substitution(x,v)), (x,v)::applySubToListOfPairs((x,v),s2)))
+        unifySetOfTuples(applySubToListOfPairs(s,Substitution(x,v)), (x,v)::applySubToListOfPairs(s2,Substitution(x,v)))
 
     case (((v, x : FOLVar)::s), s2) if !getVars(v).contains(x) && isSolvedVarIn(x,s) =>
-        Some(unifySetOfTuples(applySubToListOfPairs(s,Substitution(x,v)), (x,v)::applySubToListOfPairs((x,v),s2)))
+        unifySetOfTuples(applySubToListOfPairs(s,Substitution(x,v)), (x,v)::applySubToListOfPairs(s2,Substitution(x,v)))
     case (Nil, s2) => Some((Nil, s2))
     case _ => None
   }
-}*/
+}
