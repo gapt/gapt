f876a7cca9b1dbdf71c51b7b1331375371628100
cdunchev <cdunchev@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2010-01-27 17:29:00 +0000

    finished version of eta expansion
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@307 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ syntax/language/lambda/src/main/scala/language/lambda/etaExpansion.scala
@@ -0,0 +1,47 @@
+/*
+ * etaExpansion.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package at.logic.language.lambda
+
+import at.logic.language.lambda.symbols._
+import typedLambdaCalculus._
+import at.logic.language.lambda.typedLambdaCalculus._
+import at.logic.language.lambda.types._
+
+// etaExpansion is applied to expressions which are in \beta normal form ONLY
+
+package etaExpansion {
+  object EtaExpand {
+    def apply(term: LambdaExpression): LambdaExpression = {
+      term match {
+        case x:Var => x
+
+        case App(m,n) => App(apply(m),apply(n))
+        
+        case AbsN1(lsVars, sub) => sub.exptype match {
+            case FunctionType(_, lsArgs ) if lsVars.size < lsArgs.size => {
+                val v = LambdaFactory.createVar(VariableStringSymbol("x"), lsArgs.head, Some(1))
+                LambdaFactory.createAbs(v, LambdaFactory.createApp(sub, v))
+            }
+            case Ti() => AbsN(lsVars, sub)
+            case To() => AbsN(lsVars, sub)
+            case _ => term
+            
+        }
+      }
+    }
+  }
+
+  object EtaNormalize {
+    def apply(term: LambdaExpression): LambdaExpression = {
+      term match {
+        case t: LambdaExpression if EtaExpand.apply(term) == t => term
+        case _ => EtaExpand.apply(term)
+      }
+    }
+  }
+}
