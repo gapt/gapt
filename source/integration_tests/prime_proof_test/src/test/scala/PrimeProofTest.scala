b190ae8b861ed88540b67c12a6640815d97611b9
Tomer Libal <shaolintl@gmail.com>
2010-02-21 16:07:34 +0000

    using atp with unit refinement in order to generate all clauses resolved with unit clauses.
    git-svn-id: http://gapt.googlecode.com/svn/trunk@374 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/integration_tests/prime_proof_test/src/test/scala/PrimeProofTest.scala
+++ source/integration_tests/prime_proof_test/src/test/scala/PrimeProofTest.scala
@@ -20,7 +20,9 @@ import at.logic.parsing.calculus.xml.saveXML
 import at.logic.parsing.calculi.latex.SequentsListLatexExporter
 import at.logic.parsing.writers.FileWriter
 import at.logic.parsing.language.arithmetic.HOLTermArithmeticalExporter
-
+import at.logic.provers.atp.Prover
+import at.logic.provers.atp.commands._
+import at.logic.provers.atp.refinements.UnitRefinement
 import at.logic.language.lambda.symbols._
 import at.logic.language.lambda.types._
 import at.logic.language.hol.propositions._
@@ -79,13 +81,17 @@ class PrimeProofTest extends SpecificationWithJUnit {
 
       val holcs = pdb.axioms ::: ( seq1::seq2::seq3::seq4::Nil ) ::: csPre
 
-      // convert to fol
-      val imap = Map[at.logic.language.lambda.typedLambdaCalculus.LambdaExpression, at.logic.language.hol.logicSymbols.ConstantStringSymbol]() // the scope for most tests is just the term itself
+      // maps original types and definitions of abstractions
+      val sectionsPre = ("Types", getTypeInformation(holcs).toList.sort((x,y) => x.toString < y.toString))::Nil
+
+      // convert to fol and obtain map of definitons
+      val imap = Map[at.logic.language.lambda.typedLambdaCalculus.LambdaExpression, at.logic.language.hol.logicSymbols.ConstantStringSymbol]()
       val iid = new {var idd = 0; def nextId = {idd = idd+1; idd}}
       val cs = holcs.map(x => Sequent(
           x.antecedent.map(y => reduceHolToFol(y.asInstanceOf[HOLTerm],imap,iid).asInstanceOf[FOLFormula]),
           x.succedent.map(y => reduceHolToFol(y.asInstanceOf[HOLTerm],imap,iid).asInstanceOf[FOLFormula])
       ))
+      val sections = ("Definitions", imap.toList.map(x => (x._1, createExampleFOLConstant(x._1, x._2))))::sectionsPre
 
       val dcs = deleteTautologies( cs )
       Console.println("dcs size: " + dcs.size)
@@ -101,13 +107,33 @@ class PrimeProofTest extends SpecificationWithJUnit {
       val cssv = sequentNormalize(scss)
       Console.println("cssv size: " + cssv.size)
 
+      // apply unit resolution and subsumption on the resulted clause set
+      val ref = new at.logic.provers.atp.refinements.UnitRefinement{}
+      new Prover{}.refute(AutomatedFOLStream(cssv.map(x => at.logic.calculi.resolution.base.Clause(x.antecedent.asInstanceOf[List[HOLFormula]], x.succedent.asInstanceOf[List[HOLFormula]])), -1, ref))
+      val newUnitSet = ref.clauses.map(x => x.root).toList
+      Console.println("newUnitSet size: " + newUnitSet.size)
+      val newUnitSetSubsum = subsumedClausesRemoval(newUnitSet)
+      Console.println("newUnitSetSubsum size: " + newUnitSetSubsum.size)
+
       // done for preserving order
       val neg = cssv.filter(x => x.succedent.isEmpty)
       val mix = cssv.filter(x => !x.succedent.isEmpty && !x.antecedent.isEmpty)
       val pos = cssv.filter(x => x.antecedent.isEmpty)
 
+      // done for preserving order
+      val neg2 = newUnitSetSubsum.filter(x => x.succedent.isEmpty)
+      val mix2 = newUnitSetSubsum.filter(x => !x.succedent.isEmpty && !x.antecedent.isEmpty)
+      val pos2 = newUnitSetSubsum.filter(x => x.antecedent.isEmpty)
+
+      val subsum = sequentNormalize(cssUnit).diff(cssv)
+      Console.println("subsum size: " + subsum.size)
+      (new FileWriter("target" + separator + "test-classes" + separator + "prime2-cs-subsumed.tex") with SequentsListLatexExporter with HOLTermArithmeticalExporter)
+        .exportSequentList(subsum, sections).close
       (new FileWriter("target" + separator + "test-classes" + separator + "prime2-cs.tex") with SequentsListLatexExporter with HOLTermArithmeticalExporter)
-        .exportSequentList(neg.sort(mySort) ++ mix.sort(mySort) ++ pos.sort(mySort)).close
+        .exportSequentList(neg.sort(mySort) ++ mix.sort(mySort) ++ pos.sort(mySort), sections).close
+      (new FileWriter("target" + separator + "test-classes" + separator + "prime2-cs-unit.tex") with SequentsListLatexExporter with HOLTermArithmeticalExporter)
+        .exportSequentList(neg2.sort(mySort) ++ mix2.sort(mySort) ++ pos2.sort(mySort), sections).close
+      //saveXML( Pair("cs", cs)::Pair("dcs", dcs)::Pair("css", (css.toList))::Pair("cssv", cssv.toList)::Nil, "target" + separator + "test-classes" + separator + "prime2-cs.xml" )
       //saveXML( Pair("cs", cs)::Pair("dcs", dcs)::Pair("css", (css.toList))::Pair("cssv", cssv.toList)::Nil, "target" + separator + "test-classes" + separator + "prime2-cs.xml" )
     }
     /*"parse correctly the first-order prime proof, n=0" in {
