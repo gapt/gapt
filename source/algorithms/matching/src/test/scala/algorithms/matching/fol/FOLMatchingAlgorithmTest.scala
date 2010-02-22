fa5a6e525239ec7546991f84db949add6058e55a
Cvetan Dunchev <cdunchev@gmail.com>
2010-02-22 15:10:28 +0000

    Tests for the case of binary formula have been added
    git-svn-id: http://gapt.googlecode.com/svn/trunk@380 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/algorithms/matching/src/test/scala/algorithms/matching/fol/FOLMatchingAlgorithmTest.scala
+++ source/algorithms/matching/src/test/scala/algorithms/matching/fol/FOLMatchingAlgorithmTest.scala
@@ -123,6 +123,124 @@ class FOLMatchingAlgorithmTest extends SpecificationWithJUnit {
     // sub.get.apply(term) must beEqual (posInstance)
     }
 
+
+  "apply the MatchingSubstitution wrongly " in {
+     val term = new MyParser("f(x1, g(x1,x3), x3)").getTerm
+     val x1 = new MyParser("x1").getTerm
+     val x3 = new MyParser("x3").getTerm
+     val a = new MyParser("a").getTerm
+     val c = new MyParser("c").getTerm
+//     val l =  (new MyParser("x3").getTerm.asInstanceOf[Var])::Nil
+//     val m: scala.collection.immutable.Map[Var, LambdaExpression] = Map()
+//     val t = new FOLMatchingAlgorithm.MatchingSubstitution(l, m.+((x1.asInstanceOf[Var],a))++m.+((x3.asInstanceOf[Var],c))).apply(term)
+//     println("\n\n\n  term = "+term.toString+"\n\n")
+//     println("\n\n\n  term = "+term.toStringSimple+"\n\n")
+//     println("\n\n\n     t = "+t.toString+"\n\n")
+     //val sub1 = FOLUnificationAlgorithm.unify(term, posInstance)
+  //   println("Printing the substitution "+sub1)
+     0 must beEqual (0)
+    // println(sub.toString)
+    // sub.get.apply(term) must beEqual (posInstance)
+    }
+
+
+  "match the FOL formulas P(x1,f(x1, g(x1,x3), x3)) and P(c,f(x1, g(x1,a), x3))" in {
+     val term1 = new MyParser("f(x1, g(x1,x3), x3)").getTerm
+     val term2 = new MyParser("f(c, g(x1,a), x3)").getTerm
+     val x1 = new MyParser("x1").getTerm.asInstanceOf[FOLVar]
+     val x3 = new MyParser("x3").getTerm
+     val a = new MyParser("a").getTerm
+     val c = new MyParser("c").getTerm
+     val P1:Formula  = AllVar(x1,Atom(new ConstantStringSymbol("P"), x1::term1.asInstanceOf[FOLTerm]::Nil))
+     val P2:Formula  = AllVar(x1,Atom(new ConstantStringSymbol("P"), c.asInstanceOf[FOLConst]::term2.asInstanceOf[FOLTerm]::Nil))
+ //    val l =  (new MyParser("x3").getTerm.asInstanceOf[Var])::Nil
+     val l =  (x1.asInstanceOf[Var]::x3.asInstanceOf[Var]::Nil)
+//     val m: scala.collection.immutable.Map[Var, LambdaExpression] = Map()
+//     val t = new FOLMatchingAlgorithm.MatchingSubstitution(l, m.+((x1.asInstanceOf[Var],a))++m.+((x3.asInstanceOf[Var],c))).apply(term)
+//     println("\n\n\n  term = "+term.toString+"\n\n")
+//     println("\n\n\n  term = "+term.toStringSimple+"\n\n")
+//     println("\n\n\n     t = "+t.toString+"\n\n")
+     val sub1 = FOLMatchingAlgorithm.matchTerm(P1, P2)
+   //  println("\n\n\nPrinting the substitution: "+sub1+"\n\n\n")
+     0 must beEqual (0)
+    // println(sub.toString)
+    // sub.get.apply(term) must beEqual (posInstance)
+    }
+
+  "match the FOL formulas And(P(x1,f(x1, g(x1,x3), x3)),Q(x1)) and And(P(c,f(c, g(x1,a), x3)),Q(c))" in {
+     val term1 = new MyParser("f(x1, g(x1,x3), x3)").getTerm
+     val term2 = new MyParser("f(c, g(x1,a), x3)").getTerm
+     val x1 = new MyParser("x1").getTerm.asInstanceOf[FOLVar]
+     val x3 = new MyParser("x3").getTerm
+     val a = new MyParser("a").getTerm
+     val c = new MyParser("c").getTerm
+     val P1:Formula  = And(Atom(new ConstantStringSymbol("P"), x1::term1.asInstanceOf[FOLTerm]::Nil), Atom(new ConstantStringSymbol("Q"), x1::Nil))
+     val P2:Formula  = And(Atom(new ConstantStringSymbol("P"), c.asInstanceOf[FOLConst]::term2.asInstanceOf[FOLTerm]::Nil), Atom(new ConstantStringSymbol("Q"), c.asInstanceOf[FOLConst]::Nil))
+ //    val l =  (new MyParser("x3").getTerm.asInstanceOf[Var])::Nil
+     val l =  (x1.asInstanceOf[Var]::x3.asInstanceOf[Var]::Nil)
+//     val m: scala.collection.immutable.Map[Var, LambdaExpression] = Map()
+//     val t = new FOLMatchingAlgorithm.MatchingSubstitution(l, m.+((x1.asInstanceOf[Var],a))++m.+((x3.asInstanceOf[Var],c))).apply(term)
+//     println("\n\n\n  term = "+term.toString+"\n\n")
+//     println("\n\n\n  term = "+term.toStringSimple+"\n\n")
+//     println("\n\n\n     t = "+t.toString+"\n\n")
+     val sub1 = FOLMatchingAlgorithm.matchTerm(P1, P2)
+  //   println("\n\n\nPrinting the substitution: "+sub1+"\n\n\n")
+     sub1 must beEqual (None)
+    // println(sub.toString)
+    // sub.get.apply(term) must beEqual (posInstance)
+    }
+
+
+
+  "match the FOL formulas And(P(x2,f(x2, g(x1,x3), x3)),Q(c)) and And(P(c,f(c, g(x1,a), x1)),Q(c))" in {
+     val term1 = new MyParser("f(x2, g(x2,x3), x3)").getTerm.asInstanceOf[FOLTerm]
+     val term2 = new MyParser("f(c, g(a,x1), x1)").getTerm.asInstanceOf[FOLTerm]
+     val x1 = new MyParser("x1").getTerm.asInstanceOf[FOLVar]
+     val x2 = new MyParser("x2").getTerm.asInstanceOf[FOLVar]
+     val x3 = new MyParser("x3").getTerm.asInstanceOf[FOLVar]
+     val a = new MyParser("a").getTerm.asInstanceOf[FOLConst]
+     val c = new MyParser("c").getTerm.asInstanceOf[FOLConst]
+     val P1:Formula  = And(Atom(new ConstantStringSymbol("P"), term1::Nil), Atom(new ConstantStringSymbol("Q"), c::Nil))
+     val P2:Formula  = And(Atom(new ConstantStringSymbol("P"), term2::Nil), Atom(new ConstantStringSymbol("Q"), c::Nil))
+ //    val l =  (new MyParser("x3").getTerm.asInstanceOf[Var])::Nil
+     val l =  (x1.asInstanceOf[Var]::x3.asInstanceOf[Var]::Nil)
+//     val m: scala.collection.immutable.Map[Var, LambdaExpression] = Map()
+//     val t = new FOLMatchingAlgorithm.MatchingSubstitution(l, m.+((x1.asInstanceOf[Var],a))++m.+((x3.asInstanceOf[Var],c))).apply(term)
+//     println("\n\n\n  term = "+term.toString+"\n\n")
+//     println("\n\n\n  term = "+term.toStringSimple+"\n\n")
+//     println("\n\n     P1 = "+P1.toString+"\n\n")
+//     println("\n\n    P2 = "+P2.toString+"\n\n")
+     val sub1 = FOLMatchingAlgorithm.matchTerm(P1, P2)
+//     println("\n\nPrinting the substitution: "+sub1+"\n\n")
+     sub1 must beEqual (None)
+    // println(sub.toString)
+    // sub.get.apply(term) must beEqual (posInstance)
+    }
+
+      "match the FOL formulas And(P(f(x2, g(x1,x3), x3)),Q(x2)) and And(P(f(c, g(a,x1), x2)),Q(c))" in {
+     val term1 = new MyParser("f(x2, g(x2,x3), x3)").getTerm.asInstanceOf[FOLTerm]
+     val term2 = new MyParser("f(c, g(c,x1), x2)").getTerm.asInstanceOf[FOLTerm]
+     val x1 = new MyParser("x1").getTerm.asInstanceOf[FOLVar]
+     val x2 = new MyParser("x2").getTerm.asInstanceOf[FOLVar]
+     val x3 = new MyParser("x3").getTerm.asInstanceOf[FOLVar]
+     val a = new MyParser("a").getTerm.asInstanceOf[FOLConst]
+     val c = new MyParser("c").getTerm.asInstanceOf[FOLConst]
+     val P1:Formula  = And(Atom(new ConstantStringSymbol("P"), term1::Nil), Atom(new ConstantStringSymbol("Q"), x2::Nil))
+     val P2:Formula  = And(Atom(new ConstantStringSymbol("P"), term2::Nil), Atom(new ConstantStringSymbol("Q"), c::Nil))
+ //    val l =  (new MyParser("x3").getTerm.asInstanceOf[Var])::Nil
+     val l =  (x1.asInstanceOf[Var]::x3.asInstanceOf[Var]::Nil)
+//     val m: scala.collection.immutable.Map[Var, LambdaExpression] = Map()
+//     val t = new FOLMatchingAlgorithm.MatchingSubstitution(l, m.+((x1.asInstanceOf[Var],a))++m.+((x3.asInstanceOf[Var],c))).apply(term)
+//     println("\n\n\n  term = "+term.toString+"\n\n")
+//     println("\n\n\n  term = "+term.toStringSimple+"\n\n")
+//     println("\n\n     P1 = "+P1.toString+"\n\n")
+//     println("\n\n    P2 = "+P2.toString+"\n\n")
+     val sub1 = FOLMatchingAlgorithm.matchTerm(P1, P2)
+//     println("\n\nPrinting the substitution: "+sub1+"\n\n")
+     sub1 must beEqual (None)
+    // println(sub.toString)
+    // sub.get.apply(term) must beEqual (posInstance)
+    }
   }
 }
 
