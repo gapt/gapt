d5e26588db5e30e8d18938c5429ff1f6f9808b54
Martin.Riener <Martin.Riener@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2010-03-15 15:36:09 +0000

    Added a formula rendering component for super- and subcripts to prooftool.
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@407 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- gui/prooftool/src/test/scala/at/logic/prooftool/graphs/GraphVisualisationTest.scala
+++ gui/prooftool/src/test/scala/at/logic/prooftool/graphs/GraphVisualisationTest.scala
@@ -15,10 +15,58 @@ import at.logic.calculi.lk._
 import at.logic.calculi.lk.base._
 import java.io.{PrintWriter, FileReader, FileInputStream, InputStreamReader}
 import scala.collection.immutable.HashSet
+import scala.swing._
+import at.logic.prooftool.formuladrawing.FormulaLabel
+import java.awt.geom.Rectangle2D
+import java.awt.Dimension
 
 class GraphVisualisationTest extends SpecificationWithJUnit {
   val showWindow = false
-  
+
+    "formula parsing works" in {
+      val f = new Frame
+      val l = new FormulaLabel
+      l text = "a^bc_de_{fg}hijk^{lm}{no}"
+      f contents = l
+      f size = new Dimension(400,400)
+      if (showWindow) {
+        f visible = true
+        Thread.sleep(10000)
+      }
+      ()
+    }
+
+     "Showing Formulas works" in {
+        val reader = (new XMLReader(new InputStreamReader(new GZIPInputStream(
+                        new FileInputStream("target" + separator + "test-classes" + separator + "prime1-0.xml.gz")))) with XMLProofDatabaseParser)
+
+        val proofs = reader.getProofDatabase.proofs
+
+        //TODO: scala does not infer the inheritance of LKProof from Graph - hotfix: giving type by hand
+        val proof : at.logic.utils.ds.graphs.Graph[SequentOccurrence] = proofs.first
+         val f = new Frame
+         val scrollpane = new ScrollPane
+         val panel = new BoxPanel( Orientation.Vertical )
+         f contents_= scrollpane
+         scrollpane contents_= panel
+
+         var it = proof.graph.vertexSet.iterator
+         while (it.hasNext) {
+           val l = new FormulaLabel
+           l text_= it.next.toString
+           l size_= new Dimension(300,150)
+           l visible_= true
+           panel.contents += l
+         }
+          scrollpane visible_= true
+       
+          //f size = new Dimension(400,400)
+          //f visible = true
+          //panel.revalidate
+          //pThread.sleep(500000)
+        ()
+      }
+
     "Passing of scala graph to JavaViewer works" in {
         val g1: EmptyGraph[String] = ( )
         val g2: VertexGraph[String] = ("a", g1)
@@ -39,26 +87,12 @@ class GraphVisualisationTest extends SpecificationWithJUnit {
 
     //skip("this takes a lot of time")
     "Passing of prime proof to JavaViewer works" in {
-      /*
-      class A {
-        def rule = null;
-        def name = "abc";
-        val vertex = new SequentOccurrence(new HashSet, new HashSet)
-        def graph = null;
-      }
-      def p = new A with LKProof
-      def q : Graph[SequentOccurrence] = p
-      var pv = new ProofViewer[SequentOccurrence](p)      
-        */
-
-
-      /*  */
         val reader = (new XMLReader(new InputStreamReader(new GZIPInputStream(
                         new FileInputStream("target" + separator + "test-classes" + separator + "prime1-0.xml.gz")))) with XMLProofDatabaseParser)
 
         val proofs = reader.getProofDatabase.proofs
 
-        //TODO: the scala infer the inheritance of LKProof from Graph - hotfix: giving tpe by hand
+        //TODO: scala does not infer the inheritance of LKProof from Graph - hotfix: giving type by hand
         val proof : at.logic.utils.ds.graphs.Graph[SequentOccurrence] = proofs.first
         /*
         // --- output graph to dot format, works but commented out, because it creates additional files in the project
@@ -78,49 +112,4 @@ class GraphVisualisationTest extends SpecificationWithJUnit {
         }
         ()
     }
-    /*
-
-     "Creation of a JPanel works" should {
-     val g1: EmptyGraph[String] = ( )
-     val g2: VertexGraph[String] = ("a", g1)
-     val g21: VertexGraph[String] = ("b", g2)
-     val g3: EdgeGraph[String] = ("a", "b", g21)
-     val g4: EdgeGraph[String] = ("a", "c", EdgeGraph("b", "c", VertexGraph("c", g3)))
-     val g5 = EdgeGraph("e", "f", VertexGraph("f", VertexGraph("e", EmptyGraph[String])))
-     val g6: UnionGraph[String] = (g4, g5)
-      
-
-     var gv = new GraphVisualisation[String]
-     var frame = gv.buildFrame(g6)
-     /* if you want to see something comment the next line in */
-     // frame.show(); Thread.sleep(5000)
-
-     ()
-     }
-
-     "Placement of Nodes works" should {
-     val g1: EmptyGraph[String] = ( )
-     val g2: VertexGraph[String] = ("a", g1)
-     val g21: VertexGraph[String] = ("b", g2)
-     val g3: EdgeGraph[String] = ("a", "b", g21)
-     val g4: EdgeGraph[String] = ("a", "c", EdgeGraph("b", "c", VertexGraph("c", g3)))
-     val g5 = EdgeGraph("e", "f", VertexGraph("f", VertexGraph("e", EmptyGraph[String])))
-     val g6: UnionGraph[String] = (g4, g5)
-      
-
-     var gv = new GraphVisualisation[String]
-     var jgraph = gv.create(g6)
-     VisualisationUtils.placeNodes(jgraph)
-
-     var frame = gv.buildFrame(g6)
-     //frame.show()
-
-     frame.invalidate()
-
-     //Thread.sleep(15000)
-    
-
-     ()
-     }
-     */
 }
