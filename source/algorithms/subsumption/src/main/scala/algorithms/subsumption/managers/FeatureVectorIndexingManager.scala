04757f41ffa1cca7ec9632719f7a6b549d1f15ca
Cvetan Dunchev <cdunchev@gmail.com>
2010-03-29 11:22:42 +0000

    Feature Vector indexing
    git-svn-id: http://gapt.googlecode.com/svn/trunk@420 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ source/algorithms/subsumption/src/main/scala/algorithms/subsumption/managers/FeatureVectorIndexingManager.scala
@@ -0,0 +1,58 @@
+/*
+ * FeatureVectorIndexingManager.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package at.logic.algorithms.subsumption.managers
+
+import at.logic.utils.ds.mutable.trees._
+import at.logic.calculi.lk.base._
+import at.logic.algorithms.subsumption._
+
+
+  
+
+//val a = new VectorTreeManager with StillmabAlgorithm {val seqList = }
+trait VectorTreeManager extends SubsumptionAlgorithm  {
+//  val seqList: List[Sequent]
+//  val tree = Tree[Sequent](seqList)
+  val seqList: List[Sequent] = List()
+  var tree: MyTree[Sequent]
+
+ 
+
+  def forwardSubsumption1(vert: TreeNode[Sequent], features: List[Sequent=>Int], subsumedSeq: Sequent): Boolean = {
+    
+//    if(tree.isLeaf(vert)) {
+    if(features.isEmpty) {     
+      println("\n\nLeaf:  "+subsumedSeq+"   <--->   ");vert.print
+      vert.seqList.exists(seq => {val x = subsumes(subsumedSeq, seq); if (x) Console.println(x); x})
+//      for(seq <- vert.seqList) {
+//        if(subsumes(subsumedSeq, seq)) {
+//          println("The clause  is subsumed ! \n\n")
+//          return true
+//        }
+//      }
+//      return false
+    } else
+    {
+    println("\nvert = ")
+    vert.print
+    println("\n\nfeature vector subsumedSeq = "+features.head(subsumedSeq))
+
+    vert.children.exists(child => child._1 <= features.head(subsumedSeq) && forwardSubsumption1(child._2, features.tail, subsumedSeq))
+
+//    for (child <- vert.children)
+//    {
+//      if(child._1 <= features.head(subsumedSeq))
+//        if (forwardSubsumption1(child._2, features.tail, subsumedSeq))
+//          return true
+//    }
+//    println("\n\n    false \n\n")
+//    return false
+    }
+  }
+}
+
