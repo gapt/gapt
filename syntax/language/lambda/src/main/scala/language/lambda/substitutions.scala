c53f71bee97ec28dc0ea14802a66dc58563b9451
shaolintl <shaolintl@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2010-04-12 16:07:07 +0000

    per Martin's suggestion, substitution was changed to be parameterizable over the type of expression (lambda, hol or fol) and therefore the remaining substitution files (in hol and fol) are removed. all code should be fixed accordingly.
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@435 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- syntax/language/lambda/src/main/scala/language/lambda/substitutions.scala
+++ syntax/language/lambda/src/main/scala/language/lambda/substitutions.scala
@@ -9,45 +9,45 @@ package substitutions {
    * 1) it is a valid function, i.e. order of elements is irrelevant and each varialbe is mapped to only one element
    * 2) all mappings are applied simultaneously to a term i.e. {x |-> y, y |-> a}x = y and not a.
    */
-  class Substitution protected[substitutions](val map: scala.collection.immutable.Map[Var, LambdaExpression]) extends (LambdaExpression => LambdaExpression) {
+  class Substitution[T <: LambdaExpression] protected[substitutions](val map: scala.collection.immutable.Map[Var, T]) extends (T => T) {
     def ::(sub:Tuple2[Var, LambdaExpression]) = new Substitution(map + sub)
-    def :::(otherSubstitution:Substitution) = new Substitution(map ++ otherSubstitution.map.elements)
-    def apply(expression: LambdaExpression): LambdaExpression = applyWithChangeDBIndices(expression)
+    def :::(otherSubstitution:Substitution[T]) = new Substitution(map ++ otherSubstitution.map.elements)
+    def apply(expression: T): T = applyWithChangeDBIndices(expression)
     override def equals(a: Any) = a match {
-      case s: Substitution => map.equals(s.map)
+      case s: Substitution[_] => map.equals(s.map)
       case _ => false
     }
     override def hashCode() = map.hashCode
     override def toString = map.toString
     // the change of db indices is done automatically in the constructor of abs
-    protected def applyWithChangeDBIndices(expression: LambdaExpression): LambdaExpression = expression match {
+    protected def applyWithChangeDBIndices(expression: T): T = expression match {
       case x:Var if x.isFree => map.get(x) match {
           case Some(t) => t
-          case None => x
+          case None => x.asInstanceOf[T]
       }
-      case App(m,n) => App(applyWithChangeDBIndices(m), applyWithChangeDBIndices(n))
-      case abs: Abs => Abs(abs.variable ,applyWithChangeDBIndices(abs.expressionInScope))
+      case App(m,n) => App(applyWithChangeDBIndices(m.asInstanceOf[T]), applyWithChangeDBIndices(n.asInstanceOf[T])).asInstanceOf[T]
+      case abs: Abs => Abs(abs.variable ,applyWithChangeDBIndices(abs.expressionInScope.asInstanceOf[T])).asInstanceOf[T]
       case _ => expression
     }
 
     // make sure the overriden keys are of the applying sub
-    def compose(sub: Substitution): Substitution = Substitution(map ++ sub.map.map(x => (x._1, apply(x._2))))
+    def compose(sub: Substitution[T]): Substitution[T] = Substitution(map ++ sub.map.map(x => (x._1, apply(x._2))))
   }
 
   object Substitution {
-    def apply(subs: Iterator[Tuple2[Var, LambdaExpression]]): Substitution = new Substitution(new scala.collection.immutable.HashMap[Var, LambdaExpression]() ++ subs)
-    def apply(subs: Tuple2[Var, LambdaExpression]*): Substitution = apply(subs.elements)
-    def apply(subs: List[Tuple2[Var, LambdaExpression]]): Substitution = apply(subs.elements)
-    def apply(variable: Var, expression: LambdaExpression): Substitution = apply((variable, expression))
-    def apply(map: scala.collection.immutable.Map[Var, LambdaExpression]): Substitution = new Substitution( map )
+    def apply[T <: LambdaExpression](subs: Iterator[Tuple2[Var, T]]): Substitution[T] = new Substitution(new scala.collection.immutable.HashMap[Var, T]() ++ subs)
+    def apply[T <: LambdaExpression](subs: Tuple2[Var, T]*): Substitution[T] = apply(subs.elements)
+    def apply[T <: LambdaExpression](subs: List[Tuple2[Var, T]]): Substitution[T] = apply(subs.elements)
+    def apply[T <: LambdaExpression](variable: Var, expression: T): Substitution[T] = apply((variable, expression))
+    def apply[T <: LambdaExpression](map: scala.collection.immutable.Map[Var, T]): Substitution[T] = new Substitution( map )
     def apply() = new Substitution(new scala.collection.immutable.EmptyMap)
   }
   
   object ImplicitConverters {
-    implicit def convertSubstitutionToPair(sub: Substitution): Tuple2[Var,LambdaExpression] = {
+    implicit def convertSubstitutionToPair[T <: LambdaExpression](sub: Substitution[T]): Tuple2[Var,T] = {
       require(sub.map.size == 1)
       sub.map.elements.next
     }
-    implicit def convertPairToSubstitution(pair: Tuple2[Var, LambdaExpression]): Substitution = Substitution(pair)
+    implicit def convertPairToSubstitution[T <: LambdaExpression](pair: Tuple2[Var, T]): Substitution[T] = Substitution(pair)
   }
 }
