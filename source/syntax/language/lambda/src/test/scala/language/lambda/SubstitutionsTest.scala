cc1ec3dbc6159dc6edbaed73b239ce0e67bad614
Tomer Libal <shaolintl@gmail.com>
2010-04-12 16:07:07 +0000

    per Martin's suggestion, substitution was changed to be parameterizable over the type of expression (lambda, hol or fol) and therefore the remaining substitution files (in hol and fol) are removed. all code should be fixed accordingly.
    git-svn-id: http://gapt.googlecode.com/svn/trunk@435 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/syntax/language/lambda/src/test/scala/language/lambda/SubstitutionsTest.scala
+++ source/syntax/language/lambda/src/test/scala/language/lambda/SubstitutionsTest.scala
@@ -39,7 +39,7 @@ class SubstitutionsTest extends SpecificationWithJUnit {
     "make implicit conversion from pair to Substitution" in {
       val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
       val e = App(f, x)
-      val sigma: Substitution = (v,e)
+      val sigma: Substitution[LambdaExpression] = (v,e)
       val eta = (v,e)
       ( Substitution(eta) ) must beEqual ( sigma )
     }
@@ -54,14 +54,14 @@ class SubstitutionsTest extends SpecificationWithJUnit {
       val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
       val e = App(f, x)
       val d = (v,e)
-      val sigma: Substitution = Substitution(d)
+      val sigma: Substitution[LambdaExpression] = Substitution(d)
       ( e ) must beEqual ( sigma(v) )
     }
     "substitute correctly when Substitution is applied (2)" in {
       val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
       val e = App(f, x)
       val d = (v,e)
-      val sigma: Substitution = d
+      val sigma: Substitution[LambdaExpression] = d
       val expression = App(f, v)
       ( App(f, App(f, x)) ) must beEqual ( sigma(expression) )
     }
@@ -69,7 +69,7 @@ class SubstitutionsTest extends SpecificationWithJUnit {
       val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
       val y = LambdaVar("y", i)
       val e = App(f, x)
-      val sigma: Substitution = (v,e)
+      val sigma: Substitution[LambdaExpression] = (v,e)
       val expression = Abs(y, App(f, v))
       ( Abs(y,App(f, App(f, x))) ) must beEqual ( sigma(expression) )
     }
@@ -107,7 +107,7 @@ class SubstitutionsTest extends SpecificationWithJUnit {
     "concatenate/compose 2 Substitutions correctly" in {
       val v = LambdaVar("v", i); val x = LambdaVar("x", i); val f = LambdaVar("f", i -> i)
       val e = App(f, x)
-      val sigma: Substitution = (v,e)
+      val sigma: Substitution[LambdaExpression] = (v,e)
       val sigma1 = sigma::(Substitution())
       val sigma2 = sigma::sigma::(Substitution())
       val sigma3 = sigma1:::sigma1
@@ -118,41 +118,41 @@ class SubstitutionsTest extends SpecificationWithJUnit {
       val x = LambdaVar("x", i)
       val f = LambdaVar("f", i -> i)
       val e = App(f, v)
-      val sigma1: Substitution = (v,x)
+      val sigma1: Substitution[LambdaExpression] = (v,x)
       ( sigma1(e) ) must beEqual ( App(f,x) )
     }
   }
   "Substitution with regard to de-Bruijn indices" should {
     "not substitute for bound variables in (\\x.fx)x with sub {x |-> gy}" in {
       val term1 = App(Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))),LambdaVar("x",i))
-      val sub: Substitution = (LambdaVar("x",i), App(LambdaVar("g",i->i),LambdaVar("y",i)))
+      val sub: Substitution[LambdaExpression] = (LambdaVar("x",i), App(LambdaVar("g",i->i),LambdaVar("y",i)))
       val term2 = App(Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))),App(LambdaVar("g",i->i),LambdaVar("y",i)))
       (sub(term1)) must beEqual (term2)
     }
     "not substitute for bound variables in (\\x.fx)x with sub {x |-> (\\x.fx)c}" in {
       "- 1" in {
         val term1 = App(Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))),LambdaVar("x",i))
-        val sub: Substitution = (LambdaVar("x",i), App(Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))),LambdaVar("c",i)))
+        val sub: Substitution[LambdaExpression] = (LambdaVar("x",i), App(Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))),LambdaVar("c",i)))
         val term2 = App(Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))),App(Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))),LambdaVar("c",i)))
         (sub(term1)) must beEqual (term2)
       }
       "- 2" in {
         val term1 = App(Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))),LambdaVar("x",i))
-        val sub: Substitution = (LambdaVar("x",i), App(Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))),LambdaVar("c",i)))
+        val sub: Substitution[LambdaExpression] = (LambdaVar("x",i), App(Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))),LambdaVar("c",i)))
         val term2 = App(Abs(LambdaVar("z",i), App(LambdaVar("f",i->i),LambdaVar("z",i))),App(Abs(LambdaVar("w",i), App(LambdaVar("f",i->i),LambdaVar("w",i))),LambdaVar("c",i)))
         (sub(term1)) must beEqual (term2)
       }
     }
     "recompute correctly indices when substituting a term with bound variables into the scope of other bound variables" in {
       val term1 = Abs(LambdaVar("x",i), App(LambdaVar("F",i->i),LambdaVar("x",i)))
-      val sub: Substitution = (LambdaVar("F",i->i), Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))))
+      val sub: Substitution[LambdaExpression] = (LambdaVar("F",i->i), Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))))
       val term2 = Abs(LambdaVar("x",i), App(Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))),LambdaVar("x",i)))
       (sub(term1)) must beEqual (term2)
     }
   }
   "work correctly on subterms of abs (i.e. the variables which were bound there are no longer bound)" in {
     val term1 = Abs(LambdaVar("F",i->i),Abs(LambdaVar("x",i), App(LambdaVar("F",i->i),LambdaVar("x",i))))
-    val sub: Substitution = (term1.variable, Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))))
+    val sub: Substitution[LambdaExpression] = (term1.variable, Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))))
     val term2 = Abs(LambdaVar("x",i), App(Abs(LambdaVar("x",i), App(LambdaVar("f",i->i),LambdaVar("x",i))),LambdaVar("x",i)))
     (sub(term1.expression)) must beEqual (term2)
   }
