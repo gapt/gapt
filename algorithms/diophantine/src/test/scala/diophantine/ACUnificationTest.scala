513e712b890a0594a3a43cb33b2b87e45a586de4
Martin.Riener <Martin.Riener@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2010-04-12 10:20:12 +0000

    general ac unification - still to do: more tests(especially proper backsubstitution and nested ac unifications), code cleanup
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@433 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- algorithms/diophantine/src/test/scala/diophantine/ACUnificationTest.scala
+++ algorithms/diophantine/src/test/scala/diophantine/ACUnificationTest.scala
@@ -1,6 +1,7 @@
 package diophantine
 
 import _root_.at.logic.language.fol.FOLTerm
+import _root_.at.logic.language.fol.substitutions.Substitution
 import _root_.at.logic.language.hol.logicSymbols.ConstantStringSymbol
 import _root_.at.logic.parsing.language.simple.SimpleFOLParser
 import _root_.at.logic.parsing.readers.StringReader
@@ -10,6 +11,16 @@ import at.logic.algorithms.diophantine.Vector
 class ACUnificationTest extends SpecificationWithJUnit {
   val parse = (s:String) => (new StringReader(s) with SimpleFOLParser {}).getTerm().asInstanceOf[FOLTerm]
   val f = new ConstantStringSymbol("f")
+  def checkResult(subst:Substitution, t1:FOLTerm, t2:FOLTerm) : Boolean = {
+    val term1 = subst.applyFOL(t1)
+    val term2 = subst.applyFOL(t2)
+    println("")
+    println("problem      : "+t1+" =? "+t2)
+    println("substitution : "+subst)
+    println("checking     : "+term1+" =? "+term2)
+    println("")
+    true
+  }
 
   "AC Unification" should {
       "rewrite terms correctly" in {
@@ -34,6 +45,51 @@ class ACUnificationTest extends SpecificationWithJUnit {
         }
       }
 
+    "do normal first order unification" in {
+      val term1 = parse("g(a,h(b))")
+      val term2 = parse("g(a,b)")
+      val term3 = parse("g(x,x)")
+      val term4 = parse("g(y,x)")
+      val term5 = parse("g(h(x),x)")
+
+      val s12 = ACUnification.unify(f,term1,term2)
+      val s13 = ACUnification.unify(f,term1,term3)
+      val s14 = ACUnification.unify(f,term1,term4) //unifiable
+
+      val s23 = ACUnification.unify(f,term2,term3)
+      val s24 = ACUnification.unify(f,term2,term4) //unifiable
+
+      val s34 = ACUnification.unify(f,term3,term4) //unifiable
+
+      val s35 = ACUnification.unify(f,term3,term5)
+
+      //non unifiable
+      (s12,s13,s23,s35) match {
+        case (None,None,None,None) => true must beEqual(true)
+        case _ => true must beEqual (false)
+      }
+
+      (s14, s24,s34) match {
+        case (Some(List(r14)), Some(List(r24)),Some(List(r34))) =>
+          val tl14 = r14.applyFOL(term1)
+          val tr14 = r14.applyFOL(term4)
+          val tl24 = r24.applyFOL(term2)
+          val tr24 = r24.applyFOL(term4)
+          val tl34 = r34.applyFOL(term3)
+          val tr34 = r34.applyFOL(term4)
+
+          println(" "+tl14+" "+tr14)
+          println(" "+tl24+" "+tr24)
+          println(" "+tl34+" "+tr34)
+
+          tl14 must beEqual(tr14)
+          tl24 must beEqual(tr24)
+          tl34 must beEqual(tr34)
+        case _ => true must beEqual (false)
+      }
+    }
+
+
     "not unify f(x,a) = f(f(y,a),x)" in {
       val term1 = parse("f(x,a)")
       val term2 = parse("f(f(x,f(a,a)),y)")
@@ -44,7 +100,7 @@ class ACUnificationTest extends SpecificationWithJUnit {
         case None => true must beEqual (true)
       }
     }
-
+ 
     "unify f(x1,x2) = f(f(y1,y2),y3)" in {
       val term1 = parse("f(x1,x2)")
       val term2 = parse("f(f(y1,y2),y3)")
@@ -54,7 +110,9 @@ class ACUnificationTest extends SpecificationWithJUnit {
 
       val mgu = ACUnification unify(f,term1,term2)
       mgu match {
-        case Some(_) => true must beEqual (true)
+        case Some(substs) => true must beEqual (true)
+          for (s<-substs) checkResult(s,term1,term2)
+
         case None => true must beEqual (false)
       }
     }
@@ -66,12 +124,65 @@ class ACUnificationTest extends SpecificationWithJUnit {
 
       val mgu = ACUnification unify(f,term1,term2)
       mgu match {
-        case Some(subst) => true must beEqual (true)
-        case None => true must beEqual (false) 
+        case Some(substs) => true must beEqual (true)
+        for (s<-substs) checkResult(s,term1,term2)
+        case None => true must beEqual (false)
       }
       ()
     }
     /* */
+    "unify f(x,x) = f(y,f(y,y))" in {
+      val term1 = parse("f(x,x)")
+      val term2 = parse("f(y,f(y,y))")
 
+      val mgu = ACUnification unify(f,term1,term2)
+      mgu match {
+        case Some(substs) => true must beEqual (true)
+          for (s<-substs) checkResult(s,term1,term2)
+        case None => true must beEqual (false)
+      }
+      ()
+    }
+
+    "not unify f(x,g(x)) = f(y,f(y,a))" in {
+      val term1 = parse("f(x,g(x))")
+      val term2 = parse("f(y,f(y,a))")
+
+      val mgu = ACUnification unify(f,term1,term2)
+      mgu match {
+        case Some(substs) => true must beEqual (false)
+          for (s<-substs) checkResult(s,term1,term2)
+        case None => true must beEqual (true)
+      }
+      ()
+    }
+
+    "not unify f(x,a) = f(y,f(y,g(x)))" in {
+      val term1 = parse("f(x,a)")
+      val term2 = parse("f(y,f(y,g(x)))")
+
+      val mgu = ACUnification unify(f,term1,term2)
+      mgu match {
+        case Some(substs) => true must beEqual (false)
+          for (s<-substs) checkResult(s,term1,term2)
+        case None => true must beEqual (true)
+      }
+      ()
+    }
+
+    "unify f(x,f(g(a,b),x)) = f(y,f(y,g(a,u)))" in {
+      val term1 = parse("f(x,f(g(a,b),x))")
+      val term2 = parse("f(y,f(y,g(a,u)))")
+
+      val mgu = ACUnification unify(f,term1,term2)
+      mgu match {
+        case Some(substs) => true must beEqual (true)
+          for (s<-substs) checkResult(s,term1,term2)
+        case None => true must beEqual (false)
+      }
+      ()
+    }
+
+    
   }
 }
