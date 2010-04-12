b41f1ecd78b11ccdb2d4c4e0b0f192758b35249d
Martin Riener <martin.riener@googlemail.com>
2010-04-12 10:20:12 +0000

    general ac unification - still to do: more tests(especially proper backsubstitution and nested ac unifications), code cleanup
    git-svn-id: http://gapt.googlecode.com/svn/trunk@433 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/algorithms/diophantine/src/main/scala/diophantine/ACUnification.scala
+++ source/algorithms/diophantine/src/main/scala/diophantine/ACUnification.scala
@@ -1,14 +1,11 @@
 package diophantine
 
-import _root_.at.logic.algorithms.diophantine.{Vector, LankfordSolver, DiophantineSolver}
-import _root_.at.logic.language.hol.logicSymbols.ConstantSymbolA
-import _root_.at.logic.language.lambda.symbols.{VariableSymbolA, SymbolA, VariableStringSymbol, StringSymbol}
-import _root_.at.logic.language.lambda.typedLambdaCalculus.Var
+import at.logic.language.hol.logicSymbols.{ConstantStringSymbol, ConstantSymbolA}
 import at.logic.language.fol._
+import at.logic.algorithms.diophantine.{Vector, LankfordSolver}
+import at.logic.language.lambda.symbols.{VariableSymbolA, VariableStringSymbol}
 import substitutions.Substitution
-import collection.Map
 import collection.mutable.HashMap
-import collection.immutable.HashSet
 
 object ACUnification {
   type TermCount = (FOLTerm,Int)
@@ -17,16 +14,121 @@ object ACUnification {
   type ArrayEntry = (Vector, MapEntry)
 
   var constant_prefix = "z_"
+  var unit_constant = FOLConst(new ConstantStringSymbol("1"))
   var maxindex = 0
 
   val is_ac1 : Boolean = false
+  val debuglevel = 0 // 0... no output, 1 ... show unifiers after unification 2--- also before unification 3... maximum
+
+  def debug(level:Int, msg:String) = if (debuglevel>=level) println("DEBUG: "+msg)
 
   def getFreshVariable() : VariableSymbolA = { maxindex += 1; VariableStringSymbol(constant_prefix+maxindex) }
+
+  def unify(function: ConstantSymbolA, term1 : FOLTerm, term2 : FOLTerm) : Option[List[Substitution]] = {
+    val mgus = unify(function, List((term1, term2)), List(Substitution()) )
+    mgus match {
+      case Nil => debug(1,"Found no unifier!"); None;
+      case _   => for (mgu<-mgus) debug(1,"found unifier: "+mgu); Some(mgus)}
+  }
+
+  def unify(function: ConstantSymbolA,
+            terms : List[(FOLTerm,FOLTerm)],
+            substs : List[Substitution]) : List[Substitution] = {
+    debug(3,"unifying "+terms+" substitutions are: "+substs)
+    terms match {
+      case (term1,term2) :: rest =>
+        term1 match {
+          // comparing constant to sthg else
+          case FOLConst(c1) =>
+            term2 match {
+               // if the two constants are equal, the substitution is not changed, else not unifiable 
+              case FOLConst(c2) =>
+                if (c1==c2) collect(substs, ((s:Substitution) => unify(function, rest, List(s))))
+                else Nil
+              // second one is a var => flip & variable elimination
+              case FOLVar(v) =>
+                val ve = Substitution(term2.asInstanceOf[FOLVar],term1)
+                val newterms = rest map makesubstitute_pair(ve)
+                collect(substs, (s:Substitution) => unify(function, newterms, List(ve concatFOL s)))
+              // anything else is not unifiable
+              case _ =>
+                Nil
+            }
+          // comparing function symbol to sthg else
+          case Function(f1,args1)  =>
+            term2 match {
+              // decomposition or ac unification, if the function symbols are the same, else not unifiable 
+              case Function(f2,args2) =>
+                if (f1 != f2) {
+                  Nil
+                } else if (args1.length != args2.length) {
+                  throw new Exception("function symbols both named "+ f1 + " but with different arity "
+                          + args1.length + " and "+args2.length+ "encountered!")
+                } else if (f1 == function) {
+                  //ac unification
+                  val acunivs = ac_unify(function, term1, term2)
+                  collect(acunivs, ((acu:Substitution) =>
+                    collect(substs, ((subst:Substitution) =>
+                      unify(function, rest map makesubstitute_pair(subst), List((acu concatFOL subst))))
+                   )))
+                } else  {
+                  //non ac unification => decomposition
+                  collect(substs , (s:Substitution) => unify(function, (args1 zip args2):::rest, List(s)))
+                }
+              // variable as second term: flip & variable elimination
+              case FOLVar(v) =>
+                //occurs check
+                if (occurs(term2.asInstanceOf[FOLVar], term1)) {
+                  Nil
+                } else {
+                  val ve = Substitution(term2.asInstanceOf[FOLVar],term1)
+                  val newterms = rest map makesubstitute_pair(ve)
+                  collect(substs, (s:Substitution) => unify(function, newterms, List((ve concatFOL s))))
+                }
+              // anything else is not unifiable
+              case _ =>
+                Nil
+            }
+
+          //variable elimination
+          case FOLVar(v) =>
+            term2 match {
+              case FOLVar(w) =>
+                if (v==w) {
+                  collect(substs,(s:Substitution) => unify(function, rest, List(s)))
+                } else {
+                  val ve = Substitution(term1.asInstanceOf[FOLVar],term2)
+                  val newterms = rest map makesubstitute_pair(ve)
+                  collect(substs,(s:Substitution) => unify(function, newterms, List((ve concatFOL s).asInstanceOf[Substitution])))
+                }
+
+              case _ =>
+                //occurs check
+                if (occurs(term1.asInstanceOf[FOLVar], term2)) {
+                  Nil
+                } else {
+                  val ve = Substitution(term1.asInstanceOf[FOLVar],term2)
+                  val newterms = rest map makesubstitute_pair(ve)
+                  collect(substs, (s:Substitution) => unify(function, newterms, List((ve concatFOL s).asInstanceOf[Substitution])))
+                }
+          }
+
+          //this should be empty in the end
+          case _ =>
+            throw new Exception("there should be only variables, constants and functions in first order terms!")
+        }
+
+      case Nil =>
+        substs
+    }
+  }
   
 
-  def unify(function: ConstantSymbolA, term1 : FOLTerm, term2 : FOLTerm) : Option[Substitution] = {
+  def ac_unify(function: ConstantSymbolA, term1 : FOLTerm, term2 : FOLTerm) : List[Substitution] = {
     val counted_symbols = countSymbols(nestedFunctions_toList(function,term1), nestedFunctions_toList(function,term2))
-    val (ts1,count1) = List unzip counted_symbols 
+    val (ts1,count1) = List unzip counted_symbols
+    //println("term symbols: "+ts1)
+    //println(count1)
 
     val (lhs,rhs) = (counted_symbols partition (_._2 >0)) // this works because countSymbols olny returns values != 0
     val vlhs = Vector(lhs map (_._2))
@@ -36,15 +138,12 @@ object ACUnification {
     val vrhs_length = vrhs.length
 
     if ((lhs == Nil) || (rhs == Nil)) {
-      None
+      Nil
     }
     else {
-      val s = Substitution()
-
       val basis = LankfordSolver solve (vlhs,vrhs)
       val sums = calculateSums(basis, vlhs, vrhs)
 
-      def gzero(v : Vector) : Boolean = v.allgreaterzero
 
       var results : List[Vector] = Nil
       if (is_ac1) {
@@ -65,27 +164,40 @@ object ACUnification {
 
       for (b<-basis) {
         val v : VariableSymbolA = getFreshVariable()
-        //println(""+b+"->"+v)
+        debug(3,""+b+"->"+v)
         varmap(b) = v 
       }
 
       //some helper functions, could be factored out
       def extract(pair:(Int,List[Vector])) : List[Vector] = pair._2
+
+      //def ntimes[A](n:Int,p:(List[A],List[A])=>List[A],l:List[A]) : List[A] =
+      //  if (n <= 0) Nil else l ::: ntimes(n-1,l)
+      def ntimes[A](x:A,n:Int) : List[A] = if (n<=0) Nil else x::ntimes(x,n-1) 
       
+      def nfilter[A](l:List[A], count : (A=>Int)) : List[A] = {
+        l match {
+          case x::xs => ntimes(x,count(x)) ::: nfilter(xs,count)
+          case Nil => Nil
+        }
+      }
 
       def createVectors(mapping : HashMap[Vector, VariableSymbolA], v:List[Vector]) : List[FOLTerm]= {
-        val len = v.length
+        //val len = v.length
+        val len = if (v == Nil) 0 else v(0).length-1
+
+        //println("create vectors length="+len+" v="+v)
         val expanded : List[(Int,List[Vector])] = ((0 to len) map ((_,v))).toList //pair vector with every index of a component
         val filtered : List[List[VariableSymbolA]] =
               expanded map (x=>
-                  (x._2 filter (_.vector(x._1)>0))  //get only vectors >0 in the given component
-                          map (mapping(_))          //and convert them to VariableSymbols
+                  (nfilter (x._2, ((v:Vector)=>v.vector(x._1))))  //take the vector the number of times of the actual component
+                            map (mapping(_))          //and convert them to VariableSymbols
               )
         val ltt : List[VariableSymbolA] => FOLTerm = listToTerm(function,_)
         filtered map ltt
       }
 
-      
+      debug(2,"ac unification preresults:"+results)
       //convert results to list of terms
       var converted : List[List[FOLTerm]] = Nil
       for (r <- results)
@@ -94,11 +206,49 @@ object ACUnification {
       
 
 
-      //println(converted)
+      debug(1,"ac unification results: "+converted)
+      def zip3[A,B,C](l1:List[A], l2:List[B], l3:List[C]) : List[(A,B,C)] = {
+        (l1,l2,l3) match {
+          case (Nil,Nil,Nil) => Nil
+          case (x::xs,y::ys,z::zs) => (x,y,z) :: zip3(xs,ys,zs)
+          case _ => throw new Exception("zip3: lists not of equal length")
+        }
+      }
 
-      //TODO: unify with constants
+      var unified_terms : List[List[FOLTerm]] = Nil
+      var unifiers : List[Substitution] = Nil
+      for (c<-converted) {
+        val zc = ts1 zip c
+        //println("finding unifier for: "+zc)
+        val us = unify(function, zc, List(Substitution()) )
+        for (unifier <- us) {
+            val uterm : List[FOLTerm] = ts1 map ((x:FOLTerm)=>unifier.applyFOL(x).asInstanceOf[FOLTerm])
+            //println("yay found one:" + uterm)
+            //unifiers = subst :: unifiers
+            unifiers = unifier :: unifiers
+            unified_terms = uterm :: unified_terms 
+        }
+      }
 
-      Some(s)
+      val term_context = (ts1 map ((x:FOLTerm) => getVariableContext(x))) reduceLeft (_++_)
+
+      //remove variables not in the original term from the substitution
+      debug(1,"and the unifiers are:")
+      var reduced_unifiers : List[Substitution] = Nil
+      for (u<-unifiers) {
+        debug(1,""+u)
+        //val splitted : Tuple2[List[(FOLVar,FOLTerm)], List[(FOLVar,FOLTerm)]] = (u.mapFOL.partition(term_context contains _._1)).asInstanceOf[Tuple2[List[(FOLVar,FOLTerm)], List[(FOLVar,FOLTerm)]]]
+        val umap = (u.mapFOL.elements.toList).asInstanceOf[List[(FOLVar,FOLTerm)]]
+        
+        val in_term = umap.filter((x:(FOLVar,FOLTerm)) => (term_context contains x._1))
+        println(in_term)
+        val not_in_term = Substitution(umap.filter((x:(FOLVar,FOLTerm)) => !(term_context contains x._1)))
+        val in_term_reduced = in_term map ((x:(FOLVar,FOLTerm))=>(x._1,not_in_term.applyFOL(x._2)))
+        //reduced_unifiers = (Substitution(not_in_term).composeFOL(Substitution(in_term))) :: reduced_unifiers
+        reduced_unifiers = Substitution(in_term_reduced) :: reduced_unifiers
+      }
+
+      reduced_unifiers
     }
   }
 
@@ -166,7 +316,12 @@ object ACUnification {
     terms match {
       case x::Nil => FOLVar(x)
       case x::xs => Function(function, List(FOLVar(x),listToTerm(function,xs)))
-      //case Nil => ()
+      case Nil =>
+        if (is_ac1) {
+          unit_constant
+        } else {
+          throw new Exception("cannot convert empty list to term, there is no unit element!")
+        }
     }
   }
 
@@ -192,7 +347,6 @@ object ACUnification {
 
     def min(x:Int,y:Int) = if (x<y) x else y
     def max(x:Int,y:Int) = if (x>y) x else y
-
     
     return max(l.length,r.length) * max(lcm(l.vector),lcm(r.vector))
   }
@@ -241,6 +395,14 @@ object ACUnification {
     }
   }
 
+  def getVariableContext(term:FOLTerm) : List[FOLVar] = {
+    term match {
+      case FOLVar(_) => List(term.asInstanceOf[FOLVar])
+      case FOLConst(_) => Nil
+      case Function(_, args) => (args map ((x:FOLTerm) =>getVariableContext(x))) reduceLeft (_++_)
+    }
+  }
+
   def countSymbols(terms1:List[FOLTerm], terms2 : List[FOLTerm]) : List[TermCount] = {
     var result : List[TermCount] = Nil
     for(t <- terms1) {
@@ -274,5 +436,21 @@ object ACUnification {
     }
   }
 
+  def substitute_pair(subst : Substitution, x : (FOLTerm,FOLTerm)) : (FOLTerm,FOLTerm) = (subst.applyFOL(x._1).asInstanceOf[FOLTerm], subst.applyFOL(x._2).asInstanceOf[FOLTerm])
+  def makesubstitute_pair(subst:Substitution) : ( ((FOLTerm,FOLTerm)) => (FOLTerm, FOLTerm)) = substitute_pair(subst,_)
+
+  def collect(substitutions : List[Substitution], fun : (Substitution=>List[Substitution])) : List[Substitution] =
+    (substitutions map fun).flatten
+
+  def occurs(v:FOLVar, term:FOLTerm) : Boolean = {
+    //println("occurs "+v+" in "+term+"?")
+    term match {
+      case FOLVar(w) => v==term
+      case FOLConst(_) => false
+      case Function(_, args) => args.foldLeft(false)(_ || occurs(v,_)) 
+    }
+  }
+
+  def gzero(v : Vector) : Boolean = v.allgreaterzero
   
 }
