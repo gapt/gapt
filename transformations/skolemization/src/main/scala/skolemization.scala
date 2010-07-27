b3ae7985f5c0c6579e1c0a896e91d8d9caca621b
fraini <fraini@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2010-07-27 14:59:19 +0000

    Continued implementation of proof skolemization.
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@464 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- transformations/skolemization/src/main/scala/skolemization.scala
+++ transformations/skolemization/src/main/scala/skolemization.scala
@@ -1,68 +1,159 @@
-// This package implements formula and proof Skolemization.
 package at.logic.transformations.skolemization
 
+// This package implements formula and proof Skolemization.
+
+import at.logic.language.lambda.typedLambdaCalculus.Var
+import at.logic.language.lambda.BetaReduction._
+import at.logic.language.lambda.BetaReduction.ImplicitStandardStrategy._
 import at.logic.utils.logging.Logger
-import scala.collection.mutable.{Map,HashMap}
+import scala.collection.immutable.{Map,HashMap}
 import at.logic.calculi.occurrences._
 import at.logic.calculi.lk.quantificationRules._
 import at.logic.calculi.lk.propositionalRules._
 import at.logic.calculi.lk.definitionRules._
 import at.logic.calculi.lk.equationalRules._
-import at.logic.calculi.lk.base.{LKProof,Sequent,SequentOccurrence}
+import at.logic.calculi.lk.base.{LKProof,Sequent,SequentOccurrence,PrincipalFormulas}
 import at.logic.language.hol._
 import at.logic.language.lambda.types._
 import at.logic.language.lambda._
 import at.logic.language.lambda.substitutions._
-import at.logic.algorithms.lksk.applySubstitution
 import at.logic.algorithms.lk.getCutAncestors
 import at.logic.language.hol.logicSymbols.ConstantStringSymbol
+import at.logic.algorithms.lk.applySubstitution
 
 object skolemize {
 
   def apply(p: LKProof) : LKProof = 
   {
+    val fos = p.root.antecedent ++ p.root.succedent
+    val inst_map = fos.foldLeft(new HashMap[FormulaOccurrence, List[HOLExpression]]())( (m, fo) => m.updated(fo, Nil))
+
     val sk_s = skolemize( p.root )
-    skolemize( p, sk_s._1, sk_s._2, sk_s._3 )._1
+
+    skolemize( p, sk_s._2, inst_map, sk_s._3 )._1
   }
 
-  def skolemize(p: LKProof, sk_s: Sequent, map_ant: Map[FormulaOccurrence, Int],
-                map_succ: Map[FormulaOccurrence, Int] ) : (LKProof, Map[FormulaOccurrence, FormulaOccurrence]) = p match
+  /*
+  Idea of the algorithm: Going upwards in the prooftree, we remember the 
+  instantiations of the weak quantifiers and replace EV's by Skolem terms.
+  Going downwards, we apply the appropriate inferences (essentially 
+  leaving out the strong quantifier inferences).
+
+  TODO: check whether proof is Skolemizable (or maybe just QFC)
+
+  TODO: have to distinguish cut-ancestors/ES-ancestors!
+  */
+  def skolemize(proof: LKProof, 
+      symbol_map: Map[FormulaOccurrence, Stream[ConstantStringSymbol]],
+      inst_map: Map[FormulaOccurrence, List[HOLExpression]],
+      form_map: Map[FormulaOccurrence, HOLFormula]) :
+      (LKProof, Map[FormulaOccurrence, FormulaOccurrence]) = proof match
   {
     case Axiom(s) => {
-      val ax = Axiom( sk_s )
-      val map = new HashMap[FormulaOccurrence, FormulaOccurrence]()
-      // TODO: notation? p is a pair, but we cannot write (fo, i) for p...
-      map_ant.foreach( p => map += ( p._1 -> ax._2._1(p._2) ) )
-      map_succ.foreach( p => map += ( p._1 -> ax._2._2(p._2) ) )
-      (ax._1, map)
+      val ant = s.antecedent.toList
+      val succ = s.succedent.toList
+      val new_seq = Sequent( ant.map( fo => fo.formula ), succ.map( fo => fo.formula ) )
+      val ax = Axiom( new_seq )
+      var new_map = ant.zipWithIndex.foldLeft(new HashMap[FormulaOccurrence, FormulaOccurrence])( (m, p) => m.updated( p._1, ax._2._1( p._2 ) ))
+      new_map = succ.zipWithIndex.foldLeft(new_map)((m, p) => m.updated( p._1, ax._2._2( p._2 )))
+      (ax._1, new_map)
     }
+    case ForallRightRule(p, _, _, m, v) => handleStrongQuantRule( proof, p, m, v, symbol_map, inst_map, form_map )
+    case ExistsLeftRule(p, _, _, m, v) => handleStrongQuantRule( proof, p, m, v, symbol_map, inst_map, form_map )
+    case ForallLeftRule(p, _, a, m, t) => handleWeakQuantRule( proof, p, a, m, t, ForallLeftRule.computeAux,
+      ForallLeftRule.apply, symbol_map, inst_map, form_map )
+    case ExistsRightRule(p, _, a, m, t) => handleWeakQuantRule( proof, p, a, m, t, ExistsRightRule.computeAux,
+      ExistsRightRule.apply, symbol_map, inst_map, form_map )
+    case WeakeningLeftRule(p, _, m) => handleWeakeningRule( proof, p, m, 
+      WeakeningLeftRule.apply, symbol_map, inst_map, form_map )
+    case WeakeningRightRule(p, _, m) => handleWeakeningRule( proof, p, m, 
+      WeakeningRightRule.apply, symbol_map, inst_map, form_map )
   }
 
-  def skolemize(s: SequentOccurrence) : (Sequent, Map[FormulaOccurrence, Int], Map[FormulaOccurrence, Int]) =
+  def handleWeakQuantRule( proof: LKProof, p: LKProof, a: FormulaOccurrence, m: FormulaOccurrence, t: HOLExpression,
+      computeAux: (HOLFormula, HOLExpression) => HOLFormula,
+      constructor: (LKProof, FormulaOccurrence, HOLFormula, HOLExpression) => LKProof,
+      symbol_map: Map[FormulaOccurrence, Stream[ConstantStringSymbol]],
+      inst_map: Map[FormulaOccurrence, List[HOLExpression]],
+      form_map: Map[FormulaOccurrence, HOLFormula]
+    ) = {
+      val inst_list = inst_map( m )
+      val new_inst_map = copyMapToAncestor( inst_map.updated( m, inst_list :+ t ) )
+      val new_form = computeAux( form_map( m ), t )
+      val new_form_map = copyMapToAncestor( form_map.updated( m, new_form ) )
+      val new_proof = skolemize( p, copyMapToAncestor( symbol_map ), new_inst_map, new_form_map )
+      val ret = constructor( new_proof._1, new_proof._2( a ), form_map( m ), t ) 
+      ( ret, copyMapToDescendant( proof, ret, new_proof._2 ) )
+  }
+
+
+  def handleWeakeningRule( proof: LKProof, p: LKProof, m: FormulaOccurrence,
+      constructor: (LKProof, HOLFormula) => LKProof with PrincipalFormulas,
+      symbol_map: Map[FormulaOccurrence, Stream[ConstantStringSymbol]],
+      inst_map: Map[FormulaOccurrence, List[HOLExpression]],
+      form_map: Map[FormulaOccurrence, HOLFormula]
+    ) = {
+      val new_proof = skolemize( p, copyMapToAncestor( symbol_map - m ), 
+        copyMapToAncestor( inst_map - m ), 
+        copyMapToAncestor( form_map - m ) )
+      val ret = constructor( new_proof._1, form_map( m ) )
+      ( ret, copyMapToDescendant( proof, ret, new_proof._2 ) + ( m -> ret.prin.head ) )
+  }
+
+  def handleStrongQuantRule( proof: LKProof, p: LKProof, m: FormulaOccurrence, v: HOLVar,
+      symbol_map: Map[FormulaOccurrence, Stream[ConstantStringSymbol]],
+      inst_map: Map[FormulaOccurrence, List[HOLExpression]],
+      form_map: Map[FormulaOccurrence, HOLFormula]
+    ) = {
+      val sym_stream = symbol_map( m )
+      val sym = sym_stream.head
+      val skolem_term = Function( sym, inst_map( m ), v.exptype )
+      val sub_proof = applySubstitution( p, Substitution( v, skolem_term ) )
+      // invert the formula occurrence map.
+      val inv_map = sub_proof._2.foldLeft(new HashMap[FormulaOccurrence, FormulaOccurrence])((m, p) => m.updated(p._2, p._1) )
+      val new_symbol_map = copyMapToAncestor( symbol_map.updated( m, sym_stream.tail ) )
+      val new_inst_map = copyMapToAncestor( inst_map )
+      val new_form_map = copyMapToAncestor( form_map )
+      val new_proof = skolemize( sub_proof._1, inv_map.mapValues( new_symbol_map ), 
+        inv_map.mapValues( new_inst_map ), inv_map.mapValues( new_form_map ) )
+      // FIXME: sub_proof._2 is mutable map, so we have to construct a new immutable one.
+      val new_map = new HashMap() ++ ( sub_proof._2.mapValues( new_proof._2 ) )
+      ( new_proof._1, copyMap( proof, new_proof._1, new_map ) )
+  }
+
+  def copyMapToAncestor[A]( map: Map[FormulaOccurrence, A] ) =
+    map.map( p => (p._1.ancestors.head, p._2 ) )
+
+  def copyMapToDescendant( old_p: LKProof, new_p: LKProof, 
+                           map: Map[FormulaOccurrence, FormulaOccurrence] ) =
+    map.map( p => (old_p.getDescendantInLowerSequent( p._1 ).get, 
+      new_p.getDescendantInLowerSequent( p._2 ).get ) )
+
+  def copyMap( old_p: LKProof, new_p: LKProof, map: Map[FormulaOccurrence, FormulaOccurrence] ) =
+    map.map( p => (old_p.getDescendantInLowerSequent( p._1 ).get,
+      p._2 ) )
+
+  def skolemize(s: SequentOccurrence) : (Sequent, Map[FormulaOccurrence, Stream[ConstantStringSymbol]], Map[FormulaOccurrence, HOLFormula]) =
   {
-    val ant = s.antecedent.toList
-    val succ = s.succedent.toList
+    var stream = skolem_symbol_stream
+    val fos = s.antecedent ++ s.succedent
+    val symbol_map = fos.foldLeft(new HashMap[FormulaOccurrence, Stream[ConstantStringSymbol]]())( (m, fo) => {
+        val s = even( stream )
+        stream = odd( stream )
+        m.updated( fo, s )
+      })
 
-    val sk_ant = ant.map( fo => apply( fo.formula, 1 ) )
-    val sk_succ = succ.map( fo => apply( fo.formula, 0 ) )
-    
-    val map_ant = new HashMap[FormulaOccurrence, Int]()
-    for (i <- ant.indices)
-      map_ant += (ant(i) -> i)
-  
-    val map_succ = new HashMap[FormulaOccurrence, Int]()
-    for (i <- succ.indices)
-      map_succ += (succ(i) -> i)
+    val sk_ant = s.antecedent.map( fo => (fo, apply( fo.formula, 1, symbol_map( fo ) ) ) )
+    val sk_succ = s.succedent.map( fo => (fo, apply( fo.formula, 0, symbol_map( fo ) ) ) )
 
-    (Sequent(sk_ant, sk_succ), map_ant, map_succ)
+    (Sequent(sk_ant.map(_._2).toList, sk_succ.map(_._2).toList), symbol_map, sk_ant.toMap ++ sk_succ.toMap)
   }
 
-  def skolem_symbol_stream(n: Int): Stream[ConstantStringSymbol] =
-    Stream.cons(ConstantStringSymbol( "s_" + n ) , skolem_symbol_stream( n + 1 ) )
+  def skolem_symbol_stream_from(n: Int): Stream[ConstantStringSymbol] =
+    Stream.cons(ConstantStringSymbol( "s_" + n ), skolem_symbol_stream_from( n + 1 ) )
 
-  def skolem_symbol_stream : Stream[ConstantStringSymbol]= skolem_symbol_stream( 0 )
+  def skolem_symbol_stream = skolem_symbol_stream_from( 0 )
 
-  // TODO: in Scala 2.8 there is +: which can be used instead of Stream.cons
   def even[A]( s: Stream[A] ) : Stream[A] = Stream.cons( s.head, even(s.tail.tail) )
 
   def odd[A]( s: Stream[A] ) : Stream[A] = Stream.cons( s.tail.head, odd(s.tail.tail) )
@@ -73,7 +164,7 @@ object skolemize {
     else
       0
 
-  def apply(f: HOLFormula, pol: Int) = sk( f, pol, Nil, skolem_symbol_stream )
+  def apply(f: HOLFormula, pol: Int, symbols: Stream[ConstantStringSymbol]) = sk( f, pol, Nil, symbols )
 
   def sk(f: HOLFormula, pol: Int, vars: List[HOLVar], symbols: Stream[ConstantStringSymbol]) : HOLFormula = f match {
     case And(l, r) => And( sk( l , pol, vars, even( symbols ) ), sk( r, pol, vars, odd( symbols ) ) )
@@ -93,16 +184,11 @@ object skolemize {
       if (pol == 0)
       {
         val sub = Substitution(x, Function( symbols.head, vars, x.exptype ) )
-        println("Substituting!")
-        println( sub )
         // TODO: should not be necessary to cast here, Formula is closed under substitution
         sk( sub( f ).asInstanceOf[HOLFormula], pol, vars, symbols.tail )
       }
       else // TODO: should not be necessary to cast! try to change it in hol.scala.
-      {
-        println("Not substituting!")
         AllVar(x, sk( f, pol, vars :+ x.asInstanceOf[HOLVar], symbols ) )
-      }
     case Atom(_,_) => f
   }
 }
--- transformations/skolemization/src/test/scala/SkolemizationTest.scala
+++ transformations/skolemization/src/test/scala/SkolemizationTest.scala
@@ -21,39 +21,45 @@ import at.logic.language.lambda.symbols.ImplicitConverters._
 import scala.collection.immutable._
 import at.logic.language.lambda.symbols.VariableStringSymbol
 import at.logic.calculi.lk.base.Sequent
-import at.logic.calculi.lk.propositionalRules.{OrLeftRule, Axiom => LKAxiom}
+import at.logic.calculi.lk.propositionalRules.{AndRightRule, Axiom}
 import at.logic.calculi.lk.quantificationRules._
 import at.logic.calculi.lksk.base.TypeSynonyms._
 import at.logic.language.hol.logicSymbols._
+import skolemize._
 
 class SkolemizationTest extends SpecificationWithJUnit {
   "Skolemization" should {
-      val x = HOLVar(new VariableStringSymbol("x"), i)
+      val x = HOLVar("x", i)
+      val y = HOLVar("y", i)
       val f = AllVar( x, Atom("P", x::Nil ) )
+      val s0 = new ConstantStringSymbol( "s_0" )
+      val s1 = new ConstantStringSymbol( "s_1" )
+      val s2 = new ConstantStringSymbol( "s_2" )
+      val s3 = new ConstantStringSymbol( "s_3" )
 
     "have a correct stream of Skolem symbols" in {
-      skolemize.skolem_symbol_stream.head must beEqual( new ConstantStringSymbol( "s_0" ) )
-      skolemize.skolem_symbol_stream.tail.head must beEqual( new ConstantStringSymbol( "s_1" ) )
-      skolemize.skolem_symbol_stream.tail.tail.head must beEqual( new ConstantStringSymbol( "s_2" ) )
+      skolem_symbol_stream.head must beEqual( s0 )
+      skolem_symbol_stream.tail.head must beEqual( s1 )
+      skolem_symbol_stream.tail.tail.head must beEqual( s2 )
     }
 
     "have a correct even/odd function" in {
-      val s_even = skolemize.even(skolemize.skolem_symbol_stream)
-      val s_odd = skolemize.odd(skolemize.skolem_symbol_stream)
-      s_even.head must beEqual( new ConstantStringSymbol( "s_0" ) )
-      s_odd.head must beEqual( new ConstantStringSymbol( "s_1" ) )
-      s_even.tail.head must beEqual( new ConstantStringSymbol( "s_2" ) )
-      s_odd.tail.head must beEqual( new ConstantStringSymbol( "s_3" ) )
+      val s_even = even(skolem_symbol_stream)
+      val s_odd = odd(skolem_symbol_stream)
+      s_even.head must beEqual( s0 )
+      s_odd.head must beEqual( s1 )
+      s_even.tail.head must beEqual( s2 )
+      s_odd.tail.head must beEqual( s3 )
     }   
     
     "leave a formula with only weak quantifiers untouched" in {
-      skolemize( f, 1 ) must beEqual( f )
+      skolemize( f, 1, skolem_symbol_stream ) must beEqual( f )
     }
 
     "introduce correctly a Skolem constant" in {
-      val skfun = HOLConst( skolemize.skolem_symbol_stream.head, i )
+      val skfun = HOLConst( skolem_symbol_stream.head, i )
       val skf = Atom( "P", skfun::Nil )
-      skolemize( f, 0 ) must beEqual( skf )
+      skolemize( f, 0, skolem_symbol_stream ) must beEqual( skf )
     }
 
     "handle a binary formula correctly" in {
@@ -61,13 +67,57 @@ class SkolemizationTest extends SpecificationWithJUnit {
       val rxy = Atom( "R", x::y::Nil )
       val f2 = Imp(f, AllVar( x, ExVar( y, rxy ) ) )
 
-      val skfun0 = HOLConst( skolemize.skolem_symbol_stream.head, i )
-      val skfun1 = Function( skolemize.skolem_symbol_stream.tail.head, x::Nil, i )
+      val skfun0 = HOLConst( skolem_symbol_stream.head, i )
+      val skfun1 = Function( skolem_symbol_stream.tail.head, x::Nil, i )
       val skf1 = Atom( "P", skfun0::Nil )
       val skf2 = Atom( "R", x::skfun1::Nil )
 
       val skf = Imp( skf1, AllVar( x, skf2 ) )
-      skolemize( f2, 1 ) must beEqual( skf )
+      skolemize( f2, 1, skolem_symbol_stream ) must beEqual( skf )
     }
+
+    "handle a simple proof correctly" in {
+      val cs1 = HOLConst( s1, i )
+      val alpha = HOLVar( new VariableStringSymbol( "α" ), i )
+      val Palpha = Atom( "P", alpha::Nil )
+      val Ps0 = Atom( "P", cs1::Nil )
+      val allxPx = AllVar( x, Atom( "P", x::Nil ) )
+      val ax = Axiom( Sequent( Palpha::Nil, Palpha::Nil ) )
+      val proof = ForallRightRule( ForallLeftRule( ax._1, Palpha, allxPx, alpha ), Palpha, allxPx, alpha )
+
+      val ax_sk = Axiom( Sequent( Ps0::Nil, Ps0::Nil ) )
+      val proof_sk = ForallLeftRule( ax_sk._1, Ps0, allxPx, cs1 )
+
+      // this does not work correctly
+      //skolemize( proof ) must beEqual( proof_sk )
+      skolemize( proof ).root.getSequent.multisetEquals( proof_sk.root.getSequent) must beEqual( true )
+    }
+
+    "work for a cut-free proof" in {
+      val a = HOLVar("a", i)
+      val b = HOLVar("b", i)
+      val Rab = Atom( "R", a::b::Nil )
+      val exyRay = ExVar( y, Atom( "R", a::y::Nil ) )
+      val allxexyRxy = AllVar( x, ExVar( y, Atom( "R", x::y::Nil ) ) )
+      val ax = Axiom( Sequent( Rab::Nil, Rab::Nil ) )
+      val r1 = ExistsRightRule( ax._1, Rab, exyRay, b )
+      val r2 = ExistsLeftRule( r1, Rab, exyRay, b )
+      val r3 = ForallLeftRule( r2, exyRay, allxexyRxy, a )
+      val proof = ForallRightRule( r3, exyRay, allxexyRxy, a )
+
+      val fs0 = HOLConst( s0, i -> i )
+      val cs1 = HOLConst( s1, i )
+      val s0s1 = HOLApp( fs0, cs1 )
+      val sR = Atom( "R", cs1::s0s1::Nil )
+      val sax = Axiom( Sequent( sR::Nil, sR::Nil ) )
+      val exyRs1y = ExVar( y, Atom( "R", cs1::y::Nil ) )
+      val allxRxs0x = AllVar( x, Atom( "R", x::HOLApp( fs0, x )::Nil ) )
+
+      val sr1 = ExistsRightRule( sax._1, sR, exyRs1y, s0s1 )
+      val proof_sk = ForallLeftRule( sr1, sR, allxRxs0x, cs1 )
+
+      // this does not work correctly
+      //skolemize( proof ) must beEqual( proof_sk )
+      skolemize( proof ).root.getSequent.multisetEquals( proof_sk.root.getSequent) must beEqual( true )    }
   }
 }
