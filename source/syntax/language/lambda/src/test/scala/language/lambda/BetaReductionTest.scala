8747bf416e26978ef6b63b801e33f0752d5dced3
Tomer Libal <shaolintl@gmail.com>
2010-08-04 09:58:57 +0000

    a bug in beta normalization was fixed (didnt work for ((\x,y.xy)a)b ) i.e. with two app one after the other.
    tests were added to confirm the bug was fixed
    
    git-svn-id: http://gapt.googlecode.com/svn/trunk@476 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/syntax/language/lambda/src/test/scala/language/lambda/BetaReductionTest.scala
+++ source/syntax/language/lambda/src/test/scala/language/lambda/BetaReductionTest.scala
@@ -23,7 +23,8 @@ class BetaReductionTest extends SpecificationWithJUnit {
   level = Debug  // sets the printing of extra information (level can be: Debug, Info, Warning, Error)
 
   val v = Var("v", i, LambdaFactory); val x = Var("x", i, LambdaFactory); val y = Var("y", i, LambdaFactory);
-  val f = Var("f", i -> i, LambdaFactory)
+  val f = Var("f", i -> i, LambdaFactory); val g = Var("g", i -> i, LambdaFactory)
+  val f2 = Var("f2", i -> i, LambdaFactory); val g2 = Var("g2", i -> i, LambdaFactory)
 
   "BetaReduction" should {
     "betaReduce a simple redex" in {
@@ -51,10 +52,20 @@ class BetaReductionTest extends SpecificationWithJUnit {
         ( betaReduce(e)(Innermost, Rightmost) ) must beEqual ( App(el,App(Abs(v, App(f, v)),y)) )
     }
     "betaNormalize correctly with outermost strategy" in {
-        val er = App(Abs(v, App(Abs(x, App(f, x)),v)),y)
-        val el = Abs(v, App(Abs(x, App(f, x)),v))
-        val e = App(el,er)
-        ( betaNormalize(e)(Outermost) ) must beEqual ( App(f,App(f,y)) )
+      "- 1" in {
+          val er = App(Abs(v, App(Abs(x, App(f, x)),v)),y)
+          val el = Abs(v, App(Abs(x, App(f, x)),v))
+          val e = App(el,er)
+          ( betaNormalize(e)(Outermost) ) must beEqual ( App(f,App(f,y)) )
+      }
+      "- 2" in {
+          val e = App(App(Abs(g, Abs(y, App(g,y))), f), v)
+          ( betaNormalize(e)(Outermost) ) must beEqual ( App(f,v) )
+      }
+      "- 3" in {
+          val e = App(App(App(Abs(g2, Abs(g, Abs(y, App(g2,App(g,y))))), f2), f), v)
+          ( betaNormalize(e)(Outermost) ) must beEqual ( App(f2,App(f,v)) )
+      }
     }
     "betaNormalize correctly with innermost strategy" in {
         val er = App(Abs(v, App(Abs(x, App(f, x)),v)),y)
@@ -74,7 +85,7 @@ class BetaReductionTest extends SpecificationWithJUnit {
         val e = App(Abs(v, App(Abs(x, App(f, x)),v)),y)
         ( betaReduce(e) ) must beEqual ( App(Abs(x, App(f, x)),y) )
     }
-    "betaReduce correctly with reard to de Bruijn indices" in {
+    "betaReduce correctly with regard to de Bruijn indices" in {
       "- 1" in {
         val term1 = App(Abs(LambdaVar("x",i->i),Abs(LambdaVar("y",i),App(LambdaVar("x",i->i),LambdaVar("y",i)))),Abs(LambdaVar("z",i),LambdaVar("z",i)))
         val term2 = Abs(LambdaVar("y",i),App(Abs(LambdaVar("z",i),LambdaVar("z",i)),LambdaVar("y",i)))
@@ -98,6 +109,10 @@ class BetaReductionTest extends SpecificationWithJUnit {
         val term1 = App(AbsN(x1::x2::x3::Nil, t2),t3)
         val term2 = AbsN(x2::x3::Nil, App(App(c1,App(t3,x2)),App(t3,x3)))
         (betaReduce(term1)(Outermost, Leftmost)) must beEqual (term2)
+      }
+      "- 4" in {
+        val e = Abs(x, App(Abs(g, App(g,x)), f))
+        (betaReduce(e)(Outermost, Leftmost)) must beEqual (Abs(y, App(f, y)))
       }
     }
   }
