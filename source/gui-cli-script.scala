f71730a005f3d79710af1d21872222a35b5fa9ea
Mikheil Rukhaia <mrukhaia@gmail.com>
2010-10-13 16:14:07 +0000

    added old gui stuff again
    git-svn-id: http://gapt.googlecode.com/svn/trunk@480 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ source/gui-cli-script.scala
@@ -0,0 +1,7 @@
+import at.logic.cli.GAPScalaInteractiveShellLibrary._
+import at.logic.language.fol._
+
+println("Welcome to the GAPT shell!")
+println("See ceresHelp() for the commands.")
+
+val pt = new at.logic.prooftool.ProofViewer
