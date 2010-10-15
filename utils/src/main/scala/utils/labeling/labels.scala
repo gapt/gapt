9a8199fa8ae77a2141c6b06c8c69a1871de93d27
shaolintl <shaolintl@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2010-10-15 12:31:15 +0000

    made labels more general so they can also support the labels in lksk
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@482 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- utils/src/main/scala/utils/labeling/labels.scala
+++ utils/src/main/scala/utils/labeling/labels.scala
@@ -7,10 +7,26 @@
 
 package at.logic.utils.labeling
 
-trait Labeled[A] {
-  def label: A
+import reflect.{ClassManifest, Manifest}
+ /*
+trait Labeled[T] {
+  def label: T
+} */
+// this erasure is really disturbing! */
+
+trait Labeled
+{
+  type T
+  def label: T
+  /*def om: Manifest[T]
+  def equals[S](a: Any)(implicit m: Manifest[S]) =
+    if (om <:< m || m <:< om) {val other = a.asInstanceOf[T]; other == label}
+    else if (m == this) {val other = a.asInstanceOf[Labeled]; other.label == label}
+    else false  */
+  override def hashCode = label.hashCode
+  override def toString = label.toString
 }
 
-abstract class LabelKey
+//abstract class LabelKey
 
-trait MultiLabeled[A] extends Labeled[Map[LabelKey, A]]
+//trait MultiLabeled[A] extends Labeled[Map[LabelKey, A]]
