553a236570adc639631c3c2ca06f39fd672f8e15
shaolintl <shaolintl@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2010-11-04 12:58:09 +0000

    separated the choice of the search algorithm (BFS for example) from the execution model by using traits
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@497 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- utils/src/test/scala/utils/executionModels/NDStreamTest.scala
+++ utils/src/test/scala/utils/executionModels/NDStreamTest.scala
@@ -4,6 +4,7 @@ import org.specs._
 import org.specs.runner._
 
 import ndStream._
+import searchAlgorithms._
 
 class NDStreamTest extends SpecificationWithJUnit {
   class MyConfiguration(val n: Int) extends Configuration {
@@ -13,17 +14,17 @@ class NDStreamTest extends SpecificationWithJUnit {
   "BFSNDStream" should {
     "return None if empty" in {
       def myFun(conf: Configuration): List[Configuration] = (for {i <- 2 to conf.asInstanceOf[MyConfiguration].n} yield new MyConfiguration(i - 1)).toList
-      val st = new BFSNDStream(new MyConfiguration(6), myFun)
+      val st = new NDStream(new MyConfiguration(6), myFun) with BFSAlgorithm
       st.next must beEqual (None)
     }
     "return one result" in {
       def myFun(conf: Configuration): List[Configuration] = List(new MyConfiguration(conf.asInstanceOf[MyConfiguration].n - 1) )
-      val st = new BFSNDStream(new MyConfiguration(6), myFun)
+      val st = new NDStream(new MyConfiguration(6), myFun) with BFSAlgorithm
       st.next must beEqual (Some(0))
     }
     "return six results" in {
       def myFun(conf: Configuration): List[Configuration] = (for {i <- 1 to conf.asInstanceOf[MyConfiguration].n} yield new MyConfiguration(i - 1)).toList
-      val st = new BFSNDStream(new MyConfiguration(6), myFun)
+      val st = new NDStream(new MyConfiguration(6), myFun) with BFSAlgorithm
       st.next must beEqual (Some(0))
       st.next must beEqual (Some(0))
       st.next must beEqual (Some(0))
