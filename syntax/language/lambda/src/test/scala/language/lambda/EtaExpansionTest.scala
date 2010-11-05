39bcedb2cff527d727dad26b70d356ccf2264117
cdunchev <cdunchev@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2010-11-05 13:59:37 +0000

    The test was fixed using alpha equivalence
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@499 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- syntax/language/lambda/src/test/scala/language/lambda/EtaExpansionTest.scala
+++ syntax/language/lambda/src/test/scala/language/lambda/EtaExpansionTest.scala
@@ -20,6 +20,7 @@ class EtaExpansionTest extends SpecificationWithJUnit {
   val v = Var("v", i, LambdaFactory);
   val x = Var("x", i, LambdaFactory);
   val y = Var("y", i, LambdaFactory);
+  val z = Var("z", i, LambdaFactory);
   val f = Var("f", i -> i, LambdaFactory);
   val g = Var("g", i -> i, LambdaFactory)
   val f2 = Var("f2", i -> i, LambdaFactory);
@@ -40,42 +41,59 @@ class EtaExpansionTest extends SpecificationWithJUnit {
     "expand correctly the lambda expressions g3(x) : (i->i) to lambda y. g3(x,y)" in {
 //      etaExpansion.EtaExpand(g3) must beEqual (Abs(y,Abs(x, App(g3,x))))
 //      println("\n\ntype = "+g5.exptype.toString)
-      println("\n\neta : "+EtaExpand(App(g3,x))(disAllowedVars = EtaExpand.disAllowedVars).toString1+"\n\n")
+//      println("\n\neta : "+EtaExpand(App(g3,x))(disAllowedVars = EtaExpand.disAllowedVars).toString1+"\n\n")
+      EtaExpand(App(g3,x))(disAllowedVars = EtaExpand.disAllowedVars) must beEqual (Abs(y, AppN(g3, x.asInstanceOf[LambdaExpression]::y.asInstanceOf[LambdaExpression]::Nil)))
 //      println("\n\neta : "+etaExpansion.EtaExpand(g4).toString1+"\n\n")
 //      println("\n\neta : "+etaExpansion.EtaExpand(g5).toString1+"\n\n")
-      0 must beEqual (0)
+//      0 must beEqual (0)
     }
     "expand correctly the lambda expressions g3 : i->(i->i) to lambda x,y. g3(x,y)" in {
 //      etaExpansion.EtaExpand(g3) must beEqual (Abs(y,Abs(x, App(g3,x))))
 //      println("\n\ntype = "+g5.exptype.toString)
-      println("\n\neta : "+EtaExpand(g3)(disAllowedVars = EtaExpand.disAllowedVars).toString1+"\n\n")
+//      println("\n\neta : "+EtaExpand(g3)(disAllowedVars = EtaExpand.disAllowedVars).toString1+"\n\n")
+      EtaExpand(g3)(disAllowedVars = EtaExpand.disAllowedVars) must beEqual (AbsN(x::y::Nil, AppN(g3,x::y::Nil)))
 //      println("\n\neta : "+etaExpansion.EtaExpand(g4).toString1+"\n\n")
 //      println("\n\neta : "+etaExpansion.EtaExpand(g5).toString1+"\n\n")
-      0 must beEqual (0)
+//      0 must beEqual (0)
     }
     "expand correctly the lambda expressions g3(g3(x,y)) : i to lambda z. g3(g3(x,y),z)" in {
 //      etaExpansion.EtaExpand(g3) must beEqual (Abs(y,Abs(x, App(g3,x))))
 //      println("\n\ntype = "+g5.exptype.toString)
-      println("\n\neta : "+EtaExpand(App(g3, AppN(g3,x::y::Nil)))(disAllowedVars = EtaExpand.disAllowedVars).toString1+"\n\n")
+//      println("\n\neta : "+EtaExpand(App(g3, AppN(g3,x::y::Nil)))(disAllowedVars = EtaExpand.disAllowedVars).toString1+"\n\n")
+      EtaExpand(App(g3, AppN(g3,x::y::Nil)))(disAllowedVars = EtaExpand.disAllowedVars) must beEqual (Abs(z, AppN(g3, AppN(g3, x::y::Nil)::z::Nil)))
 //      println("\n\neta : "+etaExpansion.EtaExpand(g4).toString1+"\n\n")
 //      println("\n\neta : "+etaExpansion.EtaExpand(g5).toString1+"\n\n")
-      0 must beEqual (0)
+//      0 must beEqual (0)
     }
     "expand correctly the lambda expressions g6(f) : (i->i) to eta Abs(#7,App(App(g6, Abs(#8,App(f, #8))), #7))" in {
 //      etaExpansion.EtaExpand(g3) must beEqual (Abs(y,Abs(x, App(g3,x))))
 //      println("\n\ntype = "+g5.exptype.toString)
-      println("\n\neta : "+EtaExpand(App(g6,f))(EtaExpand.disAllowedVars).toString1+"\n\n")
+//      println("\n\neta : "+EtaExpand(App(g6,f))(EtaExpand.disAllowedVars).toStringSimple+"\n\n")
+      EtaExpand(App(g6,f))(EtaExpand.disAllowedVars) must beEqual (Abs(z,App(App(g6, Abs(x,App(f, x))), z)))
 //      println("\n\neta : "+etaExpansion.EtaExpand(g4).toString1+"\n\n")
 //      println("\n\neta : "+etaExpansion.EtaExpand(g5).toString1+"\n\n")
-      0 must beEqual (0)
+//      0 must beEqual (0)
     }
     "expand correctly the lambda expressions g6 : (i->i)->(i->i) to lambda x,y. g6(lambda z. x(z),y)" in {
 //      etaExpansion.EtaExpand(g3) must beEqual (Abs(y,Abs(x, App(g3,x))))
 //      println("\n\ntype = "+g5.exptype.toString)
-      println("\n\neta : "+EtaExpand(g6)(disAllowedVars = EtaExpand.disAllowedVars).toString1+"\n\n")
+//      println("\n\neta : "+EtaExpand(g6)(disAllowedVars = EtaExpand.disAllowedVars).toString1+"\n\n")
+      EtaExpand(g6)(disAllowedVars = EtaExpand.disAllowedVars) must beEqual (AbsN(f::y::Nil, AppN(g6, Abs(z,App(f,z))::y::Nil)))
 //      println("\n\neta : "+etaExpansion.EtaExpand(g4).toString1+"\n\n")
 //      println("\n\neta : "+etaExpansion.EtaExpand(g5).toString1+"\n\n")
-      0 must beEqual (0)
+//      0 must beEqual (0)
+    }
+
+    "expand correctly the lambda expressions f : (i->i) to f2 : (i->i)" in {
+//      etaExpansion.EtaExpand(g3) must beEqual (Abs(y,Abs(x, App(g3,x))))
+//      println("\n\ntype = "+g5.exptype.toString)
+//      println("\n\neta : "+EtaExpand(f)(disAllowedVars = EtaExpand.disAllowedVars).toString1+"\n\n")
+//      println("\n\neta : "+EtaExpand(Abs(x,App(g3,x)))(disAllowedVars = EtaExpand.disAllowedVars).toString1+"\n\n")
+      val v2 = EtaExpand(Abs(x,App(g3,x)))(disAllowedVars = EtaExpand.disAllowedVars)
+      EtaExpand(g3)(disAllowedVars = EtaExpand.disAllowedVars) must beEqual (v2)// (EtaExpand(f2)(disAllowedVars = EtaExpand.disAllowedVars))
+//      println("\n\neta : "+etaExpansion.EtaExpand(g4).toString1+"\n\n")
+//      println("\n\neta : "+etaExpansion.EtaExpand(g5).toString1+"\n\n")
+//      0 must beEqual (0)
     }
 
 
