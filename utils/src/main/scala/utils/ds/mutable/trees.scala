73c25617f63d590f0b82b171e5a4e57f4ce8bca4
cdunchev <cdunchev@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2010-12-21 23:45:00 +0000

    feature index tree for the subsumption algorithm has been modified
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@568 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- utils/src/main/scala/utils/ds/mutable/trees.scala
+++ utils/src/main/scala/utils/ds/mutable/trees.scala
@@ -16,8 +16,8 @@ package at.logic.utils.ds.mutable
 
   // V is a vertex - a set of clauses
  
-  class TreeNode[V](elem: List[V]) {
-    var seqList: List[V] = elem
+  class TreeNode[V](var seqList: List[V]) {
+//    var seqList: List[V] = elem
     var children: Map[Int, TreeNode[V]] = Map()
 //    def printElem: Unit = { for (x <- elem) }
     def print: Unit = { println("\n\nTreeNode:\nseqList  = "+seqList.toString); printMap }
@@ -34,87 +34,81 @@ package at.logic.utils.ds.mutable
      
       println("}")
     }
-    def setSeqList(l: List[V]) = { seqList = l }
-
+//    def setSeqList(l: List[V]) = { seqList = l }
   }
 
 
-  class MyTree[V](rootNode: TreeNode[V], features: List[V=>Int]) {
-    var root: TreeNode[V] = rootNode
+  class Trie[V](val seqSet: List[V], val features: List[V=>Int]) {
 
-    
-    private def print1(vert: TreeNode[V]) :Unit = {
-      vert.print
-      if (isLeaf(vert)) {
-        println("\n\nLeaf")
-        vert.print
-//        println("\n print vert : ")        
-        return;
-      }
-    //  println(vert.seqList.toString)
-      
-      for (seq <- vert.children) {
-//        println("\nisn't Leaf\n")
-        println("\n\nNode")
-        vert.print
-//        println(seq._1+"   "+print1(seq._2))
-        print1(seq._2)
-      }
 
-    }
-
-    def print = print1(root)
 
     def isLeaf(vert: TreeNode[V]): Boolean = {
       vert.children.isEmpty
     }
 
-    def createTree(vert: TreeNode[V]) = createTreeRec(root, features)
+    def createTree = createTreeRec(new TreeNode(seqSet), features)
 
-    private def createTreeRec(vert: TreeNode[V], features: List[V=>Int]): Unit = {
+    private def createTreeRec(vert: TreeNode[V], features: List[V=>Int]): Unit = features match {
 //      println("\n createTreeRec\n")
-    if(features.isEmpty) {
-   //     println("\nrec bottom\n")
-        return;
-      }
-      else {
-        val f = features.head
+      case List() => return
+     //     println("\nrec bottom\n")
+      case _ => {
      //   val f: (String) => Int = { x => x.split("a").size}
-        var i:Int = 0
-        println(vert.seqList)
-        while (i < vert.seqList.size)
-        {
-          if(vert.children.contains(f(vert.seqList(i))))
-          {
-            println("\nwhile if, i = "+i)
-            println("\nvert.children "+vert.children.toString)
-            var newl = vert.seqList(i)::(vert.children.get(f(vert.seqList(i))).get).seqList
-            println("newl = "+newl)
-            vert.children-=f(vert.seqList(i))
-            vert.children += Tuple2(f(vert.seqList(i)), new TreeNode(newl))         
-            println("\nvert.children "+vert.children.toString)
+  //      println(vert.seqList)
+
+        vert.seqList.foreach(seq => {
+          val key = features.head(seq)
+          vert.children.get(key) match {
+            case Some(node) => {
+              val sSet = seq::node.seqList
+              vert.children = vert.children.-(key)
+              vert.children = vert.children.+(Pair(key, new TreeNode(sSet)))
+            }
+            case _ => vert.children = vert.children.+(Pair(key, new TreeNode(seq::Nil)))
           }
-
-          else
-          {
-            println("\n\nwhile else, i = "+i)
-            var newl = vert.seqList(i)::Nil
-            println("\nvert.children "+vert.children.toString)
-            vert.children += Tuple2(f(vert.seqList(i)), new TreeNode(newl))
-            println("\nvert.children "+vert.children.toString)
-          }
-          i = i+1
-        }
-
-        var j: Int = 0;
-        while(j < vert.children.size) {
-//          println("\nrec\n")
-          if(vert.children.contains(j))
-            createTreeRec( vert.children(j), features.tail)
-
-          j=j+1
-        }
+        })
+        vert.children.foreach(intTreeNodePair => createTreeRec(intTreeNodePair._2, features.tail))
       }
-    }      
+    }
   }
 }
+//
+//
+//
+//
+//        while (i < vert.seqList.size)
+//        {
+//          if(vert.children.contains(f(vert.seqList(i))))
+//          {
+//            println("\nwhile if, i = "+i)
+//            println("\nvert.children "+vert.children.toString)
+//            var newl = vert.seqList(i)::(vert.children.get(f(vert.seqList(i))).get).seqList
+//            println("newl = "+newl)
+//            vert.children-=f(vert.seqList(i))
+//            vert.children += Tuple2(f(vert.seqList(i)), new TreeNode(newl))
+//            println("\nvert.children "+vert.children.toString)
+//          }
+//
+//          else
+//          {
+//            println("\n\nwhile else, i = "+i)
+//            var newl = vert.seqList(i)::Nil
+//            println("\nvert.children "+vert.children.toString)
+//            vert.children += Tuple2(f(vert.seqList(i)), new TreeNode(newl))
+//            println("\nvert.children "+vert.children.toString)
+//          }
+//          i = i+1
+//        }
+//
+//        var j: Int = 0;
+//        while(j < vert.children.size) {
+////          println("\nrec\n")
+//          if(vert.children.contains(j))
+//            createTreeRec( vert.children(j), features.tail)
+//
+//          j=j+1
+//        }
+//      }
+//    }
+//  }
+//}
