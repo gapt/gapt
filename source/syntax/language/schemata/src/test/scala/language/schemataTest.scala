47447128f12ae0a5f8ad002ddb8410aa7649861b
Cvetan Dunchev <cdunchev@gmail.com>
2011-02-08 00:57:21 +0000

    the problem with the  factories have been fixed
    git-svn-id: http://gapt.googlecode.com/svn/trunk@592 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/syntax/language/schemata/src/test/scala/language/schemataTest.scala
+++ source/syntax/language/schemata/src/test/scala/language/schemataTest.scala
@@ -1,5 +1,6 @@
 package at.logic.language.schemata
 
+import at.logic.language.lambda.symbols._
 import org.specs._
 import org.specs.runner._
 
@@ -7,9 +8,14 @@ import at.logic.language.schemata._
 
 class schemaTest extends SpecificationWithJUnit {
 
-  "TypedLambdaCalculus" should {
-    "make implicit conversion from String to Name" in {
-      0 must beEqual (0)
-    }
-  }
+
+  println("\n\n\n\n")
+
+  val v = IntVar((new VariableStringSymbol("a")).asInstanceOf[VariableSymbolA])
+  val c  = IntConst() // equivalent to calling IntConst.apply() (on the object)
+  val sch = Succ(Succ(c))
+  println("\n"+sch.toString)
+  println("\n\n\n\n")
+  0 must beEqual (0)
+
 }
