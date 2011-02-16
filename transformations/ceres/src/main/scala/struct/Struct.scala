a453512d73c7a3ed4fd8f1d40a86852ba2fa7ddf
fraini <fraini@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2011-02-16 15:50:54 +0000

    Started adaption of Struct-extraction algorithm to work for proof schemas. Towards this: Started implementation of Multisets.
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@616 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- transformations/ceres/src/main/scala/struct/Struct.scala
+++ transformations/ceres/src/main/scala/struct/Struct.scala
@@ -20,6 +20,12 @@ import at.logic.algorithms.lk.getCutAncestors
 import at.logic.utils.ds.trees._
 import at.logic.language.lambda.types.ImplicitConverters._
 
+// imports for schema stuff
+import at.logic.calculi.slk._
+import at.logic.language.schema._
+import at.logic.language.lambda.symbols._
+import at.logic.utils.ds.Multisets._
+
 import scala.collection.immutable.Set
 
 package struct {
@@ -72,6 +78,19 @@ package struct {
   case class EmptyTimesJunction() extends LeafTree[HOLExpression](EmptyTimesC) with Struct
   case class EmptyPlusJunction() extends LeafTree[HOLExpression](EmptyPlusC) with Struct
 
+  // In the construction of schematized clause sets, we use symbols
+  // that contain a name and a cut-configuration. This class represents
+  // such symbols.
+  class ClauseSetSymbol(val name: String, val cut_occs: Multiset[SchemaFormula]) extends ConstantSymbolA {
+    def compare( that: SymbolA ) : Int =
+      // TODO: implement
+      throw new Exception
+
+    def toCode() : String =
+      // TODO: implement
+      throw new Exception
+  }
+
   object StructCreators {
 
     def extract(p: LKProof) : Struct = extract( p, getCutAncestors( p ) )
@@ -79,29 +98,43 @@ package struct {
     def extract(p: LKProof, cut_occs: Set[FormulaOccurrence]):Struct = p match {
       case Axiom(so) => // in case of axioms of the form A :- A with labelled formulas, proceed as in Daniel's PhD thesis
       so match {
-        case lso : LabelledSequentOccurrence if lso.l_antecedent.size == 1 && lso.l_succedent.size == 1 => {
-          val left = lso.l_antecedent.toList.head
-          val right = lso.l_succedent.toList.head 
-          val ant = if ( cut_occs.contains( left ) )
-            Dual( A( new LabelledFormulaOccurrence( left.formula, Nil, right.skolem_label ) ) )::Nil
-          else
-            Nil
-          val suc = if ( cut_occs.contains( right ) )
-            A( new LabelledFormulaOccurrence( right.formula, Nil, left.skolem_label ) )::Nil
-          else
-            Nil
-          makeTimesJunction( ant:::suc, Nil )
-        }
-        case _ => {
-          val cutAncInAntecedent = so.antecedent.toList.filter(x => cut_occs.contains(x)).map(x => Dual(A(x)))   //
-          val cutAncInSuccedent = so.succedent.toList.filter(x => cut_occs.contains(x)).map(x => A(x))
-          makeTimesJunction(cutAncInAntecedent:::cutAncInSuccedent, Nil)
-        }
+        case lso : LabelledSequentOccurrence if lso.l_antecedent.size == 1 && lso.l_succedent.size == 1 =>
+          handleLabelledAxiom( lso, cut_occs )
+        case _ => handleAxiom( so, cut_occs )
       }
       case UnaryLKProof(_,upperProof,_,_,_) => handleUnary( upperProof, cut_occs )
       case BinaryLKProof(_, upperProofLeft, upperProofRight, _, aux1, aux2, _) => 
         handleBinary( upperProofLeft, upperProofRight, aux1::aux2::Nil, cut_occs )
       case UnaryLKskProof(_,upperProof,_,_,_) => handleUnary( upperProof, cut_occs )
+      case UnarySchemaProof(_,upperProof,_,_,_) => handleUnary( upperProof, cut_occs )
+      case SchemaProofLinkRule(so, name, indices) => handleSchemaProofLink( so, name, indices, cut_occs )
+    }
+
+    def handleSchemaProofLink( so: SequentOccurrence, name: String, indices: List[IntegerTerm], cut_occs: Set[FormulaOccurrence]) = {
+      val sym = new ClauseSetSymbol( name, cut_occs.foldLeft(HashMultiset[SchemaFormula])( (s,o) => s + o.formula.asInstanceOf[SchemaFormula] ) )
+      val atom = IndexedPredicate( sym, indices )
+      val others = so.antecedent ++ so.succedent
+      A( others.head.factory.createPrincipalFormulaOccurrence(atom, Nil, others) )
+    }
+
+    def handleLabelledAxiom( lso: LabelledSequentOccurrence, cut_occs: Set[FormulaOccurrence] ) = {
+      val left = lso.l_antecedent.toList.head
+      val right = lso.l_succedent.toList.head
+      val ant = if ( cut_occs.contains( left ) )
+        Dual( A( new LabelledFormulaOccurrence( left.formula, Nil, right.skolem_label ) ) )::Nil
+      else
+        Nil
+      val suc = if ( cut_occs.contains( right ) )
+        A( new LabelledFormulaOccurrence( right.formula, Nil, left.skolem_label ) )::Nil
+      else
+        Nil
+      makeTimesJunction( ant:::suc, Nil )
+    }
+
+    def handleAxiom( so: SequentOccurrence, cut_occs: Set[FormulaOccurrence] ) = {
+      val cutAncInAntecedent = so.antecedent.toList.filter(x => cut_occs.contains(x)).map(x => Dual(A(x)))   //
+      val cutAncInSuccedent = so.succedent.toList.filter(x => cut_occs.contains(x)).map(x => A(x))
+      makeTimesJunction(cutAncInAntecedent:::cutAncInSuccedent, Nil)
     }
 
     def handleUnary( upperProof: LKProof, cut_occs: Set[FormulaOccurrence] ) =
--- utils/src/main/scala/utils/ds/Multisets.scala
+++ utils/src/main/scala/utils/ds/Multisets.scala
@@ -7,13 +7,31 @@
 
 package at.logic.utils.ds
 
+import scala.collection.immutable.HashMap
+
 object Multisets {
 
-    trait Multiset[+A] extends Iterable[A]{
+//    trait Multiset[+A] extends Iterable[A]{
+    trait Multiset[A] {
+      def + (elem: A) : Multiset[A]
+      def - (elem: A) : Multiset[A]
     }
 
-    /*class HashMultiset[+A] extends Multiset[A] {
-        
-    }*/
+    class HashMultiset[A](val map: HashMap[A, Int]) extends Multiset[A] {
+      def + (elem: A) = new HashMultiset( map + (( elem, map.getOrElse(elem, 0) + 1 ) ))
+
+      def - (elem: A)  = new HashMultiset( if (map.contains(elem))
+        if (map.get(elem).get == 1)
+          map - (elem)
+        else
+          map + ((elem, map.get(elem).get - 1))
+      else
+        throw new Exception // element not contained in the multiset
+      )
+    }
+
+  object HashMultiset {
+    def apply[A]() = new HashMultiset(new HashMap[A,Int])
+  }
 }
 
