16c7b5bcac8cdd8a3bcd78f43cd3986fc8e5a078
Daniel Weller <fraini@gmail.com>
2011-02-17 09:42:04 +0000

    Added some combinatorics to Multiset (used for computing cut-configurations).
    git-svn-id: http://gapt.googlecode.com/svn/trunk@620 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/utils/src/test/scala/utils/ds/MultisetsTest.scala
+++ source/utils/src/test/scala/utils/ds/MultisetsTest.scala
@@ -11,10 +11,26 @@ import org.specs._
 import org.specs.runner._
 
 import Multisets._
+import scala.collection.immutable.HashSet
 
 
 class MultisetsTest extends SpecificationWithJUnit {
-    
+   "Multisets" should {
+     val m1 = HashMultiset[Int] + 1 + 1 + 2
+
+     val c0 = HashMultiset[Int]
+     val c11 = HashMultiset[Int] + 1
+     val c12 = HashMultiset[Int] + 2
+     val c21 = HashMultiset[Int] + 1 + 1
+     val c22 = HashMultiset[Int] + 1 + 2
+
+     "compute correct combinations" in {
+       combinations(0, m1) must beEqual(new HashSet() + c0 )
+       combinations(1, m1) must beEqual(new HashSet() + c0 + c11 + c12 )
+       combinations(2, m1) must beEqual(new HashSet() + c0 + c11 + c12 + c21 + c22 )
+       combinations(3, m1) must beEqual(new HashSet() + c0 + c11 + c12 + c21 + c22 + m1 )
+     }
+   }
 }
 
 
