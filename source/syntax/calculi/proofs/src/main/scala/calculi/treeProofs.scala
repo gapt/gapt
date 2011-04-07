f2b0b0f5e2756b90d467d1c84dd1aa4c0d2f497f
Daniel Weller <fraini@gmail.com>
2011-04-07 15:46:01 +0000

    Since trees are not contravariant anymore, so aren't tree proofs.
    git-svn-id: http://gapt.googlecode.com/svn/trunk@662 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/syntax/calculi/proofs/src/main/scala/calculi/treeProofs.scala
+++ source/syntax/calculi/proofs/src/main/scala/calculi/treeProofs.scala
@@ -15,8 +15,8 @@ import proofs._
 
 package treeProofs {
 
-  trait TreeProof[+V] extends Tree[V] with Proof[V]
-  trait NullaryTreeProof[+V] extends LeafTree[V] with NullaryProof[V] with TreeProof[V]
-  trait UnaryTreeProof[+V] extends UnaryTree[V] with UnaryProof[V] with TreeProof[V]
-  trait BinaryTreeProof[+V] extends BinaryTree[V] with BinaryProof[V] with TreeProof[V] 
+  trait TreeProof[V] extends Tree[V] with Proof[V]
+  trait NullaryTreeProof[V] extends LeafTree[V] with NullaryProof[V] with TreeProof[V]
+  trait UnaryTreeProof[V] extends UnaryTree[V] with UnaryProof[V] with TreeProof[V]
+  trait BinaryTreeProof[V] extends BinaryTree[V] with BinaryProof[V] with TreeProof[V] 
 }
