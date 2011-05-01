35b5f035fa939413bf8386e5adfe6508788faafb
cdunchev <cdunchev@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2011-05-01 21:26:08 +0000

    A test file for Huet alg.
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@665 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- algorithms/unification/src/test/scala/algorithms/unification/hol/HuetAlgorithmTest.scala
+++ algorithms/unification/src/test/scala/algorithms/unification/hol/HuetAlgorithmTest.scala
@@ -7,6 +7,10 @@
 
 package at.logic.algorithms.unification.hol
 
+import at.logic.language.lambda.BetaReduction
+import at.logic.language.lambda.BetaReduction._
+
+import at.logic.language.lambda.substitutions.Substitution
 import org.specs._
 import org.specs.runner._
 import at.logic.parsing.readers.StringReader
@@ -24,6 +28,8 @@ import at.logic.language.lambda.types.Definitions._
 
 import at.logic.language.lambda.symbols.ImplicitConverters._
 
+import StrategyOuterInner._
+import StrategyLeftRight._
 
 
 private class MyParser(input: String) extends StringReader(input) with SimpleFOLParser
@@ -36,17 +42,48 @@ class HuetAlgorithmTest extends SpecificationWithJUnit with org.specs.ScalaCheck
 
       class MyParser(input: String) extends StringReader(input) with SimpleHOLParser
 
+      val a = HOLConst(new ConstantStringSymbol("a"), i)
+      val F = HOLVar("F", i->i)
+      val f = HOLConst(new ConstantStringSymbol("f"), i->i)
+      val g = HOLConst(new ConstantStringSymbol("g"), i->(i->i))
+      val Ffa = HOLApp(F,HOLApp(f,a))
+      val fFa = HOLApp(f,HOLApp(F,a))
+      val gfFafFa = HOLApp(HOLApp(g, fFa), fFa)
+
+
+
       val term1a = new MyParser("F(a:i):i").getTerm()
       val term1b = new MyParser("x:i").getTerm()
 
       val term2a = new MyParser("F(f(a:i):i):i").getTerm()
       val term2b = new MyParser("g(f(x:i):i,f(x:i):i):i").getTerm()
 
+
+      val x = HOLVar("x", i)
+      val H1 = HOLVar("H1", i->i)
+      val H2 = HOLVar("H2", i->i)
+      val t = Abs(x, App(App(g, App(H1, x)),App(H2,x))).asInstanceOf[HOLExpression]
+
+//      val sigma = Substitution[HOLExpression](F, t)
+//      println("\n\nsigma = "+sigma)
+//      println("\n\nsigma() = "+sigma(gfFafFa))
+//      println("\n\nsigma_beta() = "+BetaReduction.betaNormalize(sigma(gfFafFa))(Outermost))
+
+
+//      println("\n\nt = "+t.toStringSimple)
+//      println("\n\nbeta(t) = "+BetaReduction.betaReduce(t)(Innermost, Leftmost).toStringSimple)
+
+//      (((λ#1:i.g(#3(#1:i):i, #5(#1:i):i):i))(f(a:i):i),g(f(g(#3(a:i):i, #5(a:i):i):i):i, f(((λ#1:i.g(#3(#1:i):i, #5(#1:i):i):i))(a:i)):i):i)
+//     (F:(i->i),(λ#1:i.g(#3(#1:i):i, #5(#1:i):i):i)))
+
       val termb = new MyParser("g(f(F(a:i):i):i,f(F(a:i):i):i):i").getTerm()
+      println("\n\n<"+term2a.toStringSimple+" , "+termb.toStringSimple+">\n\n")
 
-      val st = HuetAlgorithm.unify1(term2a, termb)
 
-      println("\n\n"+st.next.get.toString + " ; "+ st.next.get.toString)
+//      val st = HuetAlgorithm.unify1(term2a, termb)
+      val st = HuetAlgorithm.unify1(Ffa, gfFafFa)
+
+      println("\n\n"+st.next.get.toString + " ; "+ st.next.get.toString + " ; "+ st.next.get.toString + " ; "+ st.next.get.toString)
       println("\n\n")
 
       0 must beEqual(0)
@@ -122,15 +159,23 @@ class HuetAlgorithmTest extends SpecificationWithJUnit with org.specs.ScalaCheck
 //      val st = HuetAlgorithm.unify1(Ffx, fFx)
 //      println("\n\n"+st.next.get.toString + " ; "+ st.next.get.toString)
 //      println("\n\n")
-      val st = HuetAlgorithm.unify1(fFx, Ffx)
-      println("\n\n"+st.next.get.toString + " ; "+ st.next.get.toString)
-      println("\n\n")
+//      val st = HuetAlgorithm.unify1(fFx, Ffx)
+//      println("\n\n"+st.next.get.toString + " ; "+ st.next.get.toString)
+//      println("\n\n")
 
 //      prefixes(List(1, 2, 3))
 
+      {
+        val Fa = HOLApp(F,a)
+        val fa = HOLApp(f,a)
+        val fx = HOLApp(f,x)
+        val Ffa = HOLApp(F,fa)
+        val gfxfx = HOLApp(HOLApp(g, fx), fx)
 
-
-
+//        implicit val disAllowedVars = Set[Var]()
+//        val st = new NDStream(new ConfigurationNode((Fa, x)::(Ffa, gfxfx)::Nil , Nil), myFun1) with BFSAlgorithm
+//        return st
+      }
 
 
       0 must beEqual(0)
