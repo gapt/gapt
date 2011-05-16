edb5d5141e1e39efefe9499081d79adc9b068766
fraini <fraini@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2011-05-16 11:10:50 +0000

    Slightly improved applet to be actually somewhat useful now.
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@679 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- applet/src/main/scala/applet/AppletMain.scala
+++ applet/src/main/scala/applet/AppletMain.scala
@@ -53,13 +53,21 @@ import at.logic.algorithms.unification.hol._
 
 
 class AppletMain extends java.applet.Applet {
-  def huet(s1: String, s2: String) = {
+  def huet(s1: String, s2: String) : String = {
     class MyParser(input: String) extends StringReader(input) with SimpleHOLParser
 
-    HuetAlgorithm.unify1(new MyParser(s1).getTerm(), new MyParser(s2).getTerm()).next match {
-      case Some(_) => true
-      case None => false
+    try {
+      HuetAlgorithm.unify1(new MyParser(s1).getTerm(), new MyParser(s2).getTerm()).next match {
+        case Some(_) => "Unifiable."
+        case None => "Not unifiable."
+      }
+    }
+    catch {
+      case e : java.lang.RuntimeException => "RuntimeException (probably parsing error; check input syntax)."
+      case e : java.lang.StackOverflowError => "Stack overflow error (i.e. exhausted memory before finding a solution)."
+      case _ => "Unkown exception caugt."
     }
   }
+
   def test() = "scala test!"
 }
