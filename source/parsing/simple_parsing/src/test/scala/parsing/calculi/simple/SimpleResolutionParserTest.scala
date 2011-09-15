d06c9c341cdb51773242a76f711396ef659023e1
Martin Riener <martin.riener@googlemail.com>
2011-09-15 13:04:56 +0000

    reenabled tests for first order reolution parsing
    git-svn-id: http://gapt.googlecode.com/svn/branches/sequence_occurrence@811 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/parsing/simple_parsing/src/test/scala/parsing/calculi/simple/SimpleResolutionParserTest.scala
+++ source/parsing/simple_parsing/src/test/scala/parsing/calculi/simple/SimpleResolutionParserTest.scala
@@ -22,21 +22,28 @@ import at.logic.language.lambda.types._
 import at.logic.language.lambda.symbols.ImplicitConverters._
 import at.logic.parsing.readers.StringReader
 import at.logic.calculi.resolution.base._
-//import at.logic.calculi.resolution.robinson._
-/*
+import at.logic.calculi.resolution.robinson._
+import scala.collection.immutable.{Seq, List}
+
 class SimpleResolutionParserTest extends SpecificationWithJUnit {
-  private class MyParser(input: String) extends StringReader(input) with SimpleResolutionParserHOL
+//  private class MyParser(input: String) extends StringReader(input) with SimpleResolutionParserHOL
   private class MyParser2(input: String) extends StringReader(input) with SimpleResolutionParserFOL
 
   val pa = Atom(ConstantStringSymbol("p"),Var(ConstantStringSymbol("a"), i, hol)::Nil)
   val pfx = Atom(ConstantStringSymbol("p"),Function(ConstantStringSymbol("f"), Var(VariableStringSymbol("x"), i, hol)::Nil,i)::Nil)
   val px = Atom(ConstantStringSymbol("p"),Var(VariableStringSymbol("x"), i, hol)::Nil)
   val pffa = Atom(ConstantStringSymbol("p"),Function(ConstantStringSymbol("f"),Function(ConstantStringSymbol("f"), Var(ConstantStringSymbol("a"), i, hol)::Nil,i)::Nil, i)::Nil)
+
   val pa_fol = FOLAtom(ConstantStringSymbol("P"),FOLConst(ConstantStringSymbol("a"))::Nil)
   val pfx_fol = FOLAtom(ConstantStringSymbol("P"),FOLFunction(ConstantStringSymbol("f"), FOLVar(VariableStringSymbol("x"))::Nil)::Nil)
   val px_fol = FOLAtom(ConstantStringSymbol("P"),FOLVar(VariableStringSymbol("x"))::Nil)
   val pffa_fol = FOLAtom(ConstantStringSymbol("P"),FOLFunction(ConstantStringSymbol("f"),FOLFunction(ConstantStringSymbol("f"), FOLConst(ConstantStringSymbol("a"))::Nil)::Nil)::Nil)
+
+  def clause_to_lists(cl : Clause) : (Seq[Formula], Seq[Formula]) = (cl.negative map (_.formula), cl.positive map (_.formula))
+
+
   "SimpleResolutionParser" should {
+    /*
     "return an empty clause when given ." in {
       (new MyParser(".").getClauseList) must beEqual (Clause(Nil,Nil)::Nil)
     }
@@ -58,22 +65,32 @@ class SimpleResolutionParserTest extends SpecificationWithJUnit {
     "return the correct clauses for p(a). -p(x) | p(f(x)). -p(f(f(a)))." in {
       (new MyParser("p(a:i). -p(x:i) | p(f(x:i):i). -p(f(f(a:i):i):i).").getClauseList) must beEqual (Clause(Nil,pa::Nil)::Clause(px::Nil,pfx::Nil)::Clause(pffa::Nil,Nil)::Nil)
     }
+    */
+    "return an empty clause when given ." in {
+      (new MyParser2(".").getClauseList).map(clause_to_lists) must beEqual (Seq((Nil,Nil)))
+    }
+    "return an empty list when given nothing" in {
+      (new MyParser2("").getClauseList) must beEqual (Nil)
+    }
     "return the correct clause of -p(x). fol" in {
-      (new MyParser2("-P(x).").getClauseList) must beEqual (Clause(px_fol::Nil,Nil)::Nil)
+      (new MyParser2("-P(x).").getClauseList).map(clause_to_lists) must beEqual (Seq((px_fol::Nil,Nil)))
     }
     "return the correct clauses for p(a). -p(x) | p(f(x)). -p(f(f(a))). in fol" in {
-      (new MyParser2("P(a). -P(x) | P(f(x)). -P(f(f(a))).").getClauseList) must beEqual (Clause(Nil,pa_fol::Nil)::Clause(px_fol::Nil,pfx_fol::Nil)::Clause(pffa_fol::Nil,Nil)::Nil)
+      (new MyParser2("P(a). -P(x) | P(f(x)). -P(f(f(a))).").getClauseList).map(clause_to_lists) must beEqual (Seq( (Nil,pa_fol::Nil), (px_fol::Nil,pfx_fol::Nil), (pffa_fol::Nil,Nil)))
     }
+    /*
     "return the correct clause for p(x) | -p(x) in hol" in {
       (new MyParser("p(x:i) | -p(x:i).").getClauseList) must beEqual (Clause(px::Nil,px::Nil)::Nil)
-    }
+    }*/
     "return the correct clause for p(x) | -p(x) in fol" in {
-      (new MyParser2("P(x) | -P(x).").getClauseList) must beEqual (Clause(px_fol::Nil,px_fol::Nil)::Nil)
+      (new MyParser2("P(x) | -P(x).").getClauseList).map(clause_to_lists) must beEqual (Seq((px_fol::Nil,px_fol::Nil)))
     }
+
+    /*
     "manage to parse a complex term" in {
       val str = """p(x_10:i,m(x_3:i,m(m(p(x_4:i,one:i):i,p(x_5:i,one:i):i):i):i):i):i"""
       (new MyParser("e("+str+","+str+").").getClauseList) mustNot throwA [Exception]
-    }
+    }*/
   }
 }
-*/
+
