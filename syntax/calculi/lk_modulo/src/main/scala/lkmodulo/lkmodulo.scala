ac8a99e4003bfa237c78a62d39d43725f1dba970
Martin.Riener@googlemail.com <Martin.Riener@googlemail.com@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2011-10-03 15:47:02 +0000

    refitted lkmodulo equations to work with fol equations
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@866 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- syntax/calculi/lk_modulo/src/main/scala/lkmodulo/lkmodulo.scala
+++ syntax/calculi/lk_modulo/src/main/scala/lkmodulo/lkmodulo.scala
@@ -1,8 +1,8 @@
 package at.logic.calculi.lkmodulo
 
+import _root_.at.logic.calculi.lkmodulo.Equation
 import _root_.at.logic.calculi.proofs.UnaryRuleTypeA
-import _root_.at.logic.language.fol._
-import _root_.at.logic.language.fol.equations.Equation
+import _root_.at.logic.language.fol.{Function, FOLConst, Imp, Atom, Neg, And, Or, ExVar, AllVar, FOLVar, getFreeVariablesFOL, FOLTerm, FOLFormula, Equation => FOLEquation}
 import _root_.at.logic.language.hol.logicSymbols.{ConstantStringSymbol, ConstantSymbolA}
 import _root_.at.logic.language.hol.{Formula, HOLFormula, HOLExpression}
 import _root_.at.logic.language.lambda.substitutions.Substitution
@@ -16,12 +16,33 @@ import collection.immutable.{HashMap, HashSet}
 import collection.mutable.StringBuilder
 import math.Ordering.String
 import at.logic.parsing.language.simple.SimpleFOLParser
+import types.Equation
 
 trait LKModuloRule
 
 case object ConversionLeftRuleType extends UnaryRuleTypeA
 case object ConversionRightRuleType extends UnaryRuleTypeA
 
+
+package types {
+class Equation(val left: FOLTerm, val right : FOLTerm) {
+  def toFormula() = FOLEquation(left, right)
+}
+}
+
+object Equation {
+  def apply(left : FOLTerm , right : FOLTerm ) = new Equation(left,  right)
+
+  /*
+  def unapply(f : FOLFormula) = {
+    f match {
+      case FOLEquation(left, right) => Some(Equation(left, right))
+      case _ => None
+    }
+  }*/
+
+  implicit def equation2formula(e : Equation) = e.toFormula()
+}
 
 abstract class REequalityA {
   def equational_rules() : Set[Equation];
@@ -30,157 +51,6 @@ abstract class REequalityA {
   def reequal_to(s : FOLFormula, t : FOLFormula) : Boolean;
 }
 
-object FOLUtils {
-  def universal_closure_of(f : FOLFormula) : FOLFormula = {
-    val free_vars = getFreeVariablesFOL(f)
-    free_vars.foldRight(f)((v : FOLVar, f : FOLFormula) => AllVar(v,f))
-  }
-
-  // this is nearly the same as LambdaExpression.getFreeAndBoundVariables, but returns the variabes in the order encountered
-  def freevars_boundvars_constants_of(expression: LambdaExpression):Tuple3[List[Var],List[Var],List[Var]] = expression match {
-    case v: Var if v.isFree && v.name.isInstanceOf[VariableSymbolA]=> (List(v), List(), List())
-    case v: Var if v.name.isInstanceOf[VariableSymbolA] => (List(), List(v), List())
-    case v: Var if v.name.isInstanceOf[ConstantSymbolA] => (List(), List(), List(v))
-    case v: Var => (List(), List(), List())// not variables (constants in this case)
-    case App(exp, arg) => {
-      val mFBV = freevars_boundvars_constants_of(exp)
-      val nFBV = freevars_boundvars_constants_of(arg)
-      ((mFBV._1 ::: nFBV._1).distinct, (mFBV._2 ::: nFBV._2).distinct, (mFBV._3 ::: nFBV._3).distinct)
-    }
-    case AbsInScope(v, exp) => {
-      val mFBV = freevars_boundvars_constants_of(exp)
-      val bound = removeDoubles(mFBV._2 ::: List(v))
-      (mFBV._1, bound, mFBV._3)
-    }
-  }
-
-  def isfunctiontype(exptype : TA) : Boolean = {
-    exptype match {
-      case Ti() => true
-      case To() => false
-      case Ti() -> t2 => (isfunctiontype(t2))
-      case _ => false
-    }
-  }
-
-  def ispredicatetype(exptype : TA) : Boolean = {
-    exptype match {
-      case Ti() => false
-      case To() => true
-      case Ti() -> t2 => (ispredicatetype(t2))
-      case _ => false
-    }
-  }
-
-  def replaceLeftmostBoundOccurenceOf(variable : FOLVar, by : FOLVar, formula : FOLFormula) :
-   (Boolean, FOLFormula) = {
-    //println("replacing "+variable+" by "+by+" in "+formula)
-    formula match {
-      case Atom(_, _) => (false, formula)
-
-      case Neg(f) =>
-        val r = replaceLeftmostBoundOccurenceOf(variable, by, f )
-        (r._1, Neg(r._2))
-
-      case And(f1,f2) =>
-        val r1 = replaceLeftmostBoundOccurenceOf(variable, by, f1)
-        if (r1._1 == true)
-          (true, And(r1._2, f2))
-        else {
-          val r2 = replaceLeftmostBoundOccurenceOf(variable, by, f2)
-          (r2._1, And(f1, r2._2))
-        }
-
-      case Or(f1,f2) =>
-        val r1 = replaceLeftmostBoundOccurenceOf(variable, by, f1)
-        if (r1._1 == true)
-          (true, Or(r1._2, f2))
-        else {
-          val r2 = replaceLeftmostBoundOccurenceOf(variable, by, f2)
-          (r2._1, Or(f1, r2._2))
-        }
-
-      case ExVar(v, f)  =>
-          val r = replaceLeftmostBoundOccurenceOf(variable, by, f)
-          (r._1, ExVar(v, r._2))
-
-      case AllVar(v, f)  =>
-        if ((v =^ variable) && (v != variable)) {
-          println("Warning: comparing two variables, which have the same sytactic representatio but differ on other things (probably different binding context)")
-        }
-
-        if (v == variable) {
-          (true, AllVar(by, Substitution[LambdaExpression](variable, by).apply(f).asInstanceOf[FOLFormula]))
-        }
-        else {
-          val r = replaceLeftmostBoundOccurenceOf(variable, by, f)
-          (r._1, AllVar(v, r._2))
-        }
-
-       case _ => throw new Exception("Unknown operator encountered during renaming of outermost bound variable. Formula is: "+formula)
-
-    }
-  }
-
-
-  def replaceFreeOccurenceOf(variable : FOLVar, by : FOLVar, formula : FOLFormula) : FOLFormula = {
-    formula match {
-      case Atom(_, args) => Substitution[LambdaExpression](variable, by).apply(formula).asInstanceOf[FOLFormula]
-
-      case Neg(f) =>
-        Neg(replaceFreeOccurenceOf(variable, by, f ))
-
-      case And(f1,f2) =>
-        val r1 = replaceFreeOccurenceOf(variable, by, f1)
-        val r2 = replaceFreeOccurenceOf(variable, by, f2)
-        And(r1,r2)
-
-      case Or(f1,f2) =>
-        val r1 = replaceFreeOccurenceOf(variable, by, f1)
-        val r2 = replaceFreeOccurenceOf(variable, by, f2)
-        Or(r1,r2)
-
-      case ExVar(v, f)  =>
-        if (v.syntaxEquals(variable))
-          formula
-        else
-          ExVar(v, replaceFreeOccurenceOf(variable, by, f))
-
-      case AllVar(v, f)  =>
-        if (v.syntaxEquals(variable))
-          formula
-        else
-          AllVar(v, replaceFreeOccurenceOf(variable, by, f))
-
-       case _ => throw new Exception("Unknown operator encountered during renaming of outermost bound variable. Formula is: "+formula)
-
-    }
-  }
-
-  def removeDoubles[T](l : List[T]) : List[T] = {
-    removeDoubles_(l.reverse).reverse
-  }
-
-  private def removeDoubles_[T](l : List[T]) : List[T] = {
-    l match {
-      case head :: tail =>
-        if (tail.contains(head))
-          removeDoubles(tail)
-        else
-          head :: removeDoubles(tail)
-      case Nil => Nil
-    }
-  }
-
-  def between(lower :Int, upper : Int) : List[Int] = {
-    if (lower > upper)
-      List()
-    else
-      lower :: between (lower+1, upper)
-  }
-
-
-}
 
 class TPTP(val axioms : Seq[FOLFormula], val conjectures : Seq[FOLFormula]) {
 
@@ -218,7 +88,7 @@ object TPTP {
 }
 
 object Renaming {
-  import at.logic.calculi.lkmodulo.FOLUtils._
+  import at.logic.language.fol.Utils._
 
 
   def fol_as_tptp(f: FOLFormula) = fol_as_tptp_(tptp_renaming_of(f).asInstanceOf[FOLFormula])
@@ -240,16 +110,16 @@ object Renaming {
   def createAlphanormalizationSymbol(v : Var, n : Int) = {
     if (v.name.isInstanceOf[ConstantSymbolA]) {
       //constants
-      if (ispredicatetype(v.exptype))
+      if (isPredicateType(v.exptype))
         new ConstantStringSymbol("P_"+n)
-      else if (isfunctiontype(v.exptype))
+      else if (isFunctionType(v.exptype))
         new ConstantStringSymbol("f_"+n)
       else new ConstantStringSymbol("s_"+n)
     } else {
       //everything else => variables
-      if (ispredicatetype(v.exptype))
+      if (isPredicateType(v.exptype))
         new VariableStringSymbol("X_"+n)
-      else if (isfunctiontype(v.exptype))
+      else if (isFunctionType(v.exptype))
         new VariableStringSymbol("x_"+n)
       else
         new VariableStringSymbol("t_"+n)
@@ -259,19 +129,19 @@ object Renaming {
   def createTPTPExportSymbol(v : Var, n : Int) = {
     if (v.name.isInstanceOf[ConstantSymbolA]) {
       //constants
-      if (ispredicatetype(v.exptype))
+      if (isPredicateType(v.exptype))
         //new ConstantStringSymbol("p_"+n)
         new ConstantStringSymbol("p_" + v.name)
-      else if (isfunctiontype(v.exptype))
+      else if (isFunctionType(v.exptype))
 //        new ConstantStringSymbol("f_"+n)
         new ConstantStringSymbol("f_" + v.name)
       else
         throw new Exception("in fol, we can only rename if a symbol is of function or predicate type")
     } else {
       //everything else => variables
-      if (ispredicatetype(v.exptype))
+      if (isPredicateType(v.exptype))
         throw new Exception("in fol, we should not have predicate variables")
-      else if (isfunctiontype(v.exptype))
+      else if (isFunctionType(v.exptype))
         new VariableStringSymbol("X_"+n)
       else
         throw new Exception("in fol, we can only rename if a symbol is of function or predicate type")
@@ -281,7 +151,7 @@ object Renaming {
   def define_normalization_function(normalization_fun : ((Var, Int) => SymbolA), f : LambdaExpression) = {
     val freeandbound = freevars_boundvars_constants_of(f)
 
-    val allvars = freeandbound._1 ++ freeandbound._2 ++ freeandbound._3.filter((v:Var) => ispredicatetype(v.exptype) || isfunctiontype(v.exptype) )
+    val allvars = freeandbound._1 ++ freeandbound._2 ++ freeandbound._3.filter((v:Var) => isFirstOrderType(v.exptype) )
     //TODO: this is only applicable to variablestringsymbol and constantstringsymbol
 
 
