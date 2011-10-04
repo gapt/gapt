40a1a3b2bce93532889512b8ac12548eb85606c8
Martin Riener <martin.riener@googlemail.com>
2011-10-04 16:15:36 +0000

    forgot to add the fol utils file
    git-svn-id: http://gapt.googlecode.com/svn/trunk@867 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ source/syntax/language/fol/src/main/scala/language/fol/utils.scala
@@ -0,0 +1,19 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: marty
+ * Date: 10/3/11
+ * Time: 5:16 PM
+ */
+
+package at.logic.language.fol
+
+import _root_.at.logic.language.hol.logicSymbols.ConstantSymbolA
+import _root_.at.logic.language.lambda.substitutions.Substitution
+import _root_.at.logic.language.lambda.symbols.VariableSymbolA
+import _root_.at.logic.language.lambda.typedLambdaCalculus.{AbsInScope, App, Var, LambdaExpression}
+import _root_.at.logic.language.lambda.types.->
+import _root_.at.logic.language.lambda.types.{To, Ti, TA}
+
+
+
+}
