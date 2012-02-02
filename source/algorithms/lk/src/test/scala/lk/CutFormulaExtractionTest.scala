9191fed0d89157828023da1ebbe8e84592faf688
Martin Riener <martin.riener@googlemail.com>
2012-02-02 16:27:06 +0000

    forgot to add the tests
    git-svn-id: http://gapt.googlecode.com/svn/trunk@969 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ source/algorithms/lk/src/test/scala/lk/CutFormulaExtractionTest.scala
@@ -0,0 +1,49 @@
+package at.logic.algorithms.lk
+
+import at.logic.calculi.lk.base.{BinaryLKProof, LKProof, Sequent}
+import at.logic.calculi.occurrences.FormulaOccurrence
+import org.specs._
+
+import at.logic.language.hol._
+import at.logic.language.lambda.symbols.ImplicitConverters._
+import at.logic.calculi.lk.propositionalRules._
+import at.logic.language.lambda.types.Definitions._
+import at.logic.language.hol.logicSymbols._
+import at.logic.language.lambda.typedLambdaCalculus._
+import at.logic.language.lambda.substitutions.Substitution
+import at.logic.calculi.lk.base.types._
+
+class CutFormulaExtractionTest extends SpecificationWithJUnit {
+  "Substitutions" should {
+    val x = HOLVar( "x", i )
+    val px = Atom( "P", List(x) )
+    val ax1 = Axiom( List(px), List(px) )
+    val ax2 = Axiom( List(px), List(px) )
+    val proof = CutRule( ax1, ax2, ax1.root.succedent.toList.head, ax2.root.antecedent.toList.head )
+
+    val ax1_ = Axiom( List(px), List(px) )
+    val ax2_ = Axiom( List(px), List(px) )
+    val proof_ = CutRule( ax1_, ax2_, ax1_.root.succedent.toList.head, ax2_.root.antecedent.toList.head )
+
+    def toSequent(aux:FormulaOccurrence) = Sequent(Nil, List(aux))
+
+    "apply correctly to a proof with one cut" in {
+      val cutproofs = cutformulaExtraction.getCutsAsProofs(proof)
+      val cutformulas = cutformulaExtraction(proof)
+      println(cutformulas)
+      cutproofs must beEqual (List(proof))
+    }
+
+    "apply correctly to a proof with three cuts" in {
+      val proof2 = CutRule(proof, proof_ , proof.root.succedent.head,  proof_.root.antecedent.head )
+      val prooflist : List[BinaryLKProof] = List(proof, proof_, proof2)
+
+      val cutproofs = cutformulaExtraction.getCutsAsProofs(proof2)
+      val cutformulas = cutformulaExtraction(proof2)
+
+      println(cutformulas)
+      cutproofs must beEqual (prooflist)
+    }
+
+  }
+}
