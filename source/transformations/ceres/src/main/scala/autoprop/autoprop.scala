4d04ffe96a7e8b7744e9184c6d40cea702cdaf42
Cvetan Dunchev <cdunchev@gmail.com>
2012-03-12 13:50:49 +0000

    object NNF has been moved from here to a separate file in transformations.
    git-svn-id: http://gapt.googlecode.com/svn/trunk@1035 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/transformations/ceres/src/main/scala/autoprop/autoprop.scala
+++ source/transformations/ceres/src/main/scala/autoprop/autoprop.scala
@@ -625,27 +625,6 @@ object rulesNumber {
   }
 }
 
-object NNF {
-  def apply(f: HOLFormula) : HOLFormula = {
-    println("\nf = "+printSchemaProof.formulaToString (f))
-    f match {
-      case IndexedPredicate(f1,_) => f
-      case Imp(f1, f2) => apply(Or(Neg(f1), f2))
-      case Neg(f1) => f1 match {
-        case IndexedPredicate(f2,_) => f
-        case Neg(f2) => apply(f2)
-        case And(a,b) => Or(apply(Neg(a)).asInstanceOf[SchemaFormula], apply(Neg(b)).asInstanceOf[SchemaFormula])
-        case Or(a,b) => And(apply(Neg(a)).asInstanceOf[SchemaFormula], apply(Neg(b)).asInstanceOf[SchemaFormula])
-        case Imp(a, b) => apply(And(a, Neg(b)))
-        case _ => throw new Exception("Error in Neg in NNF")
-      }
-      case And(f1, f2) => And(apply(f1).asInstanceOf[SchemaFormula], apply(f2).asInstanceOf[SchemaFormula])
-      case Or(f1, f2) => Or(apply(f1).asInstanceOf[SchemaFormula], apply(f2).asInstanceOf[SchemaFormula])
-      case _ => throw new Exception("Missing case in autoprop.scala, object NNF")
-    }
-  }
-}
-
 object test {
   def apply(): FSequent = {
     val k = IntVar(new VariableStringSymbol("k"))
