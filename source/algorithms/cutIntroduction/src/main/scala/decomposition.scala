6342b2f1f4fbbc7d58a0d4daa1f232689939710c
Giselle Reis <giselle.mnr@gmail.com>
2012-03-16 16:18:49 +0000

    Fixed some bugs in decomposition and added the test file.
    git-svn-id: http://gapt.googlecode.com/svn/trunk@1038 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/algorithms/cutIntroduction/src/main/scala/decomposition.scala
+++ source/algorithms/cutIntroduction/src/main/scala/decomposition.scala
@@ -4,11 +4,11 @@
  *
  */
 
+package at.logic.algorithms.cutIntroduction
+
 import at.logic.language.lambda.symbols._
 import at.logic.language.fol._
 import scala.collection.mutable._
-
-package cutIntroduction {
 
 class DecompositionException(msg: String) extends Exception(msg)
 
@@ -22,14 +22,13 @@ object decomposition {
 //  }
 
 //  private def computeDeltaTable(terms: List[FOLTerm]) = {
-    // TODO: check if the terms are well formed first
     // This HashMap is mutable because I will update it frequently and do not want a new one returned...
 //    val deltaTbl = new HashMap[List[FOLTerm], List[FOLTerm, List[FOLTerm]]]
     // Adds ti : (alpha, ti) to the table
 //    terms.foreach(t => deltaTbl + (t -> ((FOLVar("alpha", i), t::Nil)::Nil)))
 //  }
 
-  private def delta(terms: List[FOLTerm]) : (FOLTerm, List[FOLTerm]) = terms.head match {
+  def delta(terms: List[FOLTerm]) : (FOLTerm, List[FOLTerm]) = terms.head match {
     // If the variables are reached
     case FOLVar(s) =>
       // If all variables are equal
@@ -41,8 +40,7 @@ object decomposition {
     case Function(h, args) =>
       // If all heads are the same
       if ( terms.forall(t => t match {
-        // Here's to hoping that it will match this h with the h above
-        case Function(h, _) => true
+        case Function(h1, _) if h1 == h => true
         case _ => false
       }) ) {
         // call delta recursively for every argument of every term
@@ -53,24 +51,23 @@ object decomposition {
             case _ => throw new DecompositionException("Mal-formed terms list.")
           })
 
-        // These two functions are used to "revert" the list of lists computed above.
-        // I still think that there must be a better way of doing it.
-        def f(args: List[List[FOLTerm]]) : List[FOLTerm] = args.head match {
+        // The list above is a list of lists of arguments. Assume that each list
+        // of arguments has elements from 1 to n. A function should be called
+        // for a list containing all elements in position i in every list. In
+        // order to do this, this function will invert this list of lists. If
+        // the list of lists was implemented with a matrix, all I had to do
+        // would be to call the function on the columns of the matrix, but since
+        // this is not the case I implemented this inverse function. 
+        def inverse(args: List[List[FOLTerm]]) : List[List[FOLTerm]] = args match {
           case Nil => Nil
-          case hd :: tl => hd :: f(args.tail)
-        }
-        def g(args: List[List[FOLTerm]]) : List[List[FOLTerm]] = args match {
-          case Nil => Nil
+          case (Nil) :: tl => Nil
           case hd :: tl => 
-            val a = f(args)
-            val tails = args.foldRight(List[List[FOLTerm]]()) ((lst, acc) => lst match {
-              case hd :: tl => tl :: acc
-              case Nil => acc
-            })
-            a::g(tails)             
+            val heads = args.foldRight(List[FOLTerm]()) ( (lst, acc) => lst.head :: acc )
+            val tails = args.foldRight(List[List[FOLTerm]]()) ( (lst, acc) => lst.tail :: acc )
+            heads::inverse(tails)             
         }
 
-        val listOfArgs = g(allargs)
+        val listOfArgs = inverse(allargs)
         val deltaOfArgs = listOfArgs.foldRight(List[(FOLTerm, List[FOLTerm])]()) ((a, acc) => delta(a) :: acc)
        
         // A delta vector can be constructed only if the lists returned from the arguments are all the same
@@ -81,27 +78,33 @@ object decomposition {
           case t => t :: acc
         })
 
-        // This must be implemented somewhere else... But it's faster now if I do it here. Please let me know where it is so I can substitute this.
+        // There must be a better way...
         def listEquals(lst1: List[FOLTerm], lst2: List[FOLTerm]) : Boolean = (lst1, lst2) match {
-          case (Nil, Nil) => false
+          case (Nil, Nil) => true
           case (hd1::tl1, hd2::tl2) => (hd1 =^ hd2) && listEquals(tl1, tl2)
           case (_, _) => false
         }
 
-        // Check if they are the same
-        val first = nonempty.head
-        if (nonempty.forall(l => listEquals(l, first))) {
-          // All terms are the same
- 
+        // If all the sets are empty
+        if (nonempty.length == 0) {
           val newargs = deltaOfArgs.foldRight(List[FOLTerm]()) ((x, acc) => x._1 :: acc)
           val u = Function(h, newargs)
-          (u, first)
+          (u, Nil) 
         }
-        // The terms returned from the arguments are different
         else {
-          return (FOLVar(new VariableStringSymbol("alpha")), terms)
+          // Check if they are the same
+          val first = nonempty.head
+          if (nonempty.forall(l => listEquals(l, first))) {
+            // All terms are the same
+            val newargs = deltaOfArgs.foldRight(List[FOLTerm]()) ((x, acc) => x._1 :: acc)
+            val u = Function(h, newargs)
+            (u, first)
+          }
+          // The terms returned from the arguments are different
+          else {
+            return (FOLVar(new VariableStringSymbol("alpha")), terms)
+          }
         }
-
       }
       // If head terms are different
       else { return (FOLVar(new VariableStringSymbol("alpha")), terms) }
@@ -109,4 +112,3 @@ object decomposition {
   
 }
 
-}
