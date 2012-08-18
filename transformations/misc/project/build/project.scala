858ec3fe10617be74a10a87d02a6c44491d8de9a
shaolintl@gmail.com <shaolintl@gmail.com@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2012-08-18 20:38:43 +0000

    added sbt support for two nested module which makes compilation and testing much faster (due to incremental compilation) in order to use it one must install sbt and execute sbt from the appropriate module root directory.
    Some notes:
    - I am using an old sbt jar file (0.7.7) as I could not make the newest one (0.12.0) use the maven repository)
    - the target directories for classes and test-classes are the same as for maven
    - I did not find how to make a maven timestamp in the target folder so maven will recompile everything when creating the jar
    - the current workflow is therefore:
    - sbt is now installed per module and therefore cannot be called from a super module
    -- compile and test using sbt
    -- call maven install after finishing which will recompile everything
    
    
    
    
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@1141 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ transformations/misc/project/build/project.scala
@@ -0,0 +1,8 @@
+import sbt._
+
+class BuildProject(info: ProjectInfo) extends DefaultProject(info) {
+
+val mavenLocal = "Local Maven Repository" at
+"file://"+Path.userHome+"/.m2/repository"
+override def outputPath = "target"
+}
