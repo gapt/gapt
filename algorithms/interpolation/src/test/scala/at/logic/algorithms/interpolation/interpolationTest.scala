ab68c1d921990f6daa3f7eae80f201308095481e
stefan.hetzl@gmail.com <stefan.hetzl@gmail.com@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2012-09-19 13:00:03 +0000

    completed interpolation algorithm.
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@1216 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- algorithms/interpolation/src/test/scala/at/logic/algorithms/interpolation/interpolationTest.scala
+++ algorithms/interpolation/src/test/scala/at/logic/algorithms/interpolation/interpolationTest.scala
@@ -8,8 +8,8 @@ import org.specs2.mutable._
 import at.logic.language.hol._
 import at.logic.language.hol.logicSymbols._
 import at.logic.calculi.occurrences._
+import at.logic.calculi.lk.base._
 import at.logic.calculi.lk.propositionalRules._
-
 
 @RunWith(classOf[JUnitRunner])
 class interpolationTest extends SpecificationWithJUnit {
@@ -21,6 +21,7 @@ class interpolationTest extends SpecificationWithJUnit {
       val npart = Set[FormulaOccurrence]()
       val ppart = Set( ax.root.antecedent(0), ax.root.succedent(0) )
       val ( nproof, pproof, ipl ) = Interpolate( ax, npart, ppart )
+
       ipl must beEqualTo( TopC )
     }
 
@@ -30,8 +31,10 @@ class interpolationTest extends SpecificationWithJUnit {
       val npart = Set( ax.root.antecedent(0), ax.root.succedent(0) )
       val ppart = Set[FormulaOccurrence]()
       val ( nproof, pproof, ipl ) = Interpolate( ax, npart, ppart )
+
       ipl must beEqualTo( BottomC )
-      // TODO: nproof.root must beEqualTo p |- p, bot as sequent (not with focc)
+      nproof.root.toFSequent must beEqualTo ( FSequent( p::Nil, p::BottomC::Nil ) )
+      pproof.root.toFSequent must beEqualTo ( FSequent( BottomC::Nil, Nil ) )
     }
 
     "correctly interpolate a single unary inference with not p" in {
@@ -42,7 +45,10 @@ class interpolationTest extends SpecificationWithJUnit {
       val npart = Set( pr.root.succedent( 0 ) )
       val ppart = Set( pr.root.antecedent( 0 ) )
       val ( nproof, pproof, ipl ) = Interpolate( pr, npart, ppart )
+
       ipl must beEqualTo( Neg( p ) )
+      nproof.root.toFSequent must beEqualTo ( FSequent( Nil, Neg( p )::Or( p, q )::Nil ) )
+      pproof.root.toFSequent must beEqualTo ( FSequent( p::Neg( p )::Nil, Nil ) )
     }
 
     "correctly interpolate a single binary inference with bot or q" in {
@@ -54,9 +60,34 @@ class interpolationTest extends SpecificationWithJUnit {
       val npart = Set( pr.root.antecedent( 0 ), pr.root.succedent( 0 ) )
       val ppart = Set( pr.root.succedent( 1 ) )
       val ( nproof, pproof, ipl ) = Interpolate( pr, npart, ppart )
+
       ipl must beEqualTo( Or( BottomC, q ) )
+      nproof.root.toFSequent must beEqualTo ( FSequent( Or( p, q )::Nil, p::Or( BottomC, q )::Nil ) )
+      pproof.root.toFSequent must beEqualTo ( FSequent( Or( BottomC, q )::Nil, q::Nil ) )
     }
+
+    "correctly interpolate a small proof of 4 inference rules" in {
+      val p = HOLConstFormula(new ConstantStringSymbol("p"))
+      val q = HOLConstFormula(new ConstantStringSymbol("q"))
+      val r = HOLConstFormula(new ConstantStringSymbol("r"))
+
+      val axp = Axiom( p::Nil, p::Nil )
+      val axq = Axiom( q::Nil, q::Nil )
+      val axr = Axiom( r::Nil, r::Nil )
+      val p1 = NegLeftRule( axq, q )
+      val p2 = OrLeftRule( p1, axr, q, r )
+      val p3 = ImpRightRule( p2, Neg( q ), r )
+      val p4 = ImpLeftRule( axp, p3, p, Or( q, r ) )
+
+      val npart = Set( p4.root.antecedent( 0 ), p4.root.antecedent( 1 ) )
+      val ppart = Set( p4.root.succedent( 0 ) )
+      val ( nproof, pproof, ipl ) = Interpolate( p4, npart, ppart )
+
+      ipl must beEqualTo( Or( BottomC, Or( q, r ) ) )
+      nproof.root.toFSequent must beEqualTo ( FSequent( p::Imp( p, Or( q, r ))::Nil, Or( BottomC, Or( q, r ) )::Nil ) )
+      pproof.root.toFSequent must beEqualTo ( FSequent( Or( BottomC, Or( q, r ) )::Nil, Imp( Neg( q ), r )::Nil ) )
+    }
+
 
   }
 }
-
