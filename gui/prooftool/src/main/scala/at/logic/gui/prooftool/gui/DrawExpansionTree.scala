9c6abef32a6955c692552464715468f9a1f2bd63
stefan.hetzl@gmail.com <stefan.hetzl@gmail.com@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2012-09-22 10:05:24 +0000

    spelling
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@1223 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- gui/prooftool/src/main/scala/at/logic/gui/prooftool/gui/DrawExpansionTree.scala
+++ gui/prooftool/src/main/scala/at/logic/gui/prooftool/gui/DrawExpansionTree.scala
@@ -22,8 +22,8 @@ class DrawExpansionTree(val expansionTree: Sequent, private val fSize: Int) exte
   //private val height = toolkit.getScreenSize.height - 150
   preferredSize = calculateOptimalSize
   dividerLocation = preferredSize.width / 2
-  leftComponent = new SplitedExpansionTree(expansionTree.antecedent, "Antecedents", ft)
-  rightComponent = new SplitedExpansionTree(expansionTree.succedent, "Consequents", ft)
+  leftComponent = new SplitedExpansionTree(expansionTree.antecedent, "Antecedent", ft)
+  rightComponent = new SplitedExpansionTree(expansionTree.succedent, "Consequent", ft)
 
   def calculateOptimalSize = {
     val width = Main.top.size.width
