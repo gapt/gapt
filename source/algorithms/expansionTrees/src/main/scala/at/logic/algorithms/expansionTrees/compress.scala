572b7e0a3ec5d54211f3c8da5023ae5ca692d4f5
Tomer Libal <shaolintl@gmail.com>
2012-10-31 22:47:02 +0000

    1) created MultiExpansionTree which is the same as ExpansionTree but allow for vectors of selected/expanded terms 2) created an algorithm which compresses a tree into a multiTree for sequential quantifiers of the same type 3) added the relevant method to cli
    git-svn-id: http://gapt.googlecode.com/svn/trunk@1280 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ source/algorithms/expansionTrees/src/main/scala/at/logic/algorithms/expansionTrees/compress.scala
@@ -0,0 +1,28 @@
+package at.logic.algorithms.expansionTrees
+
+import at.logic.calculi.expansionTrees._
+import at.logic.calculi.expansionTrees.multi.{WeakQuantifier => mWeakQuantifier, StrongQuantifier => mStrongQuantifier, And => mAnd, Or =>
+mOr, Imp => mImp, Not => mNot, Atom => mAtom, MultiExpansionTree}
+import at.logic.language.hol.{HOLExpression, HOLVar}
+
+object compressQuantifiers {
+  def apply(tree: ExpansionTree): MultiExpansionTree = tree match {
+    case Atom(f) => mAtom(f)
+    case Not(t1) => mNot(compressQuantifiers(t1))
+    case And(t1,t2) => mAnd(compressQuantifiers(t1), compressQuantifiers(t2))
+    case Or(t1,t2) => mOr(compressQuantifiers(t1), compressQuantifiers(t2))
+    case Imp(t1,t2) => mImp(compressQuantifiers(t1), compressQuantifiers(t2))
+    case WeakQuantifier(f,is) => mWeakQuantifier(f, is.flatMap(x => compressWeak(compressQuantifiers(x._1),x._2)))
+    case StrongQuantifier(f,v,t) => {val (sel, vars) = compressStrong(compressQuantifiers(t),v); mStrongQuantifier(f, vars,sel)}
+  }
+
+  private def compressStrong(tree: MultiExpansionTree, v: HOLVar): Tuple2[MultiExpansionTree, Seq[HOLVar]] = tree match {
+    case mStrongQuantifier(_, vars, sel) => (sel, vars.+:(v))
+    case _ => (tree, List(v))
+  }
+
+  private def compressWeak(tree: MultiExpansionTree, e: HOLExpression): Seq[Tuple2[MultiExpansionTree, Seq[HOLExpression]]] = tree match {
+    case mWeakQuantifier(_, is) => is.map(x => (x._1, x._2.+:(e)))
+    case _ => List((tree, List(e)))
+  }
+}
