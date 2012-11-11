227cd3062f7b9ebf047503f343b626771f35e858
cdunchev@gmail.com <cdunchev@gmail.com@85d21ef8-add0-11de-ae77-5bfb9c5a3f34>
2012-11-11 20:28:32 +0000

    During the last project meeting Alex proposed to replace the cl^{(proof, \Omega)} symbols in the displaying with symbols which do not contain the useless for the user information such as proof name and omega-configuration. Now they are replaced with unique HOLConsts of type \omega -> o
    git-svn-id: http://gapt.googlecode.com/svn/trunk/source@1296 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- transformations/ceres/src/test/scala/at/logic/transformations/ceres/clauseSets/ClauseSetsTest.scala
+++ transformations/ceres/src/test/scala/at/logic/transformations/ceres/clauseSets/ClauseSetsTest.scala
@@ -14,10 +14,15 @@ import java.io.File.separator
 import at.logic.language.hol._
 import at.logic.language.lambda.symbols.ImplicitConverters._
 import at.logic.calculi.occurrences._
-import at.logic.calculi.lk.base.Sequent
 import at.logic.transformations.ceres.struct._
 import java.io.{FileInputStream, InputStreamReader}
 import at.logic.algorithms.shlk._
+import at.logic.calculi.slk.SchemaProofDB
+import at.logic.algorithms.lk.{getAncestors, getCutAncestors}
+import at.logic.language.lambda.symbols.VariableStringSymbol
+import at.logic.calculi.lk.base.Sequent
+import at.logic.language.schema.IntVar
+import at.logic.transformations.ceres.projections.{DeleteTautology, DeleteRedundantSequents}
 
 @RunWith(classOf[JUnitRunner])
 class ClauseSetsTest extends SpecificationWithJUnit {
@@ -39,13 +44,41 @@ class ClauseSetsTest extends SpecificationWithJUnit {
 //                                 seq.multisetEquals( Sequent( Nil, b::d::Nil ) ) ) )
       ok
     }
-    "test the schematic struct" in {
+
+    "test the schematic struct in journal_example.slk" in {
+      println(Console.GREEN+"\n\n-------- schematic struct in journal_example.slk --------\n\n"+Console.RESET)
       val s = new InputStreamReader(new FileInputStream("target" + separator + "test-classes" + separator + "journal_example.lks"))
       val map = ParseQMON.parseProof(s)
-      println(Console.GREEN+"\n\n-------- schematic struct --------\n\n"+Console.RESET)
       val p = map.get("\\psi").get._2.get("root").get
       println(StructCreators.extract(p))
 //      val str =
+      println("\n\n\n\n")
+      ok
+    }
+
+
+    "test the schematic struct in sEXP.slk" in {
+      println(Console.BLUE+"\n\n---- schematic struct in sEXP.slk ----\n\n\n"+Console.RESET)
+      val s = new InputStreamReader(new FileInputStream("target" + separator + "test-classes" + separator + "sEXP.lks"))
+      SchemaProofDB.clear
+      val map = ParseQMON.parseProof(s)
+      val p1s = map.get("\\psi").get._2.get("root").get
+      val p2s = map.get("\\varphi").get._2.get("root").get
+      val p1b = map.get("\\psi").get._1.get("root").get
+      val p2b = map.get("\\varphi").get._1.get("root").get
+      println(StructCreators.extract(p1s, getCutAncestors(p1s)))
+      println("\n\n")
+      println(StructCreators.extract(p2s, getCutAncestors(p2s) ++ getAncestors(p2s.root.succedent.head)))
+      println("\n\n")
+      println(StructCreators.extract(p1b, Set.empty[FormulaOccurrence]))
+      println("\n\n")
+      println(StructCreators.extract(p2b, getAncestors(p2b.root.succedent.head)))
+      println("\n\n")
+      println("rename cl symbols to CL_i(k): ")
+      val n = IntVar(new VariableStringSymbol("n"))
+      val struct = StructCreators.extractStruct( "\\psi", n)
+      val cs : List[Sequent] = DeleteRedundantSequents( DeleteTautology( StandardClauseSet.transformStructToClauseSet(struct) ))
+      println("rename:\n"+renameCLsymbols(cs))
       println("\n\n\n\n")
       ok
     }
