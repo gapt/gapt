d0be1029d2475adac90acc949126a8ce4a69a199
Cvetan Dunchev <cdunchev@gmail.com>
2012-12-02 02:27:38 +0000

    A test for the ACNF for the CERESs method was added to the integration tests.
    git-svn-id: http://gapt.googlecode.com/svn/trunk@1339 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ source/integration_tests/simple_schema_test/src/test/scala/at/logic/simple_schema_test/acnfTest.scala
@@ -0,0 +1,45 @@
+package at.logic.simple_schema_test
+
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import org.specs2.mutable.SpecificationWithJUnit
+import at.logic.language.lambda.symbols.VariableStringSymbol
+import at.logic.calculi.occurrences.{FormulaOccurrence, defaultFormulaOccurrenceFactory}
+import java.io.{FileInputStream, InputStreamReader}
+import at.logic.calculi.slk.SchemaProofDB
+import java.io.File.separator
+import scala.io._
+import at.logic.language.lambda.typedLambdaCalculus.{LambdaExpression, Var}
+import at.logic.language.hol.HOLAbs._
+import at.logic.language.hol.HOLVar._
+import at.logic.language.lambda.types.Ti._
+import at.logic.language.lambda.types.Ti
+import at.logic.language.schema.sTerm._
+import at.logic.algorithms.shlk._
+import at.logic.calculi.lk.base.Sequent
+import at.logic.language.hol.{HOLFormula, HOLVar, HOLAbs, HOLExpression}
+import at.logic.transformations.ceres.clauseSchema._
+import at.logic.transformations.ceres.ACNF.ACNF
+import at.logic.gui.prooftool.gui.Main
+
+
+@RunWith(classOf[JUnitRunner])
+class ACNFTest extends SpecificationWithJUnit {
+  implicit val factory = defaultFormulaOccurrenceFactory
+  "ACNFTest" should {
+    "integration test should create correctly the ACNF for journal_example.lks" in {
+      println(Console.BLUE+"\n\n\n\n------- ACNF for the journal example ------- \n\n"+Console.RESET)
+      val s1 = new InputStreamReader(new FileInputStream("target" + separator + "test-classes" + separator + "journal_example.lks"))
+      val s2 = new InputStreamReader(new FileInputStream("target" + separator + "test-classes" + separator + "resSchema1.rs"))
+      val map = sFOParser.parseProof(s1)
+      ParseResSchema(s2)
+      val p = ACNF("\\varphi", "\\rho_1", 5)
+//      printSchemaProof(p)
+//      Main.display("ACNF", p) ; while(true){}
+
+      println("\n\n--- END ---\n\n")
+      ok
+    }
+  }
+}
+
