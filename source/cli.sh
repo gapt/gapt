83ea8a7b3486b1e449ef5e5f4c92b650cd314780
Martin Riener <martin.riener@googlemail.com>
2013-01-14 22:34:29 +0000

    added release detection code to cli.sh
    git-svn-id: http://gapt.googlecode.com/svn/trunk@1420 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/cli.sh
+++ source/cli.sh
@@ -4,33 +4,76 @@
 # the CLI jar package and runs the scala interpreter
 # (preference is given to the development version)
 
-export JARNAME="cli-1.2-SNAPSHOT-jar-with-dependencies.jar"
+export VERSION="1.2"
+export JARNAME="cli-$VERSION-SNAPSHOT-jar-with-dependencies.jar"
+export RELEASE="gapt-cli-$VERSION.jar"
 export SCP=""
-export POSSIBLE_PATHS=`echo $PATH | sed s/:/\\ /g`
+export RCP=""
+#export POSSIBLE_PATHS="$(echo $PATH | sed "s/:/\" \"/"g | sed "s/^/\"/" |sed "s/$/\"/")"
+export JAVA_OPTS="-Xss2m -Xmx2g"
+export OLDIFS="$IFS"
+export IFS=":"
+#echo $POSSIBLE_PATHS
 
-for I in ${POSSIBLE_PATHS} .; do
-    if test -f $I/${JARNAME};
+if test _$1_ = _-h_ -o _$1_ = _--help_ ; then
+  echo "GAPT Command Line Interface"
+  echo "  searches the path and current directory for a release or development version of the "
+  echo "  GAPT jar and executes it."
+  echo ""
+  echo "usage: cli.sh [-d]"
+  echo ""
+  echo "   -d : prefer development version over release version"
+  exit 1
+fi
+
+if [ ! -f "$JAVA_HOME"/bin/java ] ; then
+ echo "java executable not found, please check your path and set JAVA_HOME correctly"
+ exit 1
+fi
+
+for I in ${PATH} .; do
+    if [ -f "$I/${JARNAME}" ];
     then
 	export SCP=$I
 	break
     fi
 done
 
-for I in ${POSSIBLE_PATHS} .; do
-    if test -f $I/cli/target/${JARNAME};
+for I in ${PATH} .; do
+    if [ -f "$I/cli/target/${JARNAME}" ];
     then
 	export SCP="$I/cli/target"
 	break
     fi
 done
 
-if test _${SCP}_ = __ ; then 
-    echo "Could not find ${JARNAME}!"
+for I in $PATH:.; do
+    if [ -f "$I/${RELEASE}" ];
+    then
+        export RCP="$I"
+        break
+    fi
+done
+
+export IFS=$OLDIFS
+
+if test _$1_ = _-d_ ; then echo "forcing development version!" ; fi
+
+if test "_${RCP}_" = __ -o _$1_ = _-d_ ; then 
+if test "_${SCP}_" = __ ; then 
+    echo "Could not find ${JARNAME} nor ${RELEASE} in path or current directory!"
 else
     echo found ${JARNAME} in ${SCP}!
     export JAVA_OPTS="-Xss2m -Xmx2g"
-    #scala -classpath ${SCP}/${JARNAME} -i cli-script.scala
-    $JAVA_HOME/bin/java -jar ${SCP}/${JARNAME}
-    # workaround because jline somehow mixes up the terminal
-    reset
+    "$JAVA_HOME"/bin/java -jar ${SCP}/${JARNAME}
 fi
+else
+    echo "found release version ${RELEASE} in ${RCP}"
+    "$JAVA_HOME"/bin/java -jar "${RCP}"/${RELEASE}
+fi
+
+# workaround because jline somehow mixes up the terminal
+reset
+
+
+
