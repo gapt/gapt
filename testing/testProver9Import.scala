2f3824df4f37f5409396f733df78d6386d8df257
Tomer Libal <shaolintl@gmail.com>
2013-03-29 09:40:11 +0000

    modified the testing file
    git-svn-id: http://gapt.googlecode.com/svn/trunk@1520 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- testing/testProver9Import.scala
+++ testing/testProver9Import.scala
@@ -23,7 +23,7 @@ class MyFile(file: File) {
 
 class MyOption[+T >: Nothing]
 case object MyNone extends MyOption[Nothing]
-case class MyException[+T >: Nothing](exp: Exception) extends MyOption[T]
+case class MyThrowable[+T >: Nothing](exp: Throwable) extends MyOption[T]
 case class MySome[+T >:Nothing](value: T) extends MyOption[T]
 
 /**
@@ -32,9 +32,9 @@ case class MySome[+T >:Nothing](value: T) extends MyOption[T]
  **/
 object testProver9Import {
 
-  type MyMap = scala.collection.mutable.Map[String,List[Pair[String,Option[Exception]]]]
+  type MyMap = scala.collection.mutable.Map[String,List[Pair[String,Option[Throwable]]]]
 
-  def apply( dir: String, to: Long  ) {
+  def apply( dir: String, to: Long, fn: Option[String] = None ) {
     println( "Testing prover9-import with timeout " + to + "s:" )
     var n_total = 0
     var n_exp = 0
@@ -42,50 +42,64 @@ object testProver9Import {
     var n_OK = 0
 
     var list_succ = List[String]()
-    val map_failure = scala.collection.mutable.Map[String,List[Pair[String,Option[Exception]]]]()
-
-    def addOrUpdate(map: MyMap, err: String, value: String, ex: Option[Exception]) = {
-      try {
-        val ls = map(err)
-        map(err) = (value,ex) :: ls
-      } catch {
-        case nsee: NoSuchElementException =>  map(err) = List((value,ex))
-      }
-
-    }
+    val map_failure = scala.collection.mutable.Map[String,List[Pair[String,Option[Throwable]]]]()
 
     val root = new MyFile( new File( dir ) )
 
+    val cur = System.currentTimeMillis
+
+    var out = System.out
+    val err = System.err
+    System.setOut(new java.io.PrintStream(new java.io.ByteArrayOutputStream()))
+    System.setErr(new java.io.PrintStream(new java.io.ByteArrayOutputStream()))
+
     for ( file <- root.andTree if file.getName.endsWith( ".out" ) ) {
       n_total += 1
-      print( "  " + file.getName() + " " )
+      //print( "  " + file.getName() + " " )
+      out.println(n_total + " - " +( file.toString) + " - " + ((System.currentTimeMillis - cur) / 1000))
 
       val suc_import = runWithTimeout( to * 1000 ) {
-        loadProver9LKProof( file.getCanonicalPath() )
+      loadProver9LKProof( file.getCanonicalPath() )
       }
       suc_import match {
         case MyNone => {
-          println( "[ FAIL ]" )
+          //println( "[ FAIL ]" )
           addOrUpdate(map_failure, "None", file.getCanonicalPath(), None)
         }
-        case MyException(ex) => {
-          println( "[ FAIL " + ex.getClass.getName + " ]")
-          addOrUpdate(map_failure, ex.getClass.getName,file.getCanonicalPath(), Some(ex))
+        case MyThrowable(ex) => {
+          //println( "[ FAIL " + ex.getClass.getName + " ]")
+          val loc = locateGAPTLocation(ex.getStackTrace())
+          addOrUpdate(map_failure, loc,file.getCanonicalPath(), Some(ex))
         }
         case _ => {
           n_OK += 1
-          println ("[ OK ]" )
+          //println ("[ OK ]" )
           list_succ = file.getCanonicalPath() :: list_succ
         }
       }
     }
 
-    println( "\nTotal Stats: " + n_OK + "/" + n_total + " OK" )
-    println( "Successes:" )
-    list_succ.foreach( e => println ( "  " + e ) )
-    println( "Failures:" )
-    map_failure.keySet.foreach( e => println( "  " + e + " -> " + map_failure(e).size))
-    map_failure.values.flatMap(x => x).foreach( e => {println( "  " + e._1); (e._2 match {case None => (); case Some(ex) => ex.printStackTrace})})
+    System.setErr(err)
+    System.setOut(out)
+    fn match {
+      case Some(fn) => {
+        out = new PrintStream(new File(fn))
+      }
+      case _ => ()
+    }
+
+    out.println( "\nTotal Stats: " + n_OK + "/" + n_total + " OK" )
+    out.println( "Successes:" )
+    list_succ.foreach( e => out.println ( "  " + e ) )
+    out.println( "Failures:" )
+    map_failure.keySet.foreach( e => out.println( "  " + e + " -> " + map_failure(e).size))
+    map_failure.keySet.foreach(
+      e => {out.println( " Error: " + e + "\n" + "------------- list of affected files ---------------------------"); map_failure(e).foreach(
+        e => {out.println( "  " + e._1); (e._2 match {case None => out.println("timeout (" + to + ")"); case Some(ex) => ex.printStackTrace})});
+      out.println("\n============= end of list =========================================")})
+
+    out.flush
+    out.close
   }
 
   /**
@@ -97,12 +111,32 @@ object testProver9Import {
   private def runWithTimeout[T >: Nothing]( to: Long )( f: => T ) : MyOption[T] = {
     var output:MyOption[T] = MyNone
 
-    val r = new Runnable { def run() { try { output = MySome( f ) } catch { case ex: Exception => output = MyException(ex) } } }
+    val r = new Runnable { def run() { try { output = MySome( f ) } catch {
+      case ex: Throwable => output = MyThrowable(ex)
+    } } }
     val t = new Thread( r )
     t.start()
     t.join( to )
     if ( t.isAlive() ) t.stop()
 
     output
+  }
+
+  private def addOrUpdate(map: MyMap, err: String, value: String, ex: Option[Throwable]) = {
+    try {
+      val ls = map(err)
+      map(err) = (value,ex) :: ls
+    } catch {
+      case nsee: NoSuchElementException =>  map(err) = List((value,ex))
+    }
+
+  }
+
+  private def locateGAPTLocation(elems: Array[StackTraceElement]): String = {
+    // we concatenate the stack as an identifier for a specific error (we ignore the ex message)
+    // we take first 3 elements as they are normally enough to identify the location in the gapt code
+    // nornmally, we should look for patterns and cut the stack trace there in order to avoid
+    // different map keys for the same errors but with different depth of nesting
+    elems.take(3).foldLeft("")((str,e) => str ++ e.toString ++ "\n")
   }
 }
