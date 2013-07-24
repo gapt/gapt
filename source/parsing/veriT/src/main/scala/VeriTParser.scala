57affc1b03e88e0faa8b52592d4624fa7c39bd7f
Giselle Reis <giselle.mnr@gmail.com>
2013-07-24 16:56:23 +0000

    Partial progress from another computer.
    git-svn-id: http://gapt.googlecode.com/svn/trunk@1613 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/parsing/veriT/src/main/scala/VeriTParser.scala
+++ source/parsing/veriT/src/main/scala/VeriTParser.scala
@@ -17,6 +17,24 @@ object VeriTParser extends RegexParsers {
     }
   }
 
+  def addSymmetry(x: FOLTerm, y: FOLTerm) = {
+    // TODO
+  }
+ 
+  def fixOrder(pairs: List[(FOLTerm, FOLTerm)], eqs: List[FOLFormula]) : List[FOLFormula] = (pairs, eqs.head) match {
+    case ( (x, y)::tail, Neg(Atom(eq, List(a, b))) ) if x==a && y==b => 
+      eqs.head :: fixOrder(tail, eqs.tail)
+    case ( (x, y)::tail, Neg(Atom(eq, List(a, b))) ) if x==b && y==a =>
+      addSymmetry(a, b)
+      Neg(Atom(eq, List(b, a))) :: fixOrder(tail, eqs.tail)
+    case ( (x, y)::tail, Neg(Atom(eq, List(a, b))) ) =>
+      throw new Exception("ERROR: Predicate " + eqs.head + 
+      " does not have args " + x + ", " + y)
+    case ( Nil, Atom(_,_) ) => List(eqs.head)
+    case _ => throw new Exception("ERROR: mal-formed eq_congruent")
+
+  }
+
   def eq_reflexive_toExpTree(f : List[FOLFormula]) : ExpansionTree = {
     val x = FOLVar(VariableStringSymbol("x"))
     val eq = ConstantStringSymbol("=")
@@ -52,49 +70,61 @@ object VeriTParser extends RegexParsers {
     def unfoldChain(l: List[FOLFormula]) = unfoldChain_(l.tail, l(0))
     def unfoldChain_(l: List[FOLFormula], c: FOLFormula) : List[FOLFormula] = l.head match {
       case Neg(Atom(eq0, List(x0, x1))) if eq0 == eq => c match {
-        // Checking all possible cases of atom ordering, i.e.:
+        // Note that the variables are:
+        // x2=x3 ^ x0=x1
+        // Checking all possible cases of atom ordering:
+ 
         // x=y ^ y=z -> x=z
-        // x=y ^ z=y -> x=z
-        // y=x ^ y=z -> x=z
-        // y=x ^ z=y -> x=z
-        case Neg(Atom(eq1, List(x2, x3))) if x1 == x2 =>
-          val newc = Neg(Atom(eq, List(x0, x3)))
-          // Instances
-          val f1 = eq_trans.instantiate(x0)
-          val f2 = f1.instantiate(x1) // or x2, should be the same
-          val f3 = f2.instantiate(x3)
-
-          f3 :: unfoldChain_(l.tail, newc)
-
-        case Neg(Atom(eq1, List(x2, x3))) if x1 == x3 =>
-          val newc = Neg(Atom(eq, List(x0, x2)))
-          // Instances
-          val f1 = eq_trans.instantiate(x0)
-          val f2 = f1.instantiate(x1) // or x3, should be the same
-          val f3 = f2.instantiate(x2)
-
-          f3 :: unfoldChain_(l.tail, newc)
-
-        case Neg(Atom(eq1, List(x2, x3))) if x0 == x2 =>
-          val newc = Neg(Atom(eq, List(x1, x3)))
-          // Instances
-          val f1 = eq_trans.instantiate(x1)
-          val f2 = f1.instantiate(x0) // or x2, should be the same
-          val f3 = f2.instantiate(x3)
-
-          f3 :: unfoldChain_(l.tail, newc)
-
-        case Neg(Atom(eq1, List(x2, x3))) if x0 == x3 =>
+        case Neg(Atom(eq1, List(x2, x3))) if x3 == x0 =>
           val newc = Neg(Atom(eq, List(x2, x1)))
           // Instances
-          val f1 = eq_trans.instantiate(x1)
+          val f1 = eq_trans.instantiate(x2)
           val f2 = f1.instantiate(x0) // or x3, should be the same
-          val f3 = f2.instantiate(x2)
+          val f3 = f2.instantiate(x1)
+
+          f3 :: unfoldChain_(l.tail, newc)
+
+        // x=y ^ z=y -> x=z
+        case Neg(Atom(eq1, List(x2, x3))) if x3 == x1 =>
+          val newc = Neg(Atom(eq, List(x2, x0)))
+          // Instances
+          val f1 = eq_trans.instantiate(x2)
+          val f2 = f1.instantiate(x1) // or x3, should be the same
+          val f3 = f2.instantiate(x0)
+
+          addSymmetry(x0, x1)
+
+          f3 :: unfoldChain_(l.tail, newc)
+
+        // y=x ^ z=y -> x=z
+        case Neg(Atom(eq1, List(x2, x3))) if x2 == x1 =>
+          val newc = Neg(Atom(eq, List(x3, x0)))
+          // Instances
+          val f1 = eq_trans.instantiate(x3)
+          val f2 = f1.instantiate(x1) // or x2, should be the same
+          val f3 = f2.instantiate(x0)
+
+          addSymmetry(x0, x1)
+          addSymmetry(x2, x3)
+          
+          f3 :: unfoldChain_(l.tail, newc)
+        
+        // y=x ^ y=z -> x=z
+        case Neg(Atom(eq1, List(x2, x3))) if x2 == x0 =>
+          val newc = Neg(Atom(eq, List(x3, x1)))
+          // Instances
+          val f1 = eq_trans.instantiate(x3)
+          val f2 = f1.instantiate(x0) // or x2, should be the same
+          val f3 = f2.instantiate(x1)
+          
+          addSymmetry(x2, x3)
 
           f3 :: unfoldChain_(l.tail, newc)
 
         case Neg(Atom(eq1, List(x2, x3))) => throw new Exception("ERROR: the conclusion of the previous terms have" +  
-          " no literal in common with the next one. Are the literals out of order?")
+          " no literal in common with the next one. Are the literals out of order?" + 
+          "\nconclusion: " + c + "\nsecond literal: " + l.head)
+
 
         case _ => throw new Exception("ERROR: wrong format for negated equality: " + c)
       }
@@ -122,11 +152,20 @@ object VeriTParser extends RegexParsers {
     prenexToExpansionTree(eq_trans, instances)
   }
 
+  // Assuming all the antecedents of the implication are in order:
+  // ( =(x0, y0)  ^  =(x1, y1)  ^ ... ^  =(xn, yn)  ->  =(f x0...xn, f y0...yn) )
+  // in veriT is *always* ( not =(x0, y0) , not =(x1, y1) , ... , not =(xn, yn), =(f x0...xn, f y0...yn) )
   def eq_congr_toExpTree(f: List[FOLFormula]) : ExpansionTree = {
     
     def getFunctionName(f: FOLFormula) : String = f match {
-      case Imp(_, Atom(eq, List(f1, _))) => f1 match {
+      case Atom(eq, List(f1, _)) => f1 match {
           case Function(ConstantStringSymbol(n), _) => n 
+      }
+    }
+    
+    def getArgsLst(f: FOLFormula) = f match {
+      case Atom(eq, List(f1, f2)) => (f1, f2) match {
+          case (Function(_, args1), Function(_, args2)) => (args1, args2) 
       }
     }
 
@@ -156,14 +195,26 @@ object VeriTParser extends RegexParsers {
       }
     }
 
-    val instance = Utils.reverseCNF(f)
-    val fname = getFunctionName(instance)
+    val fname = getFunctionName(f.last)
     val n = f.size - 1
     val eq_congr = gen_eq_congr(n, fname)
+    
+    // Fixing the order of equalities
+    val (args1, args2) = getArgsLst(f.last)
+    val pairs = args1.zip(args2)
+    val correct = fixOrder(pairs, f)
+    val instance = Utils.reverseCNF(correct)
     
     prenexToExpansionTree(eq_congr, List(instance))
   }
 
+  // Assuming all the antecedents of the implication are in order:
+  // ( =(x0, y0)  ^  =(x1, y1)  ^ ... ^  =(xn, yn) ^ p(x0...xn)  ->  p(y0...yn) )
+  // in veriT is *always* 
+  // ( not =(x0, y0) , not =(x1, y1) , ... , not =(xn, yn), not p(x0...xn), p(y0...yn) )
+  // or
+  // ( not =(x0, y0) , not =(x1, y1) , ... , not =(xn, yn), p(x0...xn), not p(y0...yn) )
+  // TODO: fix the ordering of this one
   def eq_congr_pred_toExpTree(f: List[FOLFormula]) : ExpansionTree = {
 
     def getPredName(f: FOLFormula) : String = f match {
