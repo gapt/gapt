045daa67cfb6499372989fbf5cbd7dff181554fe
Bernhard Mallinger <b.mallinger@gmx.at>
2013-10-12 17:24:35 +0000

    Added proof generator for (n + n) + n = n + (n + n)
    git-svn-id: http://gapt.googlecode.com/svn/trunk@1655 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- examples/ProofSequences.scala
+++ examples/ProofSequences.scala
@@ -701,3 +701,285 @@ object SumOfOnesExampleProof {
   }
 }
 
+
+
+object UniformAssociativity3ExampleProof {
+  /**
+    * Auxiliary structure to deal with axioms of the schema:
+    * Forall variables cond1 -> cond2 -> ... -> condn -> consequence |- ...
+    */
+  class AllQuantifiedConditionalAxiomHelper(variables: List[FOLVar], conditions: List[FOLFormula], consequence: FOLFormula) {
+    /**
+     * Returns the full axiom
+     */
+    def get_axiom(): FOLFormula = {
+      // TODO: refactor apply_conditional_equality, combine dupliate code
+      var impl_chain = consequence
+      for (elem <- conditions.reverse) {
+        impl_chain = Imp(elem, impl_chain)
+      }
+
+      def quantify(variables: List[FOLVar], body: FOLFormula): FOLFormula = {
+        variables match {
+          case Nil => body
+          case head::tail => AllVar(head, quantify(tail, body))
+        }
+      }
+
+      quantify( variables, impl_chain )
+    }
+
+    /**
+     * Use axiom with given expressions in proof.
+     * Consequence of axiom must appear in current proof. 
+     * Instantiated conditions will of course remain in the antecedent of the returned proof
+     */
+    def apply(expressions: List[FOLTerm], p: LKProof): LKProof = {
+      assert(expressions.length == variables.length, "Number of expressions doesn't equal number of variables")
+
+      // construct implication with instantiated conditions and consequence
+      var instantiated_conditions = conditions
+      var instantiated_consequence = consequence
+      for (i <- 0 to variables.length-1) {
+        val substitute = (x:FOLFormula) => FOLSubstitution(x, variables(i), expressions(i))
+        instantiated_conditions = instantiated_conditions.map(substitute)
+        instantiated_consequence = substitute(instantiated_consequence)
+      }
+
+      val p1 = apply_conditional_equality(instantiated_conditions, instantiated_consequence, p)
+
+      // iteratively instantiate all-quantified variables with expresion
+      def instantiate_axiom(expressions: List[FOLTerm], axiom: FOLFormula, p: LKProof): LKProof = {
+        expressions match {
+          case Nil => p
+          case head::tail => {
+            val new_axiom = axiom.instantiate(head)
+            val new_p = instantiate_axiom(tail, new_axiom, p)
+
+            ForallLeftRule(new_p, new_axiom, axiom, head)
+          }
+        }
+      }
+
+      val ax = get_axiom()
+      val p2 = instantiate_axiom(expressions, ax, p1)
+
+      ContractionLeftRule(p2, ax)
+    }
+  }
+
+
+  val eq = new ConstantStringSymbol("=")
+  val s = new ConstantStringSymbol("s")
+  val p = new ConstantStringSymbol("+")
+
+  val x = FOLVar( VariableStringSymbol( "x" ))
+  val y = FOLVar( VariableStringSymbol( "y" ))
+  val z = FOLVar( VariableStringSymbol( "z" ))
+
+  val x1 = FOLVar( VariableStringSymbol( "x_1" ))
+  val x2 = FOLVar( VariableStringSymbol( "x_2" ))
+  val y1 = FOLVar( VariableStringSymbol( "y_1" ))
+  val y2 = FOLVar( VariableStringSymbol( "y_2" ))
+
+  def f1( sym: ConstantSymbolA, arg: FOLTerm ) = Function(sym, arg::Nil)
+  def f2( sym: ConstantSymbolA, arg1: FOLTerm, arg2: FOLTerm ) : FOLTerm = Function(sym, arg1::arg2::Nil)
+  def f2( arg1: FOLTerm, sym: ConstantSymbolA, arg2: FOLTerm ) : FOLTerm = f2(sym, arg1, arg2)
+
+  // Axioms
+
+  // Trans as from TransRule, possibly unify or generalise
+  val Trans = AllVar(x, AllVar(y, AllVar(z, Imp(And(Atom(eq, x::y::Nil) , Atom(eq, y::z::Nil) ), Atom(eq, x::z::Nil)))))
+  val Cs = AllVar(x, AllVar(y, Imp(Atom(eq, x::y::Nil), Atom(eq, Function(s, x::Nil)::Function(s, y::Nil)::Nil))))
+  val Symm = AllVar(x, Atom(eq, x::x::Nil))
+
+  // TODO: port these axioms to new format using AllQuantifiedConditionalAxiomHelper
+  def refl_ax(): FOLFormula = AllVar(x, refl_ax(x))
+  def refl_ax(x: FOLTerm): FOLFormula = AllVar(y, refl_ax(x, y))
+  def refl_ax(x: FOLTerm, y: FOLTerm): FOLFormula = Imp(Atom(eq, x::y::Nil), Atom(eq, y::x::Nil))
+
+  // x=y -> s(x) = s(y)
+  def cs_ax(): FOLFormula = AllVar(x, cs_ax(x))
+  def cs_ax(x: FOLTerm): FOLFormula = AllVar(y, cs_ax(x, y))
+  def cs_ax(x: FOLTerm, y: FOLTerm): FOLFormula = Imp(Atom(eq, x::y::Nil), Atom(eq, Function(s, x::Nil)::Function(s, y::Nil)::Nil))
+  
+  // x1 = x2 -> y1 = y2 -> x1 + y1 = x2 + y2
+  val cp = new AllQuantifiedConditionalAxiomHelper(x1::x2::y1::y2::Nil, Atom(eq, x1::x2::Nil)::Atom(eq, y1::y2::Nil)::Nil, Atom(eq, Function(p, x1::y1::Nil)::Function(p, x2::y2::Nil)::Nil))
+
+  // Arithmetic axioms
+  val Ax1 = AllVar(x, Atom(eq, Function(p, x::Utils.numeral(0)::Nil)::x::Nil))
+
+  // Forall x, y: s(x+y) = x+s(y)
+  def ax2_ax(): FOLFormula = AllVar(x, AllVar(y, ax2_ax(x, y) ))
+  def ax2_ax(x: FOLTerm): FOLFormula = AllVar(y, ax2_ax(x, y) )
+  def ax2_ax(x: FOLTerm, y: FOLTerm): FOLFormula = Atom(eq, f1(s, f2(x, p, y))::f2( x, p, f1(s, y))::Nil )
+
+  def apply(n: Int): LKProof = {
+    assert (n>=1, "n must be >= 1")
+    val p = gen_proof_step(0, n)
+    induction_start(n, p)
+  }
+
+  /**
+   * Close off proof ending in (n + n) + 0 = n + (n + 0) |- ...
+   */
+  def induction_start(n: Int, p0: LKProof): LKProof = {
+    // show both sides equal to n + n
+    val n_num = Utils.numeral(n)
+    val zero = Utils.numeral(0)
+    val c1 = f2(f2(n_num, p, n_num), p, zero)
+    val d1 = f2(n_num, p, n_num)
+    val e1 = f2(n_num, p, f2(n_num, p, zero))
+    val p1 = TransRule(eq, c1, d1, e1, p0)
+
+    // show (n + n) + 0 = (n + n) directly via ax1
+    val p2 = ForallLeftRule(p1, Atom(eq, c1::d1::Nil), Ax1, d1)
+    val p3 = ContractionLeftRule(p2, Ax1)
+
+    // show (n + n) = n + (n + 0) 
+    val p4 = cp(n_num::n_num::n_num::f2(n_num, p, zero)::Nil, p3)
+
+    // show n = n and n = n + 0
+    val p5 = ForallLeftRule(p4, Atom(eq, n_num::n_num::Nil), Symm, n_num)
+    val p6 = ContractionLeftRule(p5, Symm)
+
+    val p7 = reflect(f2(n_num, p, zero), n_num, p6)
+
+    val p8 = ForallLeftRule(p7, Atom(eq, f2(n_num, p, zero)::n_num::Nil), Ax1, n_num)
+    ContractionLeftRule(p8, Ax1)
+  }
+
+  /***
+    * Returns proof Pi (currently including line above and below Pi), with numerals n, i, i+1:
+    * (n + n) + i+1 = n + (n + i+1), Ax |- ...
+    *  Pi
+    * (n + n) + i   = n + (n + i), Ax |- ...
+    */
+  def gen_proof_step(i: Int, n: Int) : LKProof = {
+    val n_num = Utils.numeral(n)
+    val i_num = Utils.numeral(i)
+    val ip1_num = Utils.numeral(i+1)
+
+    val a1 = f2( f2( n_num, p, n_num ), p, ip1_num )
+    val a2 = f2( n_num, p, f2( n_num, p, ip1_num ) )
+    val b1 = f2( n_num, p, f1(s, f2( p, n_num, i_num ) ) )
+
+    val p0 =
+      if (i+1 >= n) {
+        // start, add axioms
+        val all_axioms = Trans::cp.get_axiom()::Symm::ax2_ax()::cs_ax()::refl_ax()::Ax1::Nil
+        def add_ax(start: LKProof, axioms : List[FOLFormula]): LKProof = {
+          axioms match {
+            case Nil => start
+            case head::tail => WeakeningLeftRule(add_ax(start, tail), head)
+          }
+        }
+
+        val final_expression = Atom(eq, a1::a2::Nil )
+
+        val top = Axiom( final_expression::Nil, final_expression::Nil )
+        add_ax(top, all_axioms)
+      } else {
+        gen_proof_step(i+1, n)
+      }
+
+    val p1 = TransRule(eq, a1, b1, a2, p0)
+
+    // the left side now contains
+    // (n + n) + s(i) = n + s(n + i) as well as
+    // n + s(n + i) = n + (n + s(i))
+
+    // use Cp to reduce the latter to s(n + i) = (n + s(i))
+    val x1_1 = n_num
+    val x2_1 = n_num
+    val y1_1 = f1(s, f2( n_num, p, i_num ))
+    val y2_1 = f2(n_num, p, f1(s, i_num))
+    val p8 = cp(x1_1::x2_1::y1_1::y2_1::Nil, p1)
+
+    // show x1 = x2 by symmetry
+    val p9 = ForallLeftRule(p8, Atom(eq, x1_1::x2_1::Nil ), Symm, n_num)
+    val p10 = ContractionLeftRule(p9, Symm)
+
+    // show y1 = y2 by Ax2 (i.e. s(n + i) = (n + s(i)) )
+    val p13 = show_by_ax2(n_num, i_num, p10)
+
+    // now we only have (n + n) + s(i) = n + s(n + i) left (c=e), reduce right hand side by Ax2 and Trans to s(n + (n + i) (c=d) )
+    val c1 = f2(f2(n_num, p, n_num), p, f1(s, i_num))
+    val d1 = f1(s, f2(n_num, p, f2(n_num, p, i_num)))
+    val e1 = f2(n_num, p, f1(s, f2(n_num, p, i_num)))
+    val p14 = TransRule(eq, c1, d1, e1, p13)
+
+    // show d=e by Ax2
+    val p15 = show_by_ax2(n_num, f2(n_num, p, i_num), p14)
+
+    // next goal: reduce (n + n) + s(i) = s(n + (n + i) to (n + n) + s(i) = s( (n + n) + i) using the IH, Trans and Cs)
+    val c2 = f2(f2(n_num, p, n_num), p, f1(s, i_num))
+    val d2 = f1(s, f2(f2(n_num, p, n_num), p, i_num))
+    val e2 = d1
+    val p16 = TransRule(eq, c2, d2, e2, p15)
+
+    val p17 = show_by_cs(f2(f2(n_num, p, n_num), p, i_num), f2(n_num, p, f2(n_num, p, i_num)), p16)
+
+    // now we have:
+    // (n + n) + s(i) = s( (n + n) + i) 
+    // as well as
+    // (n + n) + i = n + (n + i)
+    // -> use Ax2
+
+    // use reflection to match definition of ax2 afterwards
+    val p18 = reflect( d2, c2, p17 )
+
+    val p19 = show_by_ax2( f2(n_num, p, n_num), i_num, p18)
+    p19
+
+    // we end up with the IH (n + n) + i = n + (n + i)
+  }
+
+  def show_by_ax2(x: FOLTerm, y: FOLTerm, p: LKProof): LKProof = {
+    val p1 = ForallLeftRule(p, ax2_ax(x, y), ax2_ax(x), y)
+    val p2 = ForallLeftRule(p1, ax2_ax(x), ax2_ax(), x)
+    ContractionLeftRule(p2, ax2_ax())
+  }
+
+  def show_by_cs(x: FOLTerm, y: FOLTerm, p: LKProof): LKProof = {
+    val p1 = apply_conditional_equality( Atom(eq, x::y::Nil)::Nil, Atom(eq, Function(s, x::Nil)::Function(s, y::Nil)::Nil), p)
+
+    val p2 = ForallLeftRule(p1, cs_ax(x, y), cs_ax(x), y)
+    val p3 = ForallLeftRule(p2, cs_ax(x), cs_ax(), x)
+    ContractionLeftRule(p3, cs_ax())
+  }
+
+  /**
+    * Takes a proof s2 with end-sequent of the form
+    * (x=y), ... |- ...
+    * and return one with end-sequent of the form
+    * (y=x), ... |- ...
+    */
+  def reflect(x: FOLTerm, y: FOLTerm, p: LKProof): LKProof = {
+    val p1 = apply_conditional_equality( Atom(eq, x::y::Nil)::Nil, Atom(eq, y::x::Nil), p)
+
+    val p2 = ForallLeftRule(p1, refl_ax(x, y), refl_ax(x), y)
+    val p3 = ForallLeftRule(p2, refl_ax(x), refl_ax(), x)
+    ContractionLeftRule(p3, refl_ax())
+  }
+
+  def apply_conditional_equality(equalities: List[FOLFormula], result: FOLFormula, p: LKProof) : LKProof = {
+    equalities match {
+      case head :: Nil => {
+        val ax = Axiom(head::Nil, head::Nil)
+        ImpLeftRule(ax, p, head, result)
+      }
+      case head :: tail => {
+        val ax = Axiom(head::Nil, head::Nil)
+        var impl_chain = result
+        for (elem <- tail.reverse) {
+          impl_chain = Imp(elem, impl_chain)
+        }
+        val s2 = apply_conditional_equality(tail, result, p)
+        ImpLeftRule(ax, s2, head, impl_chain)
+      }
+    }
+  }
+}
+
+
