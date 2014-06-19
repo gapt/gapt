e4f55eb2a8bf6f5db5af78b4e2139a9108926f2c
Christoph Spoerk <christoph.spoerk@gmail.com>
2014-06-19 11:39:39 +0000

    added replacements wrapper for FOLExpressions
    git-svn-id: http://gapt.googlecode.com/svn/trunk@2078 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/syntax/language/fol/src/main/scala/at/logic/language/fol/Replacements.scala
+++ source/syntax/language/fol/src/main/scala/at/logic/language/fol/Replacements.scala
@@ -2,6 +2,7 @@ package at.logic.language.fol.replacements
 
 import at.logic.language.hol
 import at.logic.language.fol.FOLExpression
+import at.logic.language.hol.HOLExpression
 
 /**
  * Created by cthulhu on 18.06.14.
@@ -13,4 +14,8 @@ object getAtPositionFOL {
 
 object getAllPositionsFOL {
   def apply(expression: FOLExpression): List[Tuple2[List[Int], FOLExpression]] = hol.replacements.getAllPositions(expression.asInstanceOf[hol.HOLExpression]).asInstanceOf[List[Tuple2[List[Int],FOLExpression]]]
+}
+
+class Replacement(position: List[Int], expression: FOLExpression) extends hol.replacements.Replacement(position,expression) {
+  def apply(exp: FOLExpression) : FOLExpression = super.apply(exp.asInstanceOf[HOLExpression]).asInstanceOf[FOLExpression]
 }
