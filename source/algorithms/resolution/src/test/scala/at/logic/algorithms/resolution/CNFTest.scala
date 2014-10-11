2a2fb3f111816cd41922074aa3c1da518e780a12
Christoph Spoerk <christoph.spoerk@gmail.com>
2014-10-11 15:48:43 +0000

    Added a testcase for the Tseitin transformation
    git-svn-id: http://gapt.googlecode.com/svn/trunk@2185 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/algorithms/resolution/src/test/scala/at/logic/algorithms/resolution/CNFTest.scala
+++ source/algorithms/resolution/src/test/scala/at/logic/algorithms/resolution/CNFTest.scala
@@ -3,8 +3,9 @@ package at.logic.algorithms.resolution
 import org.junit.runner.RunWith
 import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.runner.JUnitRunner
-
-import at.logic.language.fol.{And, Or, Neg, FOLFormula, Atom, FOLConst}
+import at.logic.language.hol.{Atom => HOLAtom, HOLConst}
+import at.logic.language.lambda.types.To
+import at.logic.language.fol.{And, Or, Neg, Atom, FOLConst, Imp}
 import at.logic.calculi.resolution.FClause
 
 @RunWith(classOf[JUnitRunner])
@@ -17,6 +18,39 @@ class CNFTest extends SpecificationWithJUnit {
       val PavQa = Or(Pa,Qa)
       val f = And(PavQa, nQa)
       CNFp(f) must beEqualTo(Set(FClause(List(),List(Pa,Qa)),FClause(List(Qa),List())))
+    }
+  }
+
+  "the computation of TseitinCNF(f)" should {
+    "should be right, where f = ((P ∨ Q) ∧ R ) -> ¬S" in {
+      val p = Atom("P", Nil)
+      val q = Atom("Q", Nil)
+      val r = Atom("R", Nil)
+      val s = Atom("S", Nil)
+
+      val f = Imp(And(Or(p, q), r), Neg(s))
+
+      val x =  Atom("x", Nil)
+      val x0 = Atom("x0", Nil)
+      val x1 = Atom("x1", Nil)
+      val x2 = Atom("x2", Nil)
+
+      val cnf = TseitinCNF(f)
+      val expected = Set(
+        FClause(List(), List(x2)),
+        FClause(List(x1), List(x2)),
+        FClause(List(), List(x2, x0)),
+        FClause(List(), List(x1, s)),
+        FClause(List(x1, s), List()),
+        FClause(List(x0), List(r)),
+        FClause(List(x0), List(x)),
+        FClause(List(q), List(x)),
+        FClause(List(p), List(x)),
+        FClause(List(x2, x0), List(x1)),
+        FClause(List(x, r), List(x0)),
+        FClause(List(x), List(p, q))
+      )
+      cnf must beEqualTo(expected)
     }
   }
 }
