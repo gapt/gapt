980dedd5e7154f3265b07587d1887b7a790babea
Daniel Weller <fraini@gmail.com>
2014-10-16 08:18:09 +0000

    Added script/hack generating scaladoc without using maven.
    git-svn-id: http://gapt.googlecode.com/svn/trunk@2213 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ scaladoc.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm -rf scaladoc
+mkdir scaladoc
+rm -rf ~/.m2/repository/at/logic
+JAVA_OPTS="-Xmx3g" scaladoc -d scaladoc -classpath $(find ~/.m2/repository/ -iname *.jar|xargs| tr ' ' ':') $(find source -regextype posix-extended -regex '.*\.(scala|java)$'|grep -v Test)
