ea70961ca6d8f9108bfa1bf0b7dcbcaa99193a5e
Gabriel Ebner <gebner@gebner.org>
2014-11-07 15:24:53 +0000

    Ignore treeviz in scaladoc.
    git-svn-id: http://gapt.googlecode.com/svn/trunk@2303 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- scaladoc.sh
+++ scaladoc.sh
@@ -3,4 +3,4 @@
 rm -rf scaladoc
 mkdir scaladoc
 rm -rf ~/.m2/repository/at/logic
-JAVA_OPTS="-Xmx3g" scaladoc -d scaladoc -classpath $(find ~/.m2/repository/ -iname *.jar|xargs| tr ' ' ':') $(find source -regextype posix-extended -regex '.*\.(scala|java)$'|grep -v Test)
+JAVA_OPTS="-Xmx3g" scaladoc -d scaladoc -classpath $(find ~/.m2/repository/ -iname *.jar|xargs| tr ' ' ':') $(find source -regextype posix-extended -regex '.*\.(scala|java)$'|grep -v Test|grep -v gui/treeviz)
