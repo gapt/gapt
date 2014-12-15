19dc518e0f3b3e1db46d23f962a462443dbb4902
Sebastian Zivota <szivota@gmail.com>
2014-12-15 15:30:10 +0000

    Added documentation and commits for the runOnProver9Proofs script.
    git-svn-id: http://gapt.googlecode.com/svn/trunk@2365 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- testing/runOnProver9Proofs.scala
+++ testing/runOnProver9Proofs.scala
@@ -4,15 +4,36 @@ import at.logic.cli.GAPScalaInteractiveShellLibrary.loadProver9LKProof
 import at.logic.utils.executionModels.timeout._
 
 object runOnProver9Proofs {
+  /** The base prover9 path.
+   *
+   */
   val basePath = "../testing/TSTP/prover9/"
+
+  /** Where log files will be stored.
+   *
+   */
   val logDirPath = "../testing/logs"
+
+  /** The maximum time (in seconds) a test is allowed to take.
+   *
+   */
   val timeOut = 60
 
+  /** Returns a list of files, sorted by file size in ascending order.
+   *
+   * @param path The path (relative to basePath) to search for files. Defaults to "", i.e. if called with no argument, all files in the prover9 directory will be returned.
+   * @return A list of files.
+   */
   def getFileList(path: String = "") = getFileListRec(basePath + path) sortBy {
     p => new File(p).length()
   }
 
-  def getFileListRec(path: String): List[String]  = {
+  /** The recursive function that actually performs the file search.
+   *
+   * @param path The path to search for files.
+   * @return List of files below path.
+   */
+  private def getFileListRec(path: String): List[String]  = {
     val file = new File(path)
     if (file.isDirectory) {
       val children = file.listFiles
@@ -24,11 +45,21 @@ object runOnProver9Proofs {
       Nil
   }
 
+  /** Applies a test function to all files below a given path.
+    *
+    * Curried so that you can write something like
+    * {{{val test = runOnProver9Proofs(someFunction)
+    * test("ALG")}}}
+   *
+   * @param f The test to be performed.
+   * @param path The path below which the test should be performed, relative to basePath. Defaults to "", i.e. all files below basePath will be tested.
+   */
   def apply(f: (String) => Unit)(path: String = ""): Unit = {
     val files = getFileList(path)
 
     val logDir = new File(logDirPath)
 
+    // Clean up existing log files
     if (logDir.exists()) {
       val logs = logDir.listFiles()
       logs.foreach {_.delete()}
@@ -36,11 +67,17 @@ object runOnProver9Proofs {
     }
 
 
+    // Perform the test on each file
     files.foreach {f}
   }
 
+  /** Tests whether Prover9 import works.
+   *
+   * @param path The path of the proof to be imported.
+   */
   def testImport(path: String): Unit = {
 
+    // Name of the current subdirectory, i.e. ALG, ANA, etc
     val currentDir =  {
       val tmp = new File(path)
       tmp.getParentFile.getParentFile.getName
@@ -51,16 +88,16 @@ object runOnProver9Proofs {
     if (!logDir.exists())
       logDir.mkdir()
 
-    println(path)
+    // Try importing the given file for at most timeOut seconds
     try {
       withTimeout(1000 * timeOut)
       {loadProver9LKProof(path)}
     }
 
+    // The catch block is the interesting part of this function. My use case was to log only LKRuleCreationExceptions and disregard all others.
     catch {
       case e: LKRuleCreationException =>
         val logFileName = logDirPath + currentDir + ".txt"
-        println(logFileName)
         val writer = new FileWriter(logFileName, true)
         writer.append(path+"\n")
         writer.append(e.toString+"\n\n")
@@ -69,5 +106,4 @@ object runOnProver9Proofs {
       case _: Throwable =>
     }
   }
-  //println("Proof successfully imported.")
 }
