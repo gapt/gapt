d35ea1d36251e00ebab3239542af5479a881fa89
Gabriel Ebner <gebner@gebner.org>
2015-01-14 11:15:30 +0000

    Add SMT-LIB VeriT import regression test.
    git-svn-id: http://gapt.googlecode.com/svn/trunk@2394 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

---
+++ testing/src/test/scala/SmtLibVeritImport.scala
@@ -0,0 +1,26 @@
+package at.logic.testing.verit
+
+import java.io.File
+
+import at.logic.parsing.veriT.VeriTParser
+import at.logic.utils.executionModels.timeout.withTimeout
+import org.specs2.mutable._
+
+class SmtLibVeritImport extends Specification {
+  "SMT-LIB VeriT proof import" should {
+    for (file <- recursiveListFiles(new File("testing/veriT-SMT-LIB")).take(5) if file.getName.endsWith(".proof_flat")) {
+      file.getName in {
+        withTimeout(60 * 1000) {
+          VeriTParser.getExpansionProof(file.getAbsolutePath)
+          ok
+        }
+      }
+    }
+  }
+
+  def recursiveListFiles(f: File): List[File] =
+    if (f.isDirectory)
+      f.listFiles.toList.flatMap(recursiveListFiles)
+    else
+      List(f)
+}
