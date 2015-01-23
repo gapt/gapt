607f50b337ea9bb4d2ecae617fa83f7d5afff01a
Giselle Reis <giselle.mnr@gmail.com>
2015-01-23 12:30:38 +0000

    Organizing fixSymmetry source
    Removing the logger and trace messages which were
    included when the file was created and never again
    modified.
    
    git-svn-id: http://gapt.googlecode.com/svn/trunk@2420 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- src/main/scala/at/logic/algorithms/resolution/symmetry.scala
+++ src/main/scala/at/logic/algorithms/resolution/symmetry.scala
@@ -8,21 +8,18 @@ import at.logic.language.fol.{Equation => FOLEquation, FOLTerm, FOLFormula, FOLE
 import at.logic.calculi.resolution.{FClause, Clause}
 import at.logic.algorithms.lk.{applySubstitution => applySub, CleanStructuralRules, CloneLKProof}
 
-
 /**
   Sometimes, we have a resolution refutation of a set of clauses C
   and want a refutation of a set C', such that C' is C modulo applications
   of symmetry. This algorithm transforms a refutation of C to a refutation
   of C'.
 **/
-object fixSymmetry extends at.logic.utils.logging.Logger {
+object fixSymmetry {
 
   private def getSymmetryMap( to: FClause, from: FSequent ) = {
-    trace("computing symmetry map from " + from + " to " + to)
     var err = false
 
     def createMap( from: Seq[FOLFormula], to: Seq[FOLFormula] ) = {
-      trace("computing map from " + from + " to " + to)
       to.foldLeft( HashMap[FOLFormula, FOLFormula]() )( (map, to_f) => {
         val from_f = from.find( from_f => (from_f == to_f) || ( (from_f, to_f) match
         {
@@ -32,7 +29,6 @@ object fixSymmetry extends at.logic.utils.logging.Logger {
 
         if ( from_f != None )
           map + (( from_f.get, to_f ))
-          // from -= from_f.get
         else {
           err = true
           map 
@@ -59,7 +55,6 @@ object fixSymmetry extends at.logic.utils.logging.Logger {
 
   private def applySymm( p: RobinsonResolutionProof, f: FOLFormula, pos: Boolean ) =
   {
-    trace("in apply sym with f = " + f + ", pos = " + pos)
     val (left, right) = f match {
       case FOLEquation(l, r) => (l, r)
     }
@@ -81,7 +76,6 @@ object fixSymmetry extends at.logic.utils.logging.Logger {
   }
 
   private def deriveBySymmetry( to: FClause, from: FSequent ) = {
-    trace("deriving " + to + " from " + from + " by symmetry")
     val (neg_map, pos_map) = getSymmetryMap( to, from ).get
 
     val init = InitialClause(from.antecedent.map(_.asInstanceOf[FOLFormula]), from.succedent.map(_.asInstanceOf[FOLFormula]))
@@ -99,8 +93,6 @@ object fixSymmetry extends at.logic.utils.logging.Logger {
   private def handleInitialClause( cls: FClause, cs: Seq[FSequent] ) =
     cs.find( c => canDeriveBySymmetry( cls, c ) ) match {
       case None => {
-        trace("Could not find a clause which is equal to this clause modulo symmetry: " + cls)
-        trace("Clause set: " + cs)
         InitialClause(cls)
       }
       case Some( c ) => {
@@ -115,32 +107,29 @@ object fixSymmetry extends at.logic.utils.logging.Logger {
   def rec( p: RobinsonResolutionProof)(implicit cs: Seq[FSequent] ) : RobinsonResolutionProof = {
     var fac = false
     val res = p match {
-    case InitialClause(cls) => handleInitialClause( cls.toFClause, cs )
-
-    case Factor(r, p, a, s) => {
-      fac = true 
-      a match {
-        case lit1 :: Nil => {
-          val pos = p.root.succedent.contains(lit1.head)
-          Factor(rec(p), lit1.head.formula, lit1.size, pos, s)
+      case InitialClause(cls) => handleInitialClause( cls.toFClause, cs )
+     
+      case Factor(r, p, a, s) => {
+        fac = true 
+        a match {
+          case lit1 :: Nil => {
+            val pos = p.root.succedent.contains(lit1.head)
+            Factor(rec(p), lit1.head.formula, lit1.size, pos, s)
+          }
+          case lit1::lit2::Nil =>
+            Factor(rec(p), lit1.head.formula, lit1.size, lit2.head.formula, lit2.size, s)
+          case _ => throw new Exception("Factor rule for "+p.root+" does not have one or two primary formulas!")
         }
-        case lit1::lit2::Nil =>
-          Factor(rec(p), lit1.head.formula, lit1.size, lit2.head.formula, lit2.size, s)
-        case _ => throw new Exception("Factor rule for "+p.root+" does not have one or two primary formulas!")
       }
+      case Variant(r, p, s) => Variant( rec( p  ), s )
+      case Resolution(r, p1, p2, a1, a2, s) => 
+	Resolution( rec( p1 ), rec( p2 ), a1.formula.asInstanceOf[FOLFormula], a2.formula.asInstanceOf[FOLFormula], s )
+      case Paramodulation(r, p1, p2, a1, a2, p, s) => 
+        Paramodulation( rec( p1 ), rec( p2 ), a1.formula.asInstanceOf[FOLFormula], a2.formula.asInstanceOf[FOLFormula], p.formula.asInstanceOf[FOLFormula], s, p2.root.succedent.contains(a2))
+      // this case is applicable only if the proof is an instance of RobinsonProofWithInstance
+      case Instance(_,p,s) => Instance(rec(p),s)
     }
-    case Variant(r, p, s) => Variant( rec( p  ), s )
-    case Resolution(r, p1, p2, a1, a2, s) => Resolution( rec( p1 ), rec( p2 ), a1.formula.asInstanceOf[FOLFormula], a2.formula.asInstanceOf[FOLFormula], s )
-    case Paramodulation(r, p1, p2, a1, a2, p, s) => 
-      Paramodulation( rec( p1 ), rec( p2 ), a1.formula.asInstanceOf[FOLFormula], a2.formula.asInstanceOf[FOLFormula], p.formula.asInstanceOf[FOLFormula], s, p2.root.succedent.contains(a2))
-    // this case is applicable only if the proof is an instance of RobinsonProofWithInstance
-    case Instance(_,p,s) => Instance(rec(p),s)
+    (res.root.positive ++ res.root.negative).foreach( fo => assert(fo.formula.isInstanceOf[FOLFormula]))
+    res
   }
-  (res.root.positive ++ res.root.negative).foreach( fo => assert(fo.formula.isInstanceOf[FOLFormula]))
-  if (fac) {
-    trace("old: " + p.root)
-    trace("new: " + res.root)
-  }
-  res
-}
 }
