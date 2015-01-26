f1296ed0d73cb8f9bc50aef17986119dc52f230a
Giselle Reis <giselle.mnr@gmail.com>
2015-01-26 13:16:37 +0000

    Fixing syntax for veriT exporter
    Checks if the exported strings do not conflict
    with keywords in veriT and renames them when
    necessary.
    
    git-svn-id: http://gapt.googlecode.com/svn/trunk@2424 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- src/main/scala/at/logic/parsing/veriT/VeriTExporter.scala
+++ src/main/scala/at/logic/parsing/veriT/VeriTExporter.scala
@@ -54,9 +54,9 @@ object VeriTExporter {
     )
     symbols.foldLeft("(declare-sort S 0)\n") { case (acc, t) => t._3 match {
       // It is an atom
-      case To => acc ++ "(declare-fun " + toASCIIString(t._1) + " (" + "S "*t._2 + ") Bool)\n"
+      case To => acc ++ "(declare-fun " + toSMTString(t._1) + " (" + "S "*t._2 + ") Bool)\n"
       // It is a function
-      case Ti => acc ++ "(declare-fun " + toASCIIString(t._1) + " (" + "S "*t._2 + ") S)\n"
+      case Ti => acc ++ "(declare-fun " + toSMTString(t._1) + " (" + "S "*t._2 + ") S)\n"
       case _ => throw new Exception("Unexpected type for function or predicate: " + t._3)
     }
     }
@@ -66,12 +66,12 @@ object VeriTExporter {
   // (Note: here we would only use propositional formulas, but it is already
   // implemented for quantifiers just in case...)
   private def getSymbols(f: FOLExpression) : Set[(String, Int, TA)] = f match {
-    case FOLVar(s) => Set( (toASCIIString(s), 0, Ti) )
-    case FOLConst(s) => Set( (toASCIIString(s), 0, Ti) )
+    case FOLVar(s) => Set( (toSMTString(s), 0, Ti) )
+    case FOLConst(s) => Set( (toSMTString(s), 0, Ti) )
     case Atom(pred, args) => 
-      Set( (toASCIIString(pred), args.size, f.exptype) ) ++ args.foldLeft(Set[(String, Int, TA)]())( (acc, f) => getSymbols(f) ++ acc)
+      Set( (toSMTString(pred), args.size, f.exptype) ) ++ args.foldLeft(Set[(String, Int, TA)]())( (acc, f) => getSymbols(f) ++ acc)
     case Function(fun, args) => 
-      Set( (toASCIIString(fun), args.size, f.exptype) ) ++ args.foldLeft(Set[(String, Int, TA)]())( (acc, f) => getSymbols(f) ++ acc)
+      Set( (toSMTString(fun), args.size, f.exptype) ) ++ args.foldLeft(Set[(String, Int, TA)]())( (acc, f) => getSymbols(f) ++ acc)
     case And(f1, f2) => getSymbols(f1) ++ getSymbols(f2)
     case Or(f1, f2) => getSymbols(f1) ++ getSymbols(f2)
     case Imp(f1, f2) => getSymbols(f1) ++ getSymbols(f2)
@@ -84,16 +84,16 @@ object VeriTExporter {
   private def toSMTFormat(f: FOLExpression) : String = f match {
     case TopC => "true"
     case BottomC => "false"
-    case FOLVar(s) => toASCIIString(s)
-    case FOLConst(s) => toASCIIString(s)
+    case FOLVar(s) => toSMTString (s)
+    case FOLConst(s) => toSMTString (s)
     case Atom(pred, args) => 
-      if(args.size == 0) {
-        toASCIIString( pred )
+      if (args.size == 0) {
+        toSMTString (pred)
       } else {
-        "(" + toASCIIString(pred) + " " + args.foldLeft("")( (acc, t) => toSMTFormat(t) + " " + acc) + ")"
+        "(" + toSMTString (pred) + " " + args.foldLeft("")( (acc, t) => toSMTFormat(t) + " " + acc) + ")"
       }
     // Functions should have arguments.
-    case Function(fun, args) => "(" + toASCIIString(fun) + " " + args.foldRight("")( (t, acc) => toSMTFormat(t) + " " + acc) + ")"
+    case Function(fun, args) => "(" + toSMTString (fun) + " " + args.foldRight("")( (t, acc) => toSMTFormat(t) + " " + acc) + ")"
     case And(f1, f2) => "(and " + toSMTFormat(f1) + " " + toSMTFormat(f2) + ")"
     case Or(f1, f2) => "(or " + toSMTFormat(f1) + " " + toSMTFormat(f2) + ")"
     case Imp(f1, f2) => "(=> " + toSMTFormat(f1) + " " + toSMTFormat(f2) + ")"
@@ -101,16 +101,31 @@ object VeriTExporter {
     case _ => throw new Exception("Undefined formula for SMT: " + f)
   }
 
-  // TODO: move this somewhere else...
-  private def toASCIIString(s: SymbolA) : String = toASCIIString(s.toString)
-  private def toASCIIString(s: String) : String = {
+  // Note: not an exhaustive list
+  private val smt_keywords = List(
+    "set-logic", 
+    "QF_UF", 
+    "declare-sort", 
+    "declare-fun", 
+    "assert",
+    "not", "and", "or",
+    "as",
+    "true", "false")
+
+  // Transforms the string into ASCII and checks if 
+  // it does not clash with veriT keywords.
+  private def toSMTString(s: SymbolA) : String = toSMTString(s.toString)
+  private def toSMTString(s: String) : String = {
       // It's a number, append a character before it.
-      if ( s.forall(c => Character.isDigit(c)) ) {
+      val ascii = if ( s.forall(c => Character.isDigit(c)) ) {
         "n" + s
       } 
       else {
         // Attempt #3
         StringEscapeUtils.escapeJava(s).replaceAll("""\\""", "")
       }
+
+      if (smt_keywords.contains(ascii)) ascii + "_clash"
+      else ascii
   }
 }
