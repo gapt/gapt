913e6017d088adf5efd49698c87c9911577e146d
Stefan Hetzl <stefan.hetzl@gmail.com>
2015-01-27 17:01:56 +0000

    adding minisat-validaton to regression tests for veriT SMT-LIB
    git-svn-id: http://gapt.googlecode.com/svn/trunk@2434 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- testing/src/test/scala/SmtLibVeritImport.scala
+++ testing/src/test/scala/SmtLibVeritImport.scala
@@ -1,7 +1,10 @@
 package at.logic.testing.verit
 
+import at.logic.calculi.expansionTrees._
 import at.logic.parsing.veriT.VeriTParser
+import at.logic.provers.minisat._
 import at.logic.testing.{skipIfRunsLongerThan, recursiveListFiles}
+
 import org.specs2.mutable._
 
 class SmtLibVeritImport extends Specification {
@@ -16,3 +19,20 @@ class SmtLibVeritImport extends Specification {
     }
   }
 }
+
+class SmtLibVeritImportValidation extends Specification {
+  "SMT-LIB VeriT proof import and validation" should {
+    for (file <- recursiveListFiles("testing/veriT-SMT-LIB") if file.getName.endsWith(".proof_flat")) {
+      file.getName in {
+        skipIfRunsLongerThan(2 minute) {
+          val E = VeriTParser.getExpansionProof(file.getAbsolutePath)
+          E.isDefined must beTrue
+          val deep = toDeep( E.get ).toFormula
+          (new MiniSATProver).isValid( deep ) must beTrue
+          ok
+        }  
+      }
+    }
+  }
+}
+
