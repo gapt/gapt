17edaee437b3779ddff64ac140d9d2ead4d853bc
Gabriel Ebner <gebner@gebner.org>
2015-02-04 13:49:19 +0100

    Add more beauty to the CLI.

--- src/main/scala/at/logic/cli/CLIMain.scala
+++ src/main/scala/at/logic/cli/CLIMain.scala
@@ -1,20 +1,26 @@
-/*
- * GAPScalaInteractiveShellLibrary.scala
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
-
 package at.logic.cli
 
-import scala.tools.nsc.MainGenericRunner
-import at.logic.cli.GAPScalaInteractiveShellLibrary._
-import at.logic.language.fol._
-import java.io._
+import scala.sys.SystemProperties
+import scala.tools.nsc.interpreter._
+import scala.tools.nsc.Settings
 
 object CLIMain {
 
-  val script = """  
+  val welcomeMessage = """
+    *************************************
+    *    Welcome to the GAPT shell!     *
+    *                                   *
+    *  See help for a list of commands. *
+    *************************************
+
+ Copyright (C) 2009-2015  GAPT developers
+
+ This program comes with ABSOLUTELY NO WARRANTY. This is free
+ software, and you are welcome to redistribute it under certain
+ conditions; type `copying' for details.
+"""
+
+  val imports = """
   import at.logic.cli.GAPScalaInteractiveShellLibrary._
   import at.logic.language.lambda.types._
   import at.logic.language._
@@ -35,27 +41,29 @@ object CLIMain {
   import at.logic.gui.prooftool.gui.{Main => PT}
   import help.{apply => help}
   import at.logic.cli.GPL.{apply => copying, printLicense => license}
-
-  println()
-  println("    *************************************")
-  println("    *    Welcome to the GAPT shell!     *")
-  println("    *  See help for a list of commands. *")
-  println("    *************************************")
-  println()
-  println(" GAPT Copyright (C) 2014")
-  println(" This program comes with ABSOLUTELY NO WARRANTY. This is free")
-  println(" software, and you are welcome to redistribute it under certain")
-  println(" conditions; type `copying' for details.")
-  println()
   """
 
   def main(args: Array[String]) {
-    val f = File.createTempFile("cli-script", ".scala")
-    f.deleteOnExit()
-    val w = new BufferedWriter( new FileWriter(f) )
-    w.write(script)
-    w.close
-    MainGenericRunner.main(Array("-usejavacp","-i",f.getCanonicalPath, "-Yrepl-sync"))
-    sys.exit(0)
+    val settings = new Settings
+    settings.Yreplsync.value = true
+    settings.usejavacp.value = true
+
+    new SystemProperties += ("scala.shell.prompt" -> "gapt> ")
+
+    val repl = new ILoop {
+      override def printWelcome = {
+        println(welcomeMessage)
+        intp.beQuietDuring { processLine(imports) }
+
+        // If invoked as ./cli.sh script.scala,
+        // then load script.scala and exit.
+        if (args.length >= 1) {
+          withFile(args(0)) { f => interpretAllFrom(f) }
+          sys.exit(0)
+        }
+      }
+    }
+
+    repl process settings
   }
 }
