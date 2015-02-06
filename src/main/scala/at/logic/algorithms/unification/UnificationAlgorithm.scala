c35081337844a85fd8880fc8c95f6c90671a279c
frehn <fraini@gmail.com>
2015-02-06 09:46:46 +0100

    Improving documentation of unify method

--- src/main/scala/at/logic/algorithms/unification/UnificationAlgorithm.scala
+++ src/main/scala/at/logic/algorithms/unification/UnificationAlgorithm.scala
@@ -17,7 +17,7 @@ trait FinitaryUnification {
    * Calculates the complete set of most general unifiers of the terms term1 and term2.
    * @param term1 one of the terms to unify. formulas are also allowed, so we accept FOL expressions
    * @param term2 one of the terms to unify. formulas are also allowed, so we accept FOL expressions
-   * @return a list of mgus
+   * @return a list of mgus, the empty list means that term1 and term2 are not unifiable.
    */
   def unify( term1: FOLExpression, term2: FOLExpression ): List[Substitution]
 }
