77b558f14d3c8f396a9eb3b5135f77d2ee1e2cbf
Gabriel Ebner <gebner@gebner.org>
2015-02-16 13:56:56 +0100

    hoare: Array initialization example: Use prover9 syntax.

--- examples/hoare/array_init.scala
+++ examples/hoare/array_init.scala
@@ -4,15 +4,15 @@ import at.logic.language.hol.Neg
 import at.logic.language.hoare.{ForLoop, SimpleLoopProblem}
 
 val p = parse.program("for y < z do x := set(x, s(y), get(x, y)) od")
-val f = parse.fol("Imp Leq(k, z) =(get(x,k), get(x,o))")
-val g_s = parse.fol("Forall x Neg =(s(x), o)")
-val g_lr = parse.fol("Forall x Leq(x,x)")
-val g_0l = parse.fol("Forall x Leq(o,x)")
-val g_l0 = parse.fol("Forall x Imp Leq(x,o) =(o,x)") // order of equality is important...
-val g_sl = parse.fol("Forall x Forall y Imp Leq(x,y) Leq(x,s(y))")
-val g_ls = parse.fol("Forall x Forall y Imp Leq(x,s(y)) Or Leq(x,y) =(s(y),x)")
-val g_ge = parse.fol("Forall x Forall y Forall z =(get(set(x,y,z),y), z)")
-val g_gn = parse.fol("Forall x Forall y Forall z Forall w Imp Neg =(w,y) =(get(set(x,y,z),w), get(x,w))")
+val f = parse.p9("k <= z -> get(x,k) = get(x,o)")
+val g_s = parse.p9("(all x (s(x) != o))")
+val g_lr = parse.p9("(all x (x <= x))")
+val g_0l = parse.p9("(all x (o <= x))")
+val g_l0 = parse.p9("(all x (x <= o -> o = x))") // order of equality is important...
+val g_sl = parse.p9("(all x (all y (x <= y -> x <= s(y))))")
+val g_ls = parse.p9("(all x (all y (x <= s(y) -> x <= y | s(y) = x)))")
+val g_ge = parse.p9("(all x (all y (all z (get(set(x,y,z),y) = z))))")
+val g_gn = parse.p9("(all x (all y (all z (all w (w != y -> get(set(x,y,z),w) = get(x,w))))))")
 val g = List(g_s, g_lr, g_0l, g_l0, g_sl, g_ls, g_ge, g_gn)
 
 val slp = new SimpleLoopProblem(p.asInstanceOf[ForLoop], g, parse.fol("T()"), f)
