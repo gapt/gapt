71709c1abde14172a26cf6cc18f7ff0a8b3bd221
Olivier ROLAND <olivier.roland@edla.org>
2015-02-28 23:37:58 +0100

    Fix interface to Vampire prover
    Binary name for each OS is now automatically detected
    Nota: the binary for OSX is vampire_mac to match upstream vampires.tgz content

--- src/main/scala/at/logic/provers/vampire/vampire.scala
+++ src/main/scala/at/logic/provers/vampire/vampire.scala
@@ -1,7 +1,5 @@
 /**
- * Interface to the Prover9 first-order theorem prover.
- * Needs the command-line tools prover9 and tptp_to_ladr
- * to work.
+ * Interface to the Vampire first-order theorem prover.
  */
 
 package at.logic.provers.vampire
@@ -11,16 +9,15 @@ import at.logic.parsing.language.tptp.TPTPFOLExporter
 
 import java.io._
 import scala.io.Source
+import java.nio.file.{ Paths, Files }
 
 class VampireException( msg: String ) extends Exception( msg )
 
-object Vampire {
+object Vampire extends at.logic.utils.logging.Logger {
 
   def writeProblem( named_sequents: List[Tuple2[String, FSequent]], file_name: String ) =
     {
-
       val tptp = TPTPFOLExporter.tptp_problem_named( named_sequents )
-      //println("created tptp input: " + tptp)
       val writer = new FileWriter( file_name )
       writer.write( tptp )
       writer.flush
@@ -29,26 +26,20 @@ object Vampire {
   // TODO: this does not really belong here, refactor?
   // executes "prog" and feeds the contents of the file at
   // path "in" to its standard input.
-  private def exec( prog_alternatives: List[String], in: String ): ( Int, String ) =
-    {
-      for ( prog <- prog_alternatives ) {
-        try {
-          val p = Runtime.getRuntime.exec( prog )
+  private def exec( in: String ): ( Int, String ) = {
+    if ( !vampireBinaryName.isDefined )
+      throw new VampireException( "Unable to determine vampire's binary name for your OS!" )
+    val prog = vampireBinaryName.get
+    val p = Runtime.getRuntime.exec( prog )
 
-          val out = new OutputStreamWriter( p.getOutputStream )
-          out.write( Source.fromInputStream( new FileInputStream( in ) ).mkString )
-          out.close
+    val out = new OutputStreamWriter( p.getOutputStream )
+    out.write( Source.fromInputStream( new FileInputStream( in ) ).mkString )
+    out.close
 
-          val str = Source.fromInputStream( p.getInputStream ).mkString
-          p.waitFor
-          ( p.exitValue, str )
-        } catch {
-          case e: IOException => println( "could not find binary " + prog + " in path!" );
-        }
-      }
-
-      throw new VampireException( "Could not execute vampire binary!" )
-    }
+    val str = Source.fromInputStream( p.getInputStream ).mkString
+    p.waitFor
+    ( p.exitValue, str )
+  }
 
   def writeToFile( str: String, file: String ) = {
     val out = new FileWriter( file )
@@ -56,11 +47,10 @@ object Vampire {
     out.close
   }
 
-  def refute( input_file: String, output_file: String ): Int = {
-    //TODO: find correct binary (lin32, lin64, mac)
-    val ret = exec( List( "vampire_lin64", "vampire_lin32", "vampire_macosx" ), input_file )
+  def refute( input_file: String, output_file: String ): ( Int, String ) = {
+    val ret = exec( input_file )
     writeToFile( ret._2, output_file )
-    ret._1
+    ret
   }
 
   def refuteNamed( named_sequents: List[Tuple2[String, FSequent]], input_file: String, output_file: String ): Boolean =
@@ -68,9 +58,10 @@ object Vampire {
       writeProblem( named_sequents, input_file )
 
       val ret = refute( input_file, output_file )
-      ret match {
-        case 0 => true
-        case _ => throw new VampireException( "The set was satisfiable or there was a problem executing vampire!" )
+      ret._1 match {
+        case 0 if ret._2.startsWith( "Refutation found" ) => true
+        case 0 if ret._2.startsWith( "Satisfiable" ) => false
+        case _ => throw new VampireException( "There was a problem executing vampire!" )
       }
     }
 
@@ -79,11 +70,34 @@ object Vampire {
 
   def refute( sequents: List[FSequent] ): Boolean = {
     val in_file = File.createTempFile( "gapt-vampire", ".tptp", null )
-    val out_file = File.createTempFile( "gapt-vampire", "prover9", null )
+    val out_file = File.createTempFile( "gapt-vampire", "vampire", null )
     val ret = refute( sequents, in_file.getAbsolutePath, out_file.getAbsolutePath )
     in_file.delete
     out_file.delete
     ret
+  }
+
+  val vampireBinaryName: Option[String] = {
+    val osName = System.getProperty( "os.name" ).toLowerCase()
+    val osArch = System.getProperty( "os.arch" )
+    osName match {
+      case osName if osName.contains( "mac" ) => Some( "vampire_mac" )
+      case osName if osName.contains( "linux" ) && osArch.contains( "64" ) => Some( "vampire_lin64" )
+      case osName if osName.contains( "linux" ) && osArch.contains( "32" ) => Some( "vampire_lin32" )
+      case _ => None
+    }
+  }
+
+  def isInstalled(): Boolean = {
+    val box = List()
+    try {
+      Vampire.refute( box )
+    } catch {
+      case e: Throwable =>
+        warn( e.getMessage )
+        return false
+    }
+    return true
   }
 
 }
