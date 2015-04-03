56449c193e861eae4fa55bb53726c3eb5ee63992
Stefan Hetzl <stefan.hetzl@tuwien.ac.at>
2015-04-03 15:13:33 +0200

    add 1st exercise for course on automated deduction

---
+++ examples/exercises/exercise1.scala
@@ -0,0 +1,47 @@
+// setup
+:load examples/FormulaSequences.scala
+import at.logic.gapt.language.hol._
+import at.logic.gapt.provers.minisat.MiniSATProver
+import at.logic.gapt.proofs.resolution.algorithms.CNFp
+import at.logic.gapt.proofs.resolution.algorithms.TseitinCNF
+val minisat = new MiniSATProver
+
+println( """
+* Exercise A
+ Get the formula φ_3 from Example 2.1 of the course notes by:
+ gapt> PQPairs( 3 )
+
+ Transform it to CNF using the distributivity-based algorithm by:
+ gapt> CNFp( PQPairs( 3 ))
+
+ Transform it to CNF using the Tseitin-transformation by:
+ gapt> TseitinCNF( PQPairs( 3 ))
+
+ Transform φ_n for a few n larger than 3 to CNF using both of the above methods.
+
+ * What are (approximately) the largest n s.t. CNFp/TseitinCNF finishes within 5 seconds?
+
+ * What are |φ_n| for these n?
+
+ You can measure the time a command needs as follows:
+ gapt> time { TseitinCNF( PQPairs( 3 )) }
+
+ You can compute the logical complexity |φ_3| of φ_3 as follows:
+ gapt> lcomp( PQPairs( 3 ))
+
+* Exercise B
+  Get the 5th Buss tautology by:
+  gapt> BussTautology( 5 )
+
+  Get the n-th pigeon hole principle tautology by:
+  gapt> PigeonHolePrinciple( n, n-1 )
+
+  Apply minisat to these by:
+  gapt> minisat.isValid( PigeonHolePrinciple( 5, 4 ))
+  gapt> minisat.isValid( BussTautology( 5 ))
+
+ * What are (approximately) the largest n s.t. minisat finishes on the respective tautology within 5 seconds?
+
+ * What are |φ_n| for these n?
+""" )
+
