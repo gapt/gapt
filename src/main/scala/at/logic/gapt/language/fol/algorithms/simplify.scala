8ab0303c00076102ec4bcae9f43dff374906449f
frehn <fraini@gmail.com>
2015-04-07 10:50:56 +0200

    Formatting code.

--- src/main/scala/at/logic/gapt/language/fol/algorithms/simplify.scala
+++ src/main/scala/at/logic/gapt/language/fol/algorithms/simplify.scala
@@ -10,33 +10,33 @@ import at.logic.gapt.language.fol._
 object simplify {
   def apply( f: FOLFormula ): FOLFormula = f match {
     case FOLAnd( l, r ) => ( simplify( l ), simplify( r ) ) match {
-      case ( FOLTopC, r )        => r
-      case ( r, FOLTopC )        => r
-      case ( FOLBottomC, _ )     => FOLBottomC
-      case ( _, FOLBottomC )     => FOLBottomC
+      case ( FOLTopC, r )     => r
+      case ( r, FOLTopC )     => r
+      case ( FOLBottomC, _ )  => FOLBottomC
+      case ( _, FOLBottomC )  => FOLBottomC
       case ( l, r ) if l == r => l
       case ( l, r )           => FOLAnd( l, r )
     }
     case FOLOr( l, r ) => ( simplify( l ), simplify( r ) ) match {
-      case ( FOLTopC, _ )        => FOLTopC
-      case ( _, FOLTopC )        => FOLTopC
-      case ( FOLBottomC, r )     => r
-      case ( r, FOLBottomC )     => r
+      case ( FOLTopC, _ )     => FOLTopC
+      case ( _, FOLTopC )     => FOLTopC
+      case ( FOLBottomC, r )  => r
+      case ( r, FOLBottomC )  => r
       case ( l, r ) if l == r => l
       case ( l, r )           => FOLOr( l, r )
     }
     case FOLImp( l, r ) => ( simplify( l ), simplify( r ) ) match {
-      case ( FOLTopC, r )        => r
-      case ( _, FOLTopC )        => FOLTopC
-      case ( FOLBottomC, _ )     => FOLTopC
-      case ( r, FOLBottomC )     => simplify( FOLNeg( r ) )
+      case ( FOLTopC, r )     => r
+      case ( _, FOLTopC )     => FOLTopC
+      case ( FOLBottomC, _ )  => FOLTopC
+      case ( r, FOLBottomC )  => simplify( FOLNeg( r ) )
       case ( l, r ) if l == r => FOLTopC
       case ( l, r )           => FOLImp( l, r )
     }
     case FOLNeg( s ) => simplify( s ) match {
       case FOLTopC    => FOLBottomC
       case FOLBottomC => FOLTopC
-      case s       => FOLNeg( s )
+      case s          => FOLNeg( s )
     }
     case _ => f
   }
--- src/main/scala/at/logic/gapt/language/hol/algorithms/simplify.scala
+++ src/main/scala/at/logic/gapt/language/hol/algorithms/simplify.scala
@@ -5,33 +5,33 @@ import at.logic.gapt.language.hol._
 object simplify {
   def apply( f: HOLFormula ): HOLFormula = f match {
     case HOLAnd( l, r ) => ( simplify( l ), simplify( r ) ) match {
-      case ( HOLTopC, r )        => r
-      case ( r, HOLTopC )        => r
-      case ( HOLBottomC, _ )     => HOLBottomC
-      case ( _, HOLBottomC )     => HOLBottomC
+      case ( HOLTopC, r )     => r
+      case ( r, HOLTopC )     => r
+      case ( HOLBottomC, _ )  => HOLBottomC
+      case ( _, HOLBottomC )  => HOLBottomC
       case ( l, r ) if l == r => l
       case ( l, r )           => HOLAnd( l, r )
     }
     case HOLOr( l, r ) => ( simplify( l ), simplify( r ) ) match {
-      case ( HOLTopC, _ )        => HOLTopC
-      case ( _, HOLTopC )        => HOLTopC
-      case ( HOLBottomC, r )     => r
-      case ( r, HOLBottomC )     => r
+      case ( HOLTopC, _ )     => HOLTopC
+      case ( _, HOLTopC )     => HOLTopC
+      case ( HOLBottomC, r )  => r
+      case ( r, HOLBottomC )  => r
       case ( l, r ) if l == r => l
       case ( l, r )           => HOLOr( l, r )
     }
     case HOLImp( l, r ) => ( simplify( l ), simplify( r ) ) match {
-      case ( HOLTopC, r )        => r
-      case ( _, HOLTopC )        => HOLTopC
-      case ( HOLBottomC, _ )     => HOLTopC
-      case ( r, HOLBottomC )     => simplify( HOLNeg( r ) )
+      case ( HOLTopC, r )     => r
+      case ( _, HOLTopC )     => HOLTopC
+      case ( HOLBottomC, _ )  => HOLTopC
+      case ( r, HOLBottomC )  => simplify( HOLNeg( r ) )
       case ( l, r ) if l == r => HOLTopC
       case ( l, r )           => HOLImp( l, r )
     }
     case HOLNeg( s ) => simplify( s ) match {
       case HOLTopC    => HOLBottomC
       case HOLBottomC => HOLTopC
-      case s       => HOLNeg( s )
+      case s          => HOLNeg( s )
     }
     case _ => f
   }
