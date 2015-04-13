c9711c8b30ba503756056e0dbb9204f2528868d4
Sebastian Zivota <ouroboros@gmx.at>
2015-04-13 11:48:10 +0200

    Added FOL quantifier blocks and fixed induction ex

--- examples/simple/induction.scala
+++ examples/simple/induction.scala
@@ -1,41 +1,38 @@
-import at.logic.calculi.lk._
-import at.logic.gapt.proofs.lk.InductionRule
-import at.logic.language.fol._
-import at.logic.language.hol.{HOLExpression, AllVarBlock}
-import at.logic.language.fol.Substitution
+import at.logic.gapt.language.fol._
+import at.logic.gapt.proofs.lk._
 
 object inductionExamples {
 
   val (x,y,z) = (FOLVar("x"), FOLVar("y"), FOLVar("z"))
   val (a,b,c) = (FOLVar("α"), FOLVar("β"), FOLVar("γ"))
-  def S(x: FOLTerm) = Function("S", List(x))
-  def plus(x: FOLTerm, y: FOLTerm) = Function("+", List(x,y))
+  def S(x: FOLTerm) = FOLFunction("S", List(x))
+  def plus(x: FOLTerm, y: FOLTerm) = FOLFunction("+", List(x,y))
   val zero = FOLConst("0")
 
-  val assoc = AllVarBlock(List(x,y,z), Equation(plus(plus(x,y),z), plus(x, plus(y,z)))).asInstanceOf[FOLFormula]
-  val assocXYZ = Equation(plus(plus(a,b),c), plus(a, plus(b,c)))
-  val assocXY0 = Equation(plus(plus(a,b), zero), plus(a, plus(b, zero)))
-  val assocXYSZ = Equation(plus(plus(a,b),S(c)), plus(a, plus(b,S(c))))
+  val assoc = AllVarBlock(List(x,y,z), FOLEquation(plus(plus(x,y),z), plus(x, plus(y,z)))).asInstanceOf[FOLFormula]
+  val assocXYZ = FOLEquation(plus(plus(a,b),c), plus(a, plus(b,c)))
+  val assocXY0 = FOLEquation(plus(plus(a,b), zero), plus(a, plus(b, zero)))
+  val assocXYSZ = FOLEquation(plus(plus(a,b),S(c)), plus(a, plus(b,S(c))))
 
-  def add0(v: FOLTerm) = Equation(plus(v,zero), v)
-  val axAdd0 = AllVar(x, add0(x))
+  def add0(v: FOLTerm) = FOLEquation(plus(v,zero), v)
+  val axAdd0 = FOLAllVar(x, add0(x))
 
   def addS(u: FOLTerm, v: FOLTerm) =
-  Equation(
+  FOLEquation(
     plus(u, S(v)),
     S(plus(u,v))
   )
   val axAddS = AllVarBlock(List(x,y), addS(x,y))
 
-  def ref(t: FOLTerm) = Equation(t,t)
+  def ref(t: FOLTerm) = FOLEquation(t,t)
 
-  val eqXPYP0EXPY = Equation(plus(plus(a,b),zero), plus(a,b))
-  val eqYP0EY = Equation(plus(b,zero), b)
+  val eqXPYP0EXPY = FOLEquation(plus(plus(a,b),zero), plus(a,b))
+  val eqYP0EY = FOLEquation(plus(b,zero), b)
 
   val axXPYP0EXPY = Axiom(eqXPYP0EXPY)
   val axYP0EY = Axiom(eqYP0EY)
 
-  val refXPY = Axiom(Nil, List(Equation(plus(a,b),plus(a,b))))
+  val refXPY = Axiom(Nil, List(FOLEquation(plus(a,b),plus(a,b))))
 
   val inductionBase1 =
   Axiom(
@@ -48,7 +45,7 @@ object inductionExamples {
   inductionBase1,
   inductionBase1.root.antecedent.head,
   inductionBase1.root.succedent.head,
-  Equation(plus(a,b), plus(a,plus(b,zero)))
+  FOLEquation(plus(a,b), plus(a,plus(b,zero)))
   )
 
   val inductionBase3 =
--- src/main/scala/at/logic/gapt/language/fol/fol.scala
+++ src/main/scala/at/logic/gapt/language/fol/fol.scala
@@ -311,3 +311,80 @@ object FOLAllVar {
     case _ => None
   }
 }
+
+/**
+ * A block of existential quantifiers.
+ *
+ */
+object FOLExVarBlock {
+
+  /**
+   *
+   * @param vars A list of FOL variables v,,1,,, …, v,,n,,.
+   * @param sub The formula F to be quantified over.
+   * @return ∃v,,1,,…∃v,,n,,F
+   */
+  def apply( vars: List[FOLVar], sub: FOLFormula ): FOLFormula = vars match {
+    case Nil     => sub
+    case v :: vs => FOLExVar( v, FOLExVarBlock( vs, sub ) )
+  }
+
+  /**
+   *
+   * @param expression A FOLExpression
+   * @return If expression begins with an ∃-block: a pair consisting of the variables of the block and the quantified subformula.
+   */
+  def unapply( expression: FOLExpression ) = expression match {
+    case f: FOLFormula =>
+      val ( vars, sub ) = unapplyHelper( f )
+      if ( vars.nonEmpty ) Some( ( vars, sub ) )
+      else None
+    case _ => None
+  }
+
+  private def unapplyHelper( f: FOLFormula ): ( List[FOLVar], FOLFormula ) = f match {
+    case FOLExVar( v, sub ) =>
+      val ( subVars, subF ) = unapplyHelper( sub )
+      ( v :: subVars, subF )
+    case _ => ( Nil, f )
+  }
+}
+
+/**
+ * A block of universal quantifiers.
+ *
+ */
+object FOLAllVarBlock {
+
+  /**
+   *
+   * @param vars A list of FOL variables v,,1,,, …, v,,n,,.
+   * @param sub The formula F to be quantified over.
+   * @return ∀v,,1,,…∀v,,n,,F
+   */
+  def apply( vars: List[FOLVar], sub: FOLFormula ): FOLFormula = vars match {
+    case Nil     => sub
+    case v :: vs => FOLAllVar( v, FOLAllVarBlock( vs, sub ) )
+  }
+
+  /**
+   *
+   * @param expression A FOLExpression
+   * @return If expression begins with an ∀-block: a pair consisting of the variables of the block and the quantified subformula.
+   */
+  def unapply( expression: FOLExpression ) = expression match {
+    case f: FOLFormula =>
+      val ( vars, sub ) = unapplyHelper( f )
+      if ( vars.nonEmpty ) Some( ( vars, sub ) )
+      else None
+    case _ => None
+  }
+
+  private def unapplyHelper( f: FOLFormula ): ( List[FOLVar], FOLFormula ) = f match {
+    case FOLAllVar( v, sub ) =>
+      val ( subVars, subF ) = unapplyHelper( sub )
+      ( v :: subVars, subF )
+    case _ => ( Nil, f )
+  }
+}
+
