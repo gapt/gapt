6c0f58f4be9de7f1d3fcc860a633eaa02f4787ed
Gabriel Ebner <gebner@gebner.org>
2015-05-06 16:18:25 +0200

    First unit tests.

---
+++ src/test/scala/at/logic/gapt/expr/distinguishedConstantsTest.scala
@@ -0,0 +1,58 @@
+package at.logic.gapt.expr
+import at.logic.gapt.expr.types._
+import org.specs2.mutable._
+
+class DistinguishedConstantsTest extends Specification {
+  "Quantifiers" should {
+    "have correct type" in {
+      ForallC( Ti -> To ).exptype must_== ( ( ( Ti -> To ) -> To ) -> To )
+      ExistsC( Ti -> To ).exptype must_== ( ( ( Ti -> To ) -> To ) -> To )
+    }
+
+    "match themselves" in {
+      ForallC( Ti ) must beLike {
+        case ExistsC( _ )  => ko
+        case ForallC( Ti ) => ok
+      }
+
+      ExistsC( Tindex ) must beLike {
+        case ForallC( _ )      => ko
+        case ExistsC( Tindex ) => ok
+      }
+    }
+  }
+
+  "Propositional connectives" should {
+    "have correct type" in {
+      AndC().exptype must_== ( To -> ( To -> To ) )
+      OrC().exptype must_== ( To -> ( To -> To ) )
+      ImpC().exptype must_== ( To -> ( To -> To ) )
+
+      NegC().exptype must_== ( To -> To )
+
+      TopC().exptype must_== To
+      BottomC().exptype must_== To
+    }
+
+    "match themselves" in {
+      AndC() must beLike {
+        case OrC() | ImpC() | NegC() | TopC() | BottomC() => ko
+        case AndC()                                       => ok
+      }
+      OrC() must beLike {
+        case AndC() | ImpC() | NegC() | TopC() | BottomC() => ko
+        case OrC()                                         => ok
+      }
+    }
+  }
+
+  "Equality" should {
+    "have correct type" in {
+      EqC( Ti ).exptype must_== ( Ti -> ( Ti -> To ) )
+      EqC( Ti -> To ).exptype must_== ( ( Ti -> To ) -> ( ( Ti -> To ) -> To ) )
+    }
+    "match itself" in {
+      EqC( Ti ) must beLike { case EqC( Ti ) => ok }
+    }
+  }
+}
