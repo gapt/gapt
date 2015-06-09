d4b2f70c11601a44f67155fdef616d1596277251
Gabriel Ebner <gebner@gebner.org>
2015-06-09 13:51:30 +0200

    Add test for canonical solution.

---
+++ src/test/scala/at/logic/gapt/provers/inductionProver/CanonicalSolutionTest.scala
@@ -0,0 +1,45 @@
+package at.logic.gapt.provers.inductionProver
+import at.logic.gapt.expr._
+import at.logic.gapt.expr.fol.{ Utils, FOLSubstitution }
+import at.logic.gapt.proofs.expansionTrees.{ formulaToExpansionTree, ExpansionSequent }
+import at.logic.gapt.provers.sat4j.Sat4jProver
+import org.specs2.mutable._
+import org.specs2.specification.core.Fragment
+
+class CanonicalSolutionTest extends Specification {
+  import GeneralSIP._
+
+  val x = FOLVar( "x" )
+  val f0 = FOLAtom( "P", FOLConst( "0" ) )
+  val f1 = All( x, Imp( FOLAtom( "P", x ), FOLAtom( "P", FOLFunction( "s", x ) ) ) )
+  val f2 = FOLAtom( "P", alpha )
+
+  val seq0 = ExpansionSequent( Seq(), Seq() )
+  val seq1 = ExpansionSequent(
+    Seq( formulaToExpansionTree( f1, List( FOLSubstitution( x -> nu ) ), false ) ),
+    Seq() )
+  val seq2 = ExpansionSequent(
+    Seq( formulaToExpansionTree( f0, false ) ),
+    Seq( formulaToExpansionTree( f2, true ) ) )
+
+  val ts = List( FOLConst( "0" ) )
+  val us = List( FOLConst( "0" ) )
+
+  val sip = new SchematicSIP( seq0, seq1, seq2, Bottom(), ts, us )
+
+  val sol = Imp( FOLAtom( "P", FOLConst( "0" ) ), FOLAtom( "P", nu ) )
+
+  "canonicalSolution" should {
+    "have induction formula as consequence" in {
+      Fragment.foreach( 0 until 5 ) { i =>
+        s"C_$i" in {
+          val C_i = canonicalSolution( sip, i )
+          val imp = Imp( C_i, FOLSubstitution( nu -> Utils.numeral( i ) )( sol ) )
+          // we don't use equalities in this example, so we can use SAT.
+          new Sat4jProver().isValid( imp ) must beTrue
+        }
+      }
+    }
+  }
+
+}
