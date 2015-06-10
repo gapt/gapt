9d97a459ae3d0aa0d807087a121894448c8c566e
Gabriel Ebner <gebner@gebner.org>
2015-06-10 11:15:46 +0200

    Extract induction prover prototype into its own class.

--- examples/induction/prototype.scala
+++ examples/induction/prototype.scala
@@ -1,17 +1,10 @@
 import at.logic.gapt.expr._
-import at.logic.gapt.expr.fol.{Utils, FOLSubstitution}
+import at.logic.gapt.expr.fol.FOLSubstitution
 import at.logic.gapt.expr.hol.univclosure
 import at.logic.gapt.formats.prover9.Prover9TermParserLadrStyle.parseFormula
-import at.logic.gapt.grammars.findMinimalSipGrammar
-import at.logic.gapt.proofs.expansionTrees._
-import at.logic.gapt.proofs.lk.LKToExpansionProof
 import at.logic.gapt.proofs.lk.base.FSequent
-import at.logic.gapt.proofs.resolution.CNFp
 import at.logic.gapt.provers.inductionProver.GeneralSIP._
-import at.logic.gapt.provers.inductionProver.{GeneralSIP, canonicalSolution, decodeSipGrammar}
-import at.logic.gapt.provers.maxsat.QMaxSAT
-import at.logic.gapt.provers.prover9.Prover9Prover
-import at.logic.gapt.provers.veriT.VeriTProver
+import at.logic.gapt.provers.inductionProver._
 
 // doesn't work: associativity instances are too complicated
 val assocES = FSequent(
@@ -70,65 +63,7 @@ val endSequent = linearES
 
 println(s"Proving $endSequent")
 
-var instanceProofs = (0 until 3) map { n =>
-  val instanceSequent = FOLSubstitution(alpha -> Utils.numeral(n)).apply(endSequent)
-  println(s"[n=$n] Proving $instanceSequent")
-  n -> LKToExpansionProof(new Prover9Prover().getLKProof(instanceSequent).get)
-}
-
-// Ground the instance proofs.
-instanceProofs = instanceProofs map { case (n, expProof) =>
-  n -> substitute(
-    FOLSubstitution(freeVariables(toDeep(expProof).toFormula).
-      map { c => FOLVar(c.name) -> FOLConst(c.name) }.toSeq),
-    expProof)
-}
-
-//instanceProofs = instanceProofs map { case (n, expProof) =>
-//  val minExpProof = minimalExpansionSequent(expProof, new VeriTProver).get
-//  val removedInstances = extractInstances(expProof) diff extractInstances(minExpProof)
-//  println(s"[n=$n] Removing unnecessary instances $removedInstances")
-//  n -> minExpProof
-//}
-
-val termEncoding = InstanceTermEncoding(endSequent)
-val instanceLanguages = instanceProofs map { case (n, expSeq) =>
-  n -> termEncoding.encode(expSeq)
-}
-
-instanceLanguages foreach { case (n, l) =>
-  println(s"Instance language for n=$n:\n${l.map(_.toString).sorted.mkString("\n")}")
-}
-
-println("Finding grammar...")
-val grammar = findMinimalSipGrammar(instanceLanguages, new QMaxSAT)
-println(grammar)
-
-(0 until 10) foreach { n =>
-  val generatedInstanceSequent = FOLSubstitution(alpha -> Utils.numeral(n))(
-    termEncoding.decodeToFSequent(grammar.instanceGrammar(n).language))
-  println(s"Checking tautology of instance language for n=$n: "
-    + new VeriTProver().isValid(generatedInstanceSequent))
-  //  generatedInstanceSequent.antecedent.map(_.toString).sorted foreach println
-}
-
-val schematicSip = decodeSipGrammar(termEncoding, grammar)
-println(s"Gamma0 = ${schematicSip.Gamma0}")
-println(s"Gamma1 = ${schematicSip.Gamma1}")
-println(s"Gamma2 = ${schematicSip.Gamma2}")
-println(s"Sequent0 = ${schematicSip.Sequent0}")
-println(s"Sequent1 = ${schematicSip.Sequent1}")
-println(s"Sequent2 = ${schematicSip.Sequent2}")
-println(s"t = ${schematicSip.t}")
-println(s"u = ${schematicSip.u}")
-
-(0 until 3) foreach { i =>
-  val C_i = canonicalSolution(schematicSip, i)
-  println(s"C_$i =")
-  CNFp(C_i) foreach { clause => println(s"  $clause") }
-}
-
-// TODO: this is just a crutch so that we can test toLKProof...
+// TODO: just a stop-gap
 val solutionCandidates = Seq(
   "P(x,y)",
   "P(0) -> P(x)",
@@ -137,16 +72,24 @@ val solutionCandidates = Seq(
   FOLVar("x") -> GeneralSIP.nu,
   FOLVar("y") -> GeneralSIP.gamma,
   FOLVar("z") -> GeneralSIP.alpha)(parseFormula(s)))
-val maybeIndProof = solutionCandidates flatMap { cand =>
-  try {
-    Some(cand -> schematicSip.solve(cand).toLKProof)
-  } catch {
-    case _: Throwable => None
-  }
-} headOption
+val solutionFinder = new SolutionFinder {
+  override def findSolution(schematicSip: SchematicSIP): Option[FOLFormula] =
+    solutionCandidates find { cand =>
+      try {
+        schematicSip.solve(cand).toLKProof
+        true
+      } catch {
+        case _: Throwable => false
+      }
+    }
+}
+
+val sipProver = new SipProver(solutionFinder)
+
+val maybeIndProof = sipProver.getLKProofAndSolution(endSequent)
 
 maybeIndProof match {
-  case Some((solution, indProof)) =>
+  case Some((indProof, solution)) =>
     println(s"Found induction proof with solution $solution")
   case None =>
     println(s"Didn't find induction proof.")
---
+++ src/main/scala/at/logic/gapt/provers/inductionProver/SipProver.scala
@@ -0,0 +1,103 @@
+package at.logic.gapt.provers.inductionProver
+
+import at.logic.gapt.expr._
+import at.logic.gapt.expr.fol.{ Utils, FOLSubstitution }
+import at.logic.gapt.formats.veriT.VeriTParserException
+import at.logic.gapt.grammars.findMinimalSipGrammar
+import at.logic.gapt.proofs.expansionTrees._
+import at.logic.gapt.proofs.lk.LKToExpansionProof
+import at.logic.gapt.proofs.lk.base.{ LKProof, FSequent }
+import at.logic.gapt.proofs.resolution.CNFp
+import at.logic.gapt.provers.Prover
+import at.logic.gapt.provers.maxsat.QMaxSAT
+import at.logic.gapt.provers.prover9.{ Prover9, Prover9Prover }
+import at.logic.gapt.provers.veriT.VeriTProver
+import at.logic.gapt.utils.logging.Logger
+
+trait SolutionFinder {
+  def findSolution( schematicSIP: SchematicSIP ): Option[FOLFormula]
+}
+
+class SipProver( solutionFinder: SolutionFinder,
+                 instanceProver: Prover = new Prover9Prover(),
+                 instances: Seq[Int] = 0 until 3,
+                 minimizeInstanceLanguages: Boolean = false,
+                 quasiTautProver: Prover = new VeriTProver() )
+    extends Prover with Logger {
+  override def getLKProof( endSequent: FSequent ): Option[LKProof] = getLKProofAndSolution( endSequent ).map( _._1 )
+
+  def getLKProofAndSolution( endSequent: FSequent ): Option[( LKProof, FOLFormula )] = {
+    val inductionVariable = freeVariables( endSequent.formulas.toList.map( _.asInstanceOf[FOLExpression] ) ) match {
+      case singleton if singleton.size == 1 => singleton.head
+    }
+    require( inductionVariable == GeneralSIP.alpha ) // TODO: maybe relax this restriction
+
+    var instanceProofs = instances map { n =>
+      val instanceSequent = FOLSubstitution( inductionVariable -> Utils.numeral( n ) )( endSequent )
+      debug( s"[n=$n] Proving $instanceSequent" )
+      n -> LKToExpansionProof( instanceProver.getLKProof( instanceSequent ).get )
+    }
+
+    // Ground the instance proofs.
+    instanceProofs = instanceProofs map {
+      case ( n, expProof ) =>
+        n -> substitute(
+          FOLSubstitution( freeVariables( toDeep( expProof ).toFormula ).
+            map { c => FOLVar( c.name ) -> FOLConst( c.name ) }.toSeq ),
+          expProof )
+    }
+
+    if ( minimizeInstanceLanguages ) {
+      instanceProofs = instanceProofs map {
+        case ( n, expProof ) =>
+          val minExpProof = minimalExpansionSequent( expProof, new VeriTProver ).get
+          debug {
+            val removedInstances = extractInstances( expProof ) diff extractInstances( minExpProof )
+            s"[n=$n] Removing unnecessary instances $removedInstances"
+          }
+          n -> minExpProof
+      }
+    }
+
+    val termEncoding = InstanceTermEncoding( endSequent )
+    val instanceLanguages = instanceProofs map {
+      case ( n, expSeq ) =>
+        n -> termEncoding.encode( expSeq )
+    }
+
+    instanceLanguages foreach {
+      case ( n, l ) =>
+        debug( s"Instance language for n=$n:\n${l.map( _.toString ).sorted.mkString( "\n" )}" )
+    }
+
+    debug( "Finding grammar..." )
+    val grammar = findMinimalSipGrammar( instanceLanguages, new QMaxSAT )
+    debug( grammar toString )
+
+    ( 0 until 10 ) foreach { n =>
+      lazy val generatedInstanceSequent = FOLSubstitution( inductionVariable -> Utils.numeral( n ) )(
+        termEncoding.decodeToFSequent( grammar.instanceGrammar( n ).language ) )
+      debug( s"Checking tautology of instance language for n=$n: "
+        + new VeriTProver().isValid( generatedInstanceSequent ) )
+    }
+
+    val schematicSip = decodeSipGrammar( termEncoding, grammar )
+    debug( s"Gamma0 = ${schematicSip.Gamma0}" )
+    debug( s"Gamma1 = ${schematicSip.Gamma1}" )
+    debug( s"Gamma2 = ${schematicSip.Gamma2}" )
+    debug( s"Sequent0 = ${schematicSip.Sequent0}" )
+    debug( s"Sequent1 = ${schematicSip.Sequent1}" )
+    debug( s"Sequent2 = ${schematicSip.Sequent2}" )
+    debug( s"t = ${schematicSip.t}" )
+    debug( s"u = ${schematicSip.u}" )
+
+    ( 0 until 3 ) foreach { i =>
+      lazy val C_i = canonicalSolution( schematicSip, i )
+      debug( s"C_$i = ${CNFp( C_i ).map( clause => s"  $clause\n" )}" )
+    }
+
+    solutionFinder.findSolution( schematicSip ) map { solution =>
+      schematicSip.solve( solution ).toLKProof -> solution
+    }
+  }
+}
