75cf889472d90baead999ca2cb11e0e0a91303f8
Gabriel Ebner <gebner@gebner.org>
2015-07-16 21:16:15 +0200

    Actually extract recursion schemes from robinson proofs.

--- src/main/scala/at/logic/gapt/proofs/resolution/extractrecschemres.scala
+++ src/main/scala/at/logic/gapt/proofs/resolution/extractrecschemres.scala
@@ -8,37 +8,32 @@ import at.logic.gapt.proofs.lk.base.HOLSequent
 import at.logic.gapt.proofs.resolution._
 
 object extractRecSchemFromResProof {
-  def apply(p: RobinsonResolutionProof): RecursionScheme = {
-    val cnf = initialSequents(p)
-    val endSequent = HOLSequent(cnf.map(_.toFormula).map(univclosure(_)).toSeq, Seq())
-    val encoding = new InstanceTermEncoding(endSequent)
-    val (axiomRules, rest) = getRules(p, clause =>
-      encoding.encodeOption(clause.toFormula.asInstanceOf[FOLFormula] -> true))
-    val axiom = FOLFunction("A")
-    RecursionScheme(axiomRules.map(r => Rule(axiom, r)) ++ rest)
+  def apply( p: RobinsonResolutionProof ): ( RecursionScheme, InstanceTermEncoding ) = {
+    val endSequent = HOLSequent( initialSequents( p ).map( _.toFormula ).map( univclosure( _ ) ).toSeq, Seq() )
+    val encoding = new InstanceTermEncoding( endSequent )
+    apply( p, clause => encoding.encodeOption( clause.toFormula.asInstanceOf[FOLFormula] -> true ) ) -> encoding
   }
 
-  private val freshSymbols = Stream.from( 1 ).map( i => s"B$i" ).iterator
-  private def mkFreshSymbol(): String = freshSymbols.next()
-
-  def getRules(p: RobinsonResolutionProof, clauseTerm: HOLClause => Option[FOLTerm]): (Set[FOLTerm], Set[Rule]) = p match {
-    case InitialClause( clause ) => clauseTerm(clause.toHOLClause).toSet -> Set()
-    case Factor( clause, p1, List( occurrences ), subst ) =>
-      val (axiomRules, rest) = getRules(p1, clauseTerm)
-      axiomRules.map(subst.apply) -> rest
-    case Variant( clause, p1, subst ) =>
-      val (axiomRules, rest) = getRules(p1, clauseTerm)
-      axiomRules.map(subst.apply) -> rest
-    case Instance( clause, p1, subst ) =>
-      val (axiomRules, rest) = getRules(p1, clauseTerm)
-      axiomRules.map(subst.apply) -> rest
-    case Resolution( clause, p1, p2, occ1, occ2, subst ) =>
-      val (axiomRules1, rest1) = getRules(p1, clauseTerm)
-      val (axiomRules2, rest2) = getRules(p2, clauseTerm)
-      (axiomRules1 ++ axiomRules2).map(subst.apply) -> (rest1 ++ rest2)
-    case Paramodulation( clause, p1, p2, occ1, occ2, main, subst ) =>
-      val (axiomRules1, rest1) = getRules(p1, clauseTerm)
-      val (axiomRules2, rest2) = getRules(p2, clauseTerm)
-      (axiomRules1 ++ axiomRules2).map(subst.apply) -> (rest1 ++ rest2)
+  def apply( root: RobinsonResolutionProof, clauseTerm: HOLClause => Option[FOLTerm] ): RecursionScheme = {
+    val nodeMap = root.nodes.zipWithIndex.map {
+      case ( p: RobinsonResolutionProof, i ) =>
+        val fvs = freeVariables( p.root.toFormula.asInstanceOf[FOLFormula] )
+        p -> FOLFunction( s"B$i", fvs.toSeq )
+    }.toMap
+    val rules = nodeMap.flatMap {
+      case ( InitialClause( clause ), nt ) =>
+        clauseTerm( clause.toHOLClause ).map { term => Rule( nt, term ) }.toSeq
+      case ( Factor( clause, p1, List( occurrences ), subst ), nt ) =>
+        Seq( Rule( nt, subst( nodeMap( p1 ) ) ) )
+      case ( Variant( clause, p1, subst ), nt ) =>
+        Seq( Rule( nt, subst( nodeMap( p1 ) ) ) )
+      case ( Instance( clause, p1, subst ), nt ) =>
+        Seq( Rule( nt, subst( nodeMap( p1 ) ) ) )
+      case ( Resolution( clause, p1, p2, occ1, occ2, subst ), nt ) =>
+        Seq( Rule( nt, subst( nodeMap( p1 ) ) ), Rule( nt, subst( nodeMap( p2 ) ) ) )
+      case ( Paramodulation( clause, p1, p2, occ1, occ2, main, subst ), nt ) =>
+        Seq( Rule( nt, subst( nodeMap( p1 ) ) ), Rule( nt, subst( nodeMap( p2 ) ) ) )
+    }
+    RecursionScheme( rules.toSet + Rule( FOLFunction( "A" ), nodeMap( root ) ) )
   }
 }
