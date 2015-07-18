869333127f4fb61054d0765066c45391ccf08828
Gabriel Ebner <gebner@gebner.org>
2015-07-18 07:25:46 +0200

    Extract recursion schemes from proofs with induction.

--- src/main/scala/at/logic/gapt/proofs/lk/extractrecschem.scala
+++ src/main/scala/at/logic/gapt/proofs/lk/extractrecschem.scala
@@ -1,6 +1,7 @@
 package at.logic.gapt.proofs.lk
 
 import at.logic.gapt.expr._
+import at.logic.gapt.expr.fol.FOLMatchingAlgorithm
 import at.logic.gapt.expr.hol.containsQuantifier
 import at.logic.gapt.grammars._
 import at.logic.gapt.proofs.expansionTrees.InstanceTermEncoding
@@ -13,8 +14,9 @@ object extractRecSchem {
     val symbols =
       p.root.antecedent.map( occ => occ -> encoding.getSymbol( occ.formula.asInstanceOf[FOLFormula] -> true ) ) ++
         p.root.succedent.map( occ => occ -> encoding.getSymbol( occ.formula.asInstanceOf[FOLFormula] -> false ) )
-    val axiom = FOLFunction( "A" )
-    RecursionScheme( getRules( p, axiom, symbols.toMap, List() ) )
+    val context = freeVariables( p.root.toFormula.asInstanceOf[FOLFormula] ).toList
+    val axiom = FOLFunction( "A", context )
+    RecursionScheme( getRules( p, axiom, symbols.toMap, context ) )
   }
 
   private def followSymbols( symbols: Map[FormulaOccurrence, LambdaExpression], q: LKProof ) =
@@ -39,9 +41,22 @@ object extractRecSchem {
     case CutRule( q1, q2, sequent, aux1, aux2 ) if containsQuantifier( aux1.formula ) =>
       val All.Block( vars, _ ) = aux1.formula
       val symbol = Apps( FOLFunctionHead( mkFreshSymbol(), context.size + vars.size ), context )
-      val rules1 = getRules( q1, axiom, followSymbols( symbols + ( aux1 -> symbol ), q1 ), context )
-      val rules2 = getRules( q2, axiom, followSymbols( symbols + ( aux2 -> symbol ), q2 ), context )
+      val rules1 = getRules( q1, axiom, followSymbols( symbols, q1 ) + ( aux1 -> symbol ), context )
+      val rules2 = getRules( q2, axiom, followSymbols( symbols, q2 ) + ( aux2 -> symbol ), context )
       rules1 ++ rules2
+    case InductionRule( q1, q2, sequent, base, stepl, stepr, main, term ) =>
+      val All.Block( vars, _ ) = stepl.formula.asInstanceOf[FOLFormula]
+      val indVar = FOLMatchingAlgorithm.matchTerms( stepl.formula.asInstanceOf[FOLFormula], stepr.formula.asInstanceOf[FOLFormula] ).get.domain.head.asInstanceOf[FOLVar]
+      val symbol = Apps( FOLFunctionHead( mkFreshSymbol(), context.size + 1 + vars.size ), context )
+      val renaming = rename( vars.toSet, context.toSet )
+
+      val baseAxiom = Apps( App( symbol, FOLConst( "0" ) ), vars map renaming ).asInstanceOf[FOLTerm]
+      val rules1 = getRules( q1, baseAxiom, followSymbols( symbols - main, q1 ) + ( base -> App( symbol, FOLConst( "0" ) ) ), context )
+
+      val stepAxiom = Apps( App( symbol, indVar ), vars map renaming ).asInstanceOf[FOLTerm]
+      val rules2 = getRules( q2, stepAxiom, followSymbols( symbols - main, q2 ) + ( stepl -> App( symbol, indVar ) ) + ( stepr -> App( symbol, FOLFunction( "s", indVar ) ) ), indVar :: context )
+
+      rules1 ++ rules2 + Rule( Apps( symbols( main ), vars ).asInstanceOf[FOLTerm], Apps( symbol, term :: vars ).asInstanceOf[FOLTerm] )
     case BinaryLKProof( rule, q1, q2, sequent, aux1, aux2, main ) =>
       getRules( q1, axiom, followSymbols( symbols, q1 ), context ) ++ getRules( q2, axiom, followSymbols( symbols, q2 ), context )
     case UnaryLKProof( rule, q, sequent, auxs, main ) =>
