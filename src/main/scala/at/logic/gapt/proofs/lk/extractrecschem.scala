929fc2c6414d0661c23fd5df1ea3f6e2e971af62
Gabriel Ebner <gebner@gebner.org>
2015-07-20 17:11:31 +0200

    Extract recursion schemes from non-simple proofs.

--- src/main/scala/at/logic/gapt/proofs/lk/extractrecschem.scala
+++ src/main/scala/at/logic/gapt/proofs/lk/extractrecschem.scala
@@ -47,49 +47,62 @@ object extractRecSchem {
     }
   }
 
+  private def findEigenVars( occ: FormulaOccurrence, p: LKProof ): List[Var] = p match {
+    case StrongQuantifierRule( q, _, aux, main, eigenvar ) if main == occ =>
+      eigenvar :: findEigenVars( aux, q )
+    case InductionRule( q1, q2, sequent, base, stepl, stepr, main, term ) if main == occ =>
+      findEigenVars(stepr, q2).map{ case Var(s,t) => Var(s"${s}_end", t) } // hacky...
+    case UnaryLKProof( rule, q, sequent, auxs, main ) if main != occ =>
+      findEigenVars(q.root.occurrences.find( _.isAncestorOf( occ, false ) ).get, q)
+    case BinaryLKProof( rule, q1, q2, sequent, aux1, aux2, main ) if !main.contains( occ ) =>
+      Seq( q1, q2 ).
+        flatMap { q => q.root.occurrences.find( _.isAncestorOf( occ, false ) ).map( findEigenVars( _, q ) ) }.
+        maxBy(_.size)
+    case _ => Nil
+  }
+
   def getRules( p: LKProof, axiom: LambdaExpression, symbols: Map[FormulaOccurrence, LambdaExpression], context: List[Var] ): Set[HORule] = p match {
     case Axiom( sequent ) => sequent.occurrences.flatMap( symbols.get ).map { sym => HORule( axiom, sym ) } toSet
     case WeakQuantifierRule( q, sequent, aux, main, term ) =>
       val appSym = App( symbols( main ), term )
       appSym.exptype match {
         case FunctionType( To, argtypes ) -> To =>
+          val eigenvars = findEigenVars( aux, q )
           val cpsSym = Apps( Const( mkFreshSymbol(), FunctionType( To, context.map( _.exptype ) ++ argtypes ) ), context )
-          getRules( q, axiom, followSymbols( symbols - main, q ) + ( aux -> cpsSym ), context ) +
+          val expCpsSym = Apps( cpsSym, eigenvars )
+          getRules( q, expCpsSym, followSymbols( symbols - main, q ), eigenvars ++ context ) +
             HORule( axiom, App( appSym, cpsSym ) )
         case _ =>
           getRules( q, axiom, followSymbols( symbols - main, q ) + ( aux -> appSym ), context )
-      }
-    case StrongQuantifierRule( q, sequent, aux, main, eigenvar: FOLVar ) =>
-      val appSym = App( symbols( main ), eigenvar )
-      appSym.exptype match {
-        case FunctionType( To, argtypes ) -> To =>
-          val introSym = Var( mkFreshVar(), FunctionType( To, argtypes ) )
-          val fullyAppSym = App( appSym, introSym )
-          getRules( q, fullyAppSym, followSymbols( symbols - main, q ) + ( aux -> introSym ), introSym :: eigenvar :: context )
-        case To => getRules( q, appSym, followSymbols( symbols - main, q ), eigenvar :: context )
-        case _  => getRules( q, axiom, followSymbols( symbols - main, q ) + ( aux -> appSym ), eigenvar :: context )
       }
     case QuantifiedPi2Cut( q1, q2, sequent, aux1, aux2, u, v ) =>
       val symType = if ( v.isEmpty )
         FunctionType( To, context.map( _.exptype ) ++ u.map( _.exptype ) )
       else FunctionType( To, context.map( _.exptype ) ++ u.map( _.exptype ) :+ FunctionType( To, v.map( _.exptype ) ) )
       val symbol = Apps( Const( mkFreshSymbol(), symType ), context )
-      val rules1 = getRules( q1, axiom, followSymbols( symbols, q1 ) + ( aux1 -> symbol ), context )
+
+      val eigenvars = findEigenVars( aux1, q1 )
+      val hypSym = Apps( symbol, eigenvars )
+      val rules1 = if ( v.nonEmpty ) {
+        val introSym = Var( mkFreshVar(), FunctionType( To, v.map( _.exptype ) ) )
+        val fullHypSym = Apps( hypSym, introSym )
+        getRules( q1, fullHypSym, followSymbols( symbols, q1 ) + ( aux1 -> introSym ), introSym :: eigenvars ++ context )
+      } else getRules( q1, hypSym, followSymbols( symbols, q1 ), eigenvars ++ context )
+
       val rules2 = getRules( q2, axiom, followSymbols( symbols, q2 ) + ( aux2 -> symbol ), context )
       rules1 ++ rules2
     case InductionRule( q1, q2, sequent, base, stepl, stepr, main, term ) =>
       val All.Block( vars, _ ) = stepl.formula.asInstanceOf[FOLFormula]
       val indVar = FOLMatchingAlgorithm.matchTerms( stepl.formula.asInstanceOf[FOLFormula], stepr.formula.asInstanceOf[FOLFormula] ).get.domain.head.asInstanceOf[FOLVar]
       val symbol = Apps( Const( mkFreshSymbol(), FunctionType( To, context.map( _.exptype ) ++ Seq( Ti ) ++ vars.map( _.exptype ) ) ), context )
-      val renaming = rename( vars.toSet, context.map( _.asInstanceOf[FOLVar] ).toSet )
 
-      val baseAxiom = Apps( App( symbol, FOLConst( "0" ) ), vars map renaming )
-      val rules1 = getRules( q1, baseAxiom, followSymbols( symbols - main, q1 ) + ( base -> App( symbol, FOLConst( "0" ) ) ), context )
+      val baseAxiom = Apps( App( symbol, FOLConst( "0" ) ), findEigenVars(base, q1) )
+      val rules1 = getRules( q1, baseAxiom, followSymbols( symbols - main, q1 ), context )
 
-      val stepAxiom = Apps( App( symbol, indVar ), vars map renaming )
-      val rules2 = getRules( q2, stepAxiom, followSymbols( symbols - main, q2 ) + ( stepl -> App( symbol, indVar ) ) + ( stepr -> App( symbol, FOLFunction( "s", indVar ) ) ), indVar :: context )
+      val stepAxiom = Apps( App( symbol, FOLFunction("s", indVar) ), findEigenVars(stepr, q2) )
+      val rules2 = getRules( q2, stepAxiom, followSymbols( symbols - main, q2 ) + ( stepl -> App( symbol, indVar ) ), indVar :: context )
 
-      rules1 ++ rules2 + HORule( Apps( symbols( main ), vars ), Apps( symbol, term :: vars ) )
+      rules1 ++ rules2 + HORule( axiom, Apps( symbol, term :: findEigenVars(main, p) ) )
     case BinaryLKProof( rule, q1, q2, sequent, aux1, aux2, main ) =>
       getRules( q1, axiom, followSymbols( symbols, q1 ), context ) ++ getRules( q2, axiom, followSymbols( symbols, q2 ), context )
     case UnaryLKProof( rule, q, sequent, auxs, main ) =>
