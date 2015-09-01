b644ef758759ab43edaca7a4fca9b4bd71c50d27
Gabriel Ebner <gebner@gebner.org>
2015-09-01 12:50:08 +0200

    Add interface to the OpenWBO maxsat solver.

---
+++ src/main/scala/at/logic/gapt/provers/maxsat/OpenWBO.scala
@@ -0,0 +1,11 @@
+package at.logic.gapt.provers.maxsat
+
+import at.logic.gapt.proofs.resolution.HOLClause
+
+class OpenWBO extends MaxSATSolverBinary {
+  def format() = Format.MultiVLine
+  def noBinaryWarn() = "Please put the open-wbo binary into PATH"
+  def command( in: String, out: String ) = List( "open-wbo", in, out )
+  def solve( hard: List[HOLClause], soft: List[Tuple2[HOLClause, Int]] ) =
+    getFromMaxSATBinary( hard, soft )
+}
