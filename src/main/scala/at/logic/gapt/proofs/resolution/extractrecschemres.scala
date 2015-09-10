eaa30168e8b2f073d12e68bdd06a18c91fc8c46d
Gabriel Ebner <gebner@gebner.org>
2015-09-10 14:43:28 +0200

    Add empty terms in extractRecSchemFromResProof.

--- src/main/scala/at/logic/gapt/proofs/resolution/extractrecschemres.scala
+++ src/main/scala/at/logic/gapt/proofs/resolution/extractrecschemres.scala
@@ -1,19 +1,20 @@
 package at.logic.gapt.proofs.resolution
 
 import at.logic.gapt.expr._
-import at.logic.gapt.expr.hol.univclosure
+import at.logic.gapt.expr.hol.existsclosure
 import at.logic.gapt.grammars._
-import at.logic.gapt.proofs.{ FOLClause, HOLSequent }
+import at.logic.gapt.proofs.{Sequent, FOLClause}
 import at.logic.gapt.proofs.expansionTrees.InstanceTermEncoding
 
 object extractRecSchemFromResProof {
   def apply( p: ResolutionProof ): ( RecursionScheme, InstanceTermEncoding ) = {
-    val endSequent = HOLSequent( inputClauses( p ).map( _.toFormula ).map( univclosure( _ ) ).toSeq, Seq() )
+    val endSequent = existsclosure(Top() +: inputClauses( p ).map( _.toFormula ) ++: Sequent())
     val encoding = new InstanceTermEncoding( endSequent )
-    apply( p, clause => encoding.encodeOption( clause.toFormula.asInstanceOf[FOLFormula] -> true ) ) -> encoding
+    apply( p, encoding.encode(Top() -> true),
+      clause => encoding.encodeOption( clause.toFormula.asInstanceOf[FOLFormula] -> true ) ) -> encoding
   }
 
-  def apply( root: ResolutionProof, clauseTerm: FOLClause => Option[FOLTerm] ): RecursionScheme = {
+  def apply( root: ResolutionProof, emptyTerm: FOLTerm, clauseTerm: FOLClause => Option[FOLTerm] ): RecursionScheme = {
     val nodeMap = root.dagLikePostOrder.reverse.zipWithIndex.map {
       case ( p, i ) =>
         val fvs = freeVariables( p.conclusion )
@@ -24,7 +25,9 @@ object extractRecSchemFromResProof {
         clauseTerm( clause ).map { term => Rule( nt, term ) }.toSeq
       case ( Instance( p1, subst ), nt ) =>
         Seq( Rule( nt, subst( nodeMap( p1 ) ) ) )
-      case ( p, nt ) =>
+      case ( p, nt ) if p.immediateSubProofs.isEmpty =>
+        Seq(Rule(nt, emptyTerm))
+      case ( p, nt ) if p.immediateSubProofs.nonEmpty =>
         p.immediateSubProofs map { sp => Rule( nt, nodeMap( sp ) ) }
     }
     RecursionScheme( rules.toSet + Rule( FOLFunction( "A" ), nodeMap( root ) ) )
