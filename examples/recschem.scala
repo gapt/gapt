c121b9d4b969cdb9d993f2cc95f9033bd5a741fd
Gabriel Ebner <gebner@gebner.org>
2015-10-01 14:14:25 +0200

    Make recschem example work again.

--- examples/recschem.scala
+++ examples/recschem.scala
@@ -1,25 +1,24 @@
-import at.logic.gapt.cli.GAPScalaInteractiveShellLibrary.{parse, time}
-
 import at.logic.gapt.examples.{UniformAssociativity3ExampleProof, SumExampleProof, LinearEqExampleProof}
 import at.logic.gapt.expr._
 import at.logic.gapt.expr.fol.{FOLSubTerms, Numeral, FOLSubstitution, Utils}
 import at.logic.gapt.expr.hol.{univclosure, toNNF, simplify, lcomp}
 import at.logic.gapt.formats.tptp.TPTPFOLExporter
 import at.logic.gapt.grammars._
-import at.logic.gapt.proofs.Sequent
-import at.logic.gapt.proofs.expansionTrees.{removeFromExpansionSequent, ExpansionSequent, InstanceTermEncoding}
+import at.logic.gapt.proofs.{Suc, Ant, Sequent}
+import at.logic.gapt.proofs.expansionTrees.{toShallow, removeFromExpansionSequent, ExpansionSequent, InstanceTermEncoding}
 import at.logic.gapt.proofs.lk.LKToExpansionProof
 import at.logic.gapt.formats.prover9.Prover9TermParserLadrStyle.parseFormula
 import at.logic.gapt.provers.inductionProver.{SipProver, SimpleInductionProof}
 import at.logic.gapt.provers.maxsat.QMaxSAT
 import at.logic.gapt.provers.prover9.Prover9Prover
 import at.logic.gapt.provers.veriT.VeriTProver
+import at.logic.gapt.utils.time
 import org.apache.log4j.{Logger, Level}
 
 for (c <- Seq(minimizeSipGrammar.getClass, minimizeRecursionScheme.getClass))
   Logger.getLogger(c.getName).setLevel(Level.DEBUG)
 
-if (false) {
+if (true) {
 
   //val terms = TermsExtraction(SumExampleProof(7)).set
   val N = 8
@@ -38,7 +37,7 @@ if (false) {
   val targets = terms.map(FOLFunction("A") -> _)
   println(lcomp(simplify(toNNF((new RecSchemGenLangFormula(nfRecSchem))(targets)))))
 
-  val nfG = normalFormsProofVectGrammar(terms, Seq(2))
+  val nfG = normalFormsProofVectGrammar(terms.toSet, Seq(2))
   println(lcomp(simplify(toNNF(new VectGrammarMinimizationFormula(nfG).coversLanguage(terms)))))
 
   val minimized = time {
@@ -47,7 +46,7 @@ if (false) {
   println(minimized)
 
   val minG = time {
-    minimizeVectGrammar(nfG, terms, new QMaxSAT)
+    minimizeVectGrammar(nfG, terms.toSet, new QMaxSAT)
   }
   println(minG)
 
@@ -83,22 +82,12 @@ if (true) {
 //    n -> new Prover9Prover().getExpansionSequent( instanceSequent ).get
 //  }
 
-  def removeEqAxioms( eseq: ExpansionSequent ) = {
-    // removes all equality axioms that appear in examples/ProofSequences.scala
-    val R = parse.fol( "Forall x =(x,x)" )
-    val S = parse.fol( "Forall x Forall y Imp =(x,y) =(y,x)" )
-    val T = parse.fol( "Forall x Forall y Forall z Imp And =(x,y) =(y,z) =(x,z)" )
-    val Tprime = parse.fol( "Forall x Forall y Forall z Imp =(x,y) Imp =(y,z) =(x,z)" )
-    val CSuc = parse.fol( "Forall x Forall y Imp =(x,y) =(s(x),s(y))" )
-    val CPlus = parse.fol( "Forall x Forall y Forall u Forall v Imp =(x,y) Imp =(u,v) =(+(x,u),+(y,v))" )
-    val CPlusL = parse.fol( "Forall x Forall y Forall z Imp =(y,z) =(+(y,x),+(z,x))" ) // congruence plus left
-    val CgR = parse.fol( "Forall x Forall y Forall z Imp =(y,z) =(g(x,y),g(x,z))" ) // congruence of g on the right
-    val CMultR = parse.fol( "Forall x Forall y Forall z Imp =(x,y) =(*(z,x),*(z,y))" ) // congruence of mult right
-
-    val eqaxioms = Sequent( R::S::T::Tprime::CSuc::CPlus::CPlusL::CgR::CMultR::Nil, Nil )
-
-    removeFromExpansionSequent( eseq, eqaxioms )
-  }
+  val p9 = new Prover9Prover
+  def removeEqAxioms( eseq: ExpansionSequent ) =
+    eseq.zipWithIndex filter {
+      case (et, Ant(_)) => !p9.isValid(Sequent() :+ toShallow(et))
+      case (et, Suc(_)) => !p9.isValid(toShallow(et) +: Sequent())
+    } map {_._1}
   val endSequent = Sequent(
     Seq("s(x+y) = x+s(y)", "x+0 = x")
       map (s => univclosure(parseFormula(s))),
@@ -130,7 +119,7 @@ if (true) {
       case FOLFunction("A" | "G", _) => false
       case _ => true
     }
-    val instanceSeq = FOLSubstitution(FOLVar("x") -> Numeral(i))(termEncoding.decodeToFSequent(instanceLang))
+    val instanceSeq = FOLSubstitution(FOLVar("x") -> Numeral(i))(termEncoding.decodeToFSequent(instanceLang map {_.asInstanceOf[FOLTerm]}))
     val isCovered = instanceLanguages.find(_._1 == i).map(_._2.toSet subsetOf instanceLang)
     val isTaut = new VeriTProver().isValid(instanceSeq)
     println(s"$i: tautology=$isTaut covers=$isCovered")
