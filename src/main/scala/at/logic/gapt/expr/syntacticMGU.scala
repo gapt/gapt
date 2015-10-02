7f883d845cf9881fcc7b6e1a2b7e856961d38925
Gabriel Ebner <gebner@gebner.org>
2015-10-02 11:43:15 +0200

    Fix unification with binders.

--- src/main/scala/at/logic/gapt/expr/syntacticMGU.scala
+++ src/main/scala/at/logic/gapt/expr/syntacticMGU.scala
@@ -10,25 +10,26 @@ object syntacticMGU {
       case ( App( a1, b1 ), App( a2, b2 ) ) if b1.exptype == b2.exptype => unify( ( a1 -> a2 ) :: ( b1 -> b2 ) :: rest, env, bound )
       case ( c1: Const, c2: Const ) if c1 == c2                         => unify( rest, env, bound )
       case ( Abs( v1, t1 ), Abs( v2, t2 ) ) if v1.exptype == v2.exptype =>
-        val v_ = rename( v1, env.keySet ++ eqs.flatMap { p => freeVariables( p._1 ) ++ freeVariables( p._2 ) } ++ bound toList )
+        val v_ = rename( v1, ( env ++ eqs ).flatMap { p => freeVariables( p._1 ) ++ freeVariables( p._2 ) } ++ bound toList )
         unify( ( Substitution( v1 -> v_ )( t1 ) -> Substitution( v2 -> v_ )( t2 ) ) :: rest, env, bound + v_ )
-      case ( x: Var, y: Var ) if x == y => unify( rest, env, bound )
-      case ( x: Var, t ) if !( bound contains x ) =>
+      case ( x: Var, y ) if bound contains x =>
+        if ( x == y ) unify( rest, env, bound ) else None
+      case ( x: Var, t ) =>
         if ( env contains x ) {
           unify( ( env( x ) -> t ) :: rest, env, bound )
         } else {
           val t_ = Substitution( env )( t )
           if ( x == t_ ) {
             unify( rest, env, bound )
-          } else if ( freeVariables( t_ ) contains x ) {
+          } else if ( freeVariables( t_ ) intersect ( bound + x ) nonEmpty ) {
             None
           } else {
             val subst = Substitution( x -> t_ )
             unify( rest, ( env mapValues { subst( _ ) } ) + ( x -> t_ ), bound )
           }
         }
-      case ( exp, v: Var ) if !( bound contains v ) => unify( ( v -> exp ) :: rest, env, bound )
-      case _                                        => None
+      case ( exp, v: Var ) => unify( ( v -> exp ) :: rest, env, bound )
+      case _               => None
     }
   }
 
