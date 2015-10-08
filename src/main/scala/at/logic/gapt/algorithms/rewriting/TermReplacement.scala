b790dab01f6c1a053485b8c676352d1a71632465
Gabriel Ebner <gebner@gebner.org>
2015-10-08 10:54:31 +0200

    lkNew: replacements.
    Fixes #399.

--- src/main/scala/at/logic/gapt/algorithms/rewriting/TermReplacement.scala
+++ src/main/scala/at/logic/gapt/algorithms/rewriting/TermReplacement.scala
@@ -4,7 +4,7 @@ package at.logic.gapt.algorithms.rewriting
 import at.logic.gapt.expr._
 import at.logic.gapt.expr.fol.FOLSubstitution
 import at.logic.gapt.utils.logging.Logger
-import at.logic.gapt.proofs.{ HOLSequent, resolution }
+import at.logic.gapt.proofs.{ HOLSequent, resolution, lkNew }
 
 import scala.collection.mutable
 
@@ -31,6 +31,9 @@ object TermReplacement extends Logger {
   def apply( term: HOLFormula, p: Map[LambdaExpression, LambdaExpression] ): HOLFormula =
     apply( term.asInstanceOf[LambdaExpression], p ).asInstanceOf[HOLFormula]
 
+  def apply( term: HOLAtom, p: Map[LambdaExpression, LambdaExpression] ): HOLAtom =
+    apply( term.asInstanceOf[LambdaExpression], p ).asInstanceOf[HOLAtom]
+
   def apply( term: LambdaExpression, p: Map[LambdaExpression, LambdaExpression] ): LambdaExpression =
     p.foldLeft( term )( ( t, x ) => {
       /*debug(1,"looking for "+x+" in "+t);*/ apply( t, x._1, x._2 )
@@ -49,8 +52,6 @@ object TermReplacement extends Logger {
 
   def apply( f: FOLAtom, map: Map[FOLTerm, FOLTerm] ): FOLAtom =
     apply( f.asInstanceOf[FOLExpression], map.asInstanceOf[Map[FOLExpression, FOLExpression]] ).asInstanceOf[FOLAtom]
-
-  // FIXME: these polymorphic functions do not have the types you think they have...
 
   def rename_fsequent( fs: HOLSequent, what: LambdaExpression, by: LambdaExpression ): HOLSequent =
     HOLSequent(
@@ -98,6 +99,55 @@ object TermReplacement extends Logger {
       case Paramodulation( q1, l1, q2, l2, pos, dir ) =>
         Paramodulation( f( q1 ), l1, f( q2 ), l2, pos, dir )
     } )
+
+    f( proof )
+  }
+
+  def apply( proof: lkNew.LKProof, repl: Map[LambdaExpression, LambdaExpression] ): lkNew.LKProof = {
+    import lkNew._
+
+    def f( p: LKProof ): LKProof = p match {
+      case TopAxiom => TopAxiom
+      case BottomAxiom => BottomAxiom
+      case ReflexivityAxiom( term ) => ReflexivityAxiom( apply( term, repl ) )
+      case LogicalAxiom( atom ) => LogicalAxiom( apply( atom, repl ) )
+      case ArbitraryAxiom( clause ) => ArbitraryAxiom( clause map { apply( _, repl ) } )
+
+      case WeakeningLeftRule( subProof, formula ) => WeakeningLeftRule( f( subProof ), apply( formula, repl ) )
+      case WeakeningRightRule( subProof, formula ) => WeakeningRightRule( f( subProof ), apply( formula, repl ) )
+
+      case ContractionLeftRule( subProof, aux1, aux2 ) => ContractionLeftRule( f( subProof ), aux1, aux2 )
+      case ContractionRightRule( subProof, aux1, aux2 ) => ContractionRightRule( f( subProof ), aux1, aux2 )
+
+      case CutRule( leftSubProof, aux1, rightSubProof, aux2 ) => CutRule( f( leftSubProof ), aux1, f( rightSubProof ), aux2 )
+
+      case NegLeftRule( subProof, aux ) => NegLeftRule( f( subProof ), aux )
+      case NegRightRule( subProof, aux ) => NegRightRule( f( subProof ), aux )
+
+      case AndLeftRule( subProof, aux1, aux2 ) => AndLeftRule( f( subProof ), aux1, aux2 )
+      case AndRightRule( leftSubProof, aux1, rightSubProof, aux2 ) => AndRightRule( f( leftSubProof ), aux1, f( rightSubProof ), aux2 )
+
+      case OrLeftRule( leftSubProof, aux1, rightSubProof, aux2 ) => OrLeftRule( f( leftSubProof ), aux1, f( rightSubProof ), aux2 )
+      case OrRightRule( subProof, aux1, aux2 ) => OrRightRule( f( subProof ), aux1, aux2 )
+
+      case ImpLeftRule( leftSubProof, aux1, rightSubProof, aux2 ) => ImpLeftRule( f( leftSubProof ), aux1, f( rightSubProof ), aux2 )
+      case ImpRightRule( subProof, aux1, aux2 ) => ImpRightRule( f( subProof ), aux1, aux2 )
+
+      case ForallLeftRule( subProof, aux, formula, term, v ) => ForallLeftRule( f( subProof ), aux, apply( formula, repl ), apply( term, repl ), v )
+      case ForallRightRule( subProof, aux, eigen, quant ) => ForallRightRule( f( subProof ), aux, eigen, quant )
+
+      case ExistsLeftRule( subProof, aux, eigen, quant ) => ExistsLeftRule( f( subProof ), aux, eigen, quant )
+      case ExistsRightRule( subProof, aux, formula, term, v ) => ExistsRightRule( f( subProof ), aux, apply( formula, repl ), apply( term, repl ), v )
+
+      case EqualityLeftRule( subProof, eq, aux, pos ) => EqualityLeftRule( f( subProof ), eq, aux, pos )
+      case EqualityRightRule( subProof, eq, aux, pos ) => EqualityRightRule( f( subProof ), eq, aux, pos )
+
+      case InductionRule( leftSubProof, aux1, rightSubProof, aux2, aux3, term ) =>
+        InductionRule( f( leftSubProof ), aux1, f( rightSubProof ), aux2, aux3, apply( term, repl ).asInstanceOf[FOLTerm] )
+
+      case DefinitionLeftRule( subProof, aux, main )  => DefinitionLeftRule( f( subProof ), aux, apply( main, repl ) )
+      case DefinitionRightRule( subProof, aux, main ) => DefinitionRightRule( f( subProof ), aux, apply( main, repl ) )
+    }
 
     f( proof )
   }
