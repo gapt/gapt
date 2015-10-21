6cdc210a0bd68b8dbe91132c6154d00ef9a480f4
Gabriel Ebner <gebner@gebner.org>
2015-10-21 16:00:51 +0200

    TipSmtParserTest: remove debugging output.

--- src/test/scala/at/logic/gapt/formats/tip/TipSmtParserTest.scala
+++ src/test/scala/at/logic/gapt/formats/tip/TipSmtParserTest.scala
@@ -3,7 +3,6 @@ package at.logic.gapt.formats.tip
 import at.logic.gapt.expr.fol.reduceHolToFol
 import at.logic.gapt.expr.hol.instantiate
 import at.logic.gapt.expr.{ Tdata, Const }
-import at.logic.gapt.proofs.expansionTrees.extractInstances
 import at.logic.gapt.provers.prover9.Prover9
 import org.specs2.mutable._
 
@@ -16,7 +15,6 @@ class TipSmtParserTest extends Specification {
     val one = Const( "One", Tdata( "Bin" ) )
     val oneAnd = Const( "OneAnd", Tdata( "Bin" ) -> Tdata( "Bin" ) )
     val instanceSequent = problem.toSequent.map( identity, instantiate( _, Seq( one, one, oneAnd( oneAnd( one ) ) ) ) )
-    extractInstances( Prover9 getExpansionSequent reduceHolToFol( instanceSequent ) get ).map( identity, -_ ).elements foreach println
     Prover9 getRobinsonProof reduceHolToFol( instanceSequent ) must beSome
   }
 
