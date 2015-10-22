6baa552407795e36d7209b91dc5b21ff9dfbbb85
Gabriel Ebner <gebner@gebner.org>
2015-10-22 09:52:28 +0200

    Fix induction prototype script.

--- examples/induction/prototype.scala
+++ examples/induction/prototype.scala
@@ -3,7 +3,7 @@ import at.logic.gapt.expr._
 import at.logic.gapt.expr.fol.{reduceHolToFol, Utils, FOLSubstitution}
 import at.logic.gapt.expr.hol.{instantiate, univclosure}
 import at.logic.gapt.formats.prover9.Prover9TermParserLadrStyle.parseFormula
-import at.logic.gapt.formats.tip.{TipSmtParser, TipParser}
+import at.logic.gapt.formats.tip.TipSmtParser
 import at.logic.gapt.proofs.{Sequent, HOLSequent}
 import at.logic.gapt.provers.inductionProver.SimpleInductionProof._
 import at.logic.gapt.provers.inductionProver._
