8658055e27413d32a03bfde7d41f417a3729857c
Gabriel Ebner <gebner@gebner.org>
2015-11-16 10:39:18 +0100

    Add script to test TSTP import.

---
+++ testing/src/main/scala/tstp.scala
@@ -0,0 +1,28 @@
+package at.logic.gapt.testing
+
+import at.logic.gapt.formats.tptp.TptpProofParser
+import at.logic.gapt.proofs.sketch.RefutationSketchToRobinson
+import at.logic.gapt.provers.prover9.Prover9
+
+import scala.io.Source
+
+object testTstpImport extends App {
+  val Array( filename ) = args
+
+  val tstpOutput = Source fromFile filename mkString
+
+  if ( !tstpOutput.contains( "CNFRefutation" ) )
+    println( "NOT_CNF_REFUTATION" )
+
+  val ( endSequent, sketch ) = TptpProofParser parse tstpOutput
+  println( s"SKETCH_SIZE ${sketch.subProofs.size}" )
+
+  val resolution = RefutationSketchToRobinson( sketch, Prover9 ) getOrElse {
+    println( "FAIL_SKETCH_CONVERSION" )
+    sys exit 1
+  }
+
+  println( s"RESOLUTION_SIZE ${resolution.subProofs.size}" )
+
+  println( "OK" )
+}
