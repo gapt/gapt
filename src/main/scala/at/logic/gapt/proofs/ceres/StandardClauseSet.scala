53a9c3011bc0c25774f44c0caa2814957005bf75
Martin <marty@logic.at>
2015-12-11 11:12:42 +0100

    adapting to holclause bugfix

--- src/main/scala/at/logic/gapt/proofs/ceres/StandardClauseSet.scala
+++ src/main/scala/at/logic/gapt/proofs/ceres/StandardClauseSet.scala
@@ -5,7 +5,7 @@
 
 package at.logic.gapt.proofs.ceres
 
-import at.logic.gapt.proofs.{ HOLClause, HOLSequent }
+import at.logic.gapt.proofs.{ Sequent, HOLClause, HOLSequent }
 import at.logic.gapt.expr._
 import at.logic.gapt.utils.logging.Logger
 import scala.annotation.tailrec
@@ -16,8 +16,16 @@ import scala.util.control.TailCalls._
  */
 class CharacteristicClauseSet[Data] {
   def apply( struct: Struct[Data] ): Set[HOLClause] = struct match {
-    case A( fo, _ )                     => Set( HOLClause( Nil, List( fo ) ) )
-    case Dual( A( fo, _ ) )             => Set( HOLClause( List( fo ), Nil ) )
+    case A( fo: HOLAtom, _ ) => Set( HOLClause( Nil, List( fo ) ) )
+    case A( Top(), _ )       => Set()
+    case A( Bottom(), _ )    => Set( HOLClause( Nil, Nil ) )
+    case A( f, _ ) =>
+      throw new Exception( s"Encountered a formula $f as leaf in the struct. Can't convert it to a clause." )
+    case Dual( A( fo: HOLAtom, _ ) ) => Set( HOLClause( List( fo ), Nil ) )
+    case Dual( A( Top(), _ ) )       => Set( HOLClause( Nil, Nil ) )
+    case Dual( A( Bottom(), _ ) )    => Set()
+    case Dual( A( f, _ ) ) =>
+      throw new Exception( s"Encountered a formula $f as leaf in the struct. Can't convert it to a clause." )
     case EmptyPlusJunction()            => Set()
     case EmptyTimesJunction()           => Set( HOLClause( Nil, Nil ) )
     case Plus( EmptyPlusJunction(), x ) => apply( x )
@@ -36,10 +44,10 @@ class CharacteristicClauseSet[Data] {
     case _ => throw new Exception( "Unhandled case: " + struct )
   }
 
-  private def compose( fs1: HOLClause, fs2: HOLClause ) = fs1 ++ fs2
+  private def compose[T]( fs1: Sequent[T], fs2: Sequent[T] ) = fs1 ++ fs2
 
   /* Like compose, but does not duplicate common terms */
-  private def delta_compose( fs1: HOLClause, fs2: HOLClause ) = HOLClause(
+  private def delta_compose[T]( fs1: Sequent[T], fs2: Sequent[T] ) = Sequent[T](
     fs1.antecedent ++ fs2.antecedent.diff( fs1.antecedent ),
     fs1.succedent ++ fs2.succedent.diff( fs1.succedent )
   )
