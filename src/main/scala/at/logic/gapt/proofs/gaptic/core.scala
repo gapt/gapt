972f13a7082306c1c1382f72bd0d71c36614fb3e
Gabriel Ebner <gebner@gebner.org>
2015-12-15 10:31:27 +0100

    Fix build.

--- src/main/scala/at/logic/gapt/proofs/gaptic/core.scala
+++ src/main/scala/at/logic/gapt/proofs/gaptic/core.scala
@@ -29,10 +29,7 @@ case class ProofState( currentGoalIndex: Int, proofSegment: LKProof ) {
    * @param i
    * @return
    */
-  def getSubGoal( i: Int ): Option[OpenAssumption] =
-    for {
-      sg <- subGoals( i )
-    } yield Some( sg )
+  def getSubGoal( i: Int ): Option[OpenAssumption] = Option( subGoals( i ) )
 
   /**
    * Returns a string representation of a sub goal at a given index.
@@ -52,9 +49,7 @@ case class ProofState( currentGoalIndex: Int, proofSegment: LKProof ) {
    * @return
    */
   def setCurrentSubGoal( i: Int ): Option[ProofState] =
-    for {
-      _ <- subGoals( i )
-    } yield Some( ProofState( i, proofSegment ) )
+    if ( subGoals isDefinedAt i ) Some( copy( currentGoalIndex = i ) ) else None
 
   /**
    * Replaces the i-th open assumption by an arbitrary proof segment and returns the result in a new proof state.
