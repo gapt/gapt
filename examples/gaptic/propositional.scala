55df4c255ec519c1879ee0bb001065cdaba044c4
Alexander Birch <alexisbirch@gmail.com>
2015-12-18 13:16:41 +0100

    Example update. Simple test file renamed/worked into purely propositional test case.

---
+++ examples/gaptic/propositional.scala
@@ -0,0 +1,22 @@
+import at.logic.gapt.expr._
+import at.logic.gapt.proofs.Sequent
+import at.logic.gapt.proofs.gaptic._
+import at.logic.gapt.formats.prover9.Prover9TermParserLadrStyle.{parseFormula, parseTerm}
+import at.logic.gapt.proofs.gaptic.tactics._
+
+/* Test: Or right */
+val A = FOLAtom("A")
+val B = FOLAtom("B")
+
+/* Test: Lemma */
+val lemma = new Lemma(
+	Sequent(Seq("initAnt" -> Imp(A, B)), Seq("initSuc" -> Or(And(A,B), Neg(A))))
+) {
+	use(OrRightTactic("initSuc"))
+	use(NegRightTactic("initSuc_2"))
+	use(AndRightTactic("initSuc_1"))
+	use(LogicalAxiomTactic(A))
+	use(ImpLeftTactic("initAnt"))
+	use(LogicalAxiomTactic(A))
+	use(LogicalAxiomTactic(B))
+} qed
