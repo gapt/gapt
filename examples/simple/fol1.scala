4a39f66d16c63e04756fb490b5dc194cbc73bfbf
Stefan Hetzl <stefan.hetzl@tuwien.ac.at>
2015-12-21 09:35:09 +0100

    using CutTactic in simple fol1 example

--- examples/simple/fol1.scala
+++ examples/simple/fol1.scala
@@ -1,32 +1,26 @@
 import at.logic.gapt.proofs.gaptic._
 import at.logic.gapt.proofs.gaptic.tactics._
 
-/* TODO: pull these three together once CutTactic is implemented */
-
 val p = new Lemma( Sequent(
   Seq( "L" -> parseFormula( "(all x all y (P(x,y) -> Q(x,y)))" )),
   Seq( "R" -> parseFormula( "(exists x exists y (-Q(x,y) -> -P(x,y)))" )))) {
-} qed
 
-val p_left = new Lemma( Sequent(
-  Seq( "L" -> parseFormula( "(all x all y (P(x,y) -> Q(x,y)))" )),
-  Seq( "C" -> parseFormula( "(all x exists y (-P(x,y) | Q(x,y)))" )))) {
+  use(CutTactic( parseFormula( "(all x exists y (-P(x,y) | Q(x,y)))" ), "C" ))
+
+  // left subproof
+  use(WeakeningRightTactic( "R" ))
   use(ForallRightTactic( FOLVar( "z" ), "C" ))
   use(ExistsRightTactic( parseTerm( "a" ), "C1" ))
   use(ForallLeftTactic( parseTerm( "z" ), "L1" ))
   use(ForallLeftTactic( parseTerm( "a" ), "L2", "L1" ))
   use(OrRightTactic( "C1" ))
   use(ImpLeftTactic( "L2" ))
+  use(NegRightTactic( "C1_1" ))
+  use(LogicalAxiomTactic( parseFormula( "P(z,a)" )))
+  use(LogicalAxiomTactic( parseFormula( "Q(z,a)" )))
 
-    use(NegRightTactic( "C1_1" ))
-    use(LogicalAxiomTactic( parseFormula( "P(z,a)" )))
-
-    use(LogicalAxiomTactic( parseFormula( "Q(z,a)" )))
-} qed
-
-val p_right = new Lemma( Sequent(
-  Seq( "C" -> parseFormula( "(all x exists y (-P(x,y) | Q(x,y)))" )),
-  Seq( "R" -> parseFormula( "(exists x exists y (-Q(x,y) -> -P(x,y)))" )))) {
+  // right subproof
+  use(WeakeningLeftTactic( "L" ))
   use(ForallLeftTactic( parseTerm( "b" ), "C1" ))
   use(ExistsLeftTactic( FOLVar( "v" ), "C1" ))
   use(ExistsRightTactic( parseTerm( "b" ), "R1" ))
@@ -38,10 +32,8 @@ val p_right = new Lemma( Sequent(
   use(NegRightTactic( "R2_2" ))
   use(NegLeftTactic( "R2_1" ))
   use(OrLeftTactic( "C1" ))
-
-    use(NegLeftTactic( "C1" ))
-    use(LogicalAxiomTactic( parseFormula( "P(b,v)" )))
-
-    use(LogicalAxiomTactic( parseFormula( "Q(b,v)" )))
+  use(NegLeftTactic( "C1" ))
+  use(LogicalAxiomTactic( parseFormula( "P(b,v)" )))
+  use(LogicalAxiomTactic( parseFormula( "Q(b,v)" )))
 } qed
 
