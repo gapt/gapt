1bc6fc0de1acb62fb9228e0bf4608475e99a2009
Alexander Birch <alexisbirch@gmail.com>
2016-01-06 13:40:00 +0100

    Repeat update. Can now use tactic(al) zero times to reflect description on GitHub.

--- src/main/scala/at/logic/gapt/proofs/gaptic/tactics/metaTactics.scala
+++ src/main/scala/at/logic/gapt/proofs/gaptic/tactics/metaTactics.scala
@@ -3,5 +3,25 @@ package at.logic.gapt.proofs.gaptic.tactics
 import at.logic.gapt.proofs.gaptic.{ ProofState, Tactical }
 
 case class RepeatTactic( tact: Tactical ) extends Tactical {
-  def apply( proofState: ProofState ) = ???
+  def apply( proofState: ProofState ) = {
+    var stop = false
+
+    var currentState = proofState
+    var newState: Option[ProofState] = None
+
+    while ( !stop ) {
+      newState = tact( currentState )
+
+      // Prevent further iterations or prepare for next iteration
+      newState match {
+        case None =>
+          stop = true
+        case Some( x ) =>
+          currentState = x
+      }
+    }
+
+    // "Fail" if the tactic(al) did not get applied at least once
+		Option( currentState )
+  }
 }
