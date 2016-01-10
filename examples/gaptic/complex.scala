d7bc0377f472bd22ebfdfb2fdac652ae74cab147
Alexander Birch <alexisbirch@gmail.com>
2016-01-10 21:12:22 +0100

    Added destruct tactic. Works on example on github. Added the option of not providing a label, in which case it destructs the first possible formula in the following order: Ant(0),...Ant(n),Suc(0),...,Suc(m).

--- examples/gaptic/complex.scala
+++ examples/gaptic/complex.scala
@@ -1,13 +1,24 @@
-import at.logic.gapt.expr._
 import at.logic.gapt.proofs.Sequent
 import at.logic.gapt.proofs.gaptic._
 import at.logic.gapt.proofs.gaptic.tactics._
 
-val A = FOLAtom( "A" )
-val B = FOLAtom( "B" )
-
-val someLemma = new Lemma(
+val decomposeLemma = new Lemma(
 	Sequent( Seq( "label1" -> parseFormula( "(exists x (p(x) & q(x)))")), Seq( "label2" -> parseFormula("(all y (p(y) -> (q(y) | r(y))))") ) )
 ) {
 	use(DecomposeTactic)
 }
+
+val destructLemma = new Lemma(
+	Sequent(Nil, Seq( "label1" -> parseFormula("a | (b | c)"), "label2" -> parseFormula( "a & (b & c)")))
+) {
+	use(DestructTactic("label1"))
+	use(DestructTactic("label2"))
+}
+
+val destructLemma2 = new Lemma(
+	Sequent(Seq("noise1" -> parseFormula("P(x)")), Seq( "noise2" -> parseFormula("P(y)"), "label1" -> parseFormula("a | (b | c)"), "noise3" -> parseFormula("P(z)"), "label2" -> parseFormula( "a & (b & c)" )))
+) {
+	use(DestructTactic())
+	use(DestructTactic())
+}
+
--- src/main/scala/at/logic/gapt/proofs/gaptic/tactics/complexTactics.scala
+++ src/main/scala/at/logic/gapt/proofs/gaptic/tactics/complexTactics.scala
@@ -1,18 +1,75 @@
 package at.logic.gapt.proofs.gaptic.tactics
 
-import at.logic.gapt.proofs.gaptic.{ ProofState, Tactical }
+import at.logic.gapt.expr._
+import at.logic.gapt.proofs.gaptic.{ OpenAssumption, ProofState, Tactic, Tactical }
+import at.logic.gapt.proofs.{ Ant, SequentIndex, Suc }
 
 /**
- * Created by Alexander on 06-01-2016.
+ * Repeatedly applies unary rules that are unambiguous
  */
 case object DecomposeTactic extends Tactical {
   def apply( proofState: ProofState ) = {
     RepeatTactic(
-      AndLeftTactic()
-        orElse OrRightTactic()
-        orElse ImpRightTactic()
-        orElse ForallRightTactic()
-        orElse ExistsLeftTactic()
+      AndLeftTactic() orElse
+        OrRightTactic() orElse
+        ImpRightTactic() orElse
+        ForallRightTactic() orElse
+        ExistsLeftTactic()
     )( proofState )
   }
 }
+
+case class DestructTactic( applyToLabel: Option[String] = None ) extends Tactic {
+
+  override def apply( goal: OpenAssumption ) = {
+    val goalSequent = goal.s
+
+    def canDestruct( formula: HOLFormula, index: SequentIndex ): Boolean = ( formula, index ) match {
+      case ( All( _, _ ), Suc( _ ) ) => true
+      case ( Ex( _, _ ), Ant( _ ) )  => true
+      case ( And( _, _ ), _ )        => true
+      case ( Or( x, y ), _ )         => true
+      case ( Imp( x, y ), _ )        => true
+      case ( Neg( _ ), _ )           => true
+      case _                         => false
+    }
+
+    val indices = applyToLabel match {
+      case None =>
+        for ( ( ( _, formula ), index ) <- goalSequent.zipWithIndex.elements if canDestruct( formula, index ) )
+          yield index
+
+      case Some( label ) =>
+        for ( ( ( `label`, _ ), index ) <- goalSequent.zipWithIndex.elements )
+          yield index
+    }
+
+    // Select some formula index!
+    indices.headOption match {
+      case Some( i ) =>
+        val ( existingLabel, _ ) = goalSequent( i )
+
+        val tac = ForallRightTactic( applyToLabel = existingLabel ) orElse
+          ExistsLeftTactic( applyToLabel = existingLabel ) orElse
+          AndLeftTactic( applyToLabel = existingLabel ) orElse
+          AndRightTactic( applyToLabel = existingLabel ) orElse
+          OrLeftTactic( applyToLabel = existingLabel ) orElse
+          OrRightTactic( applyToLabel = existingLabel ) orElse
+          ImpLeftTactic( applyToLabel = existingLabel ) orElse
+          ImpRightTactic( applyToLabel = existingLabel ) orElse
+          NegLeftTactic( applyToLabel = existingLabel ) orElse
+          NegRightTactic( applyToLabel = existingLabel )
+
+        tac( goal )
+      case None => None
+    }
+  }
+}
+
+/**
+ * Companion object for DestructTactic
+ */
+object DestructTactic {
+  def apply( applyToLabel: String ) = new DestructTactic( Some( applyToLabel ) )
+}
+
