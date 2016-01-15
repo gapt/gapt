9ecc8b4cd0e0f4bba16154ba40febfce9b2ec512
Alexander Birch <alexisbirch@gmail.com>
2016-01-15 13:02:44 +0100

    Added insert tactic. Since I am not sure about the extend of for what cases the tactic should work, I am not completely sure whether it works as intended. Tested with a modified formulation of the drinker paradox in the meta.scala test, and a proof (however, a bit weird one) is produced.

--- examples/gaptic/meta.scala
+++ examples/gaptic/meta.scala
@@ -1,4 +1,3 @@
-
 import at.logic.gapt.proofs.Sequent
 import at.logic.gapt.proofs.gaptic._
 import at.logic.gapt.proofs.gaptic.tactics._
@@ -21,4 +20,20 @@ val lemma2 = new Lemma(
 	Sequent( Seq( "A" -> Imp( A, B ) ), Seq( "S" -> Or( And( A, B ), Neg( A ) ) ) )
 ) {
 	use(RepeatTactic(OrRightTactic( ) orElse NegRightTactic( ) orElse AndRightTactic( ) orElse ImpLeftTactic( ) orElse AxiomTactic ))
+} qed
+
+val drinker3 = new Lemma( Sequent( Nil, Seq("E" -> parseFormula("B"), "E" -> parseFormula("A"), "D" -> parseFormula( "(exists x (P(x) -> (all y P(y))))" )))) {
+	use(ExistsRightTactic( parseTerm( "c" ), "D1" ))
+	use(ImpRightTactic())
+	use(ForallRightTactic())
+	use(ExistsRightTactic( parseTerm( "y" ), "D2" ))
+	use(ImpRightTactic())
+	use(ForallRightTactic())
+	use(AxiomTactic)
+} qed
+
+val lemma3 = new Lemma( Sequent( Seq("F" -> parseFormula("A -> B")), Seq( "E" -> parseFormula("B"), "D" -> parseFormula( "(exists y (P(y) -> (all z P(z))))")))){
+	use(ImpLeftTactic())
+	use(InsertTactic(drinker3))
+	use(AxiomTactic)
 } qed
--- src/main/scala/at/logic/gapt/proofs/gaptic/tactics/metaTactics.scala
+++ src/main/scala/at/logic/gapt/proofs/gaptic/tactics/metaTactics.scala
@@ -1,7 +1,14 @@
 package at.logic.gapt.proofs.gaptic.tactics
 
+import at.logic.gapt.expr.{ StillmanSubsumptionAlgorithmFOL, StillmanSubsumptionAlgorithmHOL }
 import at.logic.gapt.proofs.gaptic.{ ProofState, Tactical }
+import at.logic.gapt.proofs.lk.{ applySubstitution, LKProof }
 
+/**
+ * Repeat tactical
+ * Repeatedly applies the given tactical to a proof state until it fails.
+ * @param tact
+ */
 case class RepeatTactic( tact: Tactical ) extends Tactical {
   def apply( proofState: ProofState ) = {
     tact( proofState ) match {
@@ -9,4 +16,34 @@ case class RepeatTactic( tact: Tactical ) extends Tactical {
       case None             => Some( proofState )
     }
   }
+}
+
+/**
+ * Insert tactical
+ * Inserts an LKProof where the end sequent subsumes the sequent of the open assumption
+ * @param insertion
+ */
+case class InsertTactic( insertion: LKProof ) extends Tactical {
+  def apply( proofState: ProofState ) = {
+    var insertedOnce = false
+
+    def f( x: ProofState, i: Int ): ProofState = x.getSubGoal( i ) match {
+      case None => x
+      case Some( goal ) =>
+        StillmanSubsumptionAlgorithmFOL.subsumes_by( insertion.endSequent, goal.conclusion ) match {
+          case None => f( x, i + 1 )
+          case Some( sub ) =>
+            insertedOnce = true
+            f( x replaceSubGoal ( i, applySubstitution( sub )( insertion ) ), 0 )
+        }
+    }
+
+    val r = f( proofState, 0 )
+
+    if ( insertedOnce )
+      Some( r )
+    else
+      None
+  }
+
 }
