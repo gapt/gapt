2bc5e648f271b01d9e97bb93f6cc493476b1fe21
Martin <marty@logic.at>
2016-01-19 00:52:41 +0100

    fixing build

--- src/main/scala/at/logic/gapt/proofs/lkskNew/LKskToExpansionProof.scala
+++ src/main/scala/at/logic/gapt/proofs/lkskNew/LKskToExpansionProof.scala
@@ -3,13 +3,13 @@ package at.logic.gapt.proofs.lkskNew
 import at.logic.gapt.expr._
 import at.logic.gapt.expr.hol.HOLPosition
 import at.logic.gapt.proofs.Sequent
-import at.logic.gapt.proofs.expansionTrees._
+import at.logic.gapt.proofs.expansion._
 
 /**
  * Extends expansion tree extraction to lksk.
  */
 object LKskToExpansionProof {
-
+  /*
   /**
    * Extracts an expansion sequent Ex(π) from an LKProof π.
    * Dp(Ex(π)) might not be tautological, e.g. if π contains quantified cuts.
@@ -114,4 +114,5 @@ object LKskToExpansionProof {
       newTree +: subSequent
 
   }
+  */
 }
