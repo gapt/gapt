d5e30101002ca24d0c1058d52db899b5b4bc9145
Gabriel Ebner <gebner@gebner.org>
2016-02-25 14:21:40 +0100

    Modernize poset proof.

--- examples/poset/poset-proof.scala
+++ examples/poset/poset-proof.scala
@@ -1,96 +1,86 @@
 package at.logic.gapt.examples.poset
 
+import at.logic.gapt.examples.TacticsProof
 import at.logic.gapt.expr._
-import at.logic.gapt.proofs.Sequent
+import at.logic.gapt.proofs.{ FiniteContext, Sequent }
 import at.logic.gapt.proofs.gaptic._
 
-object proof {
-  val f = FOLFunctionConst( "f", 2 )
-  val Seq( u, v, w, x, y, z ) = Seq( "u", "v", "w", "x", "y", "z" ) map { FOLVar( _ ) }
-  val Seq( a, b, c, d ) = Seq( "a", "b", "c", "d" ) map { FOLConst( _ ) }
-
-  val trans = All( x, All( y, All( z, ( ( f( x, y ) === x ) & ( f( y, z ) === y ) ) --> ( f( x, z ) === x ) ) ) )
-  val asymm = All( u, All( v, ( ( f( u, v ) === u ) & ( f( v, u ) === v ) ) --> ( u === v ) ) )
+object proof extends TacticsProof {
+  implicit val ctx = FiniteContext(
+    constants = Set( hoc"f: i>i>i", hoc"'=':i>i>o",
+      hoc"a:i", hoc"b:i", hoc"c:i", hoc"d:i" )
+  )
 
   val axioms =
-    ( "eqrefl" -> All( x, x === x ) ) +:
-      ( "eqsymm" -> All( x, All( y, ( x === y ) --> ( y === x ) ) ) ) +:
-      ( "eqtrans" -> All( x, All( y, All( z, ( ( x === y ) & ( y === z ) ) --> ( x === z ) ) ) ) ) +:
-      ( "eqfcongr" -> All( x, All( y, All( u, All( v, ( ( x === y ) & ( u === v ) ) --> ( f( x, u ) === f( y, v ) ) ) ) ) ) ) +:
-      ( "fcomm" -> All( x, All( y, f( x, y ) === f( y, x ) ) ) ) +:
-      ( "fassoc" -> All( x, All( y, All( z, f( f( x, y ), z ) === f( x, f( y, z ) ) ) ) ) ) +:
+    ( "eqrefl" -> hof"!x x=x" ) +:
+      ( "eqsymm" -> hof"!x!y (x=y -> y=x)" ) +:
+      ( "eqtrans" -> hof"!x!y!z (x=y & y=z -> x=z)" ) +:
+      ( "eqfcongr" -> hof"!x!y!u!v (x=y & u=v -> f(x,u) = f(y,v))" ) +:
+      ( "fcomm" -> hof"!x!y f(x,y) = f(y,x)" ) +:
+      ( "fassoc" -> hof"!x!y!z f(f(x,y),z) = f(x,f(y,z))" ) +:
       Sequent()
 
-  val transLemma = Lemma( axioms :+ ( "trans" -> trans ) ) {
+  val trans = Lemma( axioms :+ ( "trans" -> hof"!x!y!z (f(x,y)=x & f(y,z)=y -> f(x,z)=x)" ) ) {
     decompose
-    chain( "eqtrans" ).subst( y -> f( f( x, y ), z ) )
+    chain( "eqtrans" ).subst( hov"y" -> le"f(f(x,y), z)" )
     chain( "eqsymm" ); chain( "eqfcongr" ); prop; chain( "eqrefl" )
-    chain( "eqtrans" ).subst( y -> f( x, f( y, z ) ) )
+    chain( "eqtrans" ).subst( hov"y" -> le"f(x, f(y,z))" )
     chain( "fassoc" )
-    chain( "eqtrans" ).subst( y -> f( x, y ) )
+    chain( "eqtrans" ).subst( hov"y" -> le"f(x, y)" )
     chain( "eqfcongr" ); chain( "eqrefl" ); prop
     prop
   }
 
-  val asymmLemma = Lemma( axioms :+ ( "asymm" -> asymm ) ) {
+  val asymm = Lemma( axioms :+ ( "asymm" -> hof"!u!v (f(u,v)=u & f(v,u)=v -> u=v)" ) ) {
     decompose
-    chain( "eqtrans" ).subst( y -> f( u, v ) )
+    chain( "eqtrans" ).subst( hov"y" -> le"f(u, v)" )
     chain( "eqsymm" ); prop
-    chain( "eqtrans" ).subst( y -> f( v, u ) )
+    chain( "eqtrans" ).subst( hov"y" -> le"f(v, u)" )
     chain( "fcomm" )
     prop
   }
 
   val cycleImpliesEqual3 =
-    Lemma( axioms :+ ( "goal" ->
-      ( ( ( f( a, b ) === a ) & ( f( b, c ) === b ) & ( f( c, a ) === c ) ) --> ( ( a === b ) & ( b === c ) ) ) ) ) {
-      cut( trans, "trans" )
-      insert( transLemma )
-
-      cut( asymm, "asymm" )
-      insert( asymmLemma )
+    Lemma( axioms :+ ( "goal" -> hof"f(a,b)=a & f(b,c)=b & f(c,a)=c -> a=b & b=c" ) ) {
+      include( "trans", trans )
+      include( "asymm", asymm )
 
       // right side of the cut
       decompose; destruct( "goal_1" ) onAll ( chain( "asymm" ) andThen prop )
 
       // f(b,a)=b
-      chain( "trans" ).subst( y -> c ); repeat( prop )
+      chain( "trans" ).subst( hov"y" -> le"c" ); repeat( prop )
 
       // f(c,b)=c
-      chain( "trans" ).subst( y -> a ); repeat( prop )
+      chain( "trans" ).subst( hov"y" -> le"a" ); repeat( prop )
     }
 
   val cycleImpliesEqual4 =
-    Lemma( axioms :+ ( "goal" ->
-      ( ( ( f( a, b ) === a ) & ( f( b, c ) === b ) & ( f( c, d ) === c ) & ( f( d, a ) === d ) ) --> ( ( a === b ) & ( b === c ) & ( c === d ) ) ) ) ) {
-      cut( trans, "trans" )
-      insert( transLemma )
-
-      cut( asymm, "asymm" )
-      insert( asymmLemma )
-
+    Lemma( axioms :+ ( "goal" -> hof"f(a,b)=a & f(b,c)=b & f(c,d)=c & f(d,a)=d -> a=b & b=c & c=d" ) ) {
+      include( "trans", trans )
+      include( "asymm", asymm )
       decompose
 
       // first show f(c,a)=c
-      cut( f( c, a ) === c, "leq_c_a" ); forget( "goal_1" )
-      chain( "trans" ).subst( y -> d ); prop; prop
+      cut( hof"f(c, a) = c", "leq_c_a" ); forget( "goal_1" )
+      chain( "trans" ).subst( hov"y" -> le"d" ); prop; prop
 
       // and f(a,c)=a
-      cut( f( a, c ) === a, "leq_a_c" ); forget( "goal_1" )
-      chain( "trans" ).subst( y -> b ); prop; prop
+      cut( hof"f(a, c) = a", "leq_a_c" ); forget( "goal_1" )
+      chain( "trans" ).subst( hov"y" -> le"b" ); prop; prop
 
       // now show the final goals
       repeat( destruct( "goal_1" ) )
       ( chain( "asymm" ) andThen prop ).onAllSubGoals
 
       // f(b,a)=b
-      chain( "trans" ).subst( y -> c ); repeat( prop )
+      chain( "trans" ).subst( hov"y" -> le"c" ); repeat( prop )
 
       // f(c,b)=c
-      chain( "trans" ).subst( y -> a ); repeat( prop )
+      chain( "trans" ).subst( hov"y" -> le"a" ); repeat( prop )
 
       // f(d,c)=c
-      chain( "trans" ).subst( y -> a ); repeat( prop )
+      chain( "trans" ).subst( hov"y" -> le"a" ); repeat( prop )
     }
 
 }
