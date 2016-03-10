018883d467824988008faa1d0cf07263a662dd5b
Martin <marty@logic.at>
2016-03-10 13:31:27 +0100

    more polishing of the ntape example

--- examples/ntape/nTape4.scala
+++ examples/ntape/nTape4.scala
@@ -1,5 +1,6 @@
 package at.logic.gapt.examples
 
+import at.logic.gapt.expr.hol.freeHOVariables
 import at.logic.gapt.expr.{ Ti, Var, Eq }
 import at.logic.gapt.formats.llkNew.loadLLK
 import at.logic.gapt.proofs.{ HOLSequent, Sequent }
@@ -9,7 +10,7 @@ import at.logic.gapt.proofs.lkskNew.LKskProof
 /**
  * Version 3 of the higher-order n-Tape proof.
  */
-case class nTape4( size: Int ) extends nTape {
+class nTape4( val size: Int ) extends nTape {
   require( 1 < size && size < 5, "We have only instances 2 to 4." )
 
   override def proofdb() = loadLLK( getClass.getClassLoader getResourceAsStream s"ntape/ntape4-$size.llk" )
@@ -22,22 +23,39 @@ case class nTape4( size: Int ) extends nTape {
     subsumedClausesRemoval( equality :: deleteTautologies( stripped_css ).toList )
   }
 
+  lazy val preprocessed_css_hol_clauses =
+    preprocessed_css.filter( _.filter( freeHOVariables( _ ).nonEmpty ).nonEmpty )
+
   //prints the interesting terms from the expansion sequent
   override def printStatistics() = {
     println( "------------ Proof sizes --------------" )
-    println( s"Input proof            : ${input_proof.treeLike.size}" )
-    println( s"Preprocessed input     : ${preprocessed_input_proof.treeLike.size}" )
-    println( s"LKsk input proof       : ${lksk_proof.treeLike.size}" )
-    println( s"ACNF output proof      : (not available)" )
+    println( s"Input proof              : ${input_proof.treeLike.size}" )
+    println( s"Preprocessed input       : ${preprocessed_input_proof.treeLike.size}" )
+    println( s"LKsk input proof         : ${lksk_proof.treeLike.size}" )
+    println( s"ACNF output proof        : (not available)" )
     println( "------------ " )
-    println( s"Css size               : ${css.size}" )
-    println( s"Preprocessed css size  : ${preprocessed_css.size}" )
+    println( s"Css size                 : ${css.size}" )
+    println( s"Preprocessed css size    : ${preprocessed_css.size}" )
+    println( s"Clauses with HOL content : ${preprocessed_css_hol_clauses.size}" )
     println( "------------ " )
-    println( s"Refutation size (dag)  : (not available)" )
-    println( s"Refutation size (tree) : (not available)" )
-    println( s"Refutation depth       : (not available)" )
+    println( s"Refutation size (dag)    : (not available)" )
+    println( s"Refutation size (tree)   : (not available)" )
+    println( s"Refutation depth         : (not available)" )
     println( "------------ " )
     println( s"Reproved deep formula proof size (dag)  : (not available)" )
     println( s"Reproved deep formula proof size (tree) : (not available)" )
+  }
+}
+
+object nTape4 {
+  lazy val inst2 = new nTape4( 2 )
+  lazy val inst3 = new nTape4( 3 )
+  lazy val inst4 = new nTape4( 4 )
+
+  def apply( size: Int ) = size match {
+    case 2 => inst2
+    case 3 => inst3
+    case 4 => inst4
+    case _ => throw new Exception( "We have only instances 2 to 4." )
   }
 }
--- examples/ntape/nTape5.scala
+++ examples/ntape/nTape5.scala
@@ -3,15 +3,53 @@ package at.logic.gapt.examples
 import at.logic.gapt.formats.llkNew.loadLLK
 
 /**
- * Version 3 of the higher-order n-Tape proof.
+ * Version 5 of the higher-order n-Tape proof, where if-then-else is directly axiomatized i.e. it has 2 additional
+ * axioms P -> if code(P) then t else f = t and -P -> if code(P) then t else f = f which were theorems before.
+ * In contrast to [[nTape4]] it cuts on instances of the theorem C
+ * for specific upper bounds n. Since the instantiated proofs were generated manually, only nTape5(2) to nTape5(4)
+ * work.
  */
 class nTape5( _size: Int ) extends nTape4( _size ) {
   override def proofdb() = loadLLK( getClass.getClassLoader getResourceAsStream s"ntape/ntape5-$size.llk" )
 }
+
+/**
+ * Version 5 of the higher-order n-Tape proof. In contrast to [[nTape4]] it cuts on instances of the theorem C
+ * for specific upper bounds n. Since the instantiated proofs were generated manually, only nTape5(2) to nTape5(4)
+ * work.
+ */
 object nTape5 {
-  def apply( size: Int ) = new nTape5( size )
+  lazy val inst2 = new nTape5( 2 )
+  lazy val inst3 = new nTape5( 3 )
+  lazy val inst4 = new nTape5( 4 )
+
+  def apply( size: Int ) = size match {
+    case 2 => inst2
+    case 3 => inst3
+    case 4 => inst4
+    case _ => throw new Exception( "We have only instances 2 to 4." )
+  }
 }
 
-object nTape5Arith extends nTape4( 2 ) {
+/**
+ * Version 5 of the higher-order n-Tape proof, where if-then-else is still proved in arithmetic.
+ * In contrast to [[nTape4]] it cuts on instances of the theorem C
+ * for specific upper bounds n. Since the instantiated proofs were generated manually, only nTape5Arith(2) works.
+ */
+class nTape5Arith( _size: Int ) extends nTape4( _size ) {
   override def proofdb() = loadLLK( getClass.getClassLoader getResourceAsStream s"ntape/ntape5-${size}-arithmetic-ite.llk" )
+}
+
+/**
+ * Version 5 of the higher-order n-Tape proof, where if-then-else is still proved in arithmetic.
+ * In contrast to [[nTape4]] it cuts on instances of the theorem C
+ * for specific upper bounds n. Since the instantiated proofs were generated manually, only nTape5Arith(2) works.
+ */
+object nTape5Arith {
+  lazy val inst2 = new nTape5Arith( 2 )
+
+  def apply( size: Int ) = size match {
+    case 2 => inst2
+    case _ => throw new Exception( "We have only instance 2." )
+  }
 }
