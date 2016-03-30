45ceeafa75c902c39e5bb774fa2092d3447fb6de
Gabriel Ebner <gebner@gebner.org>
2016-03-30 16:16:40 +0200

    Exercises for the epsilon calculus lecture.

---
+++ examples/epsilon/epsilon.scala
@@ -0,0 +1,25 @@
+package at.logic.gapt.examples
+import at.logic.gapt.expr._
+import at.logic.gapt.proofs.epsilon.epsilonize
+import at.logic.gapt.proofs.reduction.{ HOFunctionReduction, LambdaEliminationReduction }
+import at.logic.gapt.provers.escargot.Escargot
+
+object epsilon extends Script {
+  println( "Exercises for the lecture on epsilon calculus at the TU Wien:\n" )
+
+  val threeExists = hof"∃x∃y∃z P(x,y,z)"
+  println( s"Translate $threeExists to a formula in epsilon calculus:" )
+  println( epsilonize( threeExists ) )
+
+  println( "Prove using epsilon calculus: there exist irrational numbers x and y such that x^y is rational:" )
+  val irratProblem =
+    hof"""¬rat(s2) ∧ rat(2) ∧ s2*s2 = 2 ∧ pow(s2,2) = 2 ∧
+          ∀x∀y∀z (pow (pow x y) z = pow x (y*z)) ⊃
+          ∃x∃y (¬rat(x) ∧ ¬rat(y) ∧ rat(pow x y))"""
+  println( s"Formalization: $irratProblem" )
+  val Some( irratProof ) = Escargot getEpsilonProof irratProblem
+  println( irratProof )
+
+  val reduction = LambdaEliminationReduction |> HOFunctionReduction
+  require( Escargot isValid reduction.forward( irratProof.deep )._1 )
+}
