863acd6c4a0d7c62fa635203931420af16072e0e
Gabriel Ebner <gebner@gebner.org>
2016-04-06 16:01:09 +0200

    Epsilon lecture exercises: add some newlines.

--- examples/epsilon/epsilon.scala
+++ examples/epsilon/epsilon.scala
@@ -10,13 +10,15 @@ object epsilon extends Script {
   val threeExists = hof"∃x∃y∃z P(x,y,z)"
   println( s"Translate $threeExists to a formula in epsilon calculus:" )
   println( epsilonize( threeExists ) )
+  println()
 
-  println( "Prove using epsilon calculus: there exist irrational numbers x and y such that x^y is rational:" )
+  println( "Prove using epsilon calculus: there exist irrational numbers x and y such that x^y is rational:\n" )
   val irratProblem =
     hof"""¬rat(s2) ∧ rat(2) ∧ s2*s2 = 2 ∧ pow(s2,2) = 2 ∧
           ∀x∀y∀z (pow (pow x y) z = pow x (y*z)) ⊃
           ∃x∃y (¬rat(x) ∧ ¬rat(y) ∧ rat(pow x y))"""
-  println( s"Formalization: $irratProblem" )
+  println( s"Formalization: $irratProblem\n" )
+  println( "Proof:" )
   val Some( irratProof ) = Escargot getEpsilonProof irratProblem
   println( irratProof )
 
