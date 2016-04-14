506e7626dde7103c83f0b96a23311a4c89488539
Gabriel Ebner <gebner@gebner.org>
2016-04-14 15:31:33 +0200

    user manual: document many-sorted reduction

--- cli/src/main/resources/gapt-cli-prelude.scala
+++ cli/src/main/resources/gapt-cli-prelude.scala
@@ -12,6 +12,7 @@ import at.logic.gapt.formats.xml._
 import XMLParser.XMLProofDatabaseParser
 import at.logic.gapt.formats.llkNew._
 import at.logic.gapt.grammars._
+import at.logic.gapt.proofs.reduction._
 import at.logic.gapt.proofs.drup._
 import at.logic.gapt.proofs.epsilon._
 import at.logic.gapt.proofs.expansion._
