3ec43e7626229b777831acf926c9316f64fc6f7a
Gabriel Ebner <gebner@gebner.org>
2016-04-14 12:40:13 +0200

    SequentIndex: scaladoc

--- core/src/main/scala/at/logic/gapt/proofs/sequents.scala
+++ core/src/main/scala/at/logic/gapt/proofs/sequents.scala
@@ -2,6 +2,12 @@ package at.logic.gapt.proofs
 
 import scala.collection.GenTraversable
 
+/**
+ * Represents an index of an element in a sequent.
+ *
+ * In a sequent, the elements have the following indices:
+ * Ant(0), Ant(1), ..., Ant(m) :- Suc(0), Suc(1), ..., Suc(n)
+ */
 sealed abstract class SequentIndex extends Ordered[SequentIndex] {
   def compare( that: SequentIndex ) = ( this, that ) match {
     case ( Ant( _ ), Suc( _ ) ) => -1
