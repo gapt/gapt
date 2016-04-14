f40a17ac03c6d8637d3bfada577b438cad3d5775
Gabriel Ebner <gebner@gebner.org>
2016-04-14 13:05:55 +0200

    Remove resource files that are no longer readable.

--- tests/src/test/resources/resSchema2.rs
+++
@@ -1,6 +0,0 @@
-
-\rho1(0,z) -> r( \rho2(0,z) ; \rho2(0,z) ; P(g(0,a)) )
-\rho1(k+1,z) -> r( \rho2(k+1,z) ; r( \rho2(k+1,z) ; \rho2(k+1,z) ; P(g(0,a)) ) ; P(g(k+1,a)) )
-
-\rho2(0,z) ->  |-P(a)
-\rho2(k+1,z) -> r( \rho2(k,z) ; P(z(k+1))|-P(f(z(k+1))) ; P(g(k,a)))
