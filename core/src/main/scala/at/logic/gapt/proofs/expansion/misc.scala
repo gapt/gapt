342d29a0e448a7f2559da16fa6de976cddf78de1
Gabriel Ebner <gebner@gebner.org>
2016-06-10 21:36:30 +0200

    numberOfInstancesET: fix counting

--- core/src/main/scala/at/logic/gapt/proofs/expansion/misc.scala
+++ core/src/main/scala/at/logic/gapt/proofs/expansion/misc.scala
@@ -66,6 +66,8 @@ object formulaToExpansionTree {
 
 object numberOfInstancesET {
   def apply( t: ExpansionTree ): Int =
-    t.subProofs collect { case ETWeakQuantifier( _, instances ) => instances.size } sum
+    t.treeLike.postOrder collect { case ETWeakQuantifier( _, instances ) => instances.size } sum
   def apply( s: ExpansionSequent ): Int = s.elements map apply sum
+  def apply( ep: ExpansionProof ): Int = apply( ep.expansionSequent )
+  def apply( epwc: ExpansionProofWithCut ): Int = apply( epwc.expansionWithCutAxiom )
 }
