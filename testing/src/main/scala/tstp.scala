0df321c96df6d1602dc4af8536c206a4ca7539fe
Gabriel Ebner <gebner@gebner.org>
2016-08-02 13:24:50 +0200

    Update testTstpImport script.

--- testing/src/main/scala/tstp.scala
+++ testing/src/main/scala/tstp.scala
@@ -1,28 +1,13 @@
 package at.logic.gapt.testing
 
-import at.logic.gapt.formats.StringInputFile
-import at.logic.gapt.formats.tptp.TptpProofParser
-import at.logic.gapt.proofs.sketch.RefutationSketchToResolution
-
-import better.files._
+import at.logic.gapt.proofs.expansion.numberOfInstancesET
+import at.logic.gapt.proofs.loadExpansionProof
+import at.logic.gapt.utils.logging.{ PrintMetrics, metrics }
 
 object testTstpImport extends App {
   val Array( filename ) = args
-
-  val tstpOutput = filename.toFile.contentAsString
-
-  if ( !tstpOutput.contains( "CNFRefutation" ) )
-    println( "NOT_CNF_REFUTATION" )
-
-  val ( endSequent, sketch ) = TptpProofParser parse StringInputFile( tstpOutput )
-  println( s"SKETCH_SIZE ${sketch.subProofs.size}" )
-
-  val resolution = RefutationSketchToResolution( sketch ) getOrElse {
-    println( "FAIL_SKETCH_CONVERSION" )
-    sys exit 1
-  }
-
-  println( s"RESOLUTION_SIZE ${resolution.subProofs.size}" )
-
+  metrics.current.value = PrintMetrics
+  val Some( exp ) = loadExpansionProof( filename )
+  println( s"num_insts = ${numberOfInstancesET( exp )}" )
   println( "OK" )
 }
