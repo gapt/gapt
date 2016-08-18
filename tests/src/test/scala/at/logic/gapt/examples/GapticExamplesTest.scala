8b3fb4b68c614421b46826a57e8cc1febef64ed5
Gabriel Ebner <gebner@gebner.org>
2016-08-18 14:52:09 +0200

    gaptic examples: modernize and unit test

---
+++ tests/src/test/scala/at/logic/gapt/examples/GapticExamplesTest.scala
@@ -0,0 +1,11 @@
+package at.logic.gapt.examples
+
+import org.specs2.mutable.Specification
+
+class GapticExamplesTest extends Specification {
+
+  "examples" in { gapticExamples; ok }
+
+  "drinker" in { drinker; ok }
+
+}
