cd5054c40f40ba2e1edf0cfc42e4111046eba343
Sebastian Zivota <ouroboros@gmx.at>
2016-09-05 13:52:11 +0200

    Move definition class out of Context

---
+++ core/src/main/scala/at/logic/gapt/expr/Definition.scala
@@ -0,0 +1,7 @@
+package at.logic.gapt.expr
+
+case class Definition( what: Const, by: LambdaExpression ) {
+  val Const( name, ty ) = what
+  require( ty == by.exptype )
+  require( freeVariables( by ).isEmpty, s"$this: contains free variables ${freeVariables( by )}" )
+}
