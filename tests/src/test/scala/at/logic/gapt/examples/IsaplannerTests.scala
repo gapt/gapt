d04ff60b6f494609566256470d96020a8153bfa5
Jannik Vierling <jannik.vierling@gmail.com>
2016-12-19 15:40:23 +0100

    made isaplanner tests skip tests if tip tool is not installed

--- tests/src/test/scala/at/logic/gapt/examples/IsaplannerTests.scala
+++ tests/src/test/scala/at/logic/gapt/examples/IsaplannerTests.scala
@@ -1,47 +1,58 @@
 package at.logic.gapt.examples
 
 import at.logic.gapt.examples.tip.isaplanner._
+import at.logic.gapt.formats.tip.TipSmtParser
 import org.specs2.mutable.Specification
 
 class IsaplannerTests extends Specification {
 
+  def requireTip(test: => Any) = {
+    if (TipSmtParser.isInstalled) {
+      test
+    }
+    else {
+      "tip not installed" in skipped("tip tool is not installed")
+    }
+  }
+
   "isaplanner06" in { isaplanner06; ok }
-  "isaplanner07" in { isaplanner07; ok }
-  "isaplanner08" in { isaplanner08; ok }
-  "isaplanner09" in { isaplanner09; ok }
-  "isaplanner10" in { isaplanner10; ok }
-  "isaplanner11" in { isaplanner11; ok }
-  "isaplanner12" in { isaplanner12; ok }
-  "isaplanner13" in { isaplanner13; ok }
-  "isaplanner14" in { isaplanner14; ok }
-  "isaplanner15" in { isaplanner15; ok }
-  "isaplanner16" in { isaplanner16; ok }
-  "isaplanner17" in { isaplanner17; ok }
-  "isaplanner18" in { isaplanner18; ok }
-  "isaplanner19" in { isaplanner19; ok }
 
-  "isaplanner21" in { isaplanner21; ok }
-  "isaplanner22" in { isaplanner22; ok }
-  "isaplanner23" in { isaplanner23; ok }
-  "isaplanner24" in { isaplanner24; ok }
+  requireTip {"isaplanner07" in { isaplanner07; ok }}
+  requireTip {"isaplanner08" in { isaplanner08; ok }}
+  requireTip {"isaplanner09" in { isaplanner09; ok }}
+  requireTip {"isaplanner10" in { isaplanner10; ok }}
+  requireTip {"isaplanner11" in { isaplanner11; ok }}
+  requireTip {"isaplanner12" in { isaplanner12; ok }}
+  requireTip {"isaplanner13" in { isaplanner13; ok }}
+  requireTip {"isaplanner14" in { isaplanner14; ok }}
+  requireTip {"isaplanner15" in { isaplanner15; ok }}
+  requireTip {"isaplanner16" in { isaplanner16; ok }}
+  requireTip {"isaplanner17" in { isaplanner17; ok }}
+  requireTip {"isaplanner18" in { isaplanner18; ok }}
+  requireTip {"isaplanner19" in { isaplanner19; ok }}
 
-  "isaplanner26" in { isaplanner26; ok }
-  "isaplanner27" in { isaplanner27; ok }
-  "isaplanner28" in { isaplanner28; ok }
-  "isaplanner29" in { isaplanner29; ok }
-  "isaplanner30" in { isaplanner30; ok }
-  "isaplanner31" in { isaplanner31; ok }
-  "isaplanner32" in { isaplanner32; ok }
-  "isaplanner33" in { isaplanner33; ok }
-  "isaplanner34" in { isaplanner34; ok }
-  "isaplanner35" in { isaplanner35; ok }
-  "isaplanner36" in { isaplanner36; ok }
-  "isaplanner37" in { isaplanner37; ok }
-  "isaplanner38" in { isaplanner38; ok }
-  "isaplanner39" in { isaplanner39; ok }
-  "isaplanner40" in { isaplanner40; ok }
-  "isaplanner41" in { isaplanner41; ok }
-  "isaplanner42" in { isaplanner42; ok }
-  "isaplanner43" in { isaplanner43; ok }
-  "isaplanner44" in { isaplanner44; ok }
+  requireTip {"isaplanner21" in { isaplanner21; ok }}
+  requireTip {"isaplanner22" in { isaplanner22; ok }}
+  requireTip {"isaplanner23" in { isaplanner23; ok }}
+  requireTip {"isaplanner24" in { isaplanner24; ok }}
+
+  requireTip {"isaplanner26" in { isaplanner26; ok }}
+  requireTip {"isaplanner27" in { isaplanner27; ok }}
+  requireTip {"isaplanner28" in { isaplanner28; ok }}
+  requireTip {"isaplanner29" in { isaplanner29; ok }}
+  requireTip {"isaplanner30" in { isaplanner30; ok }}
+  requireTip {"isaplanner31" in { isaplanner31; ok }}
+  requireTip {"isaplanner32" in { isaplanner32; ok }}
+  requireTip {"isaplanner33" in { isaplanner33; ok }}
+  requireTip {"isaplanner34" in { isaplanner34; ok }}
+  requireTip {"isaplanner35" in { isaplanner35; ok }}
+  requireTip {"isaplanner36" in { isaplanner36; ok }}
+  requireTip {"isaplanner37" in { isaplanner37; ok }}
+  requireTip {"isaplanner38" in { isaplanner38; ok }}
+  requireTip {"isaplanner39" in { isaplanner39; ok }}
+  requireTip {"isaplanner40" in { isaplanner40; ok }}
+  requireTip {"isaplanner41" in { isaplanner41; ok }}
+  requireTip {"isaplanner42" in { isaplanner42; ok }}
+  requireTip {"isaplanner43" in { isaplanner43; ok }}
+  requireTip {"isaplanner44" in { isaplanner44; ok }}
 }
