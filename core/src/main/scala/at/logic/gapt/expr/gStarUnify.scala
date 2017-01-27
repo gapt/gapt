605461961672cb245e6afeab4d3966cc7d492413
Lettmann <calmdown@gmx.de>
2017-01-27 10:34:07 +0100

    Added the programs to compute the set of G*-unified literals and to compute the cut-formula

--- core/src/main/scala/at/logic/gapt/expr/gStarUnify.scala
+++ core/src/main/scala/at/logic/gapt/expr/gStarUnify.scala
@@ -1,8 +1,266 @@
 package at.logic.gapt.expr
 
 /**
-  * Created by root on 26.01.17.
-  */
+ * Created by root on 26.01.17.
+ */
 object gStarUnify {
+
+  def apply(
+    setOfLiterals:             Set[FOLFormula],
+    productionRulesX:          Set[( LambdaExpression, LambdaExpression )],
+    productionRulesY:          Set[( LambdaExpression, LambdaExpression )],
+    universalEigenvariable:    FOLVar,
+    existentialEigenvariables: List[FOLVar],
+    nameOfExistentialVariable: FOLVar,
+    nameOfUniversalVariable:   FOLVar
+  ): Set[FOLFormula] = {
+
+    val productionRulesXS: scala.collection.mutable.Set[( LambdaExpression, LambdaExpression )] = scala.collection.mutable.Set[( LambdaExpression, LambdaExpression )]()
+    productionRulesX.foreach( element => {
+      productionRulesXS += element
+      productionRulesXS += element.swap
+    } )
+
+    val productionRulesYS: scala.collection.mutable.Set[( LambdaExpression, LambdaExpression )] = scala.collection.mutable.Set[( LambdaExpression, LambdaExpression )]()
+    productionRulesY.foreach( element => {
+      productionRulesYS += element
+      productionRulesYS += element.swap
+    } )
+
+    val literals = sortAndAtomize( setOfLiterals )
+    val ( posAtoms, negAtoms ) = literals
+
+    val unifiedLiterals = scala.collection.mutable.Set[FOLFormula]()
+
+    posAtoms.foreach( posAt =>
+      negAtoms.foreach( negAt =>
+        unifyLiterals(
+          posAt,
+          negAt,
+          productionRulesXS.toSet,
+          productionRulesYS.toSet,
+          universalEigenvariable,
+          existentialEigenvariables,
+          nameOfExistentialVariable,
+          nameOfUniversalVariable
+        ) match {
+          /*
+          The next line is a compromise since we do not consider negation during the unification procedure (Future work). This way we add some unnecessary literals.
+           */
+          case Some( t ) => {
+            unifiedLiterals += t
+            unifiedLiterals += Neg( t )
+          }
+          case None =>
+        } ) )
+
+    unifiedLiterals.toSet
+
+  }
+
+  private def sortAndAtomize( literals: Set[FOLFormula] ): ( Set[FOLFormula], Set[FOLFormula] ) = {
+
+    val posLiterals: scala.collection.mutable.Set[FOLFormula] = scala.collection.mutable.Set()
+    val negLiterals: scala.collection.mutable.Set[FOLFormula] = scala.collection.mutable.Set()
+
+    for ( literal <- literals ) {
+
+      literal match {
+        case Neg( t ) => negLiterals += t
+        case _        => posLiterals += literal
+      }
+
+    }
+
+    ( posLiterals.toSet, negLiterals.toSet )
+
+  }
+
+  private def unifyLiterals(
+    posAt:                     FOLFormula,
+    negAt:                     FOLFormula,
+    productionRulesX:          Set[( LambdaExpression, LambdaExpression )],
+    productionRulesY:          Set[( LambdaExpression, LambdaExpression )],
+    universalEigenvariable:    FOLVar,
+    existentialEigenvariables: List[FOLVar],
+    nameOfExistentialVariable: FOLVar,
+    nameOfUniversalVariable:   FOLVar
+  ): Option[FOLFormula] = {
+
+    val Apps( nameOfPos, argsP ): FOLFormula = posAt
+    val Apps( nameOfNeg, argsN ): FOLFormula = negAt
+
+    val unifiedLiteral: Option[FOLFormula] = nameOfPos match {
+      case t if ( ( nameOfNeg == t ) && ( argsP.length == argsN.length ) ) => {
+        val unifiedArgs = unify(
+          argsP.zip( argsN ),
+          productionRulesX,
+          productionRulesY,
+          universalEigenvariable,
+          existentialEigenvariables,
+          nameOfExistentialVariable,
+          nameOfUniversalVariable
+        )
+
+        val theUnifiedLiteral = unifiedArgs match {
+          case Some( s ) => {
+            if ( s.length == argsP.length ) {
+              Some( Apps( nameOfPos, s ).asInstanceOf[FOLFormula] )
+            } else {
+              None
+            }
+          }
+          case _ => None
+        }
+        theUnifiedLiteral
+      }
+      case _ => None
+    }
+
+    unifiedLiteral
+
+  }
+
+  private def unify(
+    zippedArgs:                List[( LambdaExpression, LambdaExpression )],
+    productionRulesX:          Set[( LambdaExpression, LambdaExpression )],
+    productionRulesY:          Set[( LambdaExpression, LambdaExpression )],
+    universalEigenvariable:    FOLVar,
+    existentialEigenvariables: List[FOLVar],
+    nameOfExistentialVariable: FOLVar,
+    nameOfUniversalVariable:   FOLVar
+  ): Option[Seq[FOLTerm]] = {
+
+    var unifiedTerms: Option[Seq[FOLTerm]] = None
+    var stopIt: Boolean = false
+    var stopItAll: Boolean = false
+
+    zippedArgs.foreach( t => {
+      stopIt = false
+      stopItAll = false
+
+      val ( tL, tR ) = t
+
+      productionRulesX.foreach( productionRuleX => if ( !stopIt ) {
+
+        val ( productionRuleXL, productionRuleXR ) = productionRuleX
+
+        if ( productionRuleXL.syntaxEquals( tL ) && productionRuleXR.syntaxEquals( tR ) ) {
+          unifiedTerms = unifiedTerms match {
+            case Some( update ) => Option( update :+ nameOfUniversalVariable )
+            case None           => Option( Seq( nameOfUniversalVariable ) )
+          }
+          stopIt = true
+          stopItAll = true
+        }
+
+      } )
+
+      stopIt = false
+
+      productionRulesY.foreach( productionRuleY => if ( ( !stopIt ) && ( !stopItAll ) ) {
+
+        val ( productionRuleYL, productionRuleYR ) = productionRuleY
+
+        if ( productionRuleYL.syntaxEquals( tL ) && productionRuleYR.syntaxEquals( tR ) ) {
+          unifiedTerms = unifiedTerms match {
+            case Some( update ) => Option( update :+ nameOfExistentialVariable )
+            case None           => Option( Seq( nameOfExistentialVariable ) )
+          }
+          stopIt = true
+          stopItAll = true
+        }
+
+      } )
+
+      if ( !stopItAll ) {
+
+        val Apps( nameOfArgL, argsOfArgL ) = tL
+        val Apps( nameOfArgR, argsOfArgR ) = tR
+
+        if ( tL.syntaxEquals( tR ) ) {
+
+          var tLWasAEigenvariable: Boolean = false
+
+          if ( tL.syntaxEquals( universalEigenvariable ) ) {
+
+            tLWasAEigenvariable = true
+
+            unifiedTerms = unifiedTerms match {
+              case Some( update ) => Option( update :+ nameOfUniversalVariable )
+              case None           => Option( Seq( nameOfUniversalVariable ) )
+            }
+
+          }
+
+          existentialEigenvariables.foreach( existentialEigenvariable => if ( tL.syntaxEquals( existentialEigenvariable ) && !tLWasAEigenvariable ) {
+
+            tLWasAEigenvariable = true
+
+            unifiedTerms = unifiedTerms match {
+              case Some( update ) => Option( update :+ nameOfExistentialVariable )
+              case None           => Option( Seq( nameOfExistentialVariable ) )
+            }
+
+          } )
+
+          if ( !tLWasAEigenvariable && ( argsOfArgL.length == 0 ) ) {
+
+            tLWasAEigenvariable = true
+
+            unifiedTerms = unifiedTerms match {
+              case Some( update ) => Option( update :+ tL.asInstanceOf[FOLTerm] )
+              case None           => Option( Seq( tR.asInstanceOf[FOLTerm] ) )
+            }
+
+          }
+
+          if ( ( !tLWasAEigenvariable ) ) {
+
+            unify(
+              argsOfArgL.zip( argsOfArgR ),
+              productionRulesX,
+              productionRulesY,
+              universalEigenvariable,
+              existentialEigenvariables,
+              nameOfExistentialVariable,
+              nameOfUniversalVariable
+            ) match {
+                case Some( r ) => unifiedTerms = unifiedTerms match {
+                  case Some( update ) => Option( update :+ Apps( nameOfArgL, r ).asInstanceOf[FOLTerm] )
+                  case None           => Option( Seq( Apps( nameOfArgL, r ).asInstanceOf[FOLTerm] ) )
+                }
+                case _ => unifiedTerms = None
+              }
+
+          }
+
+        } else if ( ( nameOfArgL == nameOfArgR ) && ( argsOfArgL.length == argsOfArgR.length ) ) {
+
+          unify(
+            argsOfArgL.zip( argsOfArgR ),
+            productionRulesX,
+            productionRulesY,
+            universalEigenvariable,
+            existentialEigenvariables,
+            nameOfExistentialVariable,
+            nameOfUniversalVariable
+          ) match {
+              case Some( r ) => unifiedTerms = unifiedTerms match {
+                case Some( update ) => Option( update :+ Apps( nameOfArgL, r ).asInstanceOf[FOLTerm] )
+                case None           => Option( Seq( Apps( nameOfArgL, r ).asInstanceOf[FOLTerm] ) )
+              }
+              case _ => unifiedTerms = None
+            }
+
+        }
+
+      }
+
+    } )
+
+    unifiedTerms
+
+  }
 
 }
--- core/src/main/scala/at/logic/gapt/expr/introducePiCut.scala
+++ core/src/main/scala/at/logic/gapt/expr/introducePiCut.scala
@@ -1,8 +1,374 @@
 package at.logic.gapt.expr
 
+import at.logic.gapt.expr.hol.CNFp
+import at.logic.gapt.proofs.Sequent
+
 /**
-  * Created by root on 26.01.17.
-  */
-object introducePiCut {
+ * Created by root on 26.01.17.
+ */
+class pi2SeHs(
+    val reducedRepresentation:         Sequent[FOLFormula], // F[x\U_1] |- G[y\U_2]
+    val universalEigenvariable:        FOLVar, // alpha
+    val existentialEigenvariables:     List[FOLVar], // beta_1,...,beta_m
+    val substitutionsForAlpha:         List[LambdaExpression], // r_1,...,r_m
+    val substitutionsForBetaWithAlpha: List[LambdaExpression] // t_1(alpha),...,t_p(alpha)
+) {
+
+  require( existentialEigenvariables.length == substitutionsForAlpha.length )
+
+  val multiplicityOfAlpha: Int = substitutionsForAlpha.length // m
+  val multiplicityOfBeta: Int = substitutionsForBetaWithAlpha.length // p
+  var balancedSolution: Option[FOLFormula] = None
+  var noSolutionHasBeenFound: Boolean = true
+
+  // (alpha,r_1),...,(alpha,r_m)
+  //////////////////////////////
+  def substitutionPairsAlpha(): Set[( LambdaExpression, LambdaExpression )] = {
+
+    val substitutionPairsAlpha = scala.collection.mutable.Set[( LambdaExpression, LambdaExpression )]()
+    substitutionsForAlpha.foreach( instance => {
+      val buffer: ( LambdaExpression, LambdaExpression ) = ( universalEigenvariable, instance )
+      substitutionPairsAlpha += buffer
+    } )
+    substitutionPairsAlpha.toSet
+  }
+
+  // (beta_i,t_1(alpha)),...,(beta_i,t_p(alpha))
+  //////////////////////////////////////////////
+  def substitutionPairsBetaI( index: Int ): Set[( LambdaExpression, LambdaExpression )] = {
+
+    val substitutionPairsBetaI = scala.collection.mutable.Set[( LambdaExpression, LambdaExpression )]()
+    substitutionsForBetaWithAlpha.foreach( instanceB => {
+      val buffer: ( LambdaExpression, LambdaExpression ) = ( existentialEigenvariables( index - 1 ), instanceB )
+      substitutionPairsBetaI += buffer
+    } )
+    substitutionPairsBetaI.toSet
+  }
+
+  // (beta_1,t_1(alpha)),...,(beta_1,t_p(alpha)),
+  //                     ...                    ,
+  // (beta_m,t_1(alpha)),...,(beta_m,t_p(alpha))
+  ///////////////////////////////////////////////
+  def substitutionPairsBeta(): Set[( LambdaExpression, LambdaExpression )] = {
+
+    val substitutionPairsBeta = scala.collection.mutable.Set[( LambdaExpression, LambdaExpression )]()
+    for ( index <- 1 to multiplicityOfAlpha ) {
+      substitutionPairsBeta ++= substitutionPairsBetaI( multiplicityOfAlpha - index + 1 )
+    }
+    substitutionPairsBeta.toSet
+  }
+
+  // (alpha->r_1),...,(alpha->r_m)
+  ////////////////////////////////
+  def substitutionsAlpha(): List[Substitution] = {
+
+    val substitutionsAlpha = scala.collection.mutable.ListBuffer[Substitution]()
+    substitutionsForAlpha.foreach( instanceA => {
+      substitutionsAlpha += Substitution( universalEigenvariable, instanceA )
+    } )
+    substitutionsAlpha.toList
+  }
+
+  // (beta_i->t_1(r_i)),...,(beta_i->t_p(r_i))
+  ////////////////////////////////////////////
+  def substitutionsBetaI( index: Int ): List[Substitution] = {
+
+    val substitutionsBeta = scala.collection.mutable.ListBuffer[Substitution]()
+    val subs: Substitution = Substitution( universalEigenvariable, substitutionsForAlpha( index - 1 ) ) // (alpha->r_i)
+    substitutionsForBetaWithAlpha.foreach( instanceB => {
+      substitutionsBeta += Substitution( existentialEigenvariables( index - 1 ), subs( instanceB ) )
+    } )
+    substitutionsBeta.toList
+  }
+
+  private def substituteRightSideOnce( sequent: Sequent[HOLFormula], index: Int ): Sequent[HOLFormula] = {
+
+    var resultingSequent: Sequent[HOLFormula] = Sequent()
+
+    sequent.succedent.foreach( formula => {
+      formula.find( existentialEigenvariables( index - 1 ) ) match {
+        case List() => resultingSequent = resultingSequent :+ formula
+        case _ => substitutionsBetaI( index ).foreach( subs => {
+          resultingSequent = resultingSequent :+ subs( formula )
+        } )
+      }
+    } )
+
+    resultingSequent
+  }
+
+  private def substituteLeftSideOnce( sequent: Sequent[HOLFormula], index: Int ): Sequent[HOLFormula] = {
+
+    var resultingSequent: Sequent[HOLFormula] = Sequent()
+
+    sequent.antecedent.foreach( formula => {
+      formula.find( existentialEigenvariables( index - 1 ) ) match {
+        case List() => resultingSequent = formula +: resultingSequent
+        case _ => substitutionsBetaI( index ).foreach( subs => {
+          resultingSequent = subs( formula ) +: resultingSequent
+        } )
+      }
+    } )
+
+    resultingSequent
+  }
+
+  // F[x\T_1] |- G[y\T_2]
+  ///////////////////////
+  def herbrandSequent(): Sequent[HOLFormula] = {
+
+    var herbrandSequent: Sequent[HOLFormula] = Sequent() :++ reducedRepresentation.succedent
+
+    for ( indexM <- 0 until multiplicityOfAlpha ) {
+      herbrandSequent = substituteRightSideOnce( herbrandSequent, multiplicityOfAlpha - indexM )
+    }
+
+    reducedRepresentation.antecedent.foreach( formula => {
+      substitutionsForAlpha.foreach( instanceA => {
+        val subs: Substitution = Substitution( universalEigenvariable, instanceA )
+        herbrandSequent = subs( formula ) +: herbrandSequent
+      } )
+    } )
+
+    val sequent: Sequent[HOLFormula] = herbrandSequent
+
+    for ( indexM <- 0 until multiplicityOfAlpha ) {
+      herbrandSequent = substituteLeftSideOnce( herbrandSequent.antecedent ++: Sequent(), multiplicityOfAlpha - indexM ) :++ sequent.succedent
+    }
+
+    herbrandSequent
+  }
+
+  // The reduced representation as a formula
+  //////////////////////////////////////////
+  val reducedRepresentationToFormula: FOLFormula = reducedRepresentation.toImplication
+
+}
+
+object introducePi2Cut {
+
+  def apply(
+    seHs:                      pi2SeHs,
+    nameOfExistentialVariable: FOLVar  = fov"yCut",
+    nameOfUniversalVariable:   FOLVar  = fov"xCut"
+  ): Option[FOLFormula] = {
+
+    val literals = scala.collection.mutable.Set[FOLFormula]()
+    val DNTA = scala.collection.mutable.Set[Sequent[FOLFormula]]()
+
+    CNFp( seHs.reducedRepresentationToFormula ).foreach( clause => if ( !clause.isTaut ) {
+      var NTAClause: Sequent[FOLFormula] = clause
+      for ( literal <- clause.succedent ) {
+        NTAClause = Neg( literal ) +: NTAClause
+      }
+      NTAClause = NTAClause.antecedent ++: Sequent()
+      DNTA += NTAClause // define for fol and hol sequents
+      clause.antecedent.foreach( atom => literals += atom )
+      clause.succedent.foreach( atom => literals += Neg( atom ) )
+    } )
+
+    val DNTAList = DNTA.toList
+
+    val unifiedLiterals: Set[FOLFormula] = gStarUnify(
+      literals.toSet,
+      seHs.substitutionPairsAlpha(),
+      seHs.substitutionPairsBeta(),
+      seHs.universalEigenvariable,
+      seHs.existentialEigenvariables,
+      rename.awayFrom( freeVariables( seHs.reducedRepresentationToFormula ) ).fresh( nameOfExistentialVariable ),
+      rename.awayFrom( freeVariables( seHs.reducedRepresentationToFormula ) ).fresh( nameOfUniversalVariable )
+    )
+
+    val allowedClausesIndex: ( List[( Set[FOLFormula], List[Int], List[( Int, List[Int] )] )] ) = allowedClausesWithIndex(
+      unifiedLiterals,
+      DNTAList,
+      seHs,
+      rename.awayFrom( freeVariables( seHs.reducedRepresentationToFormula ) ).fresh( nameOfUniversalVariable ),
+      rename.awayFrom( freeVariables( seHs.reducedRepresentationToFormula ) ).fresh( nameOfExistentialVariable )
+    )
+
+    if ( seHs.noSolutionHasBeenFound ) {
+      for ( subsetSize <- 2 to allowedClausesIndex.length; if ( seHs.noSolutionHasBeenFound ) ) {
+        for ( subset <- allowedClausesIndex.toSet.subsets( subsetSize ); if ( seHs.noSolutionHasBeenFound ) ) {
+          if ( checkCombinedClauses( DNTAList.length, subset.toList ) ) {
+            seHs.noSolutionHasBeenFound = false
+            val ( clauses, _, _ ) = subset.toList.unzip3
+            val clausesAsFormula = clauses.map( clause => clause.toList ).map( clause => And( clause ) )
+
+            seHs.balancedSolution = Option( Or( clausesAsFormula ) )
+
+          }
+        }
+      }
+    }
+
+    if ( !seHs.noSolutionHasBeenFound ) {
+      seHs.balancedSolution
+    } else {
+      None
+    }
+
+  }
+
+  private def allowedClausesWithIndex(
+    literals:              Set[FOLFormula],
+    nonTautologicalLeaves: List[Sequent[FOLFormula]],
+    seHs:                  pi2SeHs,
+    x:                     FOLVar,
+    y:                     FOLVar
+  ): ( List[( Set[FOLFormula], List[Int], List[( Int, List[Int] )] )] ) = {
+
+    var clausesPlusIndex = scala.collection.mutable.Set[( Set[FOLFormula], List[Int], List[( Int, List[Int] )] )]()
+    val literalsBuffer = scala.collection.mutable.Set( literals.toList: _* )
+
+    clausesPlusIndex = recursionAllowedClausesWithIndex( 1, literalsBuffer, clausesPlusIndex, nonTautologicalLeaves, seHs, x, y )
+
+    clausesPlusIndex.toList
+  }
+
+  private def recursionAllowedClausesWithIndex(
+    subsetSize:            Int,
+    literals:              scala.collection.mutable.Set[FOLFormula],
+    clausesPlusIndex:      scala.collection.mutable.Set[( Set[FOLFormula], List[Int], List[( Int, List[Int] )] )],
+    nonTautologicalLeaves: List[Sequent[FOLFormula]],
+    seHs:                  pi2SeHs,
+    x:                     FOLVar,
+    y:                     FOLVar
+  ): ( scala.collection.mutable.Set[( Set[FOLFormula], List[Int], List[( Int, List[Int] )] )] ) = {
+
+    for ( subset <- literals.subsets( subsetSize ); if seHs.noSolutionHasBeenFound ) {
+
+      var exists = List[Int]()
+      var indexList = List[( Int, List[Int] )]()
+
+      for ( leaf <- nonTautologicalLeaves ) {
+
+        for ( existsIndex <- 0 until seHs.multiplicityOfBeta ) {
+
+          val subs = Substitution( ( x, seHs.universalEigenvariable ), ( y, seHs.substitutionsForBetaWithAlpha( existsIndex ) ) )
+          var subsetSequent: Sequent[FOLFormula] = Sequent()
+          for ( ele <- subset ) {
+            subsetSequent = subs( ele ).asInstanceOf[FOLFormula] +: subsetSequent
+          }
+
+          if ( subsetSequent.isSubsetOf( leaf ) ) {
+            exists = exists :+ nonTautologicalLeaves.indexOf( leaf )
+          }
+        }
+
+        var betaIndexSet = List[Int]()
+        for ( forallIndex <- 0 until seHs.multiplicityOfAlpha ) {
+
+          val subs: Substitution = Substitution( ( x, seHs.substitutionsForAlpha( forallIndex ) ), ( y, seHs.existentialEigenvariables( forallIndex ) ) )
+          var subsetSequent: Sequent[FOLFormula] = Sequent()
+          for ( ele <- subset ) {
+            subsetSequent = Neg( subs( ele ).asInstanceOf[FOLFormula] ) +: subsetSequent
+          }
+
+          if ( !leaf.intersect( subsetSequent ).isEmpty ) {
+            betaIndexSet = betaIndexSet :+ forallIndex
+          }
+
+        }
+
+        val newElement: ( Int, List[Int] ) = ( nonTautologicalLeaves.indexOf( leaf ), betaIndexSet )
+        indexList = indexList :+ newElement
+
+      }
+
+      // Collects all necessary information and deletes unnecessary literals
+      //////////////////////////////////////////////////////////////////////
+      if ( exists.nonEmpty ) {
+        val clausePlusIndex = ( subset.toSet, exists, indexList )
+        clausesPlusIndex += clausePlusIndex
+      } else {
+        subset.foreach( literal => literals -= literal )
+      }
+
+      // Checks whether a single clause is already a solution
+      ///////////////////////////////////////////////////////
+      if ( exists.nonEmpty ) {
+
+        var existsIndex: Boolean = true
+        val ( _, i ) = indexList.unzip // ( i, _ )
+
+        for ( leafIndex <- 0 until nonTautologicalLeaves.length; if existsIndex ) {
+          if ( i( leafIndex ).isEmpty || !exists.contains( leafIndex ) ) { // !i.contains( leafIndex )
+            existsIndex = false
+          }
+        }
+
+        if ( existsIndex ) {
+
+          seHs.noSolutionHasBeenFound = false
+          seHs.balancedSolution = Option( And( subset ) )
+
+        }
+
+      }
+
+    }
+
+    if ( literals.toList.length <= subsetSize ) {
+      clausesPlusIndex
+    } else if ( !seHs.noSolutionHasBeenFound ) {
+      clausesPlusIndex
+    } else {
+      recursionAllowedClausesWithIndex( subsetSize + 1, literals, clausesPlusIndex, nonTautologicalLeaves, seHs, x, y )
+    }
+  }
+
+  private def checkCombinedClauses(
+    numberOfDNTAs:             Int,
+    setOfClausesPlusIndexSets: List[( Set[FOLFormula], List[Int], List[( Int, List[Int] )] )]
+  ): ( Boolean ) = {
+
+    var isSolution: Boolean = true
+
+    val ( _, existsIndexList, betaIndexList ) = setOfClausesPlusIndexSets.unzip3
+
+    for ( i <- 0 until numberOfDNTAs; if isSolution ) {
+      if ( !existsIndexList.flatten.contains( i ) ) {
+        isSolution = false
+      }
+    }
+
+    if ( isSolution ) {
+      var list: List[Int] = Nil
+      for ( i <- 0 until numberOfDNTAs ) {
+        list = list :+ i
+      }
+      val intersections = new Array[List[Int]]( numberOfDNTAs )
+      var foundEmptyIntersection: Boolean = false
+      isSolution = betaIndexList.forall( element => {
+        element.forall( ele => {
+          val ( leafIndex, satisfiedOnes ) = ele
+          if ( foundEmptyIntersection ) {
+            false
+          } else if ( list.contains( leafIndex ) ) {
+            list = list.filterNot( t => t == leafIndex )
+            intersections( leafIndex ) = satisfiedOnes
+            if ( intersections( leafIndex ).isEmpty ) {
+              foundEmptyIntersection = true
+              false
+            } else {
+              true
+            }
+          } else {
+            intersections( leafIndex ) = intersections( leafIndex ).intersect( satisfiedOnes )
+            if ( intersections( leafIndex ).isEmpty ) {
+              foundEmptyIntersection = true
+              false
+            } else {
+              true
+            }
+          }
+        } )
+      } )
+    }
+
+    isSolution
+
+  }
 
 }
