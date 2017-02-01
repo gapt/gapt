66f7a321020c95caa6cfab3b02479c53e5dfde8e
Matthias Schlaipfer <m.schlaipfer@gmail.com>
2017-02-01 17:43:49 +0100

    Applied Gabriel's comments

--- tests/src/test/scala/at/logic/gapt/proofs/expansion/deskolemizeETTest.scala
+++ tests/src/test/scala/at/logic/gapt/proofs/expansion/deskolemizeETTest.scala
@@ -3,7 +3,6 @@ package at.logic.gapt.proofs.expansion
 import at.logic.gapt.expr.Polarity.{ Negative, Positive }
 import at.logic.gapt.expr._
 import at.logic.gapt.proofs.Sequent
-import at.logic.gapt.provers.escargot.Escargot
 import at.logic.gapt.utils.SatMatchers
 import org.specs2.mutable.Specification
 
@@ -13,36 +12,20 @@ class deskolemizeETTest extends Specification with SatMatchers {
 
     val x0 = Var( "x_0", Ti )
     val s0 = Const( "s_0", Ti -> Ti )
-    val s0x0 = App( s0, x0 )
-    val seq = Sequent() :+ ETWeakQuantifier( hof"?x (P x -> !y (x = x & P y))", collection.immutable.ListMap(
+    val ep = ExpansionProof( Sequent() :+ ETWeakQuantifier( hof"?x (P x -> !y (x = x & P y))", Map(
       x0 -> ETImp(
-        ETWeakening( hof"P ${x0.name}", Negative ),
-        ETSkolemQuantifier( hof"!y (${x0.name} = ${x0.name} & P y)", s0x0, Abs( Var( "x", Ti ), hof"!y (x = x & P y)" ),
-          ETAnd( ETAtom( hoa"${x0.name} = ${x0.name}", Positive ), ETAtom( hoa"P (${s0.name} ${x0.name})", Positive ) ) )
+        ETWeakening( hof"P x_0", Negative ),
+        ETSkolemQuantifier( hof"!y (x_0 = x_0 & P y)", le"s_0 x_0", Abs( Var( "x", Ti ), hof"!y (x = x & P y)" ),
+          ETAnd( ETAtom( hoa"x_0 = x_0", Positive ), ETAtom( hoa"P (s_0 x_0)", Positive ) ) )
       ),
-      s0x0 -> ETImp(
-        ETAtom( hoa"P (${s0.name} ${x0.name})", Negative ),
-        ETWeakening( hof"!y ((${s0.name} ${x0.name}) = (${s0.name} ${x0.name}) & P y)", Positive )
+      le"s_0 x_0" -> ETImp(
+        ETAtom( hoa"P (s_0 x_0)", Negative ),
+        ETWeakening( hof"!y ((s_0 x_0) = (s_0 x_0) & P y)", Positive )
       )
-    ) )
+    ) ) )
 
-    /* TODO: Matching with desk does not work for some reason
-    val v0 = Var( "v", Ti )
-    val desk = Sequent() :+ ETWeakQuantifier( hof"?x (P x -> !y (x = x & P y))", collection.immutable.ListMap(
-      x0 -> ETImp(
-        ETWeakening( hof"P ${x0.name}", Negative ),
-        ETStrongQuantifier( hof"!y (${x0.name} = ${x0.name} & P y)", v0,
-          ETAnd( ETAtom( hoa"${x0.name} = ${x0.name}", Positive ), ETAtom( hoa"P ${v0.name}", Positive ) ) )
-      ),
-      v0 -> ETImp(
-        ETAtom( hoa"P ${v0.name}", Negative ),
-        ETWeakening( hof"!y (${v0.name} = ${v0.name} & P y)", Positive )
-      )
-    ) )
-    */
-
-    Escargot.isValid( ExpansionProof( seq ).deep ) mustEqual true
-    Escargot.isValid( deskolemizeET( ExpansionProof( seq ) ).deep ) mustEqual true
+    ep.deep must beEValidSequent
+    deskolemizeET( ep ).deep must beEValidSequent
   }
 
 }
