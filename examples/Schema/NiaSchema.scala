c81c4314834df9b38e082b7a56cfb18b0a0e3c5e
Ermine516 <cernadavid1@gmail.com>
2017-02-10 22:10:46 +0100

    Almost finish with adding the schema. One case is missing.

--- examples/Schema/NiaSchema.scala
+++ examples/Schema/NiaSchema.scala
@@ -14,6 +14,7 @@ object niaSchema extends TacticsProof {
   ctx += hoc"si:i>i"
   ctx += hoc"s:w>w"
   ctx += hoc"f:i>w"
+  ctx += hoc"max:i>i>i"
   ctx += hoc"POR:w>i>o"
   ctx += hoc"mu: w>w"
   ctx += hoc"omega: w>w"
@@ -22,6 +23,8 @@ object niaSchema extends TacticsProof {
   ctx += hos"E(f(p),n),E(f(q),n) :- E(f(p),f(q))"
   ctx += hos" :- LEQ(p,p)"
   ctx += hos"LEQ(si(p),q):- LE(p,q)"
+  ctx += hos"LEQ(max(a, b), c) :- LEQ(a, c)"
+  ctx += hos"LEQ(max(a, b), c) :- LEQ(b, c)"
 
 //The Name declaration of proof mu
   val esMu = Sequent( Seq(hof"!x?y(LEQ(x,y) & E(f(y),n))") , Seq( hof"?p?q (LE(p,q) & E(f(p),f(q)))" ) )
@@ -38,7 +41,7 @@ object niaSchema extends TacticsProof {
 
 //The Name declaration of proof phi
   val esphi = Sequent( 
-      Seq(hof"!x!y POR(s(y),x) = (E(f(x),s(y)) &  POR(y,x))", 
+      Seq(hof"!x!y POR(s(y),x) = (E(f(x),s(y)) |  POR(y,x))", 
           hof"!x POR(0,x) = E(f(x),0)",
           hof"!x?y (LEQ(x,y) & POR(n,y) )   "
          ) , 
@@ -121,6 +124,28 @@ object niaSchema extends TacticsProof {
   ctx += Context.ProofDefinitionDeclaration( le"omega 0", omegaBc )
 
 
+//The Step case of  omega
+//Need to complete
+   val esOmegaSc = Sequent(
+    Seq(( "Ant_0" -> hof"!x!y POR(s(y),x) = (E(f(x),s(y)) |  POR(y,x))"   ),
+        ( "Ant_1" -> hof"!x POR(0,x) = E(f(x),0)"   ),
+        ( "Ant_2" -> hof"!x POR(0,x)"   )),
+    Seq(( "Suc_0" -> hof"?p?q (LE(p,q) & E(f(p),f(q))) "   ))
+  )
+  val omegaSc = Lemma( esOmegaSc ) {
+    cut("cut", hof"!x?y (LEQ(x,y) & POR(s(x),y)")
+    allR(fov"A")
+    allL("Ant_2",fov"A")
+    allL("Ant_1",fov"A")
+    exR("cut",fov"A")
+    rewrite ltr "Ant_1_0" in "Ant_2_0"
+    andR
+    foTheory
+    trivial
+    pLink("mu")
+  }
+  ctx += Context.ProofDefinitionDeclaration( le"omega (s n)", omegaSc )
+
 // The Basecase of chi
 val esChiBc = Sequent(
     Seq(( "Ant_0" -> hof"!y POR(s(y),a) = (E(f(a),s(y)) | POR(y,a))"   ),
@@ -158,23 +183,74 @@ val esChiBc = Sequent(
 
 //The base case of phi
   val esphiBc = Sequent(
-    Seq(( "Ant_0" -> hof"!x?y (LEQ(x,y) & E(f(y),0))   "   )),
+    Seq(( "Ant_0" -> hof"!x!y POR(s(y),x) = (E(f(x),s(y)) |  POR(y,x))"   ),
+        ( "Ant_1" -> hof"!x POR(0,x) = E(f(x),0)"   ),
+        ( "Ant_2" -> hof"!x?y (LEQ(x,y) & POR(0,y))   "   )),
     Seq(( "Suc_0" -> hof"?p?q (LE(p,q) & E(f(p),f(q))) "   ))
   )
    val phiBc = Lemma( esphiBc ) {
-    pLink( "mu" )
+    allL("Ant_2",hoc"0i:i")
+    exL(fov"B")
+    allL("Ant_2",le"(si B)")
+    exL(fov"A")
+    exR(fov"B")
+    forget("Suc_0")
+    exR(fov"A")
+    forget("Suc_0_0")
+    andL("Ant_2_1")
+    andL
+    andR
+    foTheory
+    allL("Ant_1",fov"B")
+    allL("Ant_1",fov"A")
+    rewrite ltr "Ant_1_0" in "Ant_2_0_1"
+    rewrite ltr "Ant_1_1" in "Ant_2_1_1"
+    foTheory
   }
   ctx += Context.ProofDefinitionDeclaration( le"phi 0", phiBc)
 
 //The step case of phi
 
   val esphiSc = Sequent(
-    Seq(( "Ant_0" -> hof"!x?y (LEQ(x,y) & POR(s(n),x))   "   )),
+    Seq(( "Ant_0" -> hof"!x!y POR(s(y),x) = (E(f(x),s(y)) |  POR(y,x))"   ),
+        ( "Ant_1" -> hof"!x POR(0,x) = E(f(x),0)"   ),
+        ( "Ant_2" -> hof"!x?y (LEQ(x,y) & POR(s(n),y))   "   )),
     Seq(( "Suc_0" -> hof"?p?q (LE(p,q) & E(f(p),f(q))) "   ))
   )
    val phiSc = Lemma( esphiSc ) {
           cut("cut", hof"!x?y (LEQ(x,y) & E(f(y),s(n)))")
-  }
+          cut("cut1", hof"!x?y (LEQ(x,y) & POR(n,y))")
+          allR("cut",fov"B")
+          allR("cut1",fov"A")
+          allL("Ant_2",le"max(A,B)")
+          exL("Ant_2_0",fov"C")
+          exR("cut",fov"C")
+          exR("cut1",fov"C")
+          andL
+          andR("cut_0")
+          andR("cut1_0")
+          foTheory
+          foTheory
+          allL("Ant_0",fov"C")
+          rewrite ltr "Ant_0_0" in "Ant_2_0_1"
+          orL
+          trivial
+          andR("cut1_0")
+          foTheory
+          forget("Ant_0_0")
+          forget("Ant_2_0_0")
+          forget("Ant_2")
+          forget("Suc_0")
+          forget("cut")
+          forget("cut1")
+          forget("cut_0")
+          allL("Ant_0",fov"C")
+          allL("Ant_1",fov"C")
+          pLink( "chi" )
+          focus(1)
+          pLink( "mu" )
+          pLink( "phi" )
+  }       
   ctx += Context.ProofDefinitionDeclaration( le"phi (s n)", phiSc)
 }
 
