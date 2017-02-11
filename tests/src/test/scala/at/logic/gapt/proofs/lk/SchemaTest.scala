cd32d6189124523db64908aca87030351d5f8b86
Ermine516 <cernadavid1@gmail.com>
2017-02-11 20:48:04 +0100

    The schema code works now. I have also added a schematic version of the Urban tape proof I developed during my thesis.
    Test have been added as well

--- tests/src/test/scala/at/logic/gapt/proofs/lk/SchemaTest.scala
+++ tests/src/test/scala/at/logic/gapt/proofs/lk/SchemaTest.scala
@@ -12,13 +12,34 @@ import org.specs2.mutable.Specification
   * Created by cernadavid1 on 11.02.17.
   */
 class SchemaTest extends Specification {
-  "Testing simple schema" in {
+  "Testing simple schema basecase" in {
     val one = new LKProofSchemata()
-    one.Instantiate("taut ",Seq(le"(0)"))(tautSchema.ctx) mustNotEqual None
-    one.Instantiate("taut ",Seq(le"(s 0)"))(tautSchema.ctx) mustNotEqual None
-    one.Instantiate("taut ",Seq(le"((s:w>w) ((s:w>w) ((s:w>w) (0:w))))"))(tautSchema.ctx) mustNotEqual None
-    one.Instantiate("omega ",Seq(le"(0:w)"))(niaSchema.ctx) mustNotEqual None
-    one.Instantiate("omega ",Seq(le"((s:w>w) ((s:w>w) ((s:w>w) (0:w))))"))(niaSchema.ctx) mustNotEqual None
+    one.Instantiate("taut",Seq(le"(0)"))(tautSchema.ctx) mustNotEqual None
+  }
+
+ "Testing simple schema stepcase" in {
+    val one = new LKProofSchemata()
+    one.Instantiate("taut",Seq(le"(s 0)"))(tautSchema.ctx) mustNotEqual None
+  }
+
+   "Testing simple schema Large" in {
+     val one = new LKProofSchemata()
+     one.Instantiate("taut",Seq(le"(s (s (s (s 0))))"))(tautSchema.ctx) mustNotEqual None
+   }
+
+     "Testing simple Nia-schema basecase" in {
+      val one = new LKProofSchemata()
+      one.Instantiate("omega",Seq(le"(0:w)"))(niaSchema.ctx) mustNotEqual None
+    }
+
+  "Testing simple Nia-schema stepcase" in {
+    val one = new LKProofSchemata()
+    one.Instantiate("omega",Seq(le"((s:w>w) (0:w))"))(niaSchema.ctx) mustNotEqual None
+  }
+
+  "Testing simple Nia-schema Large" in {
+    val one = new LKProofSchemata()
+    one.Instantiate("omega",Seq(le"((s:w>w) ((s:w>w) ((s:w>w) (0:w))))"))(niaSchema.ctx) mustNotEqual None
   }
 
 }
