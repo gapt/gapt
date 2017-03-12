c60c4f9dec90557c0f53456e23028792fa5be548
Jannik Vierling <jannik.vierling@gmail.com>
2017-03-12 16:41:50 +0100

    did formatting and renaming of induction elimination test

--- tests/src/test/scala/at/logic/gapt/proofs/lk/InductionEliminationTests.scala
+++ tests/src/test/scala/at/logic/gapt/proofs/lk/InductionEliminationTests.scala
@@ -1,8 +1,8 @@
 package at.logic.gapt.proofs.lk
 
 import at.logic.gapt.expr._
-import at.logic.gapt.proofs.gaptic.{ProofState, _}
-import at.logic.gapt.proofs.{Context, Sequent}
+import at.logic.gapt.proofs.gaptic.{ ProofState, _ }
+import at.logic.gapt.proofs.{ Context, Sequent }
 import org.specs2.mutable._
 
 class InductionEliminationOnTreesTest extends Specification {
@@ -38,7 +38,7 @@ class InductionEliminationOnTreesTest extends Specification {
     regularize( proofState.result )
   }
 
-  val testTerm: LambdaExpression = le"node(node(node(leaf, leaf, 0), node(leaf, leaf, s(0)), 0), node(leaf, leaf, s(s(0))), 0)"
+  val testTerm: Expr = le"node(node(node(leaf, leaf, 0), node(leaf, leaf, s(0)), 0), node(leaf, leaf, s(s(0))), 0)"
   val inductiveGroundProof = LKProofSubstitutableDefault.applySubstitution(
     new Substitution( Map( hov"t:tree" -> testTerm ) ),
     general_proof_goal
