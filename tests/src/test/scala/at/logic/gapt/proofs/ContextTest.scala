2a05b8de86c074241687cf7b741167ee0ce46b70
Gabriel Ebner <gebner@gebner.org>
2017-03-16 11:39:33 +0100

    Context: support primitive recursive definitions

--- tests/src/test/scala/at/logic/gapt/proofs/ContextTest.scala
+++ tests/src/test/scala/at/logic/gapt/proofs/ContextTest.scala
@@ -49,4 +49,37 @@ class ContextTest extends Specification {
     ok
   }
 
+  "recursive functions" in {
+    implicit var ctx = default
+    ctx += InductiveType( "nat", hoc"0: nat", hoc"s: nat>nat" )
+    ctx += PrimRecFun( hoc"'+': nat>nat>nat", "0 + x = x", "s(x) + y = s(x + y)" )
+    ctx += hof"1 = s(0)"; ctx += hof"2 = s(1)"; ctx += hof"3 = s(2)"; ctx += hof"4 = s(3)"
+    ctx.normalize( le"2 + 2" ) must_== ctx.normalize( le"4" )
+  }
+
+  "ite" in {
+    implicit var ctx = default
+    ctx += PrimRecFun( hoc"ite: o > ?a > ?a > ?a", "ite true a b = a", "ite false a b = b" )
+
+    ctx += Ti; ctx += hoc"a: i"; ctx += hoc"b: i"
+    ctx.whnf( le"ite true a b" ) must_== le"a"
+    ctx.whnf( le"ite false a b" ) must_== le"b"
+
+    // a version of negation that reduces definitionally
+    ctx += PrimRecFun( hoc"neg : o > o", "neg true = false", "neg false = true" )
+    ctx.whnf( le"ite (neg true) a b" ) must_== le"b"
+    ctx.whnf( le"ite (neg false) a b" ) must_== le"a"
+  }
+
+  "propext" in {
+    implicit var ctx = default
+    import at.logic.gapt.proofs.gaptic._
+    ctx check Lemma( Sequent() :+ ( "goal" -> hof"!p!q ((p <-> q) -> p = q)" ) ) {
+      repeat( allR )
+      induction( hov"p: o" ).onAll( induction( hov"q: o" ) )
+      quasiprop.onAllSubGoals
+    }
+    ok
+  }
+
 }
