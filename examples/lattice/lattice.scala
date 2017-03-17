7bc64e9ba9f9b4452821d72847f6d2fc4314442c
Gabriel Ebner <gebner@gebner.org>
2017-03-17 08:56:29 +0100

    Remove defs fields of tactics proofs.

--- examples/lattice/lattice.scala
+++ examples/lattice/lattice.scala
@@ -30,8 +30,6 @@ object lattice extends TacticsProof {
   ctx += hof"LUB = (∀x∀y (x <= cup x y ∧ y <= cup x y ∧ ∀z (x<=z ∧ y<=z ⊃ cup x y <= z)))"
   ctx += hof"L3 = (POSET ∧ (GLB ∧ LUB))"
 
-  val defs = ctx.definitions.toMap
-
   //
   // Left sub proof
   //
@@ -232,4 +230,5 @@ object lattice extends TacticsProof {
     insert( p1_3 )
     insert( p3_2 )
   }
+  val proof = p
 }
--- tests/src/test/scala/at/logic/gapt/examples/LatticeTest.scala
+++ tests/src/test/scala/at/logic/gapt/examples/LatticeTest.scala
@@ -4,9 +4,7 @@ import at.logic.gapt.proofs.lk.eliminateDefinitions
 import org.specs2.mutable.Specification
 
 class LatticeTest extends Specification {
-  "lattice" in {
-    lattice.ctx.check( lattice.p )
-    ok
-  }
-  "definition elimination" in { eliminateDefinitions( lattice.defs )( lattice.p ); ok }
+  import lattice._
+  "lattice" in { ctx.check( proof ); ok }
+  "definition elimination" in { eliminateDefinitions( proof ); ok }
 }
