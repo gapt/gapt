8e0866f13e23d8c1958b22a85e1ccbc37ed63034
Jannik Vierling <jannik.vierling@gmail.com>
2017-05-02 09:39:50 +0200

    fixed extended test cases for induction elimination

--- testing/src/main/scala/regressionTests.scala
+++ testing/src/main/scala/regressionTests.scala
@@ -35,28 +35,30 @@ import scala.xml.XML
 
 class InductionEliminationTestCase( f: java.io.File ) extends RegressionTestCase( f.getParentFile.getName + "/" + f.getName ) {
 
-  private val instanceTermSize = 1
-  private val bench = TipSmtParser.fixupAndParse( f )
-  private implicit val ctx = bench.ctx
-  private val sequent = bench.toSequent.zipWithIndex.map {
-    case ( f, Ant( i ) ) => s"h$i" -> f
-    case ( f, _ )        => "goal" -> f
-  }
-  private val termGenerator = ( new EnumeratingInstanceGenerator( ctx.get[Context.BaseTypes].baseTypes.values.toList, ctx ) ).terms
-  private val strategies: List[( Duration, Tactical[_] )] = List(
-    10.seconds -> AnalyticInductionTactic( IndependentInductionAxioms(), Escargot ).aka( "analytic independent" ),
-    10.seconds -> AnalyticInductionTactic( SequentialInductionAxioms(), Escargot ).aka( "analytic sequential" ),
-    20.seconds -> new ViperTactic( TreeGrammarProverOptions().copy( quantTys = Some( Seq() ) ) ).aka( "treegrammar without quantifiers" ),
-    60.seconds -> new ViperTactic( TreeGrammarProverOptions() ).aka( "treegrammar" )
-  )
-  private val state0 = ProofState( sequent )
-
   override def timeout = Some( 10 minutes )
 
   override protected def test( implicit testRun: TestRun ): Unit = {
+
+    val instanceTermSize = 1
+    val bench = TipSmtParser.fixupAndParse( f ) --- "parsing tip problem"
+    implicit val ctx = bench.ctx
+    val sequent = bench.toSequent.zipWithIndex.map {
+      case ( f, Ant( i ) ) => s"h$i" -> f
+      case ( f, _ )        => "goal" -> f
+    }
+
+    val termGenerator = ( new EnumeratingInstanceGenerator( ctx.get[Context.BaseTypes].baseTypes.values.toList, ctx ) ).terms
+    val strategies: List[( Duration, Tactical[_] )] = List(
+      10.seconds -> AnalyticInductionTactic( IndependentInductionAxioms(), Escargot ).aka( "analytic independent" ),
+      10.seconds -> AnalyticInductionTactic( SequentialInductionAxioms(), Escargot ).aka( "analytic sequential" ),
+      20.seconds -> new ViperTactic( TreeGrammarProverOptions().copy( quantTys = Some( Seq() ) ) ).aka( "treegrammar without quantifiers" ),
+      60.seconds -> new ViperTactic( TreeGrammarProverOptions() ).aka( "treegrammar" )
+    )
+    val state0 = ProofState( sequent )
+
     strategies.view.flatMap {
       case ( duration, strategy ) =>
-        Try( withTimeout( duration ) { strategy.andThen( now )( state0 ) } ) match {
+        Try( withTimeout( duration ) { strategy.andThen( now )( state0 ) --- "searching for inductive proof" } ) match {
           case Success( Right( ( _, state_ ) ) ) =>
             Some( state_.result )
           case Failure( _: TimeOutException ) =>
@@ -71,7 +73,7 @@ class InductionEliminationTestCase( f: java.io.File ) extends RegressionTestCase
           variable =>
             findTerm( termGenerator, variable.ty.asInstanceOf[TBase], instanceTermSize )
         }
-        val instProof = instanceProof( proof, instanceTerms )
+        val instProof = instanceProof( proof, instanceTerms ) --- "creating instance proof"
         val indFreeProof = ReductiveCutElimination.eliminateInduction( instProof ) --- "eliminate inductions in instance proof"
         indFreeProof.endSequent.multiSetEquals( instProof.endSequent ) !-- "end-sequent must not be modified"
         isInductionFree( indFreeProof ) !-- "proof must be induction free"
