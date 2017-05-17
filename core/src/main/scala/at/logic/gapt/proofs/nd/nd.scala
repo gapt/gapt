10ed18f905d1b283a681eaa824d6136806c30f46
Marielle Louise Rietdijk <mariellerietdijk@gmail.com>
2017-05-17 12:06:47 +0200

    small change concerning eigenvariable condition of exists elimination

--- core/src/main/scala/at/logic/gapt/proofs/nd/nd.scala
+++ core/src/main/scala/at/logic/gapt/proofs/nd/nd.scala
@@ -955,7 +955,7 @@ object ExistsIntroRule extends ConvenienceConstructor( "ExistsIntroRule" ) {
  *    ----------------------------∃:e
  *        Γ, Π :- B
  * </pre>
- * This rule is only applicable if the eigenvariable condition is satisfied: α must not occur freely in Γ, Π, and B
+ * This rule is only applicable if the eigenvariable condition is satisfied: α must not occur freely in Π, and B
  *
  * @param leftSubProof The proof π1.
  * @param rightSubProof The proof π2.
@@ -974,8 +974,6 @@ case class ExistsElimRule( leftSubProof: NDProof, rightSubProof: NDProof, aux: S
   //eigenvariable condition
   if ( freeVariables( rightContext ) contains eigenVariable )
     throw NDRuleCreationException( s"Eigenvariable condition is violated: $rightContext contains $eigenVariable" )
-  if ( freeVariables( leftContext ) contains eigenVariable )
-    throw NDRuleCreationException( s"Eigenvariable condition is violated: $leftContext contains $eigenVariable" )
 
   val ( quantifiedVariable, subFormula ) = existentialFormula match {
     case Ex( variable, sub ) => ( variable, sub )
