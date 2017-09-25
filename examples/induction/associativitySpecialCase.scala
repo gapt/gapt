49c9100c86ad76c9f544bded43c3c6e0ff3a7d31
Gabriel Ebner <gebner@gebner.org>
2017-09-25 11:02:15 +0200

    associativitySpecialCase: fix syntax error

--- examples/induction/associativitySpecialCase.scala
+++ examples/induction/associativitySpecialCase.scala
@@ -8,15 +8,18 @@ object associativitySpecialCase extends TacticsProof {
   ctx += Context.InductiveType( ty"nat", hoc"0: nat", hoc"s: nat>nat" )
   ctx += hoc"'+': nat>nat>nat"
 
+  def tac = // FIXME(gabriel): WTF, this causes a syntax error when inlined??!?!
+    treeGrammarInduction
+      .equationalTheory( hof"0+x=x", hof"x+0=x" )
+      .instanceProver( spass )
+      .quantTys()
+      .canSolSize( 2, 2 )
+      .doForgetOne()
+
   val proof = Lemma(
     hols"""p0: !x x+0 = x, ps: !x!y x+s(y) = s(x+y),
            0p: !x 0+x = x, sp: !x!y s(x)+y = s(x+y)
            :- !x (x+x)+x = x+(x+x) """ ) {
-      treeGrammarInduction
-        .equationalTheory( hof"0+x=x", hof"x+0=x" )
-        .instanceProver( spass )
-        .quantTys()
-        .canSolSize( 2, 2 )
-        .doForgetOne()
+    tac
     }
 }
