6b3f7c8d22de7c5d90f9ddc270214b2b08c551a9
Gabriel Ebner <gebner@gebner.org>
2017-09-27 16:50:11 +0200

    Prove commutativity of addition.

---
+++ examples/induction/comm.scala
@@ -0,0 +1,21 @@
+package at.logic.gapt.examples.induction
+
+import at.logic.gapt.expr._
+import at.logic.gapt.proofs.Context.InductiveType
+import at.logic.gapt.proofs.gaptic._
+
+object comm extends TacticsProof {
+  ctx += InductiveType( ty"nat", hoc"0: nat", hoc"s: nat>nat" )
+  ctx += hoc"'+': nat>nat>nat"
+
+  val p = Lemma( hols"""
+      p0: !x x+0 = x, 0p: !x 0+x = x,
+      ps: !x!y x+s(y) = s(x+y), sp: !x!y s(x)+y = s(x+y)
+      :- !x!y x+y = y+x
+  """ ) {
+    allR
+    treeGrammarInduction
+      .quantTys()
+      .canSolSize( 1, 1 )
+  }
+}
