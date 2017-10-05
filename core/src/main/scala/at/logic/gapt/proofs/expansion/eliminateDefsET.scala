7ce3593b04938d4f07add9a8b5feca88418b8b2b
Gabriel Ebner <gebner@gebner.org>
2017-10-05 11:16:41 +0200

    eliminateDefsET: better support for bipolar definitions

--- core/src/main/scala/at/logic/gapt/proofs/expansion/eliminateDefsET.scala
+++ core/src/main/scala/at/logic/gapt/proofs/expansion/eliminateDefsET.scala
@@ -28,65 +28,50 @@ object eliminateDefsET {
         case _ => false
       }.getOrElse( return ep )
 
+    def mkDefAtom( as: Seq[Expr], pol: Polarity ) =
+      ETDefinition(
+        Substitution( vs zip as )( definedFormula ),
+        ETAtom( definitionConst( as ).asInstanceOf[Atom], pol ) )
+
     val insts0 = for {
       ETWeakQuantifierBlock( `definitionFormula`, n, insts ) <- ep.expansionSequent.antecedent
       ( as, inst ) <- insts
       repl <- inst( negReplPos ) ++ inst( posReplPos )
     } yield as -> repl
 
-    var insts = Map() ++ insts0.groupBy( _._1 ).mapValues { repls =>
-      val shallow = repls.head._2.shallow
-      ETMerge( shallow, Polarity.Positive, repls.map( _._2 ).filter( _.polarity.positive ) ) ->
-        ETMerge( shallow, Polarity.Negative, repls.map( _._2 ).filter( _.polarity.negative ) )
-    }
+    var insts = Map() ++ insts0.groupBy( _._1 ).mapValues( _.map( _._2 ) )
 
     val rest = ep.expansionSequent.filterNot { et => et.polarity.inAnt && et.shallow == definitionFormula }
-    val usesites = rest.elements.flatMap { _.subProofs }.
+    val usesites: Set[( Seq[Expr], Polarity )] = rest.elements.flatMap { _.subProofs }.
       collect { case ETAtom( Apps( `definitionConst`, args ), pol ) => ( args, pol ) }.toSet
-    insts = Map() ++
-      usesites.map { _._1 }.map { as =>
-        val ras = Substitution( vs zip as )( definedFormula )
-        as -> ( ETWeakening( ras, Polarity.Positive ), ETWeakening( ras, Polarity.Negative ) )
-      } ++
-      insts
+    insts = Map() ++ ( for ( as <- usesites.map( _._1 ) ++ insts.keys ) yield as -> insts.getOrElse( as, Vector() ) )
 
     if ( !pureFolWithoutEq ) {
-      val newNegRepl = ETMerge( definedFormula, Polarity.Negative, insts.values.map { _._2 }.map { generalizeET( _, definedFormula ) } )
-      val newPosRepl = ETMerge( definedFormula, Polarity.Positive, insts.values.map { _._1 }.map { generalizeET( _, definedFormula ) } )
-      insts = insts map { case ( as, _ ) => as -> Substitution( vs zip as )( newPosRepl -> newNegRepl ) }
+      val newRepl = Vector() ++ ( for ( ( _, is ) <- insts; i <- is ) yield generalizeET( i, definedFormula ) )
+      insts = for ( ( as, _ ) <- insts ) yield as -> Substitution( vs zip as )( newRepl ).toVector
     }
 
-    def replm: PartialFunction[Expr, Expr] = {
-      case Apps( `definitionConst`, as ) => Substitution( vs zip as )( definedFormula )
+    insts = for ( ( as, is ) <- insts )
+      yield as -> ( is ++ ( for {
+      pol <- Polarity.values
+      if usesites( as -> pol )
+      if !is.exists( _.polarity == pol )
+    } yield mkDefAtom( as, pol ) ) )
+
+    def repl( et: ExpansionTree ): ExpansionTree =
+      atomicExpansionET.mapDefinedAtom( et ) {
+        case ( sh, Apps( `definitionConst`, as ), pol ) =>
+          ETMerge( sh, pol, insts( as ).filter( _.polarity == pol ) )
+      }
+
+    val newCuts = ETCut {
+      for {
+        ( _, is ) <- insts
+        pos = is.filter( _.polarity.positive )
+        neg = is.filter( _.polarity.negative )
+        if pos.nonEmpty && neg.nonEmpty
+      } yield ETImp( ETMerge( pos ), ETMerge( neg ) )
     }
-    def replf( f: Formula ): Formula = TermReplacement( f, replm )
-    def repl( et: ExpansionTree ): ExpansionTree = et match {
-      case ETMerge( a, b )                      => ETMerge( repl( a ), repl( b ) )
-      case ETWeakening( sh, pol )               => ETWeakening( replf( sh ), pol )
-
-      case ETTop( _ ) | ETBottom( _ )           => et
-      case ETNeg( ch )                          => ETNeg( repl( ch ) )
-      case ETAnd( l, r )                        => ETAnd( repl( l ), repl( r ) )
-      case ETOr( l, r )                         => ETOr( repl( l ), repl( r ) )
-      case ETImp( l, r )                        => ETImp( repl( l ), repl( r ) )
-      case ETWeakQuantifier( sh, is )           => ETWeakQuantifier( replf( sh ), Map() ++ is.mapValues( repl ) )
-      case ETStrongQuantifier( sh, ev, ch )     => ETStrongQuantifier( replf( sh ), ev, repl( ch ) )
-      case ETSkolemQuantifier( sh, st, sd, ch ) => ETSkolemQuantifier( replf( sh ), st, sd, repl( ch ) )
-
-      case ETDefinition( _, ETAtom( Apps( `definitionConst`, as ), pol ) ) =>
-        if ( pol.positive ) insts( as )._1 else insts( as )._2
-      case ETDefinition( _, _ )                              => et
-      case ETAtom( Apps( f, _ ), _ ) if f != definitionConst => et
-    }
-
-    for ( ( _, ( pos, neg ) ) <- insts ) {
-      require( !constants( pos.deep ).contains( definitionConst ) )
-      require( !constants( neg.deep ).contains( definitionConst ) )
-    }
-
-    val newCuts = ETWeakQuantifier.withMerge(
-      ETCut.cutAxiom,
-      insts.values.map { case ( pos, neg ) => pos.shallow -> ETImp( pos, neg ) } )
 
     val newES = ETMerge( newCuts +: rest.map( repl ) )
 
