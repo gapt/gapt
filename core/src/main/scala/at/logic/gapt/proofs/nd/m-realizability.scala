c2e566df2ceec33176dd6597b505c188068784a5
Marielle Louise Rietdijk <mariellerietdijk@gmail.com>
2017-10-17 18:42:48 +0200

    improvements based on comments on previous commit (todo still: general treatment of theory axiom and generalization to inductive data types)

--- core/src/main/scala/at/logic/gapt/proofs/nd/m-realizability.scala
+++ core/src/main/scala/at/logic/gapt/proofs/nd/m-realizability.scala
@@ -1,59 +1,74 @@
 package at.logic.gapt.proofs.nd
 
 import at.logic.gapt.expr._
+import at.logic.gapt.proofs.Context.{ InductiveType, PrimRecFun }
 import at.logic.gapt.proofs._
+import at.logic.gapt.proofs.nd._
 
 object MRealizability {
 
+  implicit var systemT = Context()
+  systemT += InductiveType(
+    ty"conj ?a  ?b",
+    hoc"pair: ?a > ?b > conj ?a ?b " )
+  systemT += PrimRecFun(
+    hoc"pi1: (conj ?a ?b) > ?a",
+    "pi1(pair(x,y)) = x" )
+  systemT += PrimRecFun(
+    hoc"pi2: (conj ?a ?b) > ?b",
+    "pi2(pair(x,y)) = y" )
+  systemT += InductiveType(
+    ty"nat",
+    hoc"0 : nat",
+    hoc"s : nat > nat" )
+  systemT += PrimRecFun(
+    hoc"R: (nat > ?a > ?a) > ?a > nat > ?a",
+    "R(x,y,0) = y",
+    "R(x,y,s(z)) = x(z,R(x,y,z))" )
+  systemT += InductiveType(
+    ty"1",
+    hoc"i : 1" )
+
   def mrealize( proof: NDProof ): Expr = proof match {
     case WeakeningRule( subProof, formula ) =>
-      BetaReduction.betaNormalize(
+      normalize(
         Abs(
-          ( freeVars( proof ) :+ Var( "z", flat( formula ) ) ) ++ assumptions( subProof.conclusion ),
-          App( mrealize( subProof ), freeVars( subProof ) ++ assumptions( subProof.conclusion ) )
-        )
-      )
+          ( freeVariables( proof.conclusion ).toSeq :+ Var( "z", flat( formula ) ) ) ++ variablesAntPremise( proof, 0 ).values,
+          App( mrealize( subProof ), freeVariables( subProof.conclusion ).toSeq ++ variablesAntPremise( proof, 0 ).values ) ) )
 
     case ContractionRule( subProof, aux1, aux2 ) =>
-      BetaReduction.betaNormalize(
+      normalize(
         Abs(
-          ( freeVars( proof ) :+ Var( "z", flat( subProof.conclusion.apply( aux1 ) ) ) ) ++ assumptions( subProof.conclusion.delete( aux1, aux2 ) ),
-          App( mrealize( subProof ), ( ( freeVars( subProof ) :+
+          ( freeVariables( proof.conclusion ).toSeq :+ Var( "z", flat( subProof.conclusion.apply( aux1 ) ) ) ) ++ variablesAntPremise( proof, 0 ).-( aux1, aux2 ).values,
+          App( mrealize( subProof ), ( ( freeVariables( subProof.conclusion ).toSeq :+
             Var( "z", flat( subProof.conclusion.apply( aux1 ) ) ) ) :+
             Var( "z", flat( subProof.conclusion.apply( aux1 ) ) ) ) ++
-            assumptions( subProof.conclusion.delete( aux1, aux2 ) ) )
-        )
-      )
+            variablesAntPremise( proof, 0 ).-( aux1, aux2 ).values ) ) )
 
     case LogicalAxiom( formula ) =>
-      Abs( freeVars( proof ) :+ Var( "x", flat( formula ) ), Var( "x", flat( formula ) ) )
+      Abs( freeVariables( proof.conclusion ).toSeq :+ Var( "x", flat( formula ) ), Var( "x", flat( formula ) ) )
 
     case AndElim1Rule( subProof ) =>
-      BetaReduction.betaNormalize(
+      normalize(
         Abs(
-          freeVars( proof ) ++ assumptions( proof.conclusion ),
-          proj1( App( mrealize( subProof ), freeVars( subProof ) ++ assumptions( subProof.conclusion ) ) )
-        )
-      )
+          freeVariables( proof.conclusion ).toSeq ++ variablesAntConclusion( proof ).values,
+          le"pi1(${( App( mrealize( subProof ), freeVariables( subProof.conclusion ).toSeq ++ variablesAntPremise( proof, 0 ).values ) )})" ) )
 
     case AndElim2Rule( subProof ) =>
-      BetaReduction.betaNormalize(
+      normalize(
         Abs(
-          freeVars( proof ) ++ assumptions( proof.conclusion ),
-          proj2( App( mrealize( subProof ), freeVars( subProof ) ++ assumptions( subProof.conclusion ) ) )
-        )
-      )
+          freeVariables( proof.conclusion ).toSeq ++ variablesAntConclusion( proof ).values,
+          le"pi2(${( App( mrealize( subProof ), freeVariables( subProof.conclusion ).toSeq ++ variablesAntPremise( proof, 0 ).values ) )})" ) )
 
     case AndIntroRule( leftSubproof, rightSubproof ) =>
-      BetaReduction.betaNormalize(
+      normalize(
         Abs(
-          freeVars( proof ) ++ assumptions( proof.conclusion ),
-          pair(
-            App( mrealize( leftSubproof ), freeVars( leftSubproof ) ++ assumptions( leftSubproof.conclusion ) ),
-            App( mrealize( rightSubproof ), freeVars( rightSubproof ) ++ assumptions( rightSubproof.conclusion ) )
-          )
-        )
-      )
+          freeVariables( proof.conclusion ).toSeq ++ variablesAntConclusion( proof ).values,
+          le"pair(${
+            App( mrealize( leftSubproof ), freeVariables( leftSubproof.conclusion ).toSeq ++ variablesAntPremise( proof, 0 ).values )
+          },${
+            App( mrealize( rightSubproof ), freeVariables( rightSubproof.conclusion ).toSeq ++ variablesAntPremise( proof, 1 ).values )
+          })" ) )
 
     case OrElimRule( leftSubProof, middleSubProof, aux1, rightSubProof, aux2 ) =>
       throw new MRealizerCreationException( proof.longName, "Not implemented yet." )
@@ -64,23 +79,20 @@ object MRealizability {
     case OrIntro2Rule( subProof, leftDisjunct ) =>
       throw new MRealizerCreationException( proof.longName, "Not implemented yet." )
 
-    case ImpElimRule( leftSubProof, rightSubProof ) =>
-      Abs(
-        freeVars( proof ) ++ assumptions( proof.conclusion ),
+    case ImpElimRule( leftSubProof, rightSubProof ) => {
+      normalize( Abs(
+        freeVariables( proof.conclusion ).toSeq ++ variablesAntConclusion( proof ).values,
         App(
-          App( mrealize( leftSubProof ), freeVars( leftSubProof ) ++ assumptions( leftSubProof.conclusion ) ),
-          App( mrealize( rightSubProof ), freeVars( rightSubProof ) ++ assumptions( rightSubProof.conclusion ) )
-        )
-      )
+          normalize( App( mrealize( leftSubProof ), freeVariables( leftSubProof.conclusion ).toSeq ++ variablesAntPremise( proof, 0 ).values ) ),
+          normalize( App( mrealize( rightSubProof ), freeVariables( rightSubProof.conclusion ).toSeq ++ variablesAntPremise( proof, 1 ).values ) ) ) ) )
+    }
 
     case ImpIntroRule( subProof, aux ) =>
       Abs(
-        freeVars( proof ) ++ assumptions( proof.conclusion ),
+        freeVariables( proof.conclusion ).toSeq ++ variablesAntConclusion( proof ).values,
         Abs(
           Var( "z", flat( subProof.conclusion( aux ) ) ),
-          App( mrealize( subProof ), ( freeVars( subProof ) :+ Var( "z", flat( subProof.conclusion( aux ) ) ) ) ++ assumptions( subProof.conclusion ) )
-        )
-      )
+          App( mrealize( subProof ), ( freeVariables( subProof.conclusion ).toSeq :+ Var( "z", flat( subProof.conclusion( aux ) ) ) ) ++ variablesAntPremise( proof, 0 ).values ) ) )
 
     case NegElimRule( leftSubProof, rightSubProof ) =>
       throw new MRealizerCreationException( proof.longName, "Not implemented yet." )
@@ -92,12 +104,12 @@ object MRealizability {
       throw new MRealizerCreationException( proof.longName, "Not implemented yet." )
 
     case BottomElimRule( subProof, mainFormula ) =>
-      Abs( freeVars( proof ).diff( freeVars( subProof ) ), mrealize( subProof ) )
+      Abs( freeVariables( proof.conclusion ).toSeq.diff( freeVariables( subProof.conclusion ).toSeq ), mrealize( subProof ) )
 
     case ForallIntroRule( subProof, eigenVariable, quantifiedVariable ) =>
       throw new MRealizerCreationException( proof.longName, "Not implemented yet." )
 
-    case ForallElimRule( subProof, formula, term, variable ) =>
+    case ForallElimRule( subProof, variable ) =>
       throw new MRealizerCreationException( proof.longName, "Not implemented yet." )
 
     case ExistsIntroRule( subProof, formula, term, variable ) =>
@@ -107,64 +119,42 @@ object MRealizability {
       throw new MRealizerCreationException( proof.longName, "Not implemented yet." )
 
     case TheoryAxiom( mainFormula ) =>
-      // match with cases doesn't work..?
-      if ( mainFormula == hof"!x !y (s(x) = s(y) -> x = y )" ) Abs( Seq( Var( "x", int ), Var( "y", int ), Var( "z", one ) ), i )
-      else if ( mainFormula == hof"!x (s(x) = 0 -> ⊥ )" ) Abs( Seq( Var( "x", int ), Var( "y", one ) ), i )
-      else if ( mainFormula == hof"!x x+0 = x" ) Abs( Var( "x", int ), i )
-      else if ( mainFormula == hof"!x !y (x + s(y) = s(x + y))" ) Abs( Seq( Var( "x", int ), Var( "y", int ) ), i )
-      else if ( mainFormula == hof"!x (x * 0 = 0)" ) Abs( Var( "x", int ), i )
-      else if ( mainFormula == hof"!x!y (x * s(y) = (x * y) + x)" ) Abs( Seq( Var( "x", int ), Var( "y", int ) ), i )
-      else throw new MRealizerCreationException( proof.longName, s"$mainFormula is not an axiom in Heyting Arithmetic." )
+      throw new MRealizerCreationException( proof.longName, "Not implemented yet." )
 
     case EqualityElimRule( leftSubProof, rightSubProof, formulaA, variablex ) =>
       throw new MRealizerCreationException( proof.longName, "Not implemented yet." )
 
     case EqualityIntroRule( term ) =>
-      Abs( freeVars( proof ), i )
+      Abs( freeVariables( proof.conclusion ).toSeq, le"i" )
 
     case ExcludedMiddleRule( leftSubProof, aux1, rightSubProof, aux2 ) =>
       throw new MRealizerCreationException( proof.longName, "This rule is not admitted in Heyting Arithmetic." )
   }
 
   def flat( formula: Formula ): Ty = formula match {
-    case Bottom() => one
+    case Bottom() => ty"1"
     case Top() =>
       throw new FlattenException( formula.toString, "Not implemented yet." )
-    case Eq( _, _ ) => one
-    case Atom( _, _ ) =>
-      throw new FlattenException( formula.toString, "Heyting Arithmetic only contains the equality predicate." )
-    case And( leftformula, rightformula ) => conj( flat( leftformula ), flat( rightformula ) )
+    case Eq( _, _ )                       => ty"1"
+    case Atom( _, _ )                     => ty"1"
+    case And( leftformula, rightformula ) => TBase( "conj", flat( leftformula ), flat( rightformula ) )
     case Or( _, _ ) =>
       throw new FlattenException( formula.toString, "Not implemented yet." )
     case Imp( leftformula, rightformula ) => flat( leftformula ) -> flat( rightformula )
     case Neg( subformula ) =>
       throw new FlattenException( formula.toString, "Not implemented yet." )
-    case Ex( variable, subformula )  => conj( int, flat( subformula ) )
-    case All( variable, subformula ) => int -> flat( subformula )
+    case Ex( variable, subformula )  => TBase( "conj", variable.ty, flat( subformula ) )
+    case All( variable, subformula ) => variable.ty -> flat( subformula )
   }
 
-  val one: Ty = TBase( "1", List() )
-  val int: Ty = TBase( "int", List() )
-  def conj( left: Ty, right: Ty ): Ty = TBase( "^", List( left, right ) )
+  def variablesAntConclusion( proof: NDProof ): Map[SequentIndex, Var] =
+    proof.conclusion.zipWithIndex.antecedent.map( x => ( x._2, Var( s"x${x._2.toInt}", flat( x._1 ) ) ) ).toMap
 
-  val i: Expr = Const( "i", one )
-  val zero: Expr = Const( "0", int )
-  val suc: Expr = Const( "s", int )
-  def recursor( rtype: Ty ): Expr = Const( s"R-$rtype", ( int -> rtype -> rtype ) -> rtype -> int -> rtype )
-  def pair( left: Expr, right: Expr ): Expr = Apps( Const( "pair", left.ty -> right.ty -> conj( left.ty, right.ty ) ), left, right )
-  def proj1( term: Expr ): Expr = term.ty match {
-    case TBase( "^", List( left, right ) ) => App( Const( "proj1", conj( left, right ) -> left ), term )
-    case _                                 => throw new IllegalArgumentException( s"Can't project on $term, because it is not of conjunctive type." )
-  }
-  def proj2( term: Expr ): Expr = term.ty match {
-    case TBase( "^", List( left, right ) ) => App( Const( "proj2", conj( left, right ) -> right ), term )
-    case _                                 => throw new IllegalArgumentException( s"Can't project on $term, because it is not of conjunctive type." )
+  def variablesAntPremise( proof: NDProof, premiseNumber: Int ): Map[SequentIndex, Var] = {
+    val positions = proof.occConnectors( premiseNumber ).childrenSequent.antecedent.flatten
+    variablesAntConclusion( proof ).filterKeys( positions.contains( _ ) )
   }
 
-  private def freeVars( proof: NDProof ): Seq[Var] =
-    freeVariables( proof.conclusion ).toSeq.map( x => Var( x.name, int ) )
-  private def assumptions( sequent: Sequent[Formula] ): Seq[Var] =
-    sequent.zipWithIndex.antecedent.map( x => Var( s"x${x._2.toInt}", flat( x._1 ) ) )
 }
 
 class MRealizerCreationException( name: String, message: String ) extends Exception( s"Cannot create an m-realizer for $name: " + message )
--- examples/m-realizabilityExamples.scala
+++ examples/m-realizabilityExamples.scala
@@ -3,78 +3,80 @@ package at.logic.gapt.examples
 import at.logic.gapt.proofs.nd._
 import at.logic.gapt.expr._
 
-// Should give an exception.
+//  \x^{i}\x^{1}.x^{1}
 object logicalAxiom1 extends Script {
   val a1 = LogicalAxiom( hof"P(x)" )
   val m1 = MRealizability.mrealize( a1 )
-  print( m1 ); print( " of type " ); println( m1.ty )
+  print( m1 ); print( " of type: " ); println( m1.ty )
 }
 
+// \x^{conj 1 1}.x^{conj 1 1}
 object logicalAxiom2 extends Script {
   val a1 = LogicalAxiom( hof"0 = s(0) & 0 + 0 = 0" )
   val m1 = MRealizability.mrealize( a1 )
-  print( m1 ); print( " of type " ); println( m1.ty )
+  print( m1 ); print( " of type: " ); println( m1.ty )
 }
 
+// \x^{i -> conj i (1 -> 1)}.x^{i -> conj i (1 -> 1)}
 object logicalAxiom3 extends Script {
   val a1 = LogicalAxiom( hof"!x ?y ( x * 0 = y -> y = s(0))" )
   val m1 = MRealizability.mrealize( a1 )
-  print( m1 ); print( " of type " ); println( m1.ty )
+  print( m1 ); print( " of type: " ); println( m1.ty )
 }
 
+// \x^{i} \y^{i} \x^{1}.x^{1}
 object logicalAxiom4 extends Script {
   val a1 = LogicalAxiom( hof"x = y" )
   val m1 = MRealizability.mrealize( a1 )
-  print( m1 ); print( " of type " ); println( m1.ty )
+  print( m1 ); print( " of type: " ); println( m1.ty )
 }
 
 object theoryAxiom1 extends Script {
   val a1 = TheoryAxiom( hof"!z (s(z) = 0 -> ⊥)" )
   val m1 = MRealizability.mrealize( a1 )
-  print( m1 ); print( " of type " ); println( m1.ty )
+  print( m1 ); print( " of type: " ); println( m1.ty )
 }
 
 object theoryAxiom2 extends Script {
   val a1 = TheoryAxiom( hof"!y (y*0 = 0)" )
   val m1 = MRealizability.mrealize( a1 )
-  print( m1 ); print( " of type " ); println( m1.ty )
+  print( m1 ); print( " of type: " ); println( m1.ty )
 }
 
-// Should give an exception.
 object theoryAxiom3 extends Script {
   val a1 = TheoryAxiom( hof"!y (y*0 = y)" )
   val m1 = MRealizability.mrealize( a1 )
-  print( m1 ); print( " of type " ); println( m1.ty )
+  print( m1 ); print( " of type: " ); println( m1.ty )
 }
 
+// i
 object equalityIntro1 extends Script {
   val a1 = EqualityIntroRule( fot"s(s(s(0)))" )
   val m1 = MRealizability.mrealize( a1 )
-  print( m1 ); print( " of type " ); println( m1.ty )
+  print( m1 ); print( " of type: " ); println( m1.ty )
 }
 
+// \x^{i} \y^{i} \z^{i}.i
 object equalityIntro2 extends Script {
   val a1 = EqualityIntroRule( fot"s(s(s(x + (y * z))))" )
   val m1 = MRealizability.mrealize( a1 )
-  print( m1 ); print( " of type " ); println( m1.ty )
+  print( m1 ); print( " of type: " ); println( m1.ty )
 }
 
+// \z^{i}\y^{i}\z^{i>1}.i
 object weakeningRule1 extends Script {
-  val a1 = EqualityIntroRule( hof"s(s(y))" )
+  val a1 = EqualityIntroRule( fot"s(s(y))" )
   val a2 = WeakeningRule( a1, hof"!x( x = x + (0 * s(z)))" )
-  val m1 = MRealizability.mrealize( a1 )
-  print( m1 ); print( " of type " ); println( m1.ty )
   val m2 = MRealizability.mrealize( a2 )
-  print( m2 ); print( " of type " ); println( m2.ty )
+  print( m2 ); print( " of type: " ); println( m2.ty )
 }
 
+// \z^{i}\x^{i}\y^{i}\z^{i>1}\x^{1}.x^1
 object weakeningRule2 extends Script {
   val a1 = LogicalAxiom( hof"s(x) = s(s(y))" )
   val a2 = WeakeningRule( a1, hof"!x( x = x + (0 * s(z)))" )
-  val m1 = MRealizability.mrealize( a1 )
-  print( m1 ); print( " of type " ); println( m1.ty )
   val m2 = MRealizability.mrealize( a2 )
-  print( m2 ); print( " of type " ); println( m2.ty )
+  print( m2 ); print( " of type: " ); println( m2.ty )
 }
 
 object weakeningRule3 extends Script {
@@ -108,18 +110,38 @@ object contractionRule1 extends Script {
 }
 
 object andElim1Rule1 extends Script {
+  val a1 = LogicalAxiom( hof"0 = 0 & s(0) = s(0)" )
+  val a2 = AndElim1Rule( a1 )
+  val m1 = MRealizability.mrealize( a2 )
+  print( m1 ); print( " of type: " ); println( m1.ty )
 }
 
 object andElim2Rule1 extends Script {
+  val a1 = LogicalAxiom( hof"x = 0 & y = 0" )
+  val a2 = AndElim2Rule( a1 )
+  val m1 = MRealizability.mrealize( a2 )
+  print( m1 ); print( " of type: " ); println( m1.ty )
 }
 
-object andIntroRule1 extends Script {
+object andIntroElim1 extends Script {
+  val a1 = LogicalAxiom( hof"0 = 0" )
+  val a2 = LogicalAxiom( hof"s(0) = s(0)" )
+  val a3 = AndIntroRule( a1, a2 )
+  val a4 = AndElim1Rule( a3 )
+  val m1 = MRealizability.mrealize( a4 )
+  print( m1 ); print( " of type: " ); println( m1.ty )
 }
 
-object impElimRule1 extends Script {
+object impElimRule extends Script {
+  val a1 = LogicalAxiom( hof"s(0) = s(0) -> 0 =0" )
+  val a2 = LogicalAxiom( hof"s(0) = s(0)" )
+  val a3 = ImpElimRule( a1, a2 )
+  val m1 = MRealizability.mrealize( a3 )
+  print( m1 ); print( " of type" ); println( m1.ty )
 }
 
 object impIntroRule extends Script {
+
 }
 
 object bottomElimRule extends Script {
