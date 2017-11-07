4c360b747e5a35d8f93ddd4f8eac1413490cc729
Gabriel Ebner <gebner@gebner.org>
2017-11-07 16:14:48 +0100

    Make IndexOrFormula an actual trait.

---
+++ core/src/main/scala/at/logic/gapt/proofs/IndexOrFormula.scala
@@ -0,0 +1,19 @@
+package at.logic.gapt.proofs
+
+import at.logic.gapt.expr.Formula
+
+sealed trait IndexOrFormula {
+  def getFormula( sequent: HOLSequent ): Formula
+}
+
+object IndexOrFormula {
+  case class IsIndex( index: SequentIndex ) extends IndexOrFormula {
+    def getFormula( sequent: HOLSequent ): Formula = sequent( index )
+  }
+  case class IsFormula( formula: Formula ) extends IndexOrFormula {
+    def getFormula( sequent: HOLSequent ): Formula = formula
+  }
+
+  implicit def ofIndex( index: SequentIndex ): IndexOrFormula = IsIndex( index )
+  implicit def ofFormula( formula: Formula ): IndexOrFormula = IsFormula( formula )
+}
