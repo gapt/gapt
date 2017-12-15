7c281ae3a5171362f803ba69eaf3da772a2305f5
ermine516 <cernadavid1@gmail.com>
2017-12-15 20:18:23 +0100

    more changes and an issue

--- core/src/main/scala/at/logic/gapt/proofs/lk/NInductionToSchema.scala
+++ core/src/main/scala/at/logic/gapt/proofs/lk/NInductionToSchema.scala
@@ -4,16 +4,13 @@ import at.logic.gapt.expr._
 import at.logic.gapt.proofs.Context.ProofNames
 import at.logic.gapt.proofs.{Context, MutableContext, Sequent, SequentConnector}
 
-object CreateASchemaVersion extends LKVisitor[( List[(Expr,Var)], Context )] {
-  override protected def recurse( proof: LKProof, arg: ( List[(Expr,Var)], Context ) ): ( LKProof, SequentConnector ) = {
+object CreateASchemaVersion extends LKVisitor[( List[Expr], Context )] {
+  override protected def recurse( proof: LKProof, arg: ( List[Expr], Context ) ): ( LKProof, SequentConnector ) = {
     proof match {
-      case InductionRule( _, _, t ) =>
-        val thepairs = arg._1.map( x => ( x, arg._2.get[ProofNames].lookup( x._1 ).getOrElse( Sequent() ) ) )
-        val result = thepairs.foldLeft( thepairs.head )( ( x, y ) =>{
-          val es = proof.endSequent.map(f=> TermReplacement(f,t,y._1._2))
-          if ( es.equals( y._2 ) ) y else x
-        })
-        withIdentitySequentConnector( ProofLink( result._1._1, proof.endSequent ) )
+      case InductionRule( _, _, _ ) =>
+        val thepairs = arg._1.map( x => ( x, arg._2.get[ProofNames].lookup( x ).getOrElse( Sequent() ) ) )
+        val result = thepairs.foldLeft( thepairs.head )( ( x, y ) =>if ( proof.endSequent.equals( y._2 ) ) y else x)
+        withIdentitySequentConnector( ProofLink( result._1, proof.endSequent ) )
       case _ => super.recurse( proof, arg )
     }
   }
@@ -23,41 +20,40 @@ object ArithmeticInductionToSchema {
     val Apps( Const( nameT, tpp ), _ ) = pe
     val newNames = ctx.newNameGenerator
     val subProofNames = proof.subProofs.collect{  case p @ InductionRule( casesI, form, typeTerm ) =>
-        val newVarForDef =  typeTerm.asInstanceOf[Var] //Var(newNames.fresh( "x" ),typeTerm.ty)
+        val newVarForDef =  Var(newNames.fresh( "x" ),typeTerm.ty)
         val es = p.endSequent.map(f=> TermReplacement(f,typeTerm,newVarForDef))
-        val newForm = TermReplacement(form,typeTerm,newVarForDef).asInstanceOf[Abs]
-        val fv = newVarForDef :: freeVariables( form ).toList
-        val name = Const( newNames.fresh( "P" ),  FunctionType( typeTerm.ty, fv.map( _.ty ) ) )
-        val proofname = Apps( name, fv )
+        val fv =  freeVariables( TermReplacement(form(typeTerm),typeTerm,newVarForDef) ).toList
+        val name = Const( newNames.fresh( "Proof" ),  FunctionType(typeTerm.ty, fv.map( _.ty ) ) )
+        val proofName = Apps( name, fv )
         ctx += name
-        ctx += Context.ProofNameDeclaration( proofname,es)
-        ( (proofname,newVarForDef), ( newForm, InductionRule( casesI, newForm, newVarForDef ) ) )
+        ctx += Context.ProofNameDeclaration( proofName,es)
+        ( proofName, ( form, InductionRule( casesI, form, typeTerm ) ) )
     }.toList
     val resProof: LKProof = CreateASchemaVersion( proof, ( subProofNames.unzip._1, ctx ) )
     if ( ctx.get[ProofNames].lookup( pe ).isEmpty ) {
       ctx += Const( nameT, tpp )
       ctx += Context.ProofNameDeclaration( pe, resProof.endSequent )
     }
+    println(resProof)
     ctx += Context.ProofDefinitionDeclaration( pe, resProof )
-    subProofNames.foreach{ case ((proofname,_),(Abs( _, form ), InductionRule( casesI, _, term ) )) =>
-      casesI.foreach{ case  InductionCase( p, _, hy, ev, con ) =>
-        val sigma = syntacticMatching( form, p.endSequent( con ) ).get
-        val endSequentLeft = ctx.get[ProofNames].lookup( proofname).getOrElse( { throw new Exception( "Proof not defined" ) } )
-        val finProof = if ( ev.nonEmpty ) {
-          val newseq = endSequentLeft.map( e => e.find( term ).foldLeft( e )( ( z, w ) => {
-                if ( freeVariables( e ).contains( term.asInstanceOf[Var] ) ) z.replace( w, ev.head ) else z} ) )
-          val newex = proofname.find( term ).foldLeft( proofname )( ( z, w ) => { z.replace( w, ev.head ) } )
-          ContractionMacroRule( CutRule( ProofLink( newex, newseq ), newseq.indexOf( p.endSequent( hy.head ) ), p, hy.head ), sigma( endSequentLeft ) )
-        } else {
-          val newante = endSequentLeft.antecedent.foldLeft( List[Formula]() )( ( r, t ) => {
-            if ( p.endSequent.indexOfOption( t ).isEmpty && ( !t.contains( term ) || !freeVariables( t ).contains( term.asInstanceOf[Var] ) ) ) t :: r
-            else r
-          } )
-          val newsuc = endSequentLeft.succedent.filterNot( t => p.endSequent.indexOfOption( t ).isEmpty && ( !t.contains( term ) || !freeVariables( t ).contains( term.asInstanceOf[Var] ) ) )
-          WeakeningRightMacroRule( WeakeningLeftMacroRule( p, newante ), newsuc )
+    subProofNames.foreach{ case (proofName,(Abs( _, form ), InductionRule( casesI, _, term ) )) =>
+      casesI.foreach{ case  InductionCase( p, _, hy, _, con ) =>
+        println(p.endSequent(hy))
+        val sigma = syntacticMatching(form, p.endSequent(con)).get
+        val endSequentLeft = ctx.get[ProofNames].lookup(proofName).getOrElse({throw new Exception("Proof not defined")})
+        val finProof = if(hy.nonEmpty)  hy.foldLeft(p)((outputProof, hypoth)  => {
+            val outputSeq = sigma(endSequentLeft.replaceAt(con,p.endSequent(hypoth)))
+            ContractionMacroRule(CutRule(ProofLink(syntacticMatching(form, p.endSequent(hypoth)).get(proofName), outputSeq), outputSeq.indexOf(outputProof.endSequent(hypoth)), outputProof, hypoth), sigma(endSequentLeft))
+        })
+        else{
+          val newante = endSequentLeft.antecedent.filter(t => p.endSequent.indexOfOption(t).isEmpty && (!t.contains(term) || !freeVariables(t).contains(term.asInstanceOf[Var])))
+          val newsuc = endSequentLeft.succedent.filterNot(t => p.endSequent.indexOfOption(t).isEmpty && (!t.contains(term) || !freeVariables(t).contains(term.asInstanceOf[Var])))
+          WeakeningRightMacroRule(WeakeningLeftMacroRule(p, newante), newsuc)
         }
-        ArithmeticInductionToSchema( finProof, sigma( proofname ) )( ctx )
+        ArithmeticInductionToSchema( finProof, sigma( proofName ) )( ctx )
+
        }
+
     }
   }
 
