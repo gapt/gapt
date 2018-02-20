a186fbcc21011ed8c106575a766375166446c198
Gabriel Ebner <gebner@gebner.org>
2018-02-20 13:08:22 +0100

    Update cut-reduction benchmarking script.

--- testing/src/main/scala/cutredbench.scala
+++ testing/src/main/scala/cutredbench.scala
@@ -3,6 +3,7 @@ import at.logic.gapt.cutintro.CutIntroduction
 import at.logic.gapt.expr._
 import at.logic.gapt.examples._
 import at.logic.gapt.expr.fol.Numeral
+import at.logic.gapt.proofs.HOLSequent
 import at.logic.gapt.proofs.ceres.CERES
 import at.logic.gapt.proofs.expansion.{ ExpansionProof, eliminateCutsET }
 import at.logic.gapt.proofs.lk.{ LKProof, LKToExpansionProof, ReductiveCutElimination, eliminateDefinitions }
@@ -46,17 +47,28 @@ object cutReductionBenchmark extends Script {
   case object LKReductive extends LKMethod { def eliminate( p: LKProof ): Unit = ReductiveCutElimination( p ) }
   case object LKCERES extends LKMethod { def eliminate( p: LKProof ): Unit = CERES( p ) }
   case object CERESEXP extends LKMethod { def eliminate( p: LKProof ): Unit = CERES.CERESExpansionProof( p ) }
+  case object BogoElim extends Method {
+    type P = HOLSequent
+    def convert( p: LKProof ): P = p.endSequent
+    def eliminate( p: P ): Unit = Escargot.getExpansionProof( p ).get
+  }
   case object ExpCutElim extends Method {
     type P = ExpansionProof
     def convert( p: LKProof ): P = LKToExpansionProof( p )
     def eliminate( p: P ): Unit = eliminateCutsET( p )
   }
-  case object LKtNorm extends Method {
+  class AbstractLKtNorm( skipAtomicCuts: Boolean = false, skipPropositionalCuts: Boolean = false ) extends Method {
     type P = LKt
     def convert( p: LKProof ): P = LKToLKt( p )._1
-    def eliminate( p: LKt ): Unit = normalizeLKt( p )
+    def eliminate( p: LKt ): Unit = normalizeLKt( p, skipAtomicCuts, skipPropositionalCuts )
   }
-  val methods = List( LKReductive, LKCERES, CERESEXP, ExpCutElim, LKtNorm )
+  case object LKtNorm extends AbstractLKtNorm
+  case object LKtNormA extends AbstractLKtNorm( skipAtomicCuts = true )
+  case object LKtNormP extends AbstractLKtNorm( skipPropositionalCuts = true )
+  val methods = List( LKReductive, LKCERES, CERESEXP, BogoElim, ExpCutElim, LKtNorm, LKtNormA, LKtNormP )
+
+  def turnEqualityIntoPredicate[A: ClosedUnderReplacement]( a: A ): A =
+    TermReplacement( a, { case EqC( t ) => Const( "E", t ->: t ->: To ) } )
 
   // warmup
   for ( m <- methods ) m.robustlyMeasureElimination( LinearCutExampleProof( 3 ) )
@@ -66,7 +78,7 @@ object cutReductionBenchmark extends Script {
   def bench( name: String, n: Int, lk: LKProof, exclude: Set[Method] = Set() ): Unit = {
     val times = methods.map {
       case m if exclude( m ) => "NaN"
-      case m                 => m.robustlyMeasureElimination( lk ).toMicros.toString
+      case m                 => m.robustlyMeasureElimination( lk ).toUnit( SECONDS ).toString
     }
     println( s"$name,$n," + times.mkString( "," ) )
   }
@@ -76,13 +88,17 @@ object cutReductionBenchmark extends Script {
 
   {
     import at.logic.gapt.examples.lattice._
-    bench( "lattice", -1, eliminateDefinitions( p ), exclude = Set( LKReductive ) )
+    bench( "lattice", -1, eliminateDefinitions( p ), exclude = Set( LKReductive, BogoElim ) )
   }
 
-  for ( n <- 0 to 12; p <- CutIntroduction( LinearEqExampleProof( n ) ) ) bench( "ci_lineareq", n, p )
-  for ( n <- 0 to 12; p <- CutIntroduction( LinearExampleProof( n ) ) ) bench( "ci_linear", n, p )
+  for ( n <- 0 to 18; p <- CutIntroduction( LinearEqExampleProof( n ) ) )
+    bench( "ci_lineareq", n, turnEqualityIntoPredicate( p ) )
+  for ( n <- 0 to 18; p <- CutIntroduction( LinearExampleProof( n ) ) ) bench( "ci_linear", n, p )
+  for ( n <- 0 to 18; p <- CutIntroduction( SquareDiagonalExampleProof( n ) ) ) bench( "ci_sqdiag", n, p )
+  for ( n <- 0 to 5; p <- CutIntroduction( FactorialFunctionEqualityExampleProof( n ) ) )
+    bench( "ci_fact", n, turnEqualityIntoPredicate( p ), exclude = Set( BogoElim ) )
 
-  for ( n <- 0 to 6 ) {
+  for ( n <- 0 to 10 ) {
     val Some( res ) = Escargot.getResolutionProof( fof"P(0) & !x (P(x) -> P(s(x))) -> P${Numeral( n )}" )
     val lk = ResolutionToLKProof( res )
     bench( "linearacnf", n, lk )
