7693ee6a956e0b58dd4239b88b5d69f228f545a5
ermine516 <cernadavid1@gmail.com>
2018-03-27 18:19:55 +0200

    More new schema

--- examples/schema/SuccessorStrictMonotoneSchema.scala
+++ examples/schema/SuccessorStrictMonotoneSchema.scala
@@ -6,34 +6,31 @@ import at.logic.gapt.proofs.{ Context, Sequent }
 import at.logic.gapt.proofs.gaptic.TacticsProof
 import at.logic.gapt.proofs.gaptic._
 
-object StrongStrictMonotoneSchema extends TacticsProof {
+object SuccessorStrictMonotoneSchema extends TacticsProof {
   ctx += Context.InductiveType( "nat", hoc"0 : nat", hoc"s : nat>nat" )
   ctx += Context.Sort( "i" )
   ctx += hoc"f:i>nat"
   ctx += hoc"suc:i>i"
   ctx += hoc"z:i"
   ctx += hoc"E: nat>nat>o"
-  ctx += hoc"Eq: i>i>o"
   ctx += hoc"LE: nat>nat>o"
-  ctx += hoc"LEQ: nat>nat>o"
-  ctx += hoc"iLEQ: i>i>o"
 
   ctx += hoc"omega: nat>nat"
   ctx += hoc"phi: nat>nat"
   ctx += PrimRecFun( hoc"POR:nat>i>o", "POR 0 x = E 0 (f x) ", "POR (s y) x = (E (s y) (f x) ∨ POR y x)" )
   ctx += "LEDefinition" -> hos"POR(n,a) :- LE(f(a), s(n))"
+  ctx += "LEDefinition2" -> hos"POR(n,suc(a)) :- LE(f(a), s(n))"
   ctx += "NumericTransitivity" -> hos"E(n,f(a)),E(n,f(suc(a))) :- E(f(a), f(suc(a)))"
-  ctx += "minimalElement" -> hos"LE(n,0) :- "
-  ctx += "reflexivity" -> hos" :- iLEQ(a,a)"
-  ctx += "SucDef" -> hos":- iLEQ(a,suc(a))"
-  ctx += "ordcon" -> hos"LE(f(a),s(n)),iLEQ(a,b) :- E(n,f(b)), LE(f(b),n)"
+  ctx += "minimalElement" -> hos"LE(f(z),0) :- "
+  ctx += "ordcon" -> hos"LE(f(a),s(n)) :- E(n,f(a)), LE(f(a),n)"
+  ctx += "ordcon2" -> hos"LE(f(suc(a)),s(n)) :- E(n,f(suc(a))), LE(f(a),n)"
 
   val esOmega = Sequent(
     Seq( hof"!x POR(n,x)" ),
     Seq( hof"?x ( E(f(x), f(suc(x))) )" ) )
   ctx += Context.ProofNameDeclaration( le"omega n", esOmega )
   val esPhi = Sequent(
-    Seq( hof"?x !y ((iLEQ(x,y) -> E(n,f(y))) | LE(f(y),n))" ),
+    Seq( hof"?x ( E(n,f(x)) & E(n,f(suc(x)))) | !y (LE(f(y),n))" ),
     Seq( hof"?x ( E(f(x), f(suc(x))) )" ) )
   ctx += Context.ProofNameDeclaration( le"phi n", esPhi )
 
@@ -43,12 +40,14 @@ object StrongStrictMonotoneSchema extends TacticsProof {
       Seq( "Ant_0" -> hof"!x POR(0,x)" ),
       Seq( "Suc_0" -> hof"?x (E(f(x), f(suc(x))))" ) )
   val omegaBc = Lemma( esOmegaBc ) {
-    cut( "cut", hof"?x !y ((iLEQ(x,y) -> E(0,f(y))) | LE(f(y),0))" )
-    exR( "cut", hoc"z" )
-    allR( "cut_0", fov"a" )
+    cut( "cut", hof"?x ( E(0,f(x)) & E(0,f(suc(x)))) | !y (LE(f(y),0))" )
     orR
-    impR
-    allL( "Ant_0", fov"a" )
+    exR( "cut_0", hoc"z" )
+    andR
+    allL( "Ant_0", hoc"z" )
+    unfold( "POR" ) atMost 1 in "Ant_0_0"
+    trivial
+    allL( "Ant_0", le"(suc z)" )
     unfold( "POR" ) atMost 1 in "Ant_0_0"
     trivial
     ref( "phi" )
@@ -59,16 +58,21 @@ object StrongStrictMonotoneSchema extends TacticsProof {
       Seq( "Ant_0" -> hof"!x POR(s(n),x)" ),
       Seq( "Suc_0" -> hof"?x (E(f(x), f(suc(x))))" ) )
   val omegaSc = Lemma( esOmegaSc ) {
-    cut( "cut", hof"?x !y ((iLEQ(x,y) -> E(s(n),f(y))) | LE(f(y),s(n)))" )
-    exR( "cut", hoc"z" )
-    allR( "cut_0", fov"a" )
+    cut( "cut", hof"?x ( E(s(n),f(x)) & E(s(n),f(suc(x)))) | !y (LE(f(y),s(n)))" )
     orR
-    impR
+    allR( "cut_1", fov"a" )
+    exR( "cut_0", fov"a" )
+    andR
     allL( "Ant_0", fov"a" )
     unfold( "POR" ) atMost 1 in "Ant_0_0"
     orL
     trivial
     ref( "LEDefinition" )
+    allL( "Ant_0", le"(suc a)" )
+    unfold( "POR" ) atMost 1 in "Ant_0_0"
+    orL
+    trivial
+    ref( "LEDefinition2" )
     ref( "phi" )
 
   }
@@ -76,58 +80,39 @@ object StrongStrictMonotoneSchema extends TacticsProof {
 
   val esPhiBc =
     Sequent(
-      Seq( "Ant_0" -> hof"?x !y ((iLEQ(x,y) -> E(0,f(y))) | LE(f(y),0))" ),
+      Seq( "Ant_0" -> hof"?x ( E(0,f(x)) & E(0,f(suc(x)))) | !y (LE(f(y),0))" ),
       Seq( "Suc_0" -> hof"?x (E(f(x), f(suc(x))) )" ) )
   val phiBc = Lemma( esPhiBc ) {
+    orL
     exL( fov"a" )
-    allL( fov"a" )
-    allL( le"(suc a)" )
+    andL
     exR( fov"a" )
-    orL( "Ant_0_0" )
-    orL( "Ant_0_1" )
-    impL( "Ant_0_0" )
-    impL( "Ant_0_1" )
-    ref( "SucDef" )
-    ref( "reflexivity" )
-    impL( "Ant_0_1" )
-    ref( "SucDef" )
     ref( "NumericTransitivity" )
-    ref( "minimalElement" )
+    allL( foc"z" )
     ref( "minimalElement" )
   }
   ctx += Context.ProofDefinitionDeclaration( le"phi 0", phiBc )
 
   val esPhiSc =
     Sequent(
-      Seq( "Ant_0" -> hof"?x !y ((iLEQ(x,y) -> E(s(n),f(y))) | LE(f(y),s(n)))" ),
+      Seq( "Ant_0" -> hof"?x ( E(s(n),f(x)) & E(s(n),f(suc(x)))) | !y (LE(f(y),s(n)))" ),
       Seq( "Suc_0" -> hof"?x (E(f(x), f(suc(x))) )" ) )
   val phiSc = Lemma( esPhiSc ) {
-    cut( "cut", hof"?x !y ((iLEQ(x,y) -> E(n,f(y))) | LE(f(y),n))" )
+    cut( "cut", hof"?x ( E(n,f(x)) & E(n,f(suc(x)))) | !y (LE(f(y),n))" )
+    orR
+    orL
     exL( fov"a" )
-    exR( "cut", fov"a" )
-    exR( "cut", le"(suc a)" )
-    allL( fov"a" )
-    allR( "cut_0", fov"b" )
-    orR
-    impR
-    orL
-    focus( 1 )
-    ref( "ordcon" )
-    impL
-    ref( "reflexivity" )
-    allR( "cut_1", fov"c" )
-    orR
-    impR
-    allL( le"suc(a)" )
-    orL
-    focus( 1 )
-    ref( "ordcon" )
-    impL
-    ref( "SucDef" )
-    focus( 1 )
-    ref( "phi" )
+    andL
     exR( "Suc_0", fov"a" )
     ref( "NumericTransitivity" )
+    allR( fov"b" )
+    exR( "cut_0", fov"b" )
+    allL( fov"b" )
+    andR
+    ref( "ordcon" )
+    allL( le"(suc b)" )
+    ref( "ordcon2" )
+    ref( "phi" )
   }
   ctx += Context.ProofDefinitionDeclaration( le"phi (s n)", phiSc )
 
