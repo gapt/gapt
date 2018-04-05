0cccbc4323db1171c244e3211ad9970b2944d707
ermine516 <cernadavid1@gmail.com>
2018-04-05 14:58:07 +0200

    The last few

--- examples/schema/StrictMonotoneSchema.scala
+++ examples/schema/StrictMonotoneSchema.scala
@@ -1,10 +1,10 @@
-package at.logic.gapt.examples
+package gapt.examples
 
-import at.logic.gapt.expr._
-import at.logic.gapt.proofs.Context.PrimRecFun
-import at.logic.gapt.proofs.{ Context, Sequent }
-import at.logic.gapt.proofs.gaptic.TacticsProof
-import at.logic.gapt.proofs.gaptic._
+import gapt.expr._
+import gapt.proofs.Context._
+import gapt.proofs.Context
+import gapt.proofs.Sequent
+import gapt.proofs.gaptic._
 
 object StrictMonotoneSchema extends TacticsProof {
   ctx += Context.InductiveType( "nat", hoc"0 : nat", hoc"s : nat>nat" )
