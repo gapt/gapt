7b99169df86c4f4d7879bd5a72e805f7b1c254f3
Sebastian Zivota <sebastian.zivota@mailbox.org>
2018-06-27 15:20:16 +0200

    Refactored ProofCollection serialization

--- core/src/main/scala/gapt/formats/json/LKProofCodec.scala
+++ core/src/main/scala/gapt/formats/json/LKProofCodec.scala
@@ -1,105 +1,92 @@
 package gapt.formats.json
 import gapt.proofs.lk._
-import io.circe.{ Decoder, Encoder, HCursor, Json }
+import io.circe._
 import io.circe.syntax._
 import io.circe.generic.auto._
 import ExprCodec._
 import SequentCodec._
+import io.circe.Decoder.Result
 
 object LKProofCodec {
-  implicit val lkCollectionEncoder: Encoder[ProofCollection[LKProof]] = new ProofCollectionEncoder[LKProof]( encodeLK )
+  implicit val lkCollectionEncoder: Encoder[ProofCollection[LKProof]] = ProofCollectionCodec.proofCollectionEncoder[LKProof]( encodeLK )
 
-  implicit val lkCollectionDecoder: Decoder[ProofCollection[LKProof]] = new ProofCollectionDecoder[LKProof]( decodeLK )
+  implicit val lkCollectionDecoder: Decoder[ProofCollection[LKProof]] = ProofCollectionCodec.proofCollectionDecoder[LKProof]( decodeLK )
 
   /**
-   * Given a map from LKProofs to integers, serializes a single proof by
-   * looking up its subproofs in the collection and replacing them with their numbers.
+   * Given an encoder for subproofs, this encodes a single LK proof.
    */
-  private def encodeLK( proofMap: Map[LKProof, Int] ): Encoder[LKProof] = {
-    implicit val lkNumEncoder: Encoder[LKProof] = p => Json.fromInt( proofMap( p ) )
+  private def encodeLK( subEncoder: Encoder[LKProof] ): Encoder[LKProof] = {
+    implicit val e: Encoder[LKProof] = subEncoder
 
-    proof => {
-      val obj = proof match {
-        case p @ ProofLink( _, _ )                  => p.asJson
-        case TopAxiom                               => TopAxiom.asJson
-        case BottomAxiom                            => BottomAxiom.asJson
-        case p @ LogicalAxiom( _ )                  => p.asJson
-        case p @ ReflexivityAxiom( _ )              => p.asJson
-        case p @ WeakeningLeftRule( _, _ )          => p.asJson
-        case p @ WeakeningRightRule( _, _ )         => p.asJson
-        case p @ ContractionLeftRule( _, _, _ )     => p.asJson
-        case p @ ContractionRightRule( _, _, _ )    => p.asJson
-        case p @ CutRule( _, _, _, _ )              => p.asJson
-        case p @ NegLeftRule( _, _ )                => p.asJson
-        case p @ NegRightRule( _, _ )               => p.asJson
-        case p @ AndLeftRule( _, _, _ )             => p.asJson
-        case p @ AndRightRule( _, _, _, _ )         => p.asJson
-        case p @ OrLeftRule( _, _, _, _ )           => p.asJson
-        case p @ OrRightRule( _, _, _ )             => p.asJson
-        case p @ ImpLeftRule( _, _, _, _ )          => p.asJson
-        case p @ ImpRightRule( _, _, _ )            => p.asJson
-        case p @ ForallLeftRule( _, _, _, _, _ )    => p.asJson
-        case p @ ForallRightRule( _, _, _, _ )      => p.asJson
-        case p @ ExistsLeftRule( _, _, _, _ )       => p.asJson
-        case p @ ExistsRightRule( _, _, _, _, _ )   => p.asJson
-        case p @ ExistsSkLeftRule( _, _, _, _, _ )  => p.asJson
-        case p @ ForallSkRightRule( _, _, _, _, _ ) => p.asJson
-        case p @ EqualityLeftRule( _, _, _, _ )     => p.asJson
-        case p @ EqualityRightRule( _, _, _, _ )    => p.asJson
-        case p @ InductionRule( _, _, _ )           => p.asJson
-        case p @ DefinitionLeftRule( _, _, _ )      => p.asJson
-        case p @ DefinitionRightRule( _, _, _ )     => p.asJson
-      }
-
-      obj.mapObject( ( "name", Json.fromString( s"${proof.longName}" ) ) +: _ )
+    {
+      case p @ ProofLink( _, _ )                  => p.asJson
+      case TopAxiom                               => TopAxiom.asJson
+      case BottomAxiom                            => BottomAxiom.asJson
+      case p @ LogicalAxiom( _ )                  => p.asJson
+      case p @ ReflexivityAxiom( _ )              => p.asJson
+      case p @ WeakeningLeftRule( _, _ )          => p.asJson
+      case p @ WeakeningRightRule( _, _ )         => p.asJson
+      case p @ ContractionLeftRule( _, _, _ )     => p.asJson
+      case p @ ContractionRightRule( _, _, _ )    => p.asJson
+      case p @ CutRule( _, _, _, _ )              => p.asJson
+      case p @ NegLeftRule( _, _ )                => p.asJson
+      case p @ NegRightRule( _, _ )               => p.asJson
+      case p @ AndLeftRule( _, _, _ )             => p.asJson
+      case p @ AndRightRule( _, _, _, _ )         => p.asJson
+      case p @ OrLeftRule( _, _, _, _ )           => p.asJson
+      case p @ OrRightRule( _, _, _ )             => p.asJson
+      case p @ ImpLeftRule( _, _, _, _ )          => p.asJson
+      case p @ ImpRightRule( _, _, _ )            => p.asJson
+      case p @ ForallLeftRule( _, _, _, _, _ )    => p.asJson
+      case p @ ForallRightRule( _, _, _, _ )      => p.asJson
+      case p @ ExistsLeftRule( _, _, _, _ )       => p.asJson
+      case p @ ExistsRightRule( _, _, _, _, _ )   => p.asJson
+      case p @ ExistsSkLeftRule( _, _, _, _, _ )  => p.asJson
+      case p @ ForallSkRightRule( _, _, _, _, _ ) => p.asJson
+      case p @ EqualityLeftRule( _, _, _, _ )     => p.asJson
+      case p @ EqualityRightRule( _, _, _, _ )    => p.asJson
+      case p @ InductionRule( _, _, _ )           => p.asJson
+      case p @ DefinitionLeftRule( _, _, _ )      => p.asJson
+      case p @ DefinitionRightRule( _, _, _ )     => p.asJson
     }
   }
 
   /**
-   * Given a map from integers to proofs,
-   * @param m
-   * @return
+   * Given a rule name, a cursor, and a decoder for subproofs, this decodes a single LK proof.
    */
-  private def decodeLK( m: Map[Int, LKProof] ): Decoder[LKProof] = {
-    implicit val lkNumDecoder: Decoder[LKProof] = Decoder.decodeInt.emap( i => m.get( i ).toRight( s"Subproof $i not found" ) )
-
-    c: HCursor => {
-      val d = c.downField( "name" )
-      d.as[String].flatMap { name =>
-        val e = d.delete
-        name match {
-          case "ProofLink"            => e.as[ProofLink].map( _.asInstanceOf[LKProof] )
-          case "TopAxiom"             => e.as[TopAxiom.type].map( _.asInstanceOf[LKProof] )
-          case "BottomAxiom"          => e.as[BottomAxiom.type].map( _.asInstanceOf[LKProof] )
-          case "LogicalAxiom"         => e.as[LogicalAxiom].map( _.asInstanceOf[LKProof] )
-          case "ReflexivityAxiom"     => e.as[ReflexivityAxiom].map( _.asInstanceOf[LKProof] )
-          case "WeakeningLeftRule"    => e.as[WeakeningLeftRule].map( _.asInstanceOf[LKProof] )
-          case "WeakeningRightRule"   => e.as[WeakeningRightRule].map( _.asInstanceOf[LKProof] )
-          case "ContractionLeftRule"  => e.as[ContractionLeftRule].map( _.asInstanceOf[LKProof] )
-          case "ContractionRightRule" => e.as[ContractionRightRule].map( _.asInstanceOf[LKProof] )
-          case "CutRule"              => e.as[CutRule].map( _.asInstanceOf[LKProof] )
-          case "NegLeftRule"          => e.as[NegLeftRule].map( _.asInstanceOf[LKProof] )
-          case "NegRightRule"         => e.as[NegRightRule].map( _.asInstanceOf[LKProof] )
-          case "AndLeftRule"          => e.as[AndLeftRule].map( _.asInstanceOf[LKProof] )
-          case "AndRightRule"         => e.as[AndRightRule].map( _.asInstanceOf[LKProof] )
-          case "OrLeftRule"           => e.as[OrLeftRule].map( _.asInstanceOf[LKProof] )
-          case "OrRightRule"          => e.as[OrRightRule].map( _.asInstanceOf[LKProof] )
-          case "ImpLeftRule"          => e.as[ImpLeftRule].map( _.asInstanceOf[LKProof] )
-          case "ImpRightRule"         => e.as[ImpRightRule].map( _.asInstanceOf[LKProof] )
-          case "ForallLeftRule"       => e.as[ForallLeftRule].map( _.asInstanceOf[LKProof] )
-          case "ForallRightRule"      => e.as[ForallRightRule].map( _.asInstanceOf[LKProof] )
-          case "ExistsLeftRule"       => e.as[ExistsLeftRule].map( _.asInstanceOf[LKProof] )
-          case "ExistsRightRule"      => e.as[ExistsRightRule].map( _.asInstanceOf[LKProof] )
-          case "ExistsSkLeftRule"     => e.as[ExistsSkLeftRule].map( _.asInstanceOf[LKProof] )
-          case "ForallSkRightRule"    => e.as[ForallSkRightRule].map( _.asInstanceOf[LKProof] )
-          case "EqualityLeftRule"     => e.as[EqualityLeftRule].map( _.asInstanceOf[LKProof] )
-          case "EqualityRightRule"    => e.as[EqualityRightRule].map( _.asInstanceOf[LKProof] )
-          case "InductionRule"        => e.as[InductionRule].map( _.asInstanceOf[LKProof] )
-          case "DefinitionLeftRule"   => e.as[DefinitionLeftRule].map( _.asInstanceOf[LKProof] )
-          case "DefinitionRightRule"  => e.as[DefinitionRightRule].map( _.asInstanceOf[LKProof] )
-        }
-      }
-
+  private def decodeLK( name: String, c: ACursor, subDecoder: Decoder[LKProof] ): Result[LKProof] = {
+    implicit val d: Decoder[LKProof] = subDecoder
+    name match {
+      case "ProofLink"            => c.as[ProofLink]
+      case "TopAxiom"             => c.as[TopAxiom.type]
+      case "BottomAxiom"          => c.as[BottomAxiom.type]
+      case "LogicalAxiom"         => c.as[LogicalAxiom]
+      case "ReflexivityAxiom"     => c.as[ReflexivityAxiom]
+      case "WeakeningLeftRule"    => c.as[WeakeningLeftRule]
+      case "WeakeningRightRule"   => c.as[WeakeningRightRule]
+      case "ContractionLeftRule"  => c.as[ContractionLeftRule]
+      case "ContractionRightRule" => c.as[ContractionRightRule]
+      case "CutRule"              => c.as[CutRule]
+      case "NegLeftRule"          => c.as[NegLeftRule]
+      case "NegRightRule"         => c.as[NegRightRule]
+      case "AndLeftRule"          => c.as[AndLeftRule]
+      case "AndRightRule"         => c.as[AndRightRule]
+      case "OrLeftRule"           => c.as[OrLeftRule]
+      case "OrRightRule"          => c.as[OrRightRule]
+      case "ImpLeftRule"          => c.as[ImpLeftRule]
+      case "ImpRightRule"         => c.as[ImpRightRule]
+      case "ForallLeftRule"       => c.as[ForallLeftRule]
+      case "ForallRightRule"      => c.as[ForallRightRule]
+      case "ExistsLeftRule"       => c.as[ExistsLeftRule]
+      case "ExistsRightRule"      => c.as[ExistsRightRule]
+      case "ExistsSkLeftRule"     => c.as[ExistsSkLeftRule]
+      case "ForallSkRightRule"    => c.as[ForallSkRightRule]
+      case "EqualityLeftRule"     => c.as[EqualityLeftRule]
+      case "EqualityRightRule"    => c.as[EqualityRightRule]
+      case "InductionRule"        => c.as[InductionRule]
+      case "DefinitionLeftRule"   => c.as[DefinitionLeftRule]
+      case "DefinitionRightRule"  => c.as[DefinitionRightRule]
+      case _                      => Left( DecodingFailure( s"Rule $name not recognized.", Nil ) )
     }
   }
 }
--- core/src/main/scala/gapt/formats/json/ProofCollection.scala
+++ core/src/main/scala/gapt/formats/json/ProofCollection.scala
@@ -1,12 +1,11 @@
 package gapt.formats.json
 
-import cats.data.StateT
+import cats.{ Eval, Later }
 import gapt.proofs.DagProof
 import io.circe.Decoder.Result
 import io.circe._
 import io.circe.KeyEncoder._
 import cats.implicits._
-import cats.data.StateT._
 
 /**
  * Wraps a map from proofs to integers. Each proof must be mapped to a
@@ -35,40 +34,48 @@ object ProofCollection {
   }
 }
 
-/**
- * Serializes a collection of proofs.
- * The resulting JSON structure is an object with keys in ℕ and
- * proofs as values. Each of the proofs is serialized by the encodeProof function.
- * @param encodeProof An encoder for Proofs relative to the collection.
- */
-class ProofCollectionEncoder[P <: DagProof[P]]( encodeProof: Map[P, Int] => Encoder[P] ) extends Encoder[ProofCollection[P]] {
-  override def apply( coll: ProofCollection[P] ): Json =
-    Encoder.encodeMap[Int, P]( implicitly, encodeProof( coll.proofMap ) )( coll.proofMap.toList.map( _.swap ).toMap )
-}
+object ProofCollectionCodec {
+  /**
+   * Encodes a proof collection
+   */
+  def proofCollectionEncoder[P <: DagProof[P]]( encodeProof: Encoder[P] => Encoder[P] ): Encoder[ProofCollection[P]] = { coll =>
+    val numEncoder: Encoder[P] = p => Json.fromInt( coll.proofMap( p ) )
+    val encodeProofWithName: Encoder[P] = p => encodeProof( numEncoder )( p ).mapObject( ( "name", Json.fromString( s"${p.longName}" ) ) +: _ )
 
-/**
- * Deserializes a collection of proofs.
- * The expected format is an object with keys in ℕ and proofs as values,
- * where each subproof is referenced by a number.
- * @param decodeProof A decoder for individual proofs relative to a map.
- */
-class ProofCollectionDecoder[P <: DagProof[P]]( decodeProof: Map[Int, P] => Decoder[P] ) extends Decoder[ProofCollection[P]] {
-  type S = Map[Int, P]
-  type M[T] = StateT[Decoder.Result, S, T]
+    Encoder.encodeMap[Int, P]( implicitly, encodeProofWithName )( coll.proofMap.toList.map( _.swap ).toMap )
+  }
 
   /**
-   * Decodes a single proof by looking up its subproofs in a map,
-   * then adds it to the map with the next index.
+   * Decodes a proof collection.
    */
-  private def readProof( j: Json ): M[Unit] = for {
-    m <- get[Decoder.Result, S]
-    i = m.size
-    p <- liftF( decodeProof( m ).decodeJson( j ) )
-    _ <- set[Decoder.Result, S]( m + ( ( i, p ) ) )
-  } yield ()
+  def proofCollectionDecoder[P <: DagProof[P]]( decodeProof: ( String, ACursor, Decoder[P] ) => Result[P] ): Decoder[ProofCollection[P]] = Decoder.decodeMap[Int, Json] emap { jsonMap =>
+    var proofMap: Map[Int, Eval[Result[P]]] = Map.empty
+    val numDecoder: Decoder[P] = Decoder.decodeInt.emap { i =>
+      proofMap.get( i ) match {
+        case Some( e ) => e.value.leftMap( _.message )
+        case None      => Left( s"Proof $i not found" )
+      }
+    }
 
-  def apply( c: HCursor ): Result[ProofCollection[P]] = for {
-    l <- Decoder.decodeMap[Int, Json]( implicitly, implicitly )( c )
-    m <- l.toList.sortBy( _._1 ).map( _._2 ).traverse[M, Unit]( readProof ).runS( Map.empty )
-  } yield ProofCollection( m.toList.map( _.swap ).toMap )
+    implicit val proofDecoder: Decoder[P] = c => {
+      val d = c.downField( "name" )
+      d.as[String].flatMap { name =>
+        val e = d.delete
+        decodeProof( name, e, numDecoder )
+      }
+    }
+
+    proofMap = jsonMap map { case ( i, j ) => ( i, Later( j.as[P] ) ) }
+
+    val forced: List[( Result[P], Int )] = proofMap.toList.map {
+      case ( i, e ) => ( e.value, i )
+    }
+
+    val resultList: Result[List[( P, Int )]] = forced.traverse[Result, ( P, Int )] {
+      case ( Left( f ), i )  => Left( f )
+      case ( Right( p ), i ) => Right( ( p, i ) )
+    }
+
+    resultList.leftMap( _.message ).map( l => ProofCollection( l.toMap ) )
+  }
 }
