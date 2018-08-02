cb4a97bf13d2da56c1955b6c0ac4f0755e6bccbb
Gabriel Ebner <gebner@gebner.org>
2018-08-02 12:17:59 +0200

    Add SMT-LIB exporter for BUPs as horn problems.

--- core/src/main/scala/gapt/provers/viper/grammars/TreeGrammarProver.scala
+++ core/src/main/scala/gapt/provers/viper/grammars/TreeGrammarProver.scala
@@ -4,6 +4,7 @@ import gapt.expr._
 import gapt.expr.fol.{ folSubTerms, folTermSize }
 import gapt.expr.hol._
 import gapt.formats.babel.BabelSignature
+import gapt.formats.smt.SmtLibExporter
 import gapt.grammars.{ InductionGrammar, findMinimalInductionGrammar }
 import gapt.grammars.InductionGrammar.Production
 import gapt.proofs.Context.StructurallyInductiveTypes
@@ -203,6 +204,9 @@ class TreeGrammarProver( val ctx: Context, val sequent: HOLSequent, val options:
   def solveBUP( bup: InductionBUP ): Expr = time( "solvebup" ) {
     val qbup @ Ex( x_B, qbupMatrix ) = bup.formula
     info( s"Solution condition:\n${qbup.toSigRelativeString}\n" )
+    try
+      info( s"smt-lib:\n${SmtLibExporter.bup( bup.formula )}" )
+    catch { case e: Throwable => info( ExceptionUtils.getStackTrace( e ) ) }
 
     val solution =
       if ( options.useInterpolation )
