3a0080773ee6930795cf175b96d27c7fb3779a6a
Gabriel Ebner <gebner@gebner.org>
2018-08-29 19:25:57 +0200

    JsonToDocTest: add some failing tests for strings

--- tests/src/test/scala/gapt/formats/json/JsonToDocTest.scala
+++ tests/src/test/scala/gapt/formats/json/JsonToDocTest.scala
@@ -2,8 +2,10 @@ package gapt.formats.json
 
 import gapt.proofs.lk.LKProof
 import gapt.formats.json.lk.LKProofCodec._
+import io.circe.Json
 import org.specs2.mutable.Specification
 import io.circe.parser._
+import org.specs2.specification.core.Fragments
 
 class JsonToDocTest extends Specification {
   "The JSON to Doc converter" should {
@@ -22,5 +24,19 @@ class JsonToDocTest extends Specification {
       val parsed = parse( rendered )
       parsed must beRight( json )
     }
+  }
+
+  "roundtrip" in {
+    def roundtrip( d: Json ) =
+      io.circe.parser.parse( JsonToDoc( d ).render( 80 ) ) must beRight( d )
+
+    "double quotes" in roundtrip( Json.fromString( "\"" ) )
+    "newlines" in roundtrip( Json.fromString( "\n" ) )
+    "null byte" in roundtrip( Json.fromString( "\0" ) )
+    Fragments.foreach( 0 until 20 )( i =>
+      s"control character $i" in
+        roundtrip( Json.fromString( i.toChar.toString ) ) )
+    "decimals" in roundtrip( Json.fromBigDecimal( BigDecimal( "12e-5" ) ) )
+    "zero point one" in roundtrip( Json.fromBigDecimal( BigDecimal( "0.1" ) ) )
   }
 }
