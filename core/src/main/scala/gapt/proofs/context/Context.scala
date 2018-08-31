1e16a3e4b939ff0a47611926765f7d534d7b501e
Jannik Vierling <jannik.vierling@gmail.com>
2018-08-31 15:00:38 +0200

    improve context documentation

--- core/src/main/scala/gapt/proofs/context/Context.scala
+++ core/src/main/scala/gapt/proofs/context/Context.scala
@@ -53,38 +53,115 @@ import gapt.utils.NameGenerator
  *    to produce LK proofs modulo the background theory.
  */
 trait Context extends BabelSignature {
+
+  /**
+   * Retrieves the context's current state.
+   *
+   * @return The context's current state.
+   */
   def state: State
+
+  /**
+   * Retrieves the updates that have been applied to this context.
+   *
+   * @return A list of updates with newer updates occurring before older ones.
+   */
   def updates: List[Update]
+
+  /**
+   * Converts this context into an immutable context.
+   *
+   * @return A new immutable context that equals this context at the time of
+   * calling this method.
+   */
   def toImmutable: ImmutableContext
 
+  /**
+   * Converts this context into a mutable context.
+   *
+   * @return A new mutable context that equals this context at the time of
+   * calling this method.
+   */
   def newMutable: MutableContext = new MutableContext( toImmutable )
+
+  /**
+   * Converts this context to a read-only context.
+   *
+   * @return A new read-only context that is equal to this context at the time
+   * of invoking this method.
+   */
   def toReadonly: ReadonlyMutableContext = new ReadonlyMutableContext( toImmutable )
 
-  /** Gets a facet of this context, initializing it if it is not present yet. */
+  /**
+   * Retrieves a facet of this context.
+   *
+   * @tparam T The facet's type.
+   * @return Returns the facet associated with the given type. If this facet
+   * is not present yet, it is initialized and its initial value is returned.
+   */
   def get[T: Facet]: T = state.get[T]
 
+  /**
+   * Retrieves all the constants defined in this context.
+   *
+   * @return A list of all the constants that are currently defined in this
+   * context.
+   */
   def constants: Iterable[Const] = get[Constants].constants.values
+
+  /**
+   * Retrieves all the definitions of this context.
+   *
+   * @return The current definitions saved by this context i.e. a map
+   * associating constants with expressions.
+   */
   def definitions: Map[Const, Expr] =
     for {
       ( what, by ) <- get[Definitions].definitions
       whatC <- constant( what )
     } yield whatC -> by
 
-  /** Returns Some(const) if name is a constant. */
+  /**
+   * Retrieves constants by name.
+   *
+   * @param name The name for which a constant is to be looked up.
+   * @return Returns `Some( c )` where `c.name` equals `name` if the context
+   * currently contains such a constant `c`, otherwise `None` is retured.
+   */
   def constant( name: String ): Option[Const] = get[Constants].constants.get( name )
 
-  /** Returns Some(const) if (name,params) defines a constant. */
+  /**
+   * Retrieves constants by name and type parameters.
+   *
+   * @param name The name for which a constant is to be looked up.
+   * @param params The type parameters for which a constant is to be looked up.
+   * @return Returns `Some ( c )` if the context currently contains a constant
+   * `c` with name `name` and type parameters `params`. Otherwise `None` is
+   * returned.
+   */
   def constant( name: String, params: List[Ty] ): Option[Const] =
     get[Constants].lookup( name, params )
 
-  /** Returns Some(ctrs) if name is an inductive type with constructors ctrs. */
+  /**
+   * Retrieves inductive type constructors for a given type.
+   *
+   * @param ty The type whose inductive type constructors are to be looked up.
+   * @return Returns `Some( ctrs )` if `ty` is an inductive type having
+   * constructors `ctrs`, otherwise `None` is returned.
+   */
   def getConstructors( ty: Ty ): Option[Vector[Const]] =
     ty match {
       case ty @ TBase( _, _ ) => getConstructors( ty )
       case _                  => None
     }
 
-  /** Returns Some(ctrs) if name is an inductive type with constructors ctrs. */
+  /**
+   * Looks up inductive type constructors for a given base type.
+   *
+   * @param ty The base type whose type constructors are to be looked up.
+   * @return Returns `Some( ctrs )` if `ty` is an inductive type having
+   * constructors `ctrs`, otherwise `None` is returned.
+   */
   def getConstructors( ty: TBase ): Option[Vector[Const]] =
     for {
       declT <- get[BaseTypes].baseTypes.get( ty.name )
@@ -92,14 +169,27 @@ trait Context extends BabelSignature {
       subst <- syntacticMatching( declT, ty )
     } yield ctrs.map( subst( _ ).asInstanceOf[Const] )
 
-  /** Returns true iff ty is a defined base type. */
+  /**
+    * Checks whether the given base type is defined in this context.
+    *
+    * @param ty The base type that is to be checked.
+    * @return `true` if `ty` is defined in this context, `false` otherwise.
+    */
   def isType( ty: TBase ): Boolean = (
     for {
       declT <- get[BaseTypes].baseTypes.get( ty.name )
       _ <- syntacticMatching( declT, ty )
     } yield () ).isDefined
 
-  /** Returns Some(expandedDefinition) if c is a defined constant. */
+  /**
+    * Looks up the definition of a constant.
+    *
+    * @param c The constant with type arguments `args` whose definition is to
+    * be looked up.
+    * @return If the constant `c` has a definition, then `Some( definition )`
+    * is returned where `definition` is obtained from the definition of `c` by
+    * instantiating the type variables according to args.
+    */
   def definition( c: Const ): Option[Expr] =
     for {
       defC <- get[Constants].constants.get( c.name )
@@ -107,54 +197,131 @@ trait Context extends BabelSignature {
       defn <- get[Definitions].definitions.get( c.name )
     } yield subst( defn )
 
-  /** Returns a collection of all (expression-level) reduction rules of this context. */
+  /**
+    * Retrieves all expression-level reduction rules.
+    *
+    * @return Returns all the expression-level reduction rules currently stored
+    * in this context.
+    */
   def reductionRules: Iterable[ReductionRule] = state.getAll[ReductionRule]
 
-  /** Returns true iff the context contains the definition defn (the definitional expansion has to match as well). */
+  /**
+    * Checks whether the context contains a given definition.
+    *
+    * @param defn The definition that is to be checked.
+    * @return Returns `true` if the given definition is an instance of a
+    * definition stored in this context.
+    */
   def contains( defn: Definition ): Boolean =
     definition( defn.what ).contains( defn.by )
 
-  /** Returns the Skolem definition of skSym.  See [[gapt.expr.hol.SkolemFunctions]]. */
+  /**
+    * Retrieves the skolem definition of a constant.
+    *
+    * @param skSym The constant whose definition is to be looked up.
+    * @return Returns `Some(definition)` if the constant `skSym` is a skolem
+    * function with definition `definition`.
+    */
   def skolemDef( skSym: Const ): Option[Expr] =
     get[SkolemFunctions].skolemDefs.get( skSym )
 
   /**
-   * Adds an element to the context.
-   *
-   * If this is not a valid addition, then an exception is thrown.
-   */
+    * Applies an update to this context.
+    *
+    * @param update The update to be applied to this context.
+    * @return An immutable context obtained by converting this context to an
+    * immutable context and by applying the update to that context.
+    */
   def +( update: Update ): ImmutableContext =
     toImmutable + update
 
+  /**
+    * The context's normalizer.
+    *
+    * @return The context's normalizer.
+    */
   def normalizer = get[Reductions].normalizer
 
-  /** Normalizes an expression with the reduction rules stored in this context. */
+  /**
+    * Normalizes an expression.
+    *
+    * @param expression The expression to be normalized.
+    * @return An expression obtained by normalizing `expression` according to
+    * the reduction rules currently stored in this context.
+    */
   def normalize( expression: Expr ): Expr =
     normalizer.normalize( expression )
 
+  /**
+    * Reduces an expression to WHNF.
+    *
+    * @param expression The expression to be reduced to WHNF.
+    * @return An expression reduced to WHNF according to the reduction rules
+    * currently stored in this context.
+    */
   def whnf( expression: Expr ): Expr =
     normalizer.whnf( expression )
 
+  /**
+    * Checks if two expressions normalize to the same expression.
+    *
+    * @param a An expression to be checked.
+    * @param b An expression to be checked.
+    * @return Returns `true` if and only if `a` and `b` normalize to the same
+    * expression in this context.
+    */
   def isDefEq( a: Expr, b: Expr ): Boolean =
     normalizer.isDefEq( a, b )
+
+
+  /**
+    * Checks an object with respect to this context.
+    *
+    * This method checks whether a given object is correctly constructed
+    * according to this context. For example for a type to check against a
+    * context the base types occurring in that type must all be declared in the
+    * context.
+    *
+    * @param t The object to be checked.
+    * @tparam T The type of object to be checked.
+    */
+  def check[T: Checkable]( t: T ): Unit =
+    implicitly[Checkable[T]].check( t )( this )
+
+  /**
+    * Applies several updates to the context.
+    *
+    * @param updates The updates to be applied to the context.
+    * @return An immutable context obtained by applying iteratively applying
+    * the updates from left to right.
+    */
+  def ++( updates: Traversable[Update] ): ImmutableContext =
+    updates.foldLeft( toImmutable )( _ + _ )
+
+  /**
+    * Retrieves all the names declared in this context.
+    *
+    * @return All the names currently declared in this context.
+    */
+  def names: Iterable[String] = constants.map( _.name ) ++ get[BaseTypes].baseTypes.keySet
+
+  /**
+    * Creates a new name generator for this context.
+    *
+    * @return A new name generator that avoids all the names currently defined
+    * in the context.
+    */
+  def newNameGenerator: NameGenerator = new NameGenerator( names )
 
   def signatureLookup( s: String ): BabelSignature.VarConst =
     constant( s ) match {
       case Some( c ) => BabelSignature.IsConst( c )
       case None      => BabelSignature.IsVar
     }
+
   def notationsForToken( token: Notation.Token ): Option[Notation] = get[Notations].byToken.get( token )
   def notationsForConst( const: Notation.ConstName ): List[Notation] = get[Notations].byConst( const )
   def defaultTypeToI: Boolean = false
-
-  def check[T: Checkable]( t: T ): Unit =
-    implicitly[Checkable[T]].check( t )( this )
-
-  def ++( updates: Traversable[Update] ): ImmutableContext =
-    updates.foldLeft( toImmutable )( _ + _ )
-
-  def names: Iterable[String] = constants.map( _.name ) ++ get[BaseTypes].baseTypes.keySet
-  def newNameGenerator: NameGenerator = new NameGenerator( names )
 
   override def toString =
     s"${state}Updates:\n${updates.view.reverse.map( x => s"  $x\n" ).mkString}"
