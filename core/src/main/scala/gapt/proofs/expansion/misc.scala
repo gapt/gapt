63b3d1df6ad7e7df2ab5447ac6740b6ccf155346
Gabriel Ebner <gebner@gebner.org>
2018-11-04 13:00:27 +0100

    formulaToExpansionTree: use expansion tree terms

--- core/src/main/scala/gapt/proofs/expansion/misc.scala
+++ core/src/main/scala/gapt/proofs/expansion/misc.scala
@@ -1,7 +1,6 @@
 package gapt.proofs.expansion
 
 import gapt.expr._
-import gapt.expr.hol.instantiate
 import gapt.proofs.HOLSequent
 
 object formulaToExpansionTree {
@@ -9,30 +8,29 @@ object formulaToExpansionTree {
     for ( ( f, i ) <- sequent.zipWithIndex ) yield formulaToExpansionTree( f, i.polarity )
 
   def apply( formula: Formula, pol: Polarity ): ExpansionTree =
-    conv( formula, formula, Set(), pol )
+    apply( formula, Set.empty, pol )
 
-  def apply( formula: Formula, substitutions: Traversable[_ <: Substitution], pol: Polarity ): ExpansionTree =
-    conv( formula, formula, substitutions.toSet, pol )
+  def apply( formula: Formula, substitutions: Traversable[Substitution], pol: Polarity ): ExpansionTree =
+    ExpansionTree( formula, pol, conv( formula, substitutions.toSet, pol ) )
 
-  private def conv( formula: Formula, origFormula: Formula, substitutions: Set[Substitution], pol: Polarity ): ExpansionTree =
-    ( formula, origFormula ) match {
-      case ( a: Atom, _ )                 => ETAtom( a, pol )
-      case ( Top(), _ )                   => ETTop( pol )
-      case ( Bottom(), _ )                => ETBottom( pol )
-      case ( Neg( f ), Neg( of ) )        => ETNeg( conv( f, of, substitutions, !pol ) )
-      case ( And( f, g ), And( of, og ) ) => ETAnd( conv( f, of, substitutions, pol ), conv( g, og, substitutions, pol ) )
-      case ( Or( f, g ), Or( of, og ) )   => ETOr( conv( f, of, substitutions, pol ), conv( g, og, substitutions, pol ) )
-      case ( Imp( f, g ), Imp( of, og ) ) => ETImp( conv( f, of, substitutions, !pol ), conv( g, og, substitutions, pol ) )
-      case ( _, Quant( v, f, isAll ) ) if isAll == pol.inAnt =>
-        ETWeakQuantifier( formula, Map() ++ substitutions.groupBy( _( v ) ).
-          map { case ( term, insts ) => term -> conv( instantiate( formula, term ), f, insts, pol ) } )
-      case ( _, Quant( v, f, isAll ) ) if isAll == pol.inSuc =>
-        ETMerge( formula, pol, substitutions.groupBy( _( v ) ).
-          map {
-            case ( ev: Var, insts ) => ETStrongQuantifier( formula, ev, conv( instantiate( formula, ev ), f, insts, pol ) )
-            case ( res, insts )     => throw new IllegalArgumentException( s"Substitution maps variable ${insts.head( v )} to non-variable $res" )
-          } )
-    }
+  private def conv( origFormula: Formula, substitutions: Set[Substitution], pol: Polarity ): ETt = origFormula match {
+    case _: Atom       => ETtAtom
+    case Top()         => ETtNullary
+    case Bottom()      => ETtNullary
+    case Neg( of )     => ETtUnary( conv( of, substitutions, !pol ) )
+    case And( of, og ) => ETtBinary( conv( of, substitutions, pol ), conv( og, substitutions, pol ) )
+    case Or( of, og )  => ETtBinary( conv( of, substitutions, pol ), conv( og, substitutions, pol ) )
+    case Imp( of, og ) => ETtBinary( conv( of, substitutions, !pol ), conv( og, substitutions, pol ) )
+    case Quant( v, f, isAll ) if isAll == pol.inAnt =>
+      ETtWeak( Map() ++ substitutions.groupBy( _( v ) ).
+        map { case ( term, insts ) => term -> conv( f, insts, pol ) } )
+    case Quant( v, f, isAll ) if isAll == pol.inSuc =>
+      ETtMerge( substitutions.groupBy( _( v ) ).map {
+        case ( ev: Var, insts ) => ETtStrong( ev, conv( f, insts, pol ) )
+        case ( res, insts ) =>
+          throw new IllegalArgumentException( s"Substitution maps variable ${insts.head( v )} to non-variable $res" )
+      } )
+  }
 }
 
 object numberOfInstancesET {
