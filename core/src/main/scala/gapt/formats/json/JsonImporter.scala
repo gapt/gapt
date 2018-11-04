6dc0908897d8ebae4b7ee05aefa3a4451e3e7229
Gabriel Ebner <gebner@gebner.org>
2018-11-04 16:41:03 +0100

    JsonImporter: do not rewrap exception

--- core/src/main/scala/gapt/formats/json/JsonImporter.scala
+++ core/src/main/scala/gapt/formats/json/JsonImporter.scala
@@ -1,21 +1,13 @@
 package gapt.formats.json
 
 import gapt.formats.InputFile
-import io.circe.{ Decoder, Json }
+import io.circe.Decoder
 import io.circe.parser._
 
 object JsonImporter {
   /**
    * Imports a value for which a Decoder exists from JSON.
    */
-  def load[A]( file: InputFile )( implicit ev: Decoder[A] ): A = {
-    val json: Json = parse( file.read ) match {
-      case Left( f )  => throw new IllegalArgumentException( f.message )
-      case Right( j ) => j
-    }
-    ev.decodeJson( json ) match {
-      case Left( f )  => throw new IllegalArgumentException( f.message )
-      case Right( x ) => x
-    }
-  }
+  def load[A: Decoder]( file: InputFile ): A =
+    decode[A]( file.read ).toTry.get
 }
