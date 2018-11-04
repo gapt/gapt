35cc8cc06596fcb17da02ec09536c8db8bb63576
Gabriel Ebner <gebner@gebner.org>
2018-11-04 11:48:17 +0100

    Deskolemization: use expansion tree terms

--- core/src/main/scala/gapt/proofs/expansion/removeSkolemCongruences.scala
+++ core/src/main/scala/gapt/proofs/expansion/removeSkolemCongruences.scala
@@ -8,35 +8,26 @@ import gapt.provers.verit.VeriT
 
 object removeSkolemCongruences {
 
-  def repl( m: Map[Expr, Expr], et: ExpansionTree, newSh: Formula ): ExpansionTree =
-    ( et, newSh ) match {
-      case ( ETTop( _ ) | ETBottom( _ ), _ ) => et
-      case ( ETAtom( _, pol ), newSh: Atom ) => ETAtom( newSh, pol )
-      case ( ETWeakening( _, pol ), _ )      => ETWeakening( newSh, pol )
-      case ( ETMerge( a, b ), _ )            => ETMerge( repl( m, a, newSh ), repl( m, b, newSh ) )
-      case ( ETNeg( a ), Neg( f ) )          => ETNeg( repl( m, a, f ) )
-      case ( ETAnd( a, b ), And( f, g ) )    => ETAnd( repl( m, a, f ), repl( m, b, g ) )
-      case ( ETOr( a, b ), Or( f, g ) )      => ETOr( repl( m, a, f ), repl( m, b, g ) )
-      case ( ETImp( a, b ), Imp( f, g ) )    => ETImp( repl( m, a, f ), repl( m, b, g ) )
-      case ( ETStrongQuantifier( _, ev, ch ), _ ) =>
-        ETStrongQuantifier( newSh, ev, repl( m, ch, instantiate( newSh, ev ) ) )
-      case ( ETSkolemQuantifier( _, Apps( skC, skAs ), skD, ch ), _ ) =>
-        val newSkT = skC( TermReplacement( skAs, m ) )
-        ETSkolemQuantifier( newSh, newSkT, skD,
-          repl( m, ch, instantiate( newSh, newSkT ) ) )
-      case ( ETWeakQuantifier( _, insts ), _ ) =>
-        ETWeakQuantifier.withMerge(
-          newSh,
-          insts.view.map {
-            case ( t, ch ) =>
-              val newT = TermReplacement( t, m )
-              newT -> repl( m, ch, BetaReduction.betaNormalize( instantiate( newSh, newT ) ) )
-          } )
-    }
+  def repl( m: Map[Expr, Expr], et: ExpansionTree ): ExpansionTree =
+    ExpansionTree( et.shallow, et.polarity, repl( m, et.term ) )
+  def repl( m: Map[Expr, Expr], et: ETt ): ETt = et match {
+    case ETtNullary | ETtAtom | ETtWeakening => et
+    case ETtMerge( a, b )                    => ETtMerge( repl( m, a ), repl( m, b ) )
+    case ETtUnary( a )                       => ETtUnary( repl( m, a ) )
+    case ETtBinary( a, b )                   => ETtBinary( repl( m, a ), repl( m, b ) )
+    case ETtStrong( ev, ch )                 => ETtStrong( ev, repl( m, ch ) )
+    case ETtSkolem( Apps( skC, skAs ), skD, ch ) =>
+      val newSkT = skC( TermReplacement( skAs, m ) )
+      ETtSkolem( newSkT, skD, repl( m, ch ) )
+    case ETtWeak( insts ) =>
+      ETtWeak.withMerge( for ( ( t, ch ) <- insts.view )
+        yield TermReplacement( t, m ) -> repl( m, ch ) )
+    case ETtDef( _, _ ) => throw new MatchError( et )
+  }
 
   def remove1( m: Map[Expr, Expr], ep: ExpansionProof ): ExpansionProof =
     ExpansionProof( eliminateMerges( ep.expansionSequent.
-      map( et => ETMerge( et, repl( m, et, et.shallow ) ) ) ) )
+      map( et => ETMerge( et, repl( m, et ) ) ) ) )
 
   def getAllPossibleCongruences( ep: ExpansionProof ): Vector[( Expr, Expr )] = {
     val skSyms = ep.skolemFunctions.skolemDefs.keySet
--- core/src/main/scala/gapt/proofs/expansion/skolemNormalForm.scala
+++ core/src/main/scala/gapt/proofs/expansion/skolemNormalForm.scala
@@ -1,72 +1,71 @@
 package gapt.proofs.expansion
 import gapt.expr._
-import gapt.expr.hol.instantiate
 import gapt.utils.NameGenerator
 
 import scala.collection.mutable
 
 object tautAtomicExpansionET {
   import Polarity._
-  def apply( formula: Formula )( implicit nameGen: NameGenerator ): ( ExpansionTree, ExpansionTree ) =
-    formula match {
-      case Top()    => ETTop( InAntecedent ) -> ETTop( InSuccedent )
-      case Bottom() => ETBottom( InAntecedent ) -> ETBottom( InSuccedent )
-      case Neg( f ) =>
-        val ( f1, f2 ) = apply( f )
-        ETNeg( f2 ) -> ETNeg( f1 )
-      case And( f, g ) =>
-        val ( f1, f2 ) = apply( f )
-        val ( g1, g2 ) = apply( g )
-        ETAnd( f1, g1 ) -> ETAnd( f2, g2 )
-      case Or( f, g ) =>
-        val ( f1, f2 ) = apply( f )
-        val ( g1, g2 ) = apply( g )
-        ETOr( f1, g1 ) -> ETOr( f2, g2 )
-      case Imp( f, g ) =>
-        val ( f1, f2 ) = apply( f )
-        val ( g1, g2 ) = apply( g )
-        ETImp( f2, g1 ) -> ETImp( f1, g2 )
-      case All( x0, _ ) =>
-        val x = nameGen.fresh( x0 )
-        val ( f1, f2 ) = apply( instantiate( formula, x ) )
-        ETWeakQuantifier( formula, Map( x -> f1 ) ) -> ETStrongQuantifier( formula, x, f2 )
-      case Ex( x0, _ ) =>
-        val x = nameGen.fresh( x0 )
-        val ( f1, f2 ) = apply( instantiate( formula, x ) )
-        ETStrongQuantifier( formula, x, f1 ) -> ETWeakQuantifier( formula, Map( x -> f2 ) )
-      case atom: Atom => ETAtom( atom, InAntecedent ) -> ETAtom( atom, InSuccedent )
-    }
+  def apply( formula: Formula )( implicit nameGen: NameGenerator ): ( ExpansionTree, ExpansionTree ) = {
+    val ( f1, f2 ) = asTerm( formula )
+    ExpansionTree( formula, InAntecedent, f1 ) -> ExpansionTree( formula, InSuccedent, f2 )
+  }
+  def asTerm( formula: Formula )( implicit nameGen: NameGenerator ): ( ETt, ETt ) = formula match {
+    case Top() | Bottom() => ETtNullary -> ETtNullary
+    case _: Atom          => ETtAtom -> ETtAtom
+    case Neg( f ) =>
+      val ( f1, f2 ) = asTerm( f )
+      ETtUnary( f2 ) -> ETtUnary( f1 )
+    case And( f, g ) =>
+      val ( f1, f2 ) = asTerm( f )
+      val ( g1, g2 ) = asTerm( g )
+      ETtBinary( f1, g1 ) -> ETtBinary( f2, g2 )
+    case Or( f, g ) =>
+      val ( f1, f2 ) = asTerm( f )
+      val ( g1, g2 ) = asTerm( g )
+      ETtBinary( f1, g1 ) -> ETtBinary( f2, g2 )
+    case Imp( f, g ) =>
+      val ( f1, f2 ) = asTerm( f )
+      val ( g1, g2 ) = asTerm( g )
+      ETtBinary( f2, g1 ) -> ETtBinary( f1, g2 )
+    case All( x0, f ) =>
+      val x = nameGen.fresh( x0 )
+      val ( f1, f2 ) = asTerm( f )
+      ETtWeak( x -> f1 ) -> ETtStrong( x, f2 )
+    case Ex( x0, f ) =>
+      val x = nameGen.fresh( x0 )
+      val ( f1, f2 ) = asTerm( f )
+      ETtStrong( x, f1 ) -> ETtWeak( x -> f2 )
+  }
 }
 
 object nonProofTheoreticSkolemTerms {
   def apply( ep: ExpansionProof ): Set[Expr] = apply( ep.expansionSequent )
   def apply( es: ExpansionSequent ): Set[Expr] = {
     val occs = mutable.Map[Expr, Set[( List[Expr], List[Int] )]]().withDefaultValue( Set.empty )
-    def gatherOccs( et: ExpansionTree, weak: List[Expr], pos: List[Int] ): Unit =
+    def gatherOccs( et: ETt, weak: List[Expr], pos: List[Int] ): Unit =
       et match {
-        case ETTop( _ ) | ETBottom( _ ) =>
-        case ETWeakening( _, _ )        =>
-        case ETAtom( _, _ )             =>
-        case ETMerge( a, b ) =>
+        case ETtNullary | ETtWeakening | ETtAtom =>
+        case ETtMerge( a, b ) =>
           gatherOccs( a, weak, 1 :: pos )
           gatherOccs( b, weak, 2 :: pos )
-        case ETNeg( a ) => gatherOccs( a, weak, 1 :: pos )
-        case BinaryExpansionTree( child1, child2 ) =>
+        case ETtUnary( a ) => gatherOccs( a, weak, 1 :: pos )
+        case ETtBinary( child1, child2 ) =>
           gatherOccs( child1, weak, 1 :: pos )
           gatherOccs( child2, weak, 2 :: pos )
-        case ETDefinition( _, ch ) =>
+        case ETtDef( _, ch ) =>
           gatherOccs( ch, weak, 1 :: pos )
-        case ETStrongQuantifier( _, _, ch ) =>
+        case ETtStrong( _, ch ) =>
           gatherOccs( ch, weak, 1 :: pos )
-        case ETWeakQuantifier( _, insts ) =>
+        case ETtWeak( insts ) =>
           for ( ( i, ch ) <- insts )
             gatherOccs( ch, i :: weak, 1 :: pos )
-        case ETSkolemQuantifier( _, skT, skD, ch ) =>
+        case ETtSkolem( skT, _, ch ) =>
           gatherOccs( ch, weak, 1 :: pos )
           occs( skT ) += ( ( weak, pos ) )
       }
 
-    for ( ( e, i ) <- es.elements.zip( Stream.from( 1 ) ) ) gatherOccs( e, Nil, i :: Nil )
+    for ( ( e, i ) <- es.elements.zip( Stream.from( 1 ) ) ) gatherOccs( e.term, Nil, i :: Nil )
 
     // Which Skolem terms occur more than once?
     occs.collect { case ( skT, os ) if os.size > 1 => skT }.toSet
@@ -76,9 +75,10 @@ object nonProofTheoreticSkolemTerms {
 object moveSkolemNodesToCuts {
 
   def apply( ep: ExpansionProof ): ExpansionProof = {
-    implicit val nameGen: NameGenerator = rename.awayFrom( ep.eigenVariables ++ freeVariablesET( ep ) )
     val bad = nonProofTheoreticSkolemTerms( ep )
-    val cuts = mutable.Buffer[( ExpansionTree, ExpansionTree )]()
+    if ( bad.isEmpty ) return ep
+    implicit val nameGen: NameGenerator = rename.awayFrom( ep.eigenVariables ++ freeVariablesET( ep ) )
+    val cuts = mutable.Buffer[ETCut.Cut]()
     def go( et: ExpansionTree ): ExpansionTree =
       et match {
         case ETWeakening( _, _ ) | ETBottom( _ ) | ETTop( _ ) | ETAtom( _, _ ) => et
