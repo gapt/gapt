fa2da90c3a54c2ab15f9a1627e7963b44031cff2
Jannik Vierling <jannik.vierling@gmail.com>
2019-05-15 15:39:49 +0200

    update year in CLI welcome message

--- cli/src/main/scala/CLIMain.scala
+++ cli/src/main/scala/CLIMain.scala
@@ -14,7 +14,7 @@ object CLIMain {
     *    Welcome to the GAPT shell!     *
     *************************************
 
- Copyright (C) 2009-2018  GAPT developers
+ Copyright (C) 2009-2019  GAPT developers
 
  This program comes with ABSOLUTELY NO WARRANTY. This is free
  software, and you are welcome to redistribute it under certain
