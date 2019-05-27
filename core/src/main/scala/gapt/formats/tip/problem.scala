56a9f15b450e20800d6db3665dbb4d9eb4b3621c
Jannik Vierling <jannik.vierling@gmail.com>
2019-05-27 10:38:37 +0200

    avoid generating invalid reduction rules

--- core/src/main/scala/gapt/formats/tip/problem.scala
+++ core/src/main/scala/gapt/formats/tip/problem.scala
@@ -92,16 +92,21 @@ case class TipProblem(
 
   private val functionDefinitionReductionRules: Seq[ConditionalReductionRule] = {
     functions.flatMap { _.definitions }.flatMap {
-      case All.Block( _, Imp.Block( cs, Eq( lhs @ Apps( _: Const, _ ), rhs ) ) ) =>
+      case All.Block( _, Imp.Block( cs, Eq( lhs @ Apps( _: Const, _ ), rhs ) ) ) if isReductionRule( cs, lhs, rhs ) =>
         Some( ConditionalReductionRule( cs, lhs, rhs ) )
-      case All.Block( _, Imp.Block( cs, Neg( lhs @ Atom( _, _ ) ) ) ) =>
+      case All.Block( _, Imp.Block( cs, Neg( lhs @ Atom( _, _ ) ) ) ) if isReductionRule( cs, lhs, Bottom() ) =>
         Some( ConditionalReductionRule( cs, lhs, Bottom() ) )
-      case All.Block( _, Imp.Block( cs, lhs @ Atom( _, _ ) ) ) =>
+      case All.Block( _, Imp.Block( cs, lhs @ Atom( _, _ ) ) ) if isReductionRule( cs, lhs, Top() ) =>
         Some( ConditionalReductionRule( cs, lhs, Top() ) )
-      case All.Block( _, Imp.Block( cs, Iff( lhs, rhs ) ) ) =>
+      case All.Block( _, Imp.Block( cs, Iff( lhs, rhs ) ) ) if isReductionRule( cs, lhs, rhs ) =>
         Some( ConditionalReductionRule( cs, lhs, rhs ) )
       case _ => None
     }
+  }
+
+  private def isReductionRule( cs: Seq[Formula], lhs: Expr, rhs: Expr ): Boolean = {
+    ( cs.flatMap { freeVariables( _ ) } ++ freeVariables( rhs ) ).toSet.subsetOf( freeVariables( lhs ) ) &&
+      !lhs.isInstanceOf[Var]
   }
 
   override def toString: String = toSequent.toSigRelativeString( context )
