c484c13fca75cd281d229da6b2b708fb0262ca0a
Andreas H. From <andro.from@gmail.com>
2019-06-09 15:14:41 +0200

    count atomic and quantified induction targets

--- testing/src/main/scala/viper.scala
+++ testing/src/main/scala/viper.scala
@@ -1,5 +1,8 @@
 package gapt.testing
 
+import gapt.expr.Expr
+import gapt.expr.formula.hol.{ containsQuantifier, isExtendedAtom }
+import gapt.expr.formula.{ All, Formula, Imp }
 import gapt.utils._
 
 import scala.concurrent.duration._
@@ -9,6 +12,7 @@ import gapt.proofs.expansion.ExpansionProofToLK
 import gapt.proofs.lk.LKProof
 import gapt.proofs.lk.rules.InductionRule
 import gapt.proofs.lk.transformations.{ LKToExpansionProof, cleanStructuralRules }
+import gapt.proofs.lk.util.extractInductionAxioms
 import gapt.provers.viper.aip.axioms.{ IndependentInductionAxioms, SequentialInductionAxioms }
 import gapt.provers.viper.spin.SpinOptions
 import gapt.provers.viper.{ AipOptions, Viper, ViperOptions }
@@ -42,6 +46,17 @@ object testViper extends App {
     val expPrf = LKToExpansionProof( prf1 )( ctx )
     ExpansionProofToLK( expPrf )( ctx ).toOption.get
   }
+
+  def removeOuterAlls( e: Expr ): Expr =
+    e match {
+      case All( _, e1 ) => removeOuterAlls( e1 )
+      case _            => e
+    }
+
+  def inductionTarget( e: Expr ): Expr =
+    removeOuterAlls( e ) match {
+      case Imp( _, e1 ) => removeOuterAlls( e1 )
+    }
 
   val logger = Logger( "testViper" )
 
@@ -97,11 +112,20 @@ object testViper extends App {
     prf match {
       case None =>
       case Some( prf1 ) =>
-        val prf2 = cleanProof( prf1 )( problem.context )
+        val prf2 = cleanProof( prf1 )( problem.ctx )
         val ( inds, depth ) = countInductions( prf2 )
 
         logger.metric( "inductions", inds )
         logger.metric( "induction_depth", depth )
+
+        val axs = extractInductionAxioms( prf2 )( problem.ctx )
+        val targets = axs.map( inductionTarget )
+
+        val atomic = targets.count( e => isExtendedAtom( e.asInstanceOf[Formula] ) )
+        val quantified = targets.count( e => containsQuantifier( e.asInstanceOf[Formula] ) )
+
+        logger.metric( "atomic", atomic )
+        logger.metric( "quantified", quantified )
     }
 
   }
