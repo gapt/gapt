5f268b03616eec831858a8e2701ac74916cc2c1a
Jannik Vierling <jannik.vierling@gmail.com>
2019-10-26 10:22:29 +0200

    add pattern based name generation

---
+++ tests/src/test/scala/gapt/utils/NameGeneratorTest.scala
@@ -0,0 +1,24 @@
+package gapt.utils
+
+import org.specs2.mutable.Specification
+
+class NameGeneratorTest extends Specification {
+
+  "generated names based on pattern" in {
+    "should conform to pattern" in {
+      val pattern = ( n: Int ) => s"name_${n}"
+      ( new NameGenerator( Nil ) ).freshWithIndex( pattern ) mustEqual pattern( 0 )
+    }
+
+    "should be fresh" in {
+      val pattern = ( n: Int ) => s"name_${n}"
+      val generator = new NameGenerator( List( "name_0" ) )
+      generator.freshWithIndex( pattern ) mustEqual pattern( 1 )
+    }
+
+    "should be minimal" in {
+      val pattern = ( n: Int ) => s"name_${n}"
+      ( new NameGenerator( Nil ) ).freshWithIndex( pattern ) mustEqual pattern( 0 )
+    }
+  }
+}
