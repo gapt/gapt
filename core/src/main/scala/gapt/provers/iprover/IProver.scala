404607e33b3d6d3c509ec5ec645306cdafcf719a
Jannik Vierling <jannik.vierling@gmail.com>
2019-11-03 19:46:11 +0100

    refactor iProver interface

--- core/src/main/scala/gapt/provers/iprover/IProver.scala
+++ core/src/main/scala/gapt/provers/iprover/IProver.scala
@@ -15,36 +15,84 @@ import gapt.utils.{ ExternalProgram, Maybe, runProcess }
 object IProver extends IProver
 
 class IProver extends ResolutionProver with ExternalProgram {
+
+  val iproverOptions = Seq(
+    "iproveropt",
+    "--schedule", "none",
+    "--pure_diseq_elim", "false",
+    "--splitting_mode", "none",
+    "--sub_typing", "false",
+    "--tptp_safe_out", "true" )
+
   override def getResolutionProof( seq: Iterable[HOLClause] )( implicit ctx: Maybe[MutableContext] ): Option[ResolutionProof] =
     renameConstantsToFi.wrap( seq.toSeq )(
-      ( renaming, cnf: Seq[HOLClause] ) => {
-        val labelledCNF = cnf.zipWithIndex.map { case ( clause, index ) => s"formula$index" -> clause.asInstanceOf[FOLClause] }.toMap
-        val tptpIn = TptpFOLExporter.exportLabelledCNF( labelledCNF ).toString
-        val output = runProcess.withTempInputFile( Seq(
-          "iproveropt",
-          "--schedule", "none",
-          "--pure_diseq_elim", "false",
-          "--splitting_mode", "none",
-          "--sub_typing", "false",
-          "--tptp_safe_out", "true" ), tptpIn )
-        val lines = output.split( "\n" ).toSeq
-        if ( lines.exists( _.startsWith( "% SZS status Unsatisfiable" ) ) ) {
-          val tptpDerivation = lines.
-            dropWhile( !_.startsWith( "% SZS output start CNFRefutation" ) ).drop( 1 ).
-            takeWhile( !_.startsWith( "% SZS output end CNFRefutation" ) ).
-            mkString( "\n" )
-          RefutationSketchToResolution( TptpProofParser.parse(
-            StringInputFile( tptpDerivation ),
-            labelledCNF.view.mapValues { Seq( _ ) }.toMap ) ) match {
-            case Right( proof ) => Some( proof )
-            case Left( error )  => throw new IllegalArgumentException( error.toString )
-          }
-        } else if ( lines.exists( _.startsWith( "% SZS status Satisfiable" ) ) ) {
-          None
-        } else {
-          throw new IllegalArgumentException
-        }
-      } )
+      ( _, cnf: Seq[HOLClause] ) =>
+        new IProverInvoker( cnf.map { _.asInstanceOf[FOLClause] } )
+          .getResolutionProof() )
+
+  private class IProverInvoker( val inputCNF: Seq[FOLClause] ) {
+
+    def getResolutionProof(): Option[ResolutionProof] =
+      parseProverOutput( proverOutput )
+
+    private val inputClauseLabels: Labels =
+      Labels( inputCNF )
+
+    private val tptpInput: String =
+      convertInputCnfToTptp
+
+    private def proverOutput: String =
+      runIProverOnTptpInput()
+
+    private def runIProverOnTptpInput(): String =
+      runProcess.withTempInputFile( iproverOptions, tptpInput )
+
+    private def convertInputCnfToTptp: String =
+      TptpFOLExporter.exportLabelledCNF( inputClauseLabels.toMap ).toString
+
+    private def parseProverOutput( proverOutput: String ): Option[ResolutionProof] =
+      tryExtractTptpDerivation( proverOutput ).map { parseResolutionProof }
+
+    private def parseResolutionProof( tptpDerivation: String ): ResolutionProof = {
+      RefutationSketchToResolution( TptpProofParser.parse(
+        StringInputFile( tptpDerivation ),
+        inputClauseLabels.toMap.view.mapValues { Seq( _ ) }.toMap ) ) match {
+        case Right( proof ) => proof
+        case Left( error )  => throw new IllegalArgumentException( error.toString )
+      }
+    }
+
+    private def tryExtractTptpDerivation( proverOutput: String ): Option[String] = {
+      val lines = proverOutput.split( "\n" ).toSeq
+      if ( lines.exists( _.startsWith( "% SZS status Unsatisfiable" ) ) ) {
+        val derivation = lines
+          .dropWhile( !_.startsWith( "% SZS output start CNFRefutation" ) )
+          .drop( 1 ).
+          takeWhile( !_.startsWith( "% SZS output end CNFRefutation" ) ).
+          mkString( "\n" )
+        Some( derivation )
+      } else if ( lines.exists( _.startsWith( "% SZS status Satisfiable" ) ) ) {
+        None
+      } else {
+        throw new IllegalArgumentException
+      }
+    }
+  }
+
+  private case class Labels( cnf: Seq[FOLClause] ) {
+
+    def toMap: Map[String, FOLClause] =
+      labelledCNF
+
+    private val labelledCNF: Map[String, FOLClause] =
+      labelCNF( cnf )
+
+    private def labelCNF( cnf: Seq[FOLClause] ): Map[String, FOLClause] =
+      cnf.zipWithIndex.map {
+        case ( clause, index ) =>
+          s"formula$index" -> clause.asInstanceOf[FOLClause]
+      }.toMap
+  }
 
   override val isInstalled: Boolean =
     try {
