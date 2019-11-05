27eb58fdb45b1f82f6bc62b7aa1c8285a8d09a8a
Jannik Vierling <jannik.vierling@gmail.com>
2019-11-05 20:54:45 +0100

    flatten class structure

--- core/src/main/scala/gapt/provers/iprover/IProver.scala
+++ core/src/main/scala/gapt/provers/iprover/IProver.scala
@@ -12,11 +12,23 @@ import gapt.proofs.{ FOLClause, HOLClause }
 import gapt.provers.{ ResolutionProver, renameConstantsToFi }
 import gapt.utils.{ ExternalProgram, Maybe, runProcess }
 
-object IProver extends IProver
+object IProver extends ResolutionProver with ExternalProgram {
 
-class IProver extends ResolutionProver with ExternalProgram {
+  override def getResolutionProof( seq: Iterable[HOLClause] )( implicit ctx: Maybe[MutableContext] ): Option[ResolutionProof] =
+    new IProverInstance( new IProverInput( seq.toSeq.map { _.asInstanceOf[FOLClause] } ) ).getResolutionProof()
 
-  val iproverOptions = Seq(
+  override val isInstalled: Boolean =
+    try {
+      runProcess.withExitValue( Seq( "iproveropt" ) )._1 == 2
+      true
+    } catch {
+      case _: IOException => false
+    }
+}
+
+class IProverInstance( val input: IProverInput ) {
+
+  private val iproverOptions = Seq(
     "iproveropt",
     "--schedule", "none",
     "--pure_diseq_elim", "false",
@@ -24,116 +36,101 @@ class IProver extends ResolutionProver with ExternalProgram {
     "--sub_typing", "false",
     "--tptp_safe_out", "true" )
 
-  override def getResolutionProof( seq: Iterable[HOLClause] )( implicit ctx: Maybe[MutableContext] ): Option[ResolutionProof] = {
-    val input = new IProverInput( seq.toSeq.map { _.asInstanceOf[FOLClause] } )
-    new IProverInvoker( input ).getResolutionProof()
+  def getResolutionProof(): Option[ResolutionProof] =
+    runIProverOn( input.tptpWithSafeNames )
+      .tptpDerivation
+      .map { parseResolutionProof }
+      .map { revertToOriginalNames }
+
+  private def revertToOriginalNames(
+    refutation: ResolutionProof ): ResolutionProof = {
+    val originalNames: Map[Const, Const] = input.originalNamesToSafeNames.map( _.swap )
+    TermReplacement.hygienic( refutation, originalNames )
   }
 
-  private class IProverInvoker( val input: IProverInput ) {
+  private def parseResolutionProof( tptpDerivation: String ): ResolutionProof =
+    new TptpDerivationParser( input.clauseLabels )
+      .parseAsResolutionProof( tptpDerivation )
 
-    def getResolutionProof(): Option[ResolutionProof] =
-      runIProverOn( input.tptpWithSafeNames )
-        .tptpDerivation
-        .map { parseResolutionProof }
-        .map { revertToOriginalNames }
+  private def runIProverOn( input: String ): IProverOutput =
+    new IProverOutput( iProver( input ) )
 
-    private def revertToOriginalNames(
-      refutation: ResolutionProof ): ResolutionProof = {
-      val originalNames: Map[Const, Const] = input.originalNamesToSafeNames.map( _.swap )
-      TermReplacement.hygienic( refutation, originalNames )
-    }
+  private def iProver( input: String ): String =
+    runProcess.withTempInputFile( iproverOptions, input )
 
-    private def parseResolutionProof( tptpDerivation: String ): ResolutionProof =
-      new TptpDerivationParser( input.clauseLabels )
-        .parseAsResolutionProof( tptpDerivation )
-
-    private def runIProverOn( input: String ): IProverOutput =
-      new IProverOutput( iProver( input ) )
-
-    private def iProver( input: String ): String =
-      runProcess.withTempInputFile( iproverOptions, input )
-
-  }
-
-  class IProverInput( val cnf: Seq[FOLClause] ) {
-
-    private type Renaming = Map[Const, Const]
-
-    val ( cnfWithSafeNames, originalNamesToSafeNames ) = useSafeNames( cnf )
-
-    val clauseLabels: Labels = Labels( cnfWithSafeNames )
-
-    val tptpWithSafeNames: String =
-      TptpFOLExporter.exportLabelledCNF( clauseLabels.toMap ).toString
-
-    private def useSafeNames( cnf: Seq[FOLClause] ): ( Seq[FOLClause], Renaming ) = {
-      val ( cnfWithSafeNames, safeNames ) =
-        renameConstantsToFi[Seq[HOLClause], Seq[HOLClause]]( cnf )
-      ( cnfWithSafeNames.map { _.asInstanceOf[FOLClause] }, safeNames )
-    }
-
-  }
-
-  class IProverOutput( val rawOutput: String ) {
-
-    private val outputLines: Array[String] =
-      rawOutput.split( "\n" )
-
-    val tptpDerivation: Option[String] =
-      if ( statusIsUnsatisfiable )
-        Some( extractTptpDerivation )
-      else if ( isStatusSatisfiable )
-        None
-      else
-        throw new IllegalArgumentException( "invalid prover output" )
-
-    private def isStatusSatisfiable: Boolean =
-      outputLines.exists( _.startsWith( "% SZS status Satisfiable" ) )
-
-    private def statusIsUnsatisfiable: Boolean =
-      outputLines.exists( _.startsWith( "% SZS status Unsatisfiable" ) )
-
-    private def extractTptpDerivation: String = {
-      outputLines
-        .dropWhile( !_.startsWith( "% SZS output start CNFRefutation" ) )
-        .drop( 1 ).
-        takeWhile( !_.startsWith( "% SZS output end CNFRefutation" ) ).
-        mkString( "\n" )
-    }
-  }
-
-  class TptpDerivationParser( val labels: Labels ) {
-
-    def parseAsResolutionProof( tptpDerivation: String ): ResolutionProof = {
-      RefutationSketchToResolution( TptpProofParser.parse(
-        StringInputFile( tptpDerivation ),
-        labels.toMap.view.mapValues { Seq( _ ) }.toMap ) ) match {
-        case Right( proof ) => proof
-        case Left( error )  => throw new IllegalArgumentException( error.toString )
-      }
-    }
-  }
-
-  case class Labels( cnf: Seq[FOLClause] ) {
-
-    def toMap: Map[String, FOLClause] =
-      labelledCNF
-
-    private val labelledCNF: Map[String, FOLClause] =
-      labelCNF( cnf )
-
-    private def labelCNF( cnf: Seq[FOLClause] ): Map[String, FOLClause] =
-      cnf.zipWithIndex.map {
-        case ( clause, index ) =>
-          s"formula$index" -> clause.asInstanceOf[FOLClause]
-      }.toMap
-  }
-
-  override val isInstalled: Boolean =
-    try {
-      runProcess.withExitValue( Seq( "iproveropt" ) )._1 == 2
-      true
-    } catch {
-      case ex: IOException => false
-    }
 }
+
+class IProverInput( val cnf: Seq[FOLClause] ) {
+
+  private type Renaming = Map[Const, Const]
+
+  val ( cnfWithSafeNames, originalNamesToSafeNames ) = useSafeNames( cnf )
+
+  val clauseLabels: Labels = Labels( cnfWithSafeNames )
+
+  val tptpWithSafeNames: String =
+    TptpFOLExporter.exportLabelledCNF( clauseLabels.toMap ).toString
+
+  private def useSafeNames( cnf: Seq[FOLClause] ): ( Seq[FOLClause], Renaming ) = {
+    val ( cnfWithSafeNames, safeNames ) =
+      renameConstantsToFi[Seq[HOLClause], Seq[HOLClause]]( cnf )
+    ( cnfWithSafeNames.map { _.asInstanceOf[FOLClause] }, safeNames )
+  }
+
+}
+
+class IProverOutput( val rawOutput: String ) {
+
+  private val outputLines: Array[String] =
+    rawOutput.split( "\n" )
+
+  val tptpDerivation: Option[String] =
+    if ( statusIsUnsatisfiable )
+      Some( extractTptpDerivation )
+    else if ( isStatusSatisfiable )
+      None
+    else
+      throw new IllegalArgumentException( "invalid prover output" )
+
+  private def isStatusSatisfiable: Boolean =
+    outputLines.exists( _.startsWith( "% SZS status Satisfiable" ) )
+
+  private def statusIsUnsatisfiable: Boolean =
+    outputLines.exists( _.startsWith( "% SZS status Unsatisfiable" ) )
+
+  private def extractTptpDerivation: String = {
+    outputLines
+      .dropWhile( !_.startsWith( "% SZS output start CNFRefutation" ) )
+      .drop( 1 ).
+      takeWhile( !_.startsWith( "% SZS output end CNFRefutation" ) ).
+      mkString( "\n" )
+  }
+}
+
+class TptpDerivationParser( val labels: Labels ) {
+
+  def parseAsResolutionProof( tptpDerivation: String ): ResolutionProof = {
+    RefutationSketchToResolution( TptpProofParser.parse(
+      StringInputFile( tptpDerivation ),
+      labels.toMap.view.mapValues { Seq( _ ) }.toMap ) ) match {
+      case Right( proof ) => proof
+      case Left( error )  => throw new IllegalArgumentException( error.toString )
+    }
+  }
+}
+
+case class Labels( cnf: Seq[FOLClause] ) {
+
+  def toMap: Map[String, FOLClause] =
+    labelledCNF
+
+  private val labelledCNF: Map[String, FOLClause] =
+    labelCNF( cnf )
+
+  private def labelCNF( cnf: Seq[FOLClause] ): Map[String, FOLClause] =
+    cnf.zipWithIndex.map {
+      case ( clause, index ) =>
+        s"formula$index" -> clause.asInstanceOf[FOLClause]
+    }.toMap
+}
+
