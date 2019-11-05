7f3bd83fb954d591cbf7df3fa5ade360c0895e6c
Jannik Vierling <jannik.vierling@gmail.com>
2019-11-05 21:58:13 +0100

    clean up HOL to FOL tests

--- tests/src/test/scala/gapt/expr/formula/fol/hol2folTest.scala
+++ tests/src/test/scala/gapt/expr/formula/fol/hol2folTest.scala
@@ -24,38 +24,24 @@ import gapt.utils.Counter
 import org.specs2.mutable._
 
 class hol2folTest extends Specification {
-  def imap = Map[Expr, String]() // the scope for most tests is just the term itself
-  def iid = new Counter
-
   "HOL terms" should {
-    val hx = Var( "x", Ti ->: Ti )
-    val ha = Const( "a", To ->: Ti )
-    val hb = Const( "b", To ->: Ti )
-    val fx = FOLVar( "x" )
-    val fa = FOLConst( "a" )
-    val fb = FOLConst( "b" )
-    //TODO: fix the tests
     "be correctly reduced into FOL terms for" in {
       "Atom - A(x:(i->i), a:o->i)" in {
         implicit val d = new Hol2FolDefinitions
-        val hol = Atom( Const( "A", ( Ti ->: Ti ) ->: ( To ->: Ti ) ->: To ), hx :: ha :: Nil )
-        val fol = FOLAtom( "A", fx :: fa :: Nil )
+        val hol = hoa"A( #v(x: i>i), #c(a:o>i) )"
+        val fol = foa"A( #v(x:i), #c(a:i) )"
         reduceHolToFol( hol ) must beEqualTo( fol )
       }
       "Function - f(x:(i->i), a:(o->i)):(o->o)" in {
         implicit val d = new Hol2FolDefinitions
-        val hol = HOLFunction( Const( "f", ( Ti ->: Ti ) ->: ( ( To ->: Ti ) ->: ( To ->: To ) ) ), hx :: ha :: Nil )
-        val fol = FOLFunction( "f", fx :: fa :: Nil )
+        val hol = le"f( #v(x:i>i), #c(a:o>i))"
+        val fol = fot"f(x, a)"
         reduceHolToFol( hol ) must beEqualTo( fol )
       }
       "Connective - And A(x:(i->i), a:(o->i)) B(x:(i->i), b:(o->i))" in {
         implicit val d = new Hol2FolDefinitions
-        val hA = Atom( Const( "A", ( Ti ->: Ti ) ->: ( ( To ->: Ti ) ->: To ) ), hx :: ha :: Nil )
-        val hB = Atom( Const( "B", ( Ti ->: Ti ) ->: ( ( To ->: Ti ) ->: To ) ), hx :: hb :: Nil )
-        val hol = And( hA, hB )
-        val fA = FOLAtom( "A", fx :: fa :: Nil )
-        val fB = formula.fol.FOLAtom( "B", fx :: fb :: Nil )
-        val fol = And( fA, fB )
+        val hol = hof"A( #v(x:i>i), #c(a:o>i) ) & B( #v(x:i>i), #c(b:o>i) )"
+        val fol = fof"A( #v(x:i), #c(a:i) ) & B( #v(x:i), #c(b:i) )"
         reduceHolToFol( hol ) must beEqualTo( fol )
       }
       "Abstraction - f(Abs x:(i->i) A(x:(i->i), a:(o->i))):(o->o)" in {
@@ -70,30 +56,16 @@ class hol2folTest extends Specification {
         val fol = fot"f('q_{1}'(y))"
         red must beEqualTo( fol )
       }
-
-      //TODO: check if this test case is really what we want
-      "Two terms - f(Abs x:(i->i) A(x:(i->i), y:(o->i))):(o->o) and g(Abs x:(i->i) A(x:(i->i), z:(o->i))):(o->o)" in {
+      "Matching abstractions should reuse definition" in {
         implicit val d = new Hol2FolDefinitions
-        val ( f1 ) = reduceHolToFol( le"f(λ(x:i>i) A(x, y:o>i)):o>o" )
-        val ( f2 ) = reduceHolToFol( le"g(λ(x:i>i) A(x, z:o>i)):o>o" )
-
-        List( f1, f2 ) must beEqualTo(
-          List( fot"f('q_{1}'(y))", fot"g('q_{1}'(z))" ) )
+        reduceHolToFol( le"f(λ(x:i>i) A(x, y:o>i)):o>o" ) mustEqual fot"f('q_{1}'(y))"
+        reduceHolToFol( le"g(λ(x:i>i) A(x, z:o>i)):o>o" ) mustEqual fot"g('q_{1}'(z))"
       }
-
       "Correctly convert from type o to i on the termlevel" in {
         implicit val d = new Hol2FolDefinitions
-        val List( sp, sq ) = List( "P", "Q" )
-        val List( x, y ) = List( "x", "y" ).map( x => Atom( Var( x, To ), List() ) )
-        val f1 = Atom( Const( sp, To ->: To ), List( Imp( x, y ) ) )
-        val f2 = FOLAtom( sp, List( FOLFunction(
-          ImpC.name,
-          List(
-            FOLVar( "x" ),
-            FOLVar( "y" ) ) ) ) )
-
-        val red = reduceHolToFol( f1 )
-        red must beEqualTo( f2 )
+        val holAtom = hoa"P( #v(x:o) → #v(y:o) )"
+        val folAtom = foa"P(#c(→: i>i>i) #v(x:i)  #v(y:i) )"
+        reduceHolToFol( holAtom ) must beEqualTo( folAtom )
       }
     }
   }
