b165423717b9e1e959be6cc0c9e74e01d797e017
Jannik Vierling <jannik.vierling@gmail.com>
2019-12-10 20:32:18 +0100

    add forgotten file

---
+++ core/src/main/scala/gapt/formats/tip/compiler/package.scala
@@ -0,0 +1,18 @@
+package gapt.formats.tip
+
+import gapt.expr.ty.TBase
+import gapt.formats.tip.parser.TipSmtDatatype
+import gapt.proofs.context.update.InductiveType
+
+package object compiler {
+  def tipSmtDatatypeToInductiveType( datatype: TipSmtDatatype ): InductiveType = {
+    val constructorDefinitions = datatype.constructors.map {
+      c =>
+        c.name -> c.fields.map {
+          f =>
+            Some( f.name ) -> TBase( f.typ.typename )
+        }
+    }
+    InductiveType( datatype.name, Nil, constructorDefinitions: _* )
+  }
+}
