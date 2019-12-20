1bfd69fe264a99bba10a07fe08afb0ae95e5b823
fachammer <fabian.achammer@gmail.com>
2019-12-20 16:56:26 +0100

    make unit tests check for substitution equivalence
    instead of checking witnesses for equivalence it is better to check
    for equivalence of the first order formulas after substitution.
    as solutions to formula equations don't have to be unique this makes
    more sense and should also fix test reproduction problems on Travis CI

--- tests/src/test/scala/gapt/logic/hol/SolveFormulaEquationTest.scala
+++ tests/src/test/scala/gapt/logic/hol/SolveFormulaEquationTest.scala
@@ -12,23 +12,29 @@ import gapt.expr._
 import scala.util.Success
 import gapt.provers.sat._
 import org.specs2.matcher.Matcher
+import gapt.expr.subst.Substitution
+import gapt.expr.formula.hol.instantiate
+import org.specs2.specification.core.Fragment
 
 class SolveFormulaEquationTest extends Specification {
 
-  private def beLogicallyEquivalentTo( thatFormula: Formula ): Matcher[Formula] =
-    ( Sat4j isValid Iff( _: Formula, thatFormula ), s"is not logically equivalent to '$thatFormula'" )
+  private def beAnEquivalentSubstitutionFor( formula: Formula, equivalentSubstitution: Substitution ): Matcher[Substitution] = { substitution: Substitution =>
+    val firstOrderPart = solveFormulaEquation.extractFirstOrderPart( formula )
+    val substitutedFormula = BetaReduction.betaNormalize( substitution( firstOrderPart ) )
+    val equivalentSubstitutedFormula = BetaReduction.betaNormalize( equivalentSubstitution( firstOrderPart ) )
+    val isValid = Sat4j isValid Iff( substitutedFormula, equivalentSubstitutedFormula )
+    ( isValid, s"$substitution is not an equivalent substitution for $formula" )
+  }
+
+  private def succeedFor( formulaEquation: Formula, expectedEquivalentSubstitution: Substitution ): Fragment = {
+    s"succeed for $formulaEquation" >> {
+      solveFormulaEquation( formulaEquation ) must
+        beAnEquivalentSubstitutionFor( formulaEquation, expectedEquivalentSubstitution )
+    }
+  }
 
   "solveFormulaEquation" should {
-    "succeed for ?X X(a)" in {
-      solveFormulaEquation( hof"?X X(a)" ).map.get( Var( "X", Ti ->: To ) ) must beSome.which {
-        case Abs( FOLVar( "x" ), witness ) => witness.asInstanceOf[Formula] must beLogicallyEquivalentTo( fof"x=a" )
-      }
-    }
-
-    "succeed for ?X ((X(a) & -X(f(b))) | (X(f(b)) & -X(a)))" in {
-      solveFormulaEquation( hof"?X ((X(a) & -X(f(b))) | (X(f(b)) & -X(a)))" ).map.get( Var( "X", Ti ->: To ) ) must beSome.which {
-        case Abs( FOLVar( "x" ), witness ) => witness.asInstanceOf[Formula] must beLogicallyEquivalentTo( fof"(-f(b)=a -> x=a) & ((-(-f(b)=a)) & -a=f(b) -> x=f(b))" )
-      }
-    }
+    succeedFor( hof"?X X(a)", Substitution( Map( Var( "X", Ti ->: To ) -> le"^x x=a" ) ) )
+    succeedFor( hof"?X ((X(a) & -X(f(b))) | (X(f(b)) & -X(a)))", Substitution( Map( Var( "X", Ti ->: To ) -> le"^x (-f(b)=a -> x=a) & ((-(-f(b)=a)) & -a=f(b) -> x=f(b))" ) ) )
   }
 }
