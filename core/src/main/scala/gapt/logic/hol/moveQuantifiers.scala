bd551ae3f0cf473eb42324d5b61f8af6037a0e17
fachammer <fabian.achammer@gmail.com>
2020-01-16 22:52:02 +0100

    refactor moveQuantifiers and add unit tests
    - also preserve position of terms in moveQuantifiers not only logical equivalence
    - improve code readability
    - add documentation
    - handle negation in formula

--- core/src/main/scala/gapt/logic/hol/moveQuantifiers.scala
+++ core/src/main/scala/gapt/logic/hol/moveQuantifiers.scala
@@ -6,19 +6,30 @@ import gapt.expr.subst.Substitution
 import gapt.expr.util.{ freeVariables, rename, variables }
 
 object moveQuantifiers {
+
+  /**
+   * Moves all quantifiers of the given type (increases the scope) as far as possible by applying
+   * the following equivalences in the right to left direction:
+   *
+   * - ∃z (α[z/x] ∨ β[z/y]) <-> (∃x α) ∨ (∃y β) where z neither occurs in α nor in β,
+   * - ∀z (α[z/x] ∧ β[z/y]) <-> (∀x α) ∧ (∀y β) where z neither occurs in α nor in β,
+   * - Qx (α C β) <-> (Qx α) C β where Q is a quantifier, C ∈ {∧,∨} and x does not occur in β,
+   * - Qx (β C α) <-> β C (Qx α) where Q is a quantifier, C ∈ {∧,∨} and x does not occur in β.
+   *
+   * @param quantifier the type of quantifier to move up
+   * @param formula formula in which to move the quantifier up
+   * @return result of applying the above equivalences in the right to left equivalences
+   *         as often as possible to the input formula
+   */
   def up( quantifier: QuantifierHelper, formula: Formula ): Formula = formula match {
     case AndOr( quantifier( variableAlpha, alpha ), quantifier( variableBeta, beta ), connective ) if quantifier isCompatibleWith connective =>
-      fuseQuantifierOverConnective( quantifier, connective, List(
-        variableAlpha -> alpha,
-        variableBeta -> beta ) )
-
-    case AndOr( alpha, quantifier( variable, beta ), connective ) =>
-      fuseQuantifierOverConnective( quantifier, connective, List(
-        variable -> alpha,
-        variable -> beta ) )
+      fuseQuantifierOverConnective( quantifier, connective, List( variableAlpha -> alpha, variableBeta -> beta ) )
 
     case AndOr( quantifier( variable, beta ), alpha, connective ) =>
-      up( quantifier, connective( alpha, quantifier( variable, beta ) ) )
+      fuseQuantifierOverConnective( quantifier, connective, List( variable -> beta, variable -> alpha ) )
+
+    case AndOr( alpha, quantifier( variable, beta ), connective ) =>
+      fuseQuantifierOverConnective( quantifier, connective, List( variable -> alpha, variable -> beta ) )
 
     case Quant( variable, alpha, pol ) =>
       Quant( variable, up( quantifier, alpha ), pol )
@@ -31,9 +42,27 @@ object moveQuantifiers {
       else
         formula
 
-    case _ => formula
+    case Neg( innerFormula ) => Neg( up( quantifier, innerFormula ) )
+
+    case _                   => formula
   }
 
+  /**
+   * Moves all quantifiers of the given type down (decreases the scope) as far as possible by applying
+   * the following equivalences in the left to right direction:
+   *
+   * - ∃x (α ∨ β) <-> (∃x α) ∨ (∃x β),
+   * - ∀x (α ∧ β) <-> (∀x α) ∧ (∀x β),
+   * - Qx (α C β) <-> (Qx α) C β where Q is a quantifier, C ∈ {∧, ∨} and x does not occur in β,
+   * - Qx (β C α) <-> β C (Qx α) where Q is a quantifier, C ∈ {∧, ∨} and x does not occur in β.
+   *
+   * Moving stops when function encounters
+   *
+   * @param quantifier the type of quantifier to move down
+   * @param formula formula in which to move the quantifier down
+   * @return result of applying the above equivalences in the left to right direction
+   *         as often as possible to the input formula
+   */
   def down( quantifier: QuantifierHelper, formula: Formula ): Formula = formula match {
     case quantifier( variable, AndOr( alpha, beta, connective ) ) if quantifier isCompatibleWith connective =>
       connective(
@@ -46,7 +75,9 @@ object moveQuantifiers {
         down( quantifier, quantifier( variable, beta ) ) )
 
     case quantifier( variable, AndOr( beta, alpha, connective ) ) if variable isNotFreeIn alpha =>
-      down( quantifier, quantifier( variable, connective( alpha, beta ) ) )
+      connective(
+        down( quantifier, quantifier( variable, beta ) ),
+        down( quantifier, alpha ) )
 
     case Quant( variable, alpha, pol ) =>
       Quant( variable, down( quantifier, alpha ), pol )
@@ -56,7 +87,9 @@ object moveQuantifiers {
         down( quantifier, alpha ),
         down( quantifier, beta ) )
 
-    case _ => formula
+    case Neg( innerFormula ) => Neg( down( quantifier, innerFormula ) )
+
+    case _                   => formula
   }
 
   private implicit class QuantifierConnectiveHelper( quantifierHelper: QuantifierHelper ) {
---
+++ tests/src/test/scala/gapt/logic/hol/MoveQuantifiersTest.scala
@@ -0,0 +1,70 @@
+package gapt.logic.hol
+
+import gapt.expr._
+import gapt.expr.formula.constants.QuantifierC
+import gapt.expr.formula.{ All, Ex, Formula, Or, QuantifierHelper }
+import org.specs2.mutable.Specification
+import org.specs2.specification.core.Fragment
+
+class MoveQuantifiersTest extends Specification {
+  def testWithMessage(
+    quantifier:      QuantifierHelper,
+    formula:         Formula,
+    expectedFormula: Formula,
+    testFunction:    ( QuantifierHelper, Formula ) => Formula,
+    message:         ( QuantifierHelper, Formula, Formula ) => String ): Fragment = {
+    message( quantifier, formula, expectedFormula ) in {
+      testFunction( quantifier, formula ) must beEqualTo( expectedFormula )
+    }
+  }
+
+  "moveQuantifiers.up" should {
+    def succeedFor = testWithMessage( _, _, _, moveQuantifiers.up, {
+      case ( quantifier, formula, expectedFormula ) => s"move '$quantifier' up in $formula to get $expectedFormula"
+    } )
+
+    def notMove( quantifier: QuantifierHelper, formula: Formula ) =
+      testWithMessage( quantifier, formula, formula, moveQuantifiers.up, {
+        case ( quantifier, formula, _ ) => s"not move '$quantifier' up in $formula"
+      } )
+
+    notMove( Ex, hof"R(a)" )
+    succeedFor( All, hof"!x x=a & !x x=b", hof"!x (x=a & x=b)" )
+    succeedFor( Ex, hof"?x x=a | ?x x=b", hof"?x (x=a | x=b)" )
+    succeedFor( All, hof"(!x x=a) & R(b)", hof"!x (x=a & R(b))" )
+    succeedFor( All, hof"(!x R(x)) | R(b)", hof"!x (R(x) | R(b))" )
+    succeedFor( All, hof"?x (R(x, b) & !y R(x, y))", hof"?x !y (R(x, b) & R(x, y))" )
+    succeedFor( Ex, hof"?x !u ((?w !y R(y, w)) & ?z S(z, x))", hof"?x !u ?w ?z ((!y R(y, w)) & S(z, x))" )
+    succeedFor( Ex, hof"(?x ((?y R(y)) & R(x))) | (?x S(x))", hof"?x ?y ((R(y) & R(x)) | S(x))" )
+    succeedFor( All, hof"-((!x R(x)) & (!x S(x)))", hof"-(!x (R(x) & S(x)))" )
+
+    val q = new QuantifierHelper( new QuantifierC( "q" ) )
+    notMove( q, q( fov"x", hof"R(x) | S(x)" ) )
+  }
+
+  "moveQuantifiers.down" should {
+    def succeedFor( quantifier: QuantifierHelper, formula: Formula, expectedFormula: Formula ): Fragment =
+      testWithMessage( quantifier, formula, expectedFormula, moveQuantifiers.down, {
+        case ( quantifier, formula, expectedFormula ) => s"move '${quantifier.q.name}' down in $formula to get $expectedFormula"
+      } )
+
+    def notMove( quantifier: QuantifierHelper, formula: Formula ): Fragment =
+      testWithMessage( quantifier, formula, formula, moveQuantifiers.down, {
+        case ( quantifier, formula, _ ) => s"not move '${quantifier.q.name}' down in $formula"
+      } )
+
+    notMove( All, hof"R(a)" )
+    notMove( Ex, hof"R(a) & R(b)" )
+    notMove( All, hof"?x R(x)" )
+    succeedFor( Ex, hof"?x (R(x) & R(b))", hof"(?x R(x)) & R(b)" )
+    succeedFor( All, hof"!x (R(a) | R(x))", hof"R(a) | (!x R(x))" )
+    succeedFor( Ex, hof"?x (R(x) | S(x))", hof"(?x R(x)) | (?x S(x))" )
+    notMove( All, hof"!x (R(x) | S(x))" )
+    notMove( All, hof"!x ?y !z R(x, y, z)" )
+    succeedFor( All, hof"-(!x (R(x) & S(x)))", hof"-((!x R(x)) & (!x S(x)))" )
+
+    val q = new QuantifierHelper( new QuantifierC( "q" ) )
+    notMove( q, q( fov"x", hof"R(x) | S(x)" ) )
+    succeedFor( q, q( fov"x", hof"R(x) | S(a)" ), Or( q( fov"x", hof"R(x)" ), hof"S(a)" ) )
+  }
+}
