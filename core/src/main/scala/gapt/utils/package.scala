c6df706b1cb0bd3e48193d846bd28f8dc60fcae7
Jannik Vierling <jannik.vierling@gmail.com>
2020-03-05 15:14:21 +0100

    clean up crossProduct

--- core/src/main/scala/gapt/utils/package.scala
+++ core/src/main/scala/gapt/utils/package.scala
@@ -11,9 +11,9 @@ package object utils {
     } yield ( e1, e2 )
   }
 
-  def crossProduct[T]( lists: Iterable[Iterable[T]] ): Iterable[List[T]] = lists match {
-    case Nil          => List( Nil )
-    case head :: rest => for { x <- head; y <- crossProduct( rest ) } yield x :: y
-  }
-
+  def crossProduct[T]( xs: Seq[Iterable[T]] ): Iterable[Seq[T]] =
+    xs match {
+      case Seq()              => Seq( Seq() )
+      case Seq( x, xss @ _* ) => for { y <- x; ys <- crossProduct( xss ) } yield y +: ys
+    }
 }
