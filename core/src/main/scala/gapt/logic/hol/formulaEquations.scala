08bba637593b1c3d015782752bb3705bf8b64b01
Jannik Vierling <jannik.vierling@gmail.com>
2020-03-05 16:23:08 +0100

    move solveFormulaEquation to separate file

--- core/src/main/scala/gapt/logic/hol/formulaEquations.scala
+++ core/src/main/scala/gapt/logic/hol/formulaEquations.scala
@@ -338,38 +338,3 @@ object witnessForSecondOrderQuantifierElimination {
     } ) )
   }
 }
-
-/**
- * Takes a formula equation F[X₁,...,Xₙ] and if successful returns a substitution of the second order
- * variables X_1,...,X_n such that applying the substitution to F is a valid first-order formula.
- * See [[witnessForSecondOrderQuantifierElimination]] for a detailed description when this succeeds and when it fails.
- * In addition, this method uses Escargot to check if the found first-order formula is valid and returns a Failure,
- * if it is not.
- */
-object solveFormulaEquation {
-
-  private def isHigherOrderPredicateType( t: Ty ) =
-    t match {
-      case FunctionType( To, _ ) => true
-      case _                     => false
-    }
-
-  private def isHigherOrderPredicateVariable( x: Var ) =
-    isHigherOrderPredicateType( x.ty )
-
-  /**
-   * Attempts to solve formula equations.
-   * @param f The formula equation `F[X₁,...,Xₙ]` to be solved.
-   */
-  def apply( f: Formula ): Try[Substitution] = {
-    val xs = freeVariables( f ).filter( isHigherOrderPredicateVariable ).toSeq
-    val h = Ex.Block( xs, f )
-    witnessForSecondOrderQuantifierElimination( h ).flatMap( {
-      case ( substitution, firstOrderPart ) =>
-        if ( Escargot.isValid( BetaReduction.betaNormalize( substitution( firstOrderPart ) ) ) )
-          Success( substitution )
-        else
-          Failure( new Exception( s"""the given formula equation ${f} is not solvable""" ) )
-    } )
-  }
-}
