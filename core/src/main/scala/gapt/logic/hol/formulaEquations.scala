09b3e395ab8e535fa3ecf89093dcba9d9ce467b1
Jannik Vierling <jannik.vierling@gmail.com>
2020-03-05 14:35:41 +0100

    make solveFormulaEquations work with formulas of the form F[X]

--- core/src/main/scala/gapt/logic/hol/formulaEquations.scala
+++ core/src/main/scala/gapt/logic/hol/formulaEquations.scala
@@ -3,6 +3,7 @@ package gapt.logic.hol
 import gapt.expr.formula.{ Formula, _ }
 import gapt.expr.subst.Substitution
 import gapt.expr.ty.{ FunctionType, To, Ty }
+import gapt.expr.util.freeVariables
 import gapt.expr.util.variables
 import gapt.expr.{ Abs, BetaReduction, Expr, Var, VarOrConst }
 import gapt.logic.Polarity
@@ -343,20 +344,36 @@ object witnessForSecondOrderQuantifierElimination {
 }
 
 /**
- * Takes a formula equation ∃X_1 ... ∃X_n φ and if successful returns a substitution of the second order
- * variables X_1,...,X_n such that applying the substitution to φ is a valid first-order formula.
+ * Takes a formula equation F[X₁,...,Xₙ] and if successful returns a substitution of the second order
+ * variables X_1,...,X_n such that applying the substitution to F is a valid first-order formula.
  * See [[witnessForSecondOrderQuantifierElimination]] for a detailed description when this succeeds and when it fails.
  * In addition, this method uses Escargot to check if the found first-order formula is valid and returns a Failure,
  * if it is not.
  */
 object solveFormulaEquation {
-  def apply( formula: Formula ): Try[Substitution] = {
-    witnessForSecondOrderQuantifierElimination( formula ).flatMap( {
+
+  private def isHigherOrderPredicateType( t: Ty ) =
+    t match {
+      case FunctionType( To, _ ) => true
+      case _                     => false
+    }
+
+  private def isHigherOrderPredicateVariable( x: Var ) =
+    isHigherOrderPredicateType( x.ty )
+
+  /**
+   * Attempts to solve formula equations.
+   * @param f The formula equation `F[X₁,...,Xₙ]` to be solved.
+   */
+  def apply( f: Formula ): Try[Substitution] = {
+    val xs = freeVariables( f ).filter( isHigherOrderPredicateVariable ).toSeq
+    val h = Ex.Block( xs, f )
+    witnessForSecondOrderQuantifierElimination( h ).flatMap( {
       case ( substitution, firstOrderPart ) =>
         if ( Escargot.isValid( BetaReduction.betaNormalize( substitution( firstOrderPart ) ) ) )
           Success( substitution )
         else
-          Failure( new Exception( s"""the given formula equation ${formula} is not valid""" ) )
+          Failure( new Exception( s"""the given formula equation ${f} is not solvable""" ) )
     } )
   }
 }
