94d17ca78d286989c691162c9e53246cf41d4bbb
Jannik Vierling <jannik.vierling@gmail.com>
2021-01-28 19:35:08 +0100

    add test for PredicateReductionET

--- tests/src/test/scala/gapt/proofs/reduction/PredicateReductionTest.scala
+++ tests/src/test/scala/gapt/proofs/reduction/PredicateReductionTest.scala
@@ -1,7 +1,16 @@
 package gapt.proofs.reduction
 
 import gapt.expr._
+import gapt.logic.Polarity
+import gapt.proofs.Sequent
 import gapt.proofs.SequentMatchers
+import gapt.proofs.expansion.ETtAtom
+import gapt.proofs.expansion.ETtBinary
+import gapt.proofs.expansion.ETtStrong
+import gapt.proofs.expansion.ETtWeak
+import gapt.proofs.expansion.ExpansionProof
+import gapt.proofs.expansion.ExpansionTree
+import gapt.provers.escargot.Escargot
 import org.specs2.mutable._
 
 class PredicateReductionTest extends Specification with SequentMatchers {
@@ -21,6 +30,38 @@ class PredicateReductionTest extends Specification with SequentMatchers {
     "introduce axioms for function symbols" in {
       val ( f, _ ) = PredicateReductionET forward hos":- P(f(x))"
       f.antecedent must contain( allOf( hof"!x (is_i(x) -> is_i(f(x)))" ) )
+    }
+  }
+  "predicate backtranslation should" in {
+    "unguard universal quantifiers" in {
+      val ( f, back ) = PredicateReductionET forward hos"!x P(x:a) :- P(#c(c:a))"
+      f must beMultiSetEqual( hos"⊤ -> is_a(#c(c:a)), ?x is_a(x:a), !x (is_a(x) -> P(x:a)) :- P(c)" )
+      val p_ = ExpansionProof(
+        ExpansionTree(
+          hof"⊤ -> is_a(#c(c:a))",
+          Polarity.Negative,
+          ETtBinary( ETtAtom, ETtAtom ) ) +:
+          ExpansionTree(
+            hof"!x (is_a(x:a) -> P(x))",
+            Polarity.Negative,
+            ETtWeak( hov"x_0 : a" -> ETtBinary( ETtAtom, ETtAtom ) ) ) +:
+            Sequent[ExpansionTree]() :+
+            ExpansionTree(
+              hof"P( #c(c:a) )",
+              Polarity.Positive,
+              ETtAtom ) )
+      val p = ExpansionTree(
+        hof"!x P(x : a)",
+        Polarity.Negative,
+        ETtWeak( hov"x_0 : a" -> ETtAtom ) ) +:
+        Sequent[ExpansionTree]() :+
+        ExpansionTree(
+          hof"P(#c(c:a))",
+          Polarity.Positive,
+          ETtAtom )
+      println( p_ )
+      println( back( p_ ) )
+      back( p_ ).shallow must beMultiSetEqual( p.shallow )
     }
   }
 }
