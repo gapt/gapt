68dcc984619dfee3c9f983741ccc81af2af38686
Jannik Vierling <jannik.vierling@gmail.com>
2021-03-01 17:45:22 +0100

    parse biconditional as two implications

--- tests/src/test/scala/gapt/formats/leancop/LeanCoPParserTest.scala
+++ tests/src/test/scala/gapt/formats/leancop/LeanCoPParserTest.scala
@@ -1,11 +1,21 @@
 package gapt.formats.leancop
 
 import gapt.expr._
+import gapt.expr.formula.Iff
 import gapt.formats.ClasspathInputFile
 import gapt.utils.SatMatchers
 import org.specs2.mutable.Specification
 
 class LeanCoPParserTest extends Specification with SatMatchers {
+
+  "parse biconditional to two implications" in {
+    LeanCoPParser.parseAll( LeanCoPParser.biconditional, "'P' <=> 'Q'" ) match {
+      case LeanCoPParser.Success( f, _ ) =>
+        f must_== fof"P <-> Q"
+      case _ => failure
+    }
+    ok
+  }
 
   "parse universally quantified formula" in {
     LeanCoPParser.parseAll( LeanCoPParser.quantified, "! [_X, _Y] : ('P'(_X, _Y))" ) match {
