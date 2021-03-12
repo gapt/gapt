a94377d6d95dbe4a8eaaee5f7dad885a557a6c72
ermine516 <cernadavid1@gmail.com>
2021-03-12 16:32:22 +0100

    added print statements

--- core/src/main/scala/gapt/provers/viper/grammars/DummyProver.scala
+++ core/src/main/scala/gapt/provers/viper/grammars/DummyProver.scala
@@ -15,16 +15,16 @@ import gapt.formats._
 import gapt.formats.leancop._
 import gapt.provers.groundFreeVariables
 
-
 case class DummyProver( insts: Map[Formula, Option[ExpansionProof]] ) extends OneShotProver {
   override def getExpansionProof( seq: HOLSequent )( implicit ctx: Maybe[MutableContext] ): Option[ExpansionProof] = {
-    val (seqground, subst) = groundFreeVariables( seq )
+    val ( seqground, subst ) = groundFreeVariables( seq )
     val reduction = ErasureReductionET
     val ( folProblem, back ) = reduction forward seqground
     folProblem match {
       case Sequent( _, Seq( f ) ) => {
+        print( "checking for proof: " + f + "\n" + insts.keySet.toString() + "\n" )
         insts.get( f ) match {
-          case Some( Some( t ) ) => Some(TermReplacement.undoGrounding(back( t ),subst ))
+          case Some( Some( t ) ) => Some( TermReplacement.undoGrounding( back( t ), subst ) )
           case _                 => None
         }
       }
@@ -37,12 +37,11 @@ object DummyProverHelper {
   def getListOfExpPrf( dir: String ): List[ExpansionProof] = {
     val d = new File( dir )
     if ( d.exists && d.isDirectory ) {
-<<<<<<< HEAD
       val paths = d.listFiles.toList.map( t => InputFile.fromJavaFile( t ) )
-      val expSeq = paths.map( t =>{
-        print("Parsing: "+t.toString()+"\n")
+      val expSeq = paths.map( t => {
+        print( "Parsing: " + t.toString() + "\n" )
         LeanCoPParser.getExpansionProof( t )
-       }).flatten
+      } ).flatten
       val expPrf = expSeq.map( t => ExpansionProof( t ) )
       if ( expSeq.size != paths.size ) List[ExpansionProof]()
       else expPrf
@@ -51,19 +50,16 @@ object DummyProverHelper {
   def MakeProofDict( dir: String ): Map[Formula, Option[ExpansionProof]] = {
     val ExpPrf = getListOfExpPrf( dir )
     val lPairs = ExpPrf.map( t => ( t.shallow.succedent.toList( 0 ) -> Some( t ) ) )
-      val expPrf = expSeq.map( t => ExpansionProof( t ) )
-      if ( expSeq.size != paths.size ) List[ExpansionProof]()
-      expPrf
-    } else List[ExpansionProof]()
-  }
-  def MakeProofDict( dir: String ): Map[Formula, Option[ExpansionProof]] = {
-    val ExpPrf = getListOfExpPrf( dir )
-    val lPairs = ExpPrf.map( t => ExpansionProofToLK( t ) match {
-      case Left( r )  => None
-      case Right( r ) => Some( ( r.conclusion.succedent.toList( 0 ) -> Some( t ) ) )
-    } ).flatten
->>>>>>> Current stage of development
     if ( lPairs.size != ExpPrf.size ) Map[Formula, Option[ExpansionProof]]()
     else lPairs.toMap
+  }
+  def enoughSolutions( dir: String, n: Int ): List[String] = {
+    val d = new File( dir )
+    if ( d.exists && d.isDirectory ) {
+      val dict = d.listFiles.toList.filter( _.isDirectory )
+      if ( dict.isEmpty && d.listFiles.length == n ) return ( d.getAbsolutePath() +: List.empty[String] )
+      else if ( dict.isEmpty ) return List.empty[String]
+      else return dict.map( x => enoughSolutions( x.getAbsolutePath(), n ) ).flatten
+    } else return List.empty[String]
   }
 }
