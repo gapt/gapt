309f9dc4177d958ca09067448e09104afbacf6c8
Fabian Achammer <fabian@achammer.dev>
2024-02-02 16:05:56 +0100

    separate scala 2 macro definition
    allows to implement the macro with the new scala 3 macro system while keeping the old version for comparison and testing.
    will implement the scala 3 macro at a later stage and leave it unimplemented for now

---
+++ core/src/main/scala-3/gapt/proofs/gaptic/macros.scala
@@ -0,0 +1,14 @@
+package gapt.proofs.gaptic
+
+import gapt.formats.babel.BabelSignature
+
+import language.experimental.macros
+
+trait LemmaHelper[T] {
+  def apply[U]( tacticsProof: => Tactic[U] ): T = macro ???
+
+  // Implementations need to define a function with the following signature:
+  // (Overloading is implemented ad-hoc since we allow subclasses to add implicit arguments.)
+  //
+  // def handleTacticBlock( block: ProofState => ProofState ): T
+}
