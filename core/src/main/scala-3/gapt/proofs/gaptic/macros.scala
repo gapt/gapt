9896d616abfb01537b382e3dbbb7adb9f63df3c7
Fabian Achammer <fabian@achammer.dev>
2024-02-09 16:27:38 +0100

    fix compile error with old macro keyword

--- core/src/main/scala-3/gapt/proofs/gaptic/macros.scala
+++ core/src/main/scala-3/gapt/proofs/gaptic/macros.scala
@@ -5,7 +5,7 @@ import gapt.formats.babel.BabelSignature
 import language.experimental.macros
 
 trait LemmaHelper[T] {
-  def apply[U]( tacticsProof: => Tactic[U] ): T = macro ???
+  def apply[U]( tacticsProof: => Tactic[U] ): T = ???
 
   // Implementations need to define a function with the following signature:
   // (Overloading is implemented ad-hoc since we allow subclasses to add implicit arguments.)
