2e1fee1ef6843a8d3f948ce5aff52e21aaf43736
Fabian Achammer <fabian@achammer.dev>
2024-09-06 11:56:07 +0200

    show gapt welcome message and prompt in gapt shell
    this required copying some of the implementation of the scala repl from the scala compiler (see the source code comment for more information)

--- cli/src/main/scala-3/GaptRepl.scala
+++ cli/src/main/scala-3/GaptRepl.scala
@@ -2,20 +2,71 @@ package gapt.cli
 
 import gapt.formats.ClasspathInputFile
 import dotty.tools.repl._
+import java.io.PrintStream
+import org.jline.reader.Completer
+import scala.jdk.CollectionConverters._
+import dotty.tools.dotc.core.Contexts.Context
+import org.jline.reader.EndOfFileException
+import org.jline.reader.UserInterruptException
+import scala.annotation.tailrec
 
 case class GaptRepl() {
-  sys.props("scala.shell.prompt") = sys.props("line.separator") + "gapt> "
 
-  def run(): Unit =
-    val repl = new dotty.tools.repl.ReplDriver(
-      Array(
+  class GaptTerminal extends JLineTerminal {
+    override protected def promptStr: String = "gapt"
+  }
+
+  class GaptReplDriver(out: PrintStream = Console.out, classLoader: Option[ClassLoader] = None) extends ReplDriver(Array(
         "-usejavacp",
         "-feature",
         "-deprecation",
         "-language:postfixOps",
         "-language:implicitConversions"
-      )
-    )
+      )) {
+
+    override def runUntilQuit(using initialState: State)(): State = {
+      // Most of this implementation is copied from the scala compiler version 3.3.1
+      // (see https://github.com/scala/scala3/blob/release-3.3.1/compiler/src/dotty/tools/repl/ReplDriver.scala)
+      // This is necessary since the ReplDriver implementation is not extensible
+      // enough to allow setting the prompt string and welcome message.
+      // However, this means that future changes in the scala compiler might have to be incorporated here.
+
+      // These first two lines are new
+      val terminal = new GaptTerminal
+      out.println(welcomeMessage)
+
+      // The rest is copied from the implementation linked above
+
+      /** Blockingly read a line, getting back a parse result */
+      def readLine()(using state: State): ParseResult = {
+        val completer: Completer = { (_, line, candidates) =>
+          val comps = completions(line.cursor, line.line, state)
+          candidates.addAll(comps.asJava)
+        }
+        given Context = state.context
+        try {
+          val line = terminal.readLine(completer)
+          ParseResult(line)
+        } catch {
+          case _: EndOfFileException |
+              _: UserInterruptException => // Ctrl+D or Ctrl+C
+            Quit
+        }
+      }
+
+      @tailrec def loop(using state: State)(): State = {
+        val res = readLine()
+        if (res == Quit) state
+        else loop(using interpret(res))()
+      }
+
+      try runBody { loop() }
+      finally terminal.close()
+    }
+  }
+
+  def run(): Unit =
+    val repl = new GaptReplDriver
     val state = repl.runQuietly(readPredefFile)(using repl.initialState)
     repl.runUntilQuit(using state)()
 
