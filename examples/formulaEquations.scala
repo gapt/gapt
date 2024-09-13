9d2e92f2e2f0780aa3e53abe02e94b7d05a1bba6
Fabian Achammer <fabian@achammer.dev>
2024-09-13 15:43:59 +0200

    add induction example

--- examples/formulaEquations.scala
+++ examples/formulaEquations.scala
@@ -3,6 +3,16 @@ package gapt.examples
 import gapt.expr._
 import gapt.proofs._
 import gapt.logic.hol.scan.scan._
+import gapt.expr.formula.Formula
+import gapt.expr.formula.And
+import gapt.proofs.resolution.structuralCNF
+import gapt.expr.formula.Atom
+import gapt.expr.ty.Ti
+import gapt.expr.ty.To
+import gapt.logic.hol.scan.scan
+import gapt.expr.util.constants
+import gapt.expr.util.rename
+import gapt.expr.util.freeVariables
 
 object formulaEquations {
   private def feq(vars: Set[Var], clauses: Set[HOLClause]) = {
@@ -44,4 +54,38 @@ object formulaEquations {
     Set(hov"X:i>o"),
     Set(hcl"A(u):-X(u)", hcl"X(u) :- X(v), A(u)", hcl"X(u) :- B(u)")
   )
+
+  def Q = And(Set(
+    fof"!u s(u) != 0",
+    fof"!u u + 0 = u",
+    fof"!u !v u + s(v) = s(u+v)"
+  ))
+
+  def ind(expr: Expr): Formula = {
+    assert(expr.ty == Ti ->: To)
+    BetaReduction.betaNormalize(hof"($expr(0) & !u ($expr(u) -> $expr(s(u)))) -> !u $expr(u)")
+  }
+
+  def soaFree(v: Var) = {
+    Q & ind(v)
+  }
+
+  def inductiveTheorem(theorem: Formula) = {
+    val freshConstant = rename(hoc"P:i>o", freeVariables(theorem))
+    val freshVariable = rename(hov"X:i>o", freeVariables(theorem))
+    val formula = hof"($Q & ${ind(freshConstant)}) -> $theorem"
+    val clauses = cnf(formula)
+    val renamedClauses = clauses.map(c => {
+      c.map {
+        case Atom(head, args) if head == freshConstant => Atom(freshVariable, args)
+        case a                                         => a
+      }
+    })
+    feq(Set(freshVariable), renamedClauses)
+  }
+
+  def cnf(formula: Formula) = {
+    val sequent = hos"$formula :-"
+    structuralCNF(sequent, structural = false).map(_.conclusion.map(_.asInstanceOf[Atom]))
+  }
 }
