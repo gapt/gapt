a79f64246b1123c78f2886279fb6253cf81c742f
Fabian Achammer <fabian@achammer.dev>
2025-03-04 16:46:31 +0100

    do not delete temp files to investigate vampire errors

--- core/src/main/scala/gapt/utils/tempFiles.scala
+++ core/src/main/scala/gapt/utils/tempFiles.scala
@@ -5,8 +5,8 @@ import os._
 object withTempFile {
   def apply[T](block: Path => T): T = {
     val tempFile = temp(prefix = "gapt-")
-    try block(tempFile)
-    finally remove(tempFile)
+    block(tempFile)
+    // finally remove(tempFile)
   }
 
   def fromString[T](content: String)(block: Path => T): T =
