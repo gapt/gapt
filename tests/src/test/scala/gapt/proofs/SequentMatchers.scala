ee24254612c26df282dca0eeb8e83270e2eb92e2
Jannik Vierling <jannik.vierling@gmail.com>
2025-08-20 07:54:57 +0200

    add LK proof matchers

--- tests/src/test/scala/gapt/proofs/SequentMatchers.scala
+++ tests/src/test/scala/gapt/proofs/SequentMatchers.scala
@@ -2,10 +2,22 @@ package gapt.proofs
 
 import gapt.formats.babel.BabelSignature
 import org.specs2.matcher.{Matcher, Matchers}
+import org.specs2.matcher.Matchers.pairFunctionToMatcher
 
 trait SequentMatchers extends Matchers {
 
-  def beMultiSetEqual[A](expected: Sequent[A])(implicit sig: BabelSignature): Matcher[Sequent[A]] = { (actual: Sequent[A]) =>
+  def beMultiSetEqual[A](expected: Sequent[A])(implicit sig: BabelSignature): Matcher[Sequent[A]] =
+    SequentMatchers.beMultiSetEqual(expected)(sig)
+
+  def beSetEqual[A](expected: Sequent[A])(implicit sig: BabelSignature): Matcher[Sequent[A]] =
+    beMultiSetEqual(expected.distinct) ^^ { (actual: Sequent[A]) => actual.distinct }
+
+}
+
+object SequentMatchers {
+
+  def beMultiSetEqual[A](expected: Sequent[A])(implicit sig: BabelSignature): Matcher[Sequent[A]] = {
+    (actual: Sequent[A]) =>
     (
       actual multiSetEquals expected,
       s"""
@@ -18,11 +30,8 @@ trait SequentMatchers extends Matchers {
          |   ${actual diff expected toSigRelativeString}
          | Additional elements in expected:
          |   ${expected diff actual toSigRelativeString}
-       """.stripMargin
+         """.stripMargin
     )
   }
-
-  def beSetEqual[A](expected: Sequent[A])(implicit sig: BabelSignature): Matcher[Sequent[A]] =
-    beMultiSetEqual(expected.distinct) ^^ { (actual: Sequent[A]) => actual.distinct }
 
 }
