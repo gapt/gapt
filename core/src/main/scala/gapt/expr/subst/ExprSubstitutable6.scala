18e289b67a20d59112dcc44fb8b00abbb1890f88
Fabian Achammer <fabian@achammer.dev>
2025-10-16 13:41:13 +0200

    add AtomClosedUnderFOLSub implicit

--- core/src/main/scala/gapt/expr/subst/ExprSubstitutable6.scala
+++ core/src/main/scala/gapt/expr/subst/ExprSubstitutable6.scala
@@ -3,11 +3,16 @@ package gapt.expr.subst
 import gapt.expr.ClosedUnderFOLSub
 import gapt.expr.formula.fol.FOLFormula
 import gapt.expr.formula.fol.FOLTerm
+import gapt.expr.formula.Atom
 
 trait ExprSubstitutable6 extends ExprSubstitutable5 {
   implicit val FOLTermClosedUnderFOLSub: ClosedUnderFOLSub[FOLTerm] =
     (sub, x) => applySub(sub, x).asInstanceOf[FOLTerm]
 
   implicit val FOLFormulaClosedUnderFOLSub: ClosedUnderFOLSub[FOLFormula] =
-    (sub, x) => applySub(sub, x).asInstanceOf[FOLFormula]
+    (sub, x) =>
+      applySub(sub, x).asInstanceOf[FOLFormula]
+
+  implicit val AtomClosedUnderFOLSub: ClosedUnderFOLSub[Atom] =
+    (sub, x) => applySub(sub, x).asInstanceOf[Atom]
 }
