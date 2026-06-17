73e48ae5139582116470a183aa3868d15e051b7c
Fabian Achammer <fabian@achammer.dev>
2026-06-17 14:01:02 +0200

    do not generate tstp files where vampire answers satisfiable

--- testing/src/main/scala/generateTstpFiles.scala
+++ testing/src/main/scala/generateTstpFiles.scala
@@ -8,42 +8,47 @@ import gapt.utils.TimeOutException
 import scala.collection.parallel.CollectionConverters._
 import os.Path
 
-case class Prover(name: String, fn: Path => String)
+case class Prover(name: String, fn: Path => Option[String])
 
 val vampire = Prover(
   "vampire",
   { p =>
-    runProcess(
+    val output = runProcess(
       Seq(
         "vampire",
         "--proof",
         "tptp",
-        "--include",
         p.toString
       ),
       cwd = pwd / "testing" / "TPTP" / "problems"
     )
+    if output.contains("% SZS status Satisfiable") then None
+    else Some(output)
   }
 )
 
 val eprover = Prover(
   "eprover",
   { p =>
-    runProcess(
+    val output = runProcess(
       Seq("eprover", "-p", "--tptp3-format"),
       os.read(p),
       cwd = pwd / "testing" / "TPTP" / "problems"
     )
+    // eprover outputs lines that are supposed to be comments by prepending the
+    // hash symbol '#'. however, that is not valid TPTP syntax for comments
+    // so we remove those lines
+    Some(output.linesIterator.filterNot(_.startsWith("#")).mkString("\n"))
   }
 )
 
 val metis = Prover(
   "metis",
   { p =>
-    runProcess(
+    Some(runProcess(
       Seq("metis", "--show", "proof", p.toString),
       cwd = pwd / "testing" / "TPTP" / "problems"
-    )
+    ))
   }
 )
 
@@ -61,14 +66,17 @@ val metis = Prover(
   configurations.par.foreach { (prover, problem) =>
     try {
       System.out.println(s"running ${prover.name} on $problem")
-      val output = withTimeout(timeout) {
-        prover.fn(problem)
+      val out = withTimeout(timeout) { prover.fn(problem) }
+      if out.isEmpty then {
+        System.out.println(s"got no derivation from ${prover.name} on $problem")
+      } else {
+        val output = out.get
+        System.out.println(s"got result from ${prover.name} on $problem")
+        val inputRelativePath = problem.relativeTo(pwd / "testing" / "TPTP" / "problems" / "Problems")
+        val outputPath = pwd / "testing" / "TSTP" / "Solutions" / inputRelativePath / s"${prover.name}.tstp"
+        os.write.over(outputPath, output, createFolders = true)
+        System.out.println(s"wrote ${prover.name} result to $outputPath")
       }
-      System.out.println(s"got result from ${prover.name} on $problem")
-      val inputRelativePath = problem.relativeTo(pwd / "testing" / "TPTP" / "problems" / "Problems")
-      val outputPath = pwd / "testing" / "TSTP" / "Solutions" / inputRelativePath / s"${prover.name}.tstp"
-      os.write.over(outputPath, output, createFolders = true)
-      System.out.println(s"wrote ${prover.name} result to $outputPath")
     } catch {
       case _: TimeOutException => {
         System.err.println(s"${prover.name} timed out after ${timeout} on $problem")
